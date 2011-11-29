26b4f3e6590d3b804245fd775a5c4ceed3253e85
남창우 <no-reply@cubrid.org>
2011-11-29 06:04:13 +0000

    [CUBRIDSUS-6266][CR] fix a bug terminating client connection and transaction when ER_DB_NO_MODIFICATIONS error occur

--- src/connection/client_support.c
+++ src/connection/client_support.c
@@ -638,16 +638,10 @@ css_receive_data_from_server_with_timeout (unsigned int eid, char **buffer,
 				    buffer, size);
       if (css_Errno == NO_ERRORS)
 	{
-	  errid = css_test_for_server_errors (entry, eid);
-	  if (errid == ER_DB_NO_MODIFICATIONS)
-	    {
-	      css_Errno = REQUEST_REFUSED;
-	    }
-	  else
-	    {
-	      /* 0 means that it isn't a communication error with server */
-	      return 0;
-	    }
+	  (void) css_test_for_server_errors (entry, eid);
+
+	  /* 0 means that it isn't a communication error with server */
+	  return 0;
 	}
       /* css_Errno != NO_ERROR */
       if (css_Errno == SERVER_ABORTED)
