58d29950ecb23602f25874a44c8ee5ac0982a826
김시완 <no-reply@cubrid.org>
2012-03-30 05:18:05 +0000

    [CUBRIDSUS-7358][CR] restore a version string in JDBC

--- src/jdbc/cubrid/jdbc/driver/CUBRIDDriver.java
+++ src/jdbc/cubrid/jdbc/driver/CUBRIDDriver.java
@@ -58,7 +58,7 @@ import cubrid.jdbc.jci.UJCIUtil;
 
 public class CUBRIDDriver implements Driver {
 	// version
-	static final String version_string = "8.4.1.1000";
+	static final String version_string = "@JDBC_DRIVER_VERSION_STRING@";
 	public static final int major_version;
 	public static final int minor_version;
 	public static final int patch_version;
