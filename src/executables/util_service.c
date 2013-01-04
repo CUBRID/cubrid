74845979920aad64bb5ef8777688715ddf631a7f
杨广为 <no-reply@cubrid.org>
2013-01-04 09:18:14 +0000

    [CUBRIDSUS-9992] Clear error message on Windows

--- src/executables/util_service.c
+++ src/executables/util_service.c
@@ -732,13 +732,14 @@ proc_execute (const char *file, const char *args[], bool wait_child,
     }
 
   GetStartupInfo (&si);
+  si.dwFlags = si.dwFlags | STARTF_USESTDHANDLES;
   si.hStdOutput = GetStdHandle (STD_OUTPUT_HANDLE);
   si.hStdError = GetStdHandle (STD_ERROR_HANDLE);
+  inherited_handle = TRUE;
 
   if (close_output)
     {
       si.hStdOutput = NULL;
-      inherited_handle = FALSE;
     }
 
   if (close_err)
