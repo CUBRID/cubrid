0cec4ce456e9936bbb8d5f50323ea47802c65598
김시완 <no-reply@cubrid.org>
2013-03-15 08:30:23 +0000

    [CUBRIDSUS-10851][CR] CCI, JDBC connect to a next alter-host by the CAS_ER_FREE_SERVER

--- src/cci/cas_cci.c
+++ src/cci/cas_cci.c
@@ -4568,7 +4568,10 @@ cas_connect_internal (T_CON_HANDLE * con_handle, T_CCI_ERROR * err_buf,
 		  return CCI_ER_NO_ERROR;
 		}
 
-	      if (error == CCI_ER_COMMUNICATION || error == CCI_ER_CONNECT)
+	      if (error == CCI_ER_COMMUNICATION
+		  || error == CCI_ER_CONNECT
+		  || error == CCI_ER_LOGIN_TIMEOUT
+		  || error == CAS_ER_FREE_SERVER)
 		{
 		  hm_set_host_status (con_handle, i, false);
 		}
