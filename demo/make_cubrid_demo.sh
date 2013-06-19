88a32139c31f311b020c8163991fd453f86b5daf
오이석 <no-reply@cubrid.org>
2013-06-19 03:09:05 +0000

    [CUBRIDSUS-11481][CR] add en_US charset to create demodb

--- demo/make_cubrid_demo.sh
+++ demo/make_cubrid_demo.sh
@@ -4,5 +4,5 @@ if [ -z "$CUBRID" ]; then
 	exit 1
 fi
 
-cubrid createdb --db-volume-size=100M --log-volume-size=100M demodb > /dev/null 2>&1
+cubrid createdb --db-volume-size=100M --log-volume-size=100M demodb en_US > /dev/null 2>&1
 cubrid loaddb -u dba -s $CUBRID/demo/demodb_schema -d $CUBRID/demo/demodb_objects demodb > /dev/null 2>&1
