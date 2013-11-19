5b33a77fed905e8e0074c4560fe5659f2e81d1b0
오이석 <no-reply@cubrid.org>
2013-11-19 09:20:17 +0000

    [CUBRIDSUS-12518][CR] fix a warning - missing prototype of tran_is_blocked()

--- src/communication/network_interface_cl.h
+++ src/communication/network_interface_cl.h
@@ -176,9 +176,7 @@ extern int log_drop_lob_locator (const char *locator);
 
 extern TRAN_STATE tran_server_commit (bool retain_lock);
 extern TRAN_STATE tran_server_abort (void);
-#if defined(ENABLE_UNUSED_FUNCTION)
 extern bool tran_is_blocked (int tran_index);
-#endif
 extern int tran_server_has_updated (void);
 extern int tran_server_is_active_and_has_updated (void);
 extern int tran_wait_server_active_trans (void);
