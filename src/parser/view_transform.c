4e64803f134f423560f23f80658da7347281b08a
黄耀 <no-reply@cubrid.org>
2014-03-14 09:53:15 +0000

    [CUBRIDSUS-13334][CR] fix " INSERT INTO view_2 SELECT * FROM view_2 LIMIT 0" crashed when view is updatable in [RQG] multithread do ddl/dml

--- src/parser/view_transform.c
+++ src/parser/view_transform.c
@@ -9424,6 +9424,16 @@ mq_class_lambda (PARSER_CONTEXT * parser, PT_NODE * statement,
 	  spec->info.spec.path_entities = NULL;
 	  if (newspec)
 	    {
+	      if (newspec->info.spec.entity_name == NULL)
+		{
+		  newspec->info.spec.entity_name =
+		    spec->info.spec.entity_name;
+		  /* spec will be free later, we don't want the entity_name
+		   * will be freed
+		   */
+		  spec->info.spec.entity_name = NULL;
+		}
+
 	      newspec->info.spec.range_var->info.name.original =
 		spec->info.spec.range_var->info.name.original;
 	      newspec->info.spec.location = spec->info.spec.location;
