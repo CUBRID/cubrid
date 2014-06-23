8cdb85d9d00a8d08d32926a707fda6124643bd3e
최승권 <no-reply@cubrid.org>
2014-06-23 05:14:21 +0000

    [CUBRIDSUS-14023][CR] fix a unique violation of index when updating a multi-row.

--- src/storage/btree.c
+++ src/storage/btree.c
@@ -23795,14 +23795,6 @@ start_locking:
 	   * current query is based on some classes
 	   * contained in the class hierarchy.
 	   */
-	  if (btrs_helper.cp_oid_cnt > 1)
-	    {
-	      er_log_debug (ARG_FILE_LINE,
-			    "cp_oid_cnt > 1 in an unique index\n"
-			    "index inconsistency.(unique violation).\n");
-	      assert (false);
-	      goto error;
-	    }
 
 	  for (i = 0; i < btrs_helper.cp_oid_cnt; i++)
 	    {
@@ -23862,6 +23854,21 @@ start_locking:
        * TRAN_REP_CLASS_REP_INSTANCE
        * TRAN_SERIALIZABLE
        */
+      if (!BTREE_IS_UNIQUE (bts->btid_int.unique_pk) || num_classes == 0)
+	{
+	  ;
+	}
+      else
+	{
+	  if (btrs_helper.cp_oid_cnt > 1)
+	    {
+	      er_log_debug (ARG_FILE_LINE,
+			    "cp_oid_cnt > 1 in an unique index\n"
+			    "index inconsistency.(unique violation).\n");
+	      assert (false);
+	      goto error;
+	    }
+	}
 
       if (btrs_helper.saved_inst_oid.pageid != NULL_PAGEID
 	  || (bts->prev_oid_pos == -1 && btrs_helper.curr_key_locked))
