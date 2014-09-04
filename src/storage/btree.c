d98f01db4d93539a448fe1286a314903a9cbd27c
박부식 <no-reply@cubrid.org>
2014-09-04 01:15:59 +0000

    [CUBRIDSUS-14403][CR] add assertion code in btree_range_search() called for UPDATE

--- src/storage/btree.c
+++ src/storage/btree.c
@@ -30716,6 +30716,7 @@ btree_handle_current_oid_and_locks (THREAD_ENTRY * thread_p, BTREE_SCAN * bts,
 	      if (btrs_helper->rec_oid_cnt > 1)
 		{
 		  index_scan_id_p->duplicate_key_locked = true;
+		  assert (!BTREE_IS_UNIQUE (bts->btid_int.unique_pk));
 		}
 	    }
 	}
@@ -30964,6 +30965,7 @@ btree_handle_current_oid_and_locks (THREAD_ENTRY * thread_p, BTREE_SCAN * bts,
 	      if (btrs_helper->rec_oid_cnt > 1)
 		{
 		  index_scan_id_p->duplicate_key_locked = true;
+		  assert (!BTREE_IS_UNIQUE (bts->btid_int.unique_pk));
 		}
 	    }
 	}
