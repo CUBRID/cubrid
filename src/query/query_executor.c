9190534d6c2481c2efcf67bc14327dd4866d9140
오이석 <no-reply@cubrid.org>
2015-05-26 08:43:43 +0000

    [CUBRIDSUS-16992][CR] fixed to allow synccoldb to purge _db_collation records

--- src/query/query_executor.c
+++ src/query/query_executor.c
@@ -7415,12 +7415,15 @@ qexec_open_scan (THREAD_ENTRY * thread_p, ACCESS_SPEC_TYPE * curr_spec,
       assert (!force_select_lock);
 
       /* We expect to update or delete a non MVCC objects via a scan 
-       * are only db_serial and db_ha_apply_info objects.
+       * are only db_serial, db_ha_apply_info and _db_collation objects.
        */
       assert ((scan_op_type != S_DELETE && scan_op_type != S_UPDATE)
 	      || oid_is_serial (&ACCESS_SPEC_CLS_OID (curr_spec))
 	      || (oid_check_cached_class_oid
 		  (OID_CACHE_HA_APPLY_INFO_CLASS_ID,
+		   &ACCESS_SPEC_CLS_OID (curr_spec)))
+	      || (oid_check_cached_class_oid
+		  (OID_CACHE_COLLATION_CLASS_ID,
 		   &ACCESS_SPEC_CLS_OID (curr_spec))));
     }
   else
