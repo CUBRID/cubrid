0cdc5bc5c72ad81a52e01016e440e756383437ce
Remzi Ibram <no-reply@cubrid.org>
2015-06-15 13:41:56 +0000

    [CUBRIDSUS-16212][CR] Fix compile error from revision 57501.

--- src/storage/btree.c
+++ src/storage/btree.c
@@ -31225,10 +31225,12 @@ btree_key_append_object_unique (THREAD_ENTRY * thread_p,
 
       /* Cannot consider this a new key. */
       insert_helper->is_unique_key_added_or_deleted = false;
+#if defined (SERVER_MODE)
       /* We don't want to unlock the object we will relocate. Others cannot
        * delete it until we are finished.
        */
       OID_SET_NULL (&insert_helper->saved_locked_oid);
+#endif
     }
   else
     {
