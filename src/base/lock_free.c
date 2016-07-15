773435d3c1fff1c4002b5d9983ef995a62792c68
Remzi Ibram <remzi.ibram@arnia.ro>
2016-07-15 14:30:38 +0300

    Fix saving entry in tran->temp_entry in lf_list_insert_internal

--- src/base/lock_free.c
+++ src/base/lock_free.c
@@ -1305,7 +1305,7 @@ restart_search:
 	    {
 	      /* found an entry with the same key. */
 
-	      if (!LF_LIST_BF_IS_FLAG_SET (behavior_flags, LF_LIST_BF_INSERT_GIVEN) && entry != NULL)
+	      if (!LF_LIST_BF_IS_FLAG_SET (behavior_flags, LF_LIST_BF_INSERT_GIVEN) && *entry != NULL)
 		{
 		  /* save this for further (local) use. */
 		  assert (tran->temp_entry == NULL);
