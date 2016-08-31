d6168ef6a0cac32aaefc9b17ffe68ff3e7cea28a
ESeok Oh <eseok.oh@navercorp.com>
2016-08-31 17:19:15 +0900

    rewrite debugging code of slocator_force

--- src/communication/network_interface_sr.c
+++ src/communication/network_interface_sr.c
@@ -974,7 +974,7 @@ end:
 void
 slocator_force (THREAD_ENTRY * thread_p, unsigned int rid, char *request, int reqlen)
 {
-  int size;
+  int received_size;
   int success;
   LC_COPYAREA *copy_area = NULL;
   char *ptr;
@@ -985,7 +985,7 @@ slocator_force (THREAD_ENTRY * thread_p, unsigned int rid, char *request, int re
   char *content_ptr = NULL, *new_content_ptr = NULL;
   int num_objs;
   char *packed_desc = NULL;
-  int packed_desc_size, packed_desc_len;
+  int packed_desc_size;
   int start_multi_update;
   int end_multi_update;
   LC_COPYAREA_MANYOBJS *mobjs;
@@ -1011,9 +1011,8 @@ slocator_force (THREAD_ENTRY * thread_p, unsigned int rid, char *request, int re
     {
       if (num_objs > 0)
 	{
-	  csserror = css_receive_data_from_client (thread_p->conn_entry, rid, &packed_desc, &size);
-	  packed_desc_len = size;
-	  assert (packed_desc_size == packed_desc_len);
+	  csserror = css_receive_data_from_client (thread_p->conn_entry, rid, &packed_desc, &received_size);
+	  assert (packed_desc_size == received_size);
 	}
 
       if (csserror)
@@ -1031,11 +1030,11 @@ slocator_force (THREAD_ENTRY * thread_p, unsigned int rid, char *request, int re
 
 	  if (content_size > 0)
 	    {
-	      csserror = css_receive_data_from_client (thread_p->conn_entry, rid, &new_content_ptr, &size);
+	      csserror = css_receive_data_from_client (thread_p->conn_entry, rid, &new_content_ptr, &received_size);
 
 	      if (new_content_ptr != NULL)
 		{
-		  memcpy (content_ptr, new_content_ptr, size);
+		  memcpy (content_ptr, new_content_ptr, received_size);
 		  free_and_init (new_content_ptr);
 		}
 
@@ -1055,7 +1054,7 @@ slocator_force (THREAD_ENTRY * thread_p, unsigned int rid, char *request, int re
 	   * Don't need to send the content since it is not updated.
 	   */
 
-	  locator_pack_copy_area_descriptor (num_objs, copy_area, packed_desc, packed_desc_len);
+	  locator_pack_copy_area_descriptor (num_objs, copy_area, packed_desc, packed_desc_size);
 	  ptr = or_pack_int (reply, success);
 	  ptr = or_pack_int (ptr, packed_desc_size);
 	  ptr = or_pack_int (ptr, 0);
