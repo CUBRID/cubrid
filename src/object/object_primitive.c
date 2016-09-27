b32d19e086dfd22f5b6cabcdafdda2b9e1c0f66f
Lucian Lazar <lucianlazar2211@gmail.com>
2016-09-27 10:56:25 +0300

    Removed copy of the compressed_string in setval function

--- src/object/object_primitive.c
+++ src/object/object_primitive.c
@@ -11079,8 +11079,6 @@ mr_setval_string (DB_VALUE * dest, const DB_VALUE * src, bool copy)
 	  error =
 	    db_make_varchar (dest, src_precision, src_str, src_length, DB_GET_STRING_CODESET (src),
 			     DB_GET_STRING_COLLATION (src));
-	  dest->data.ch.medium.compressed_buf = src->data.ch.medium.compressed_buf;
-	  dest->data.ch.info.compressed_need_clear = false;
 	}
       else
 	{
@@ -11099,30 +11097,10 @@ mr_setval_string (DB_VALUE * dest, const DB_VALUE * src, bool copy)
 			       DB_GET_STRING_COLLATION (src));
 	      dest->need_clear = true;
 	    }
-
-	  /* Copy the possible compressed string stored in the DB_VALUE */
-	  if (src->data.ch.medium.compressed_buf == NULL)
-	    {
-	      dest->data.ch.medium.compressed_buf = NULL;
-	    }
-	  else
-	    {
-	      new_compressed_buf = db_private_alloc (NULL, src->data.ch.medium.compressed_size + 1);
-	      if (new_compressed_buf == NULL)
-		{
-		  db_value_domain_init (dest, DB_TYPE_VARCHAR, src_precision, 0);
-		  assert (er_errid () != NO_ERROR);
-		  error = er_errid ();
-		}
-	      else
-		{
-		  memcpy (new_compressed_buf, src->data.ch.medium.compressed_buf, src->data.ch.medium.compressed_size);
-		  new_compressed_buf[src->data.ch.medium.compressed_size] = '\0';
-		  dest->data.ch.medium.compressed_buf = new_compressed_buf;
-		}
-	    }
-	  dest->data.ch.info.compressed_need_clear = src->data.ch.info.compressed_need_clear;
 	}
+
+      dest->data.ch.medium.compressed_buf = src->data.ch.medium.compressed_buf;
+      dest->data.ch.info.compressed_need_clear = false;
 
       dest->data.ch.medium.compressed_size = src->data.ch.medium.compressed_size;
 
