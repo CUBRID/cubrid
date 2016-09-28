b0a69a3daa8ba672c0ace3b8d7461556b9fe5ddd
Lucian Lazar <lucianlazar2211@gmail.com>
2016-09-28 18:38:28 +0300

    Revert last commit

--- src/object/object_primitive.c
+++ src/object/object_primitive.c
@@ -8317,12 +8317,12 @@ pr_midxkey_compare_element (char *mem1, char *mem2, TP_DOMAIN * dom1, TP_DOMAIN 
   c = tp_value_compare_with_error (&val1, &val2, do_coercion, total_order, &comparable);
 
 clean_up:
-  if (DB_NEED_CLEAR (&val1))
+  if (!DB_IS_NULL (&val1) && val1.need_clear == true)
     {
       pr_clear_value (&val1);
     }
 
-  if (DB_NEED_CLEAR (&val2))
+  if (!DB_IS_NULL (&val2) && val2.need_clear == true)
     {
       pr_clear_value (&val2);
     }
