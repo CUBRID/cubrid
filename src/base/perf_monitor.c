9edf5185217fd9e491db54e356c33547c3111411
ESeok Oh <eseok.oh@navercorp.com>
2016-09-28 18:27:45 +0900

    remove unneeded check from perfmon_add_stat_at_offset

--- src/base/perf_monitor.c
+++ src/base/perf_monitor.c
@@ -4089,12 +4089,6 @@ perfmon_add_stat_at_offset (THREAD_ENTRY * thread_p, PERF_STAT_ID psid, const in
   assert (pstat_Global.initialized);
   assert (psid >= 0 && psid < PSTAT_COUNT);
 
-  if (!perfmon_is_perf_tracking ())
-    {
-      /* No need to collect statistics since no one is interested. */
-      return;
-    }
-
   metadata = &pstat_Metadata[psid];
 
   /* Update statistics. */
