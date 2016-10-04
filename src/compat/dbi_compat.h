47c3ddd39796ffc5e5296b75ec3c0fa4c9ad9672
Kisoo Han <kisoo-han@cubrid.com>
2016-10-04 14:52:47 +0900

    [CBRD-20399] Extension of DEFAULT Clause in CREATE statements

--- src/compat/dbi_compat.h
+++ src/compat/dbi_compat.h
@@ -1536,7 +1536,9 @@
 
 #define ER_PRECISION_OVERFLOW			    -1192
 #define ER_PARTITION_EXPRESSION_TOO_LONG	    -1193
-#define ER_LAST_ERROR                               -1194
+
+#define ER_QSTR_DEFAULT_EXPRESSION_TOO_LONG         -1194
+#define ER_LAST_ERROR                               -1195
 
 #define DB_TRUE 1
 #define DB_FALSE 0
