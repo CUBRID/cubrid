32a20da9b777aade1f55785bd26060c48dd35341
David Iancu <davidiancu@yahoo.com>
2016-10-19 14:35:56 +0300

    removed blank

--- src/query/xasl_cache.c
+++ src/query/xasl_cache.c
@@ -255,7 +255,6 @@ xcache_initialize (THREAD_ENTRY * thread_p)
   xcache_check_logging ();
 
   xcache_Soft_capacity = prm_get_integer_value (PRM_ID_XASL_CACHE_MAX_ENTRIES);
-
   if (xcache_Soft_capacity <= 0)
     {
       xcache_log ("disabled.\n");
