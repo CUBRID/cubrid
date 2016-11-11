9c5ae9e3e54d2e770ca3121a820200c17f17f534
alexstanro <alexandru.stan@arnia.ro>
2016-11-11 19:18:29 +0200

    comment added

--- src/query/scan_manager.c
+++ src/query/scan_manager.c
@@ -2284,7 +2284,7 @@ scan_get_index_oidset (THREAD_ENTRY * thread_p, SCAN_ID * s_id, DB_BIGINT * key_
     }
   else
     {
-      /**/
+      /* Clear output val list to avoid memory leak. */
       REGU_VARIABLE_LIST p;
       for (p = iscan_id->indx_cov.regu_val_list; p; p = p->next)
 	{
