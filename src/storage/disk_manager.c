6645454db80f97def5b19ea036ec0efe81073dcc
Remzi Ibram <remzi.ibram@arnia.ro>
2016-11-28 12:21:08 +0200

    update comment

--- src/storage/disk_manager.c
+++ src/storage/disk_manager.c
@@ -1159,7 +1159,7 @@ disk_rv_undo_format (THREAD_ENTRY * thread_p, LOG_RCV * rcv)
     }
   else
     {
-      /* must be next volume that was not added yet to cache */
+      /* must be next volume that was not added yet to cache or a temporary volume */
       assert (disk_Cache->nvols_perm <= volid);
       assert (disk_Cache->vols[volid].purpose == DISK_UNKNOWN_PURPOSE);
       assert (disk_Cache->vols[volid].nsect_free == 0);
