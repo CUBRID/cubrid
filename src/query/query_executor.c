f00e65a3811a4ffeb4393b7eb6c92cc2d8bf608b
alexstanro <alexandru.stan@arnia.ro>
2016-12-14 17:55:05 +0200

    fixed memory leak

--- src/query/query_executor.c
+++ src/query/query_executor.c
@@ -2420,6 +2420,7 @@ qexec_clear_xasl (THREAD_ENTRY * thread_p, XASL_NODE * xasl, bool final)
     }
 
   xasl->query_in_progress = query_save_state;
+  xasl->status = XASL_INITIALIZED;
 
   return pg_cnt;
 }
