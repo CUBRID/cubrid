3c5d386435c6b29c75ebf345a5de16866fd82af3
ESeok Oh <eseok.oh@navercorp.com>
2017-01-05 22:17:59 +0900

    [CBRD-20764] fix typo (#417)

--- src/broker/broker_log_replay.c
+++ src/broker/broker_log_replay.c
@@ -1490,7 +1490,7 @@ get_args (int argc, char *argv[])
 	  break;
 	case 'p':
 	  dbpasswd = optarg;
-#if defined (LIUNUX)
+#if defined (LINUX)
 	  memset (optarg, '*', strlen (optarg));
 #endif
 	  break;
