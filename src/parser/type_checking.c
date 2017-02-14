b14c3630c3a98938e6ed20d645d49d22a411f1a8
razvanradulescu <razvan.radulescu@arnia.ro>
2017-02-14 13:34:16 +0200

    Do not clear COLLATE modifier flag

--- src/parser/type_checking.c
+++ src/parser/type_checking.c
@@ -10344,7 +10344,7 @@ pt_eval_expr_type (PARSER_CONTEXT * parser, PT_NODE * node)
 	      cast_type = pt_make_prim_data_type (parser, PT_TYPE_VARCHAR);
 	      cast_type->info.data_type.collation_id = PT_GET_COLLATION_MODIFIER (node);
 	      cast_type->info.data_type.units = lc->codeset;
-	      PT_EXPR_INFO_CLEAR_FLAG (node, PT_EXPR_INFO_CAST_COLL_MODIFIER);
+	      //PT_EXPR_INFO_CLEAR_FLAG (node, PT_EXPR_INFO_CAST_COLL_MODIFIER);
 	    }
 	  else
 	    {
