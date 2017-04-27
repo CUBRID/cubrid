7ec24741277e920dc57da300d3beca5ad61c5390
ESeok Oh <eseok.oh@navercorp.com>
2017-04-27 15:49:50 +0900

    validates limit arguments at execution time

--- src/query/stream_to_xasl.c
+++ src/query/stream_to_xasl.c
@@ -1923,6 +1923,20 @@ stx_build_xasl_node (THREAD_ENTRY * thread_p, char *ptr, XASL_NODE * xasl)
   ptr = or_unpack_int (ptr, &offset);
   if (offset == 0)
     {
+      xasl->limit_offset = NULL;
+    }
+  else
+    {
+      xasl->limit_offset = stx_restore_regu_variable (thread_p, &xasl_unpack_info->packed_xasl[offset]);
+      if (xasl->limit_offset == NULL)
+	{
+	  goto error;
+	}
+    }
+
+  ptr = or_unpack_int (ptr, &offset);
+  if (offset == 0)
+    {
       xasl->limit_row_count = NULL;
     }
   else
--- src/query/xasl_to_stream.c
+++ src/query/xasl_to_stream.c
@@ -2819,6 +2819,13 @@ xts_process_xasl_node (char *ptr, const XASL_NODE * xasl)
 
   ptr = or_pack_int (ptr, xasl->ordbynum_flag);
 
+  offset = xts_save_regu_variable (xasl->limit_offset);
+  if (offset == ER_FAILED)
+    {
+      return NULL;
+    }
+  ptr = or_pack_int (ptr, offset);
+
   offset = xts_save_regu_variable (xasl->limit_row_count);
   if (offset == ER_FAILED)
     {
@@ -5626,6 +5633,7 @@ xts_sizeof_xasl_node (const XASL_NODE * xasl)
 	   + PTR_SIZE		/* instnum_val */
 	   + PTR_SIZE		/* save_instnum_val */
 	   + OR_INT_SIZE	/* instnum_flag */
+	   + PTR_SIZE		/* limit_offset */
 	   + PTR_SIZE		/* limit_row_count */
 	   + PTR_SIZE		/* fptr_list */
 	   + PTR_SIZE		/* scan_ptr */
