bcd030cbe77c7c8e433bae9c408a9f2f85091898
Remzi Ibram <remzi.ibram@arnia.ro>
2017-05-29 12:31:17 +0300

    fix if 0

--- src/executables/util_sa.c
+++ src/executables/util_sa.c
@@ -3731,7 +3731,7 @@ restoreslave (UTIL_FUNCTION_ARG * arg)
   master_host_name = utility_get_option_string_value (arg_map, RESTORESLAVE_MASTER_HOST_NAME_S, 0);
   source_state = utility_get_option_string_value (arg_map, RESTORESLAVE_SOURCE_STATE_S, 0);
 
-#if defined 0			/* temporary disables */
+#if 0				/* temporary disables */
   if (master_host_name == NULL || source_state == NULL)
     {
       goto print_restoreslave_usage;
@@ -3769,7 +3769,7 @@ restoreslave (UTIL_FUNCTION_ARG * arg)
       goto print_restoreslave_usage;
     }
 
-#if defined 0			/* temporary disables */
+#if 0				/* temporary disables */
   if (check_ha_db_and_node_list (database_name, master_host_name) == false)
     {
       fprintf (stderr,
@@ -3826,7 +3826,7 @@ restoreslave (UTIL_FUNCTION_ARG * arg)
       return EXIT_FAILURE;
     }
 
-#if defined 0			/* temporary disables */
+#if 0				/* temporary disables */
   if (init_ha_catalog)
     {
       error_code = delete_all_ha_apply_info ();
