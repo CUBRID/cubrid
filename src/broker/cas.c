ecda62f44031d0aa5553572c9bf7bcdad7b9645a
Paul Gheorghe <paul.gheorghe@arnia.ro>
2017-08-10 16:21:34 +0300

    AddJsonWithLibrary: move ux set utype for json

--- src/broker/cas.c
+++ src/broker/cas.c
@@ -1943,7 +1943,6 @@ process_request (SOCKET sock_fd, T_NET_BUF * net_buf, T_REQ_INFO * req_info)
   if (DOES_CLIENT_MATCH_THE_PROTOCOL (req_info->client_version, PROTOCOL_V2))
     {
       ux_set_utype_for_enum (CCI_U_TYPE_STRING);
-      ux_set_utype_for_json (CCI_U_TYPE_STRING);
     }
 #endif /* !CAS_FOR_ORACLE && !CAS_FOR_MYSQL */
 
@@ -1959,6 +1958,7 @@ process_request (SOCKET sock_fd, T_NET_BUF * net_buf, T_REQ_INFO * req_info)
       ux_set_utype_for_timestampltz (CCI_U_TYPE_TIMESTAMP);
     }
 #endif
+  ux_set_utype_for_json (CCI_U_TYPE_STRING);
 
   net_buf->client_version = req_info->client_version;
   set_hang_check_time ();
