a30796c2bcc53b5ae28c6e1b9abfa62b33047a7f
Remzi Ibram <remzi.ibram@arnia.ro>
2017-10-19 10:02:24 +0300

    remzi: use __func__

--- src/base/porting.h
+++ src/base/porting.h
@@ -978,9 +978,7 @@ extern "C"
 }
 #endif
 
-#ifdef _MSC_VER
-#define PORTABLE_FUNC_NAME __FUNCTION__
-#elif defined (__GNUC__)
+#ifdef _MSC_VER || defined (__GNUC__)
 #define PORTABLE_FUNC_NAME __func__
 #else
 #define PORTABLE_FUNC_NAME "(unknown)"
