cd36742dfa6cf0d87a326d3705225a1353a046a6
ESeok Oh <eseok.oh@navercorp.com>
2017-10-20 20:21:15 +0900

    [CBRD-21558] inline functions for string compression (#839)
    * inline or_get_varchar_compression_lengths, db_set_compressed_string and db_get_compressed_size
    
    * change OR_GET_STRING_SIZE_BYTE as or_get_string_size_byte

--- src/base/object_representation.h
+++ src/base/object_representation.h
@@ -1518,8 +1518,10 @@ extern int or_mvcc_set_log_lsa_to_record (RECDES * record, LOG_LSA * lsa);
 extern char *or_pack_sha1 (char *ptr, const SHA1Hash * sha1);
 extern char *or_unpack_sha1 (char *ptr, SHA1Hash * sha1);
 
+STATIC_INLINE int or_get_string_size_byte (OR_BUF * buf, int *error) __attribute__ ((ALWAYS_INLINE));
 /* Get the compressed and the decompressed lengths of a string stored in buffer */
-extern int or_get_varchar_compression_lengths (OR_BUF * buf, int *compressed_size, int *decompressed_size);
+STATIC_INLINE int or_get_varchar_compression_lengths (OR_BUF * buf, int *compressed_size, int *decompressed_size)
+  __attribute__ ((ALWAYS_INLINE));
 
 extern int or_packed_spacedb_size (const SPACEDB_ALL * all, const SPACEDB_ONEVOL * vols, const SPACEDB_FILES * files);
 extern char *or_pack_spacedb (char *ptr, const SPACEDB_ALL * all, const SPACEDB_ONEVOL * vols,
@@ -1530,5 +1532,90 @@ extern char *or_unpack_spacedb (char *ptr, SPACEDB_ALL * all, SPACEDB_ONEVOL ** 
 extern int classobj_decompose_property_oid (const char *buffer, int *volid, int *fileid, int *pageid);
 extern void classobj_initialize_default_expr (DB_DEFAULT_EXPR * default_expr);
 extern int classobj_get_prop (DB_SEQ * properties, const char *name, DB_VALUE * pvalue);
+
+/* Because of the VARNCHAR and STRING encoding, this one could not be changed for over 255, just lower. */
+#define OR_MINIMUM_STRING_LENGTH_FOR_COMPRESSION 255
+
+/*
+ * or_get_string_size_byte - read string size byte value from or buffer
+ *    return: byte value read
+ *    buf(in/out): or buffer
+ *    error(out): NO_ERROR or error code
+ *
+ * NOTE that it is really same as or_get_byte function. It is duplicated to inline the function for performance.
+ */
+STATIC_INLINE int
+or_get_string_size_byte (OR_BUF * buf, int *error)
+{
+  int size_prefix;
+
+  if ((buf->ptr + OR_BYTE_SIZE) > buf->endptr)
+    {
+      *error = or_underflow (buf);
+      size_prefix = 0;
+    }
+  else
+    {
+      size_prefix = OR_GET_BYTE (buf->ptr);
+      buf->ptr += OR_BYTE_SIZE;
+      *error = NO_ERROR;
+    }
+  return size_prefix;
+}
+
+/* or_get_varchar_compression_lengths() - Function to get the compressed length and the uncompressed length of 
+ *					  a compressed string.
+ * 
+ * return                 : NO_ERROR or error_code.
+ * buf(in)                : The buffer where the string is stored.
+ * compressed_size(out)   : The compressed size of the string. Set to 0 if the string was not compressed.
+ * decompressed_size(out) : The uncompressed size of the string.
+ */
+STATIC_INLINE int
+or_get_varchar_compression_lengths (OR_BUF * buf, int *compressed_size, int *decompressed_size)
+{
+  int compressed_length = 0, decompressed_length = 0, rc = NO_ERROR, net_charlen = 0;
+  int size_prefix = 0;
+
+  /* Check if the string is compressed */
+  size_prefix = or_get_string_size_byte (buf, &rc);
+  if (rc != NO_ERROR)
+    {
+      assert (size_prefix == 0);
+      return rc;
+    }
+
+  if (size_prefix == OR_MINIMUM_STRING_LENGTH_FOR_COMPRESSION)
+    {
+      /* String was compressed */
+      /* Get the compressed size */
+      rc = or_get_data (buf, (char *) &net_charlen, OR_INT_SIZE);
+      compressed_length = OR_GET_INT ((char *) &net_charlen);
+      if (rc != NO_ERROR)
+	{
+	  return rc;
+	}
+      *compressed_size = compressed_length;
+
+      net_charlen = 0;
+
+      /* Get the decompressed size */
+      rc = or_get_data (buf, (char *) &net_charlen, OR_INT_SIZE);
+      decompressed_length = OR_GET_INT ((char *) &net_charlen);
+      if (rc != NO_ERROR)
+	{
+	  return rc;
+	}
+      *decompressed_size = decompressed_length;
+    }
+  else
+    {
+      /* String was not compressed so we set compressed_size to 0 to know that no compression happened. */
+      *compressed_size = 0;
+      *decompressed_size = size_prefix;
+    }
+
+  return rc;
+}
 
 #endif /* _OBJECT_REPRESENTATION_H_ */
