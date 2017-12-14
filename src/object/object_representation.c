637179f3c8e12ec71c2a2ea309dbf313acdd1036
Paul Gheorghe <paul.gheorghe@arnia.ro>
2017-12-14 16:09:12 +0200

    FixJsonSchemaLeak: it isn't expected that this value be deleted, so cloning it results in a mem leak. 		   we simply pass the schema from the domain, which gets decached and deleted when server is closed

--- src/object/object_representation.c
+++ src/object/object_representation.c
@@ -6891,9 +6891,9 @@ or_get_value (OR_BUF * buf, DB_VALUE * value, TP_DOMAIN * domain, int expected, 
 		}
 	      else
 		{
-		  char *s;
+		  const char *s;
 
-		  s = db_private_strdup (NULL, db_json_get_schema_raw_from_validator (domain->json_validator));
+		  s = db_json_get_schema_raw_from_validator (domain->json_validator);
 		  value->data.json.schema_raw = s;
 		}
 	    }
