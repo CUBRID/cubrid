734d4f6eaa09bf8a1a55215f5b1acda57c80e48a
Paul Gheorghe <paul.gheorghe@arnia.ro>
2018-01-18 15:46:57 +0200

    ServerServerCom: make slave_channel singleton, 		connect to master

--- src/communication/slave_replication_channel.cpp
+++ src/communication/slave_replication_channel.cpp
@@ -1,30 +1,67 @@
 #include "slave_replication_channel.hpp"
 
-#include "connection_defs.h"
+#include "connection_globals.h"
+#include "connection_sr.h"
 
-slave_replication_channel::slave_replication_channel(const std::string& hostname, int port) : master_hostname (hostname), master_port (port)
+slave_replication_channel *slave_replication_channel::singleton = NULL;
+
+slave_replication_channel::slave_replication_channel(const std::string& hostname, const std::string &master_server_name, int port) : master_hostname (hostname),
+                                                                                                                                     master_server_name (master_server_name),
+                                                                                                                                     master_port (port)
 {
-  
+  master_conn_entry = css_make_conn (-1);
+  request_id = -1;
 }
 
 slave_replication_channel::~slave_replication_channel()
 {
-  
+  css_free_conn (master_conn_entry);
 }
 
 int slave_replication_channel::connect_to_master()
 {
-  master_comm_sock_fd = connect_to (master_hostname.c_str (), master_port);
-  
-  if (IS_INVALID_SOCKET (master_comm_sock_fd))
+  int length = 0;
+
+  if (master_conn_entry == NULL)
+    {
+      return ER_FAILED;
+    }
+
+  length = master_server_name.length ();
+
+  if (css_common_connect (master_conn_entry,&request_id, master_hostname.c_str (),
+                          SERVER_REQUEST_CONNECT_NEW_SLAVE, master_server_name.c_str (), length, css_Service_id))
     {
       return REQUEST_REFUSED;
     }
-    
+
   return NO_ERRORS;
 }
 
 int slave_replication_channel::get_master_comm_sock_fd ()
 {
   return master_comm_sock_fd;
+}
+
+void slave_replication_channel::init (const std::string &hostname, const std::string &server_name, int port)
+{
+  if (singleton == NULL)
+    {
+      std::lock_guard<std::mutex> guard (replication_channel::singleton_mutex);
+      if (singleton == NULL)
+        {
+          singleton = new slave_replication_channel (hostname, server_name, port);
+        }
+    }
+}
+
+void slave_replication_channel::reset_singleton ()
+{
+  delete singleton;
+  singleton = NULL;
+}
+ 
+slave_replication_channel *slave_replication_channel::get_channel ()
+{
+  return singleton;
 }
--- src/communication/slave_replication_channel.hpp
+++ src/communication/slave_replication_channel.hpp
@@ -2,19 +2,29 @@
 #define _SLAVE_REPLICATION_CHANNEL_HPP
 
 #include "replication_channel.hpp"
+#include "connection_defs.h"
 
 class slave_replication_channel : public replication_channel
 {
 public:
-  slave_replication_channel (const std::string &hostname, int port);
-  ~slave_replication_channel ();
-  
+
   int connect_to_master ();
   int get_master_comm_sock_fd ();
-private:
-  std::string master_hostname;
-  int master_port, master_comm_sock_fd;
   
+  static void init (const std::string &hostname, const std::string &server_name, int port);
+  static void reset_singleton();
+  static slave_replication_channel *get_channel ();
+
+private:
+  std::string master_hostname, master_server_name;
+  int master_port, master_comm_sock_fd;
+  unsigned short request_id;
+  CSS_CONN_ENTRY *master_conn_entry;
+
+  static slave_replication_channel *singleton;
+  
+  slave_replication_channel (const std::string &hostname, const std::string &server_name, int port);
+  ~slave_replication_channel ();
 };
 
 
