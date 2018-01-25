3436decfdf5dd217c13394f9d5077c8d7cf1d621
Paul Gheorghe <paul.gheorghe@arnia.ro>
2018-01-25 21:16:07 +0200

    ServerServerCom: fix compile

--- src/connection/server_support.c
+++ src/connection/server_support.c
@@ -3476,7 +3476,7 @@ css_process_new_slave (SOCKET master_fd)
   _er_log_debug (ARG_FILE_LINE, "css_process_new_slave:" "received new slave fd from master fd=%d, current_state=%d\n", new_fd, css_get_hb_node_state());
 
   assert ((css_ha_server_state () == HA_SERVER_STATE_ACTIVE ||
-          css_ha_server_state () == HA_SERVER_TO_BE_ACTIVE) &&
+          css_ha_server_state () == HA_SERVER_STATE_TO_BE_ACTIVE) &&
           master_replication_channel::get_channel () != NULL);
 
   master_replication_channel::get_channel ()->add_slave_connection (new_fd);
