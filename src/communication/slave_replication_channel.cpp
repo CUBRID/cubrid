8d081a60973ae07733caaff5c34bf98d70aaf4cc
Paul Gheorghe <paul.gheorghe@arnia.ro>
2018-01-25 18:01:02 +0200

    ServerServerCom: mistake

--- src/communication/slave_replication_channel.cpp
+++ src/communication/slave_replication_channel.cpp
@@ -21,7 +21,7 @@ class slave_dummy_send_msg : public cubthread::entry_task
       if (!IS_INVALID_SOCKET (channel->get_master_conn_entry()->fd))
         {
           int rc = channel->send (channel->get_master_conn_entry()->fd, "hello", replication_channel::get_max_timeout());
-          assert (rc != NO_ERRORS);
+          assert (rc == NO_ERRORS);
           _er_log_debug (ARG_FILE_LINE, "slave::execute:" "sent:hello\n");
         }
     }
