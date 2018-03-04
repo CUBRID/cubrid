3d09950e294ead540965c5c6d5a2233282f23a14
Bogdan SOLOMENCO <bsolomenco@gmail.com>
2018-03-04 10:22:16 +0200

    ELT()

--- src/parser/func_type.cpp
+++ src/parser/func_type.cpp
@@ -97,9 +97,8 @@ std::vector<func_signature> func_signature::lead_lag = {//original code doesn't 
 };
 
 std::vector<func_signature> func_signature::elt = {
-//{1, {PT_GENERIC_TYPE_DISCRETE_NUMBER, PT_GENERIC_TYPE_STRING}, {PT_GENERIC_TYPE_STRING}},
+  {PT_TYPE_NULL, {PT_GENERIC_TYPE_ANY}, {}},
   {1, {PT_GENERIC_TYPE_DISCRETE_NUMBER, PT_TYPE_VARCHAR}, {PT_TYPE_VARCHAR}},
-  {PT_TYPE_NULL, {PT_TYPE_NULL}, {}},
 };
 
 std::vector<func_signature> func_signature::insert = {
