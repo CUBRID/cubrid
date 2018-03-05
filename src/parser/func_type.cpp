c33a87e25674cf568a1cbc2931a96ed9ca2b3b59
Bogdan SOLOMENCO <bsolomenco@gmail.com>
2018-03-05 13:05:20 +0200

    LAG()

--- src/parser/func_type.cpp
+++ src/parser/func_type.cpp
@@ -94,6 +94,7 @@ std::vector<func_signature> func_signature::lead_lag = {//original code doesn't 
   {0, {PT_GENERIC_TYPE_NUMBER}, {}},
   {0, {PT_GENERIC_TYPE_STRING}, {}},
   {0, {PT_GENERIC_TYPE_DATETIME}, {}},
+  {0, {PT_GENERIC_TYPE_BIT}, {}},
 };
 
 std::vector<func_signature> func_signature::elt = {
