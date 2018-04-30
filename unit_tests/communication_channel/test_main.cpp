dfb24081906036e12da532c783a6841e3d702a23
Remzi Ibram <remzi.ibram@arnia.ro>
2018-04-30 12:11:51 +0300

    fix missing include - for real this time

--- unit_tests/communication_channel/test_main.cpp
+++ unit_tests/communication_channel/test_main.cpp
@@ -21,6 +21,7 @@
 #include <thread>
 #include <vector>
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
--- unit_tests/transfer_channel/test_main.cpp
+++ unit_tests/transfer_channel/test_main.cpp
@@ -24,6 +24,7 @@
 #include <thread>
 #include <vector>
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
