f4a4ce7ea60de828d5197884a3ef86d4bb140cc1
Bogdan SOLOMENCO <bsolomenco@gmail.com>
2018-05-24 16:35:20 +0300

    fix without exact mathcing

--- src/parser/type_checking.c
+++ src/parser/type_checking.c
@@ -13331,13 +13331,15 @@ namespace Func
    */
   const func_signature* get_signature (parser_node* node, const std::vector<func_signature>& signatures)
   {
-    const func_signature* signature = get_signature(node, signatures, &cmp_types_normal);
 #if 0 /// try without exact matching
+    const func_signature* signature = get_signature(node, signatures, &cmp_types_normal);
+#else
+    const func_signature* signature = nullptr;
+#endif
     if(signature == NULL)
       {
         signature = get_signature(node, signatures, &cmp_types_generic);
       }
-#endif
     if(signature == NULL)
       {
         signature = get_signature(node, signatures, &cmp_types_castable);
