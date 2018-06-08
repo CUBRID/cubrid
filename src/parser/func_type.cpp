68a9466d0b911acde339100a21fd5babbb2674b6
Bogdan SOLOMENCO <bsolomenco@gmail.com>
2018-06-08 09:43:57 +0300

    double_number: no NULL in signature

--- src/parser/func_type.cpp
+++ src/parser/func_type.cpp
@@ -46,7 +46,6 @@ std::vector<func_signature> func_signature::bigint_discrete = {
 std::vector<func_signature> func_signature::double_number = {
   {PT_TYPE_DOUBLE, {PT_GENERIC_TYPE_NUMBER}, {}},
   {PT_TYPE_DOUBLE, {PT_TYPE_MAYBE}, {}},
-  {PT_TYPE_DOUBLE, {PT_TYPE_NULL}, {}},
   {PT_TYPE_DOUBLE, {PT_TYPE_NA}, {}},
 };
 
