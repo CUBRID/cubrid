65833ecf3937c337167be8892752a12651f477e6
alexstanro <alexandru.stan@arnia.ro>
2018-07-27 12:56:39 +0300

    Fixed locking issue

--- src/transaction/lock_manager.c
+++ src/transaction/lock_manager.c
@@ -6284,15 +6284,6 @@ lock_object (THREAD_ENTRY * thread_p, const OID * oid, const OID * class_oid, LO
 
   if (OID_IS_ROOTOID (class_oid))
     {
-      if (lock == old_class_lock && lock == new_class_lock)
-	{
-	  // try to acquire the same class lock again.
-	  // Since we release a class lock only at EOT, we don't really need to keep updating lock counter.
-
-	  granted = LK_GRANTED;
-	  goto end;
-	}
-
       if (old_class_lock < new_class_lock)
 	{
 	  granted = lock_internal_perform_lock_object (thread_p, tran_index, class_oid, NULL, new_class_lock,
