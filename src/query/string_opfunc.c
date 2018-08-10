508a2d3fc6e1be6f467c19e4cf2c0491b21b3be7
ESeok Oh <eseok.oh@navercorp.com>
2018-08-10 22:48:37 +0900

    indent

--- src/query/string_opfunc.c
+++ src/query/string_opfunc.c
@@ -3447,8 +3447,7 @@ db_json_keys (DB_VALUE * result, DB_VALUE * arg[], int const num_args)
     case DB_TYPE_VARCHAR:
     case DB_TYPE_NCHAR:
     case DB_TYPE_VARNCHAR:
-      error_code = db_json_keys_func (db_get_string (arg[0]), result_json, path.c_str (),
-                                      db_get_string_size (arg[0]));
+      error_code = db_json_keys_func (db_get_string (arg[0]), result_json, path.c_str (), db_get_string_size (arg[0]));
       break;
     case DB_TYPE_JSON:
       error_code = db_json_keys_func (*(db_get_json_document (arg[0])), result_json, path.c_str ());
@@ -3633,7 +3632,7 @@ db_json_merge (DB_VALUE * result, DB_VALUE * arg[], int const num_args)
 	case DB_TYPE_NCHAR:
 	case DB_TYPE_VARNCHAR:
 	  error_code = db_json_convert_string_and_call (db_get_string (arg[i]), db_get_string_size (arg[i]),
-                                                        db_json_merge_func, accumulator);
+							db_json_merge_func, accumulator);
 	  break;
 
 	case DB_TYPE_NULL:
@@ -28398,8 +28397,7 @@ db_value_to_json_doc (const DB_VALUE & value, REFPTR (JSON_DOC, json))
     case DB_TYPE_VARCHAR:
     case DB_TYPE_NCHAR:
     case DB_TYPE_VARNCHAR:
-      error_code = db_json_get_json_from_str (db_get_string (&value), json,
-                                              db_get_string_size (&value));
+      error_code = db_json_get_json_from_str (db_get_string (&value), json, db_get_string_size (&value));
       if (error_code != NO_ERROR)
 	{
 	  assert (json == NULL);
