2812d300c067ea96bde66ed6430f05e5b7a77533
Lucian Lazar <lucianlazar2211@gmail.com>
2018-10-22 18:24:06 +0300

    Used a forward declaration to avoid future wrong casts

--- src/storage/heap_file.h
+++ src/storage/heap_file.h
@@ -181,10 +181,8 @@ struct heap_hfid_table_entry
   FILE_TYPE ftype;		/* value - FILE_HEAP or FILE_HEAP_REUSE_SLOTS */
 };
 
-
-
-
-
+//forward declaration
+struct func_pred;
 typedef struct function_index_info FUNCTION_INDEX_INFO;
 struct function_index_info
 {
@@ -192,7 +190,7 @@ struct function_index_info
   int expr_stream_size;
   int col_id;
   int attr_index_start;
-  void *expr;
+  struct func_pred *expr;
 };
 
 typedef struct func_pred_unpack_info FUNC_PRED_UNPACK_INFO;
