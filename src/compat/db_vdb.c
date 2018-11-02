f08d2f005ebd1411e33adfee305a7a6d1f6e4bda
razvanradulescu <razvan.radulescu@arnia.ro>
2018-11-02 16:42:26 +0200

    indent

--- src/compat/db_vdb.c
+++ src/compat/db_vdb.c
@@ -1740,8 +1740,8 @@ db_execute_and_keep_statement_local (DB_SESSION * session, int stmt_ndx, DB_QUER
       /* now, execute the statement by calling do_execute_statement() */
       err = do_execute_statement (parser, statement);
       if (((err == ER_QPROC_XASLNODE_RECOMPILE_REQUESTED || err == ER_QPROC_INVALID_XASLNODE)
-	  && session->stage[stmt_ndx] == StatementPreparedStage)
-          || (err == ER_QPROC_XASLNODE_RECOMPILE_REQUESTED && session->stage[stmt_ndx] == StatementExecutedStage)
+	   && session->stage[stmt_ndx] == StatementPreparedStage)
+	  || (err == ER_QPROC_XASLNODE_RECOMPILE_REQUESTED && session->stage[stmt_ndx] == StatementExecutedStage))
 	{
 	  /* The cache entry was deleted before 'execute' */
 	  if (statement->xasl_id)
