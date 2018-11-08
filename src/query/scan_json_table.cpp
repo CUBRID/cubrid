f8568ae82cdab66464a66049bb91639c017c28ba
Radu Doros <radudoros@gmail.com>
2018-11-08 11:53:59 +0200

    reset cursor's m_need_advance_row and m_row_was_expanded members to defaults during final clear

--- src/query/scan_json_table.cpp
+++ src/query/scan_json_table.cpp
@@ -266,7 +266,9 @@ namespace cubscan
 	      cursor.delete_input_doc ();
 
 	      cursor.m_child = 0;
+	      cursor.m_need_advance_row = false;
 	      cursor.m_is_row_fetched = false;
+	      cursor.m_row_was_expanded = false;
 	    }
 
 	  m_specp->m_root_node->clear_iterators (is_final_clear);
