54f2ee5ba7ed6beadd26788b1b41da000ecf0da5
ESeok Oh <eseok.oh@navercorp.com>
2018-11-19 16:07:56 +0900

    add license header; [skip ci]

--- src/parser/parse_type.hpp
+++ src/parser/parse_type.hpp
@@ -1,5 +1,28 @@
-#ifndef _PARSER_TYPE_HPP_
-#define _PARSER_TYPE_HPP_
+/*
+ * Copyright (C) 2008 Search Solution Corporation. All rights reserved by Search Solution.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+/*
+ * parse_type.hpp
+ */
+
+#ifndef _PARSE_TYPE_HPP_
+#define _PARSE_TYPE_HPP_
 
 #include "parse_tree.h"
 #include <string>
@@ -105,4 +128,4 @@ typedef pt_arg_type PT_ARG_TYPE;
 
 const char *str (const pt_arg_type &type, string_buffer &sb);
 
-#endif // _PARSER_TYPE_HPP_
+#endif // _PARSE_TYPE_HPP_
