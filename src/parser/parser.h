507dcdf2b4dc494c012852a10124e7e42f659584
Remzi Ibram <ribram@users.noreply.github.com>
2018-12-04 16:01:52 +0200

    [CBRD-22604] generic collation checking (#1383)
    http://jira.cubrid.org/browse/CBRD-22604
    
    Include collation checking while processing argument & signature compatibility (first type checking step).
    
      1. if argument signature doesn't care for collations (e.g. json document) or equivalent type doesn't have a collation, argument does not contribute to common collation and will not be coerced to common collation/codeset.
      2. else if argument is string type or maybe, it will contribute to common collation. if argument is maybe, it will use system collation/codeset, but can be forced to another collation/codeset.
      3. else argument is cast to string using common collation/codeset.
    
    In apply signature step, arguments maybe be coerced to common collation/codeset.
    
    Other changes:
    
      1. changed error messages to make them more clear.
      2. avoid double error report.

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -611,10 +611,13 @@ extern "C"
   extern DB_OBJECT *pt_resolve_serial (PARSER_CONTEXT * parser, PT_NODE * serial_name_node);
   extern int pt_check_grammar_charset_collation (PARSER_CONTEXT * parser, PT_NODE * charset_node, PT_NODE * coll_node,
 						 int *charset, int *coll_id);
-  extern bool pt_get_collation_info (PT_NODE * node, PT_COLL_INFER * coll_infer);
+  extern bool pt_get_collation_info (const PT_NODE * node, PT_COLL_INFER * coll_infer);
   extern int pt_common_collation (PT_COLL_INFER * arg1_coll_infer, PT_COLL_INFER * arg2_coll_infer,
 				  PT_COLL_INFER * arg3_coll_infer, const int args_w_coll, bool op_has_3_args,
 				  int *common_coll, INTL_CODESET * common_cs);
+  extern PT_NODE *pt_coerce_node_collation (PARSER_CONTEXT * parser, PT_NODE * node, const int coll_id,
+					    const INTL_CODESET codeset, bool force_mode, bool use_collate_modifier,
+					    PT_TYPE_ENUM wrap_type_for_maybe, PT_TYPE_ENUM wrap_type_collection);
   extern PT_NODE *pt_make_tuple_value_reference (PARSER_CONTEXT * parser, PT_NODE * name, PT_NODE * select_list,
 						 CURSOR_ID * cursor_p);
   extern PT_NODE *pt_make_query_show_collation (PARSER_CONTEXT * parser, int like_where_syntax,
