2e02130b506ec5a8e74c45ea93d6304d29a40e85
razvanradulescu <razvan.radulescu@arnia.ro>
2019-02-22 17:13:12 +0200

    Fix assert

--- src/transaction/log_tran_table.c
+++ src/transaction/log_tran_table.c
@@ -1939,7 +1939,7 @@ logtb_clear_tdes (THREAD_ENTRY * thread_p, LOG_TDES * tdes)
   LSA_SET_NULL (&tdes->rcv.sysop_start_postpone_lsa);
   LSA_SET_NULL (&tdes->rcv.atomic_sysop_start_lsa);
 
-  if (VACUUM_IS_THREAD_VACUUM (thread_p))
+  if (!VACUUM_IS_THREAD_VACUUM (thread_p))
     {
       tdes->replication_log_generator.clear_transaction ();
     }
