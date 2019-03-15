633319f81a1a0e951a68d32f4bfbc7145d9c44e9
Remzi Ibram <remzi.ibram@arnia.ro>
2019-03-15 18:25:27 +0200

    msgcat_set_log.hpp

--- src/transaction/log_common_impl.h
+++ src/transaction/log_common_impl.h
@@ -27,6 +27,7 @@
 #include "file_io.h"
 #include "log_comm.h"
 #include "log_lsa.hpp"
+#include "msgcat_set_log.hpp"
 #include "recovery.h"
 #include "release_string.h"
 #include "storage_common.h"
@@ -45,40 +46,7 @@
 
 #define LOGPB_BUFFER_NPAGES_LOWER        128
 
-/*
- * Message id in the set MSGCAT_SET_LOG
- * in the message catalog MSGCAT_CATALOG_CUBRID (file cubrid.msg).
- */
-#define MSGCAT_LOG_STARTS                               1
-#define MSGCAT_LOG_LOGARCHIVE_NEEDED                    2
-#define MSGCAT_LOG_BACKUPINFO_NEEDED                    3
-#define MSGCAT_LOG_NEWLOCATION                          4
-#define MSGCAT_LOG_LOGINFO_COMMENT                      5
-#define MSGCAT_LOG_LOGINFO_COMMENT_ARCHIVE_NONEEDED     6
-#define MSGCAT_LOG_LOGINFO_COMMENT_MANY_ARCHIVES_NONEEDED 7
-#define MSGCAT_LOG_LOGINFO_COMMENT_FROM_RENAMED         8
-#define MSGCAT_LOG_LOGINFO_ARCHIVE                      9
-#define MSGCAT_LOG_LOGINFO_KEYWORD_ARCHIVE              10
-#define MSGCAT_LOG_LOGINFO_REMOVE_REASON                11
-#define MSGCAT_LOG_LOGINFO_ACTIVE                       12
-#define MSGCAT_LOG_FINISH_COMMIT                        13
-#define MSGCAT_LOG_FINISH_ABORT                         14
-#define MSGCAT_LOG_INCOMPLTE_MEDIA_RECOVERY             15
-#define MSGCAT_LOG_RESETLOG_DUE_INCOMPLTE_MEDIA_RECOVERY 16
-#define MSGCAT_LOG_DATABASE_BACKUP_WAS_TAKEN            17
-#define MSGCAT_LOG_MEDIACRASH_NOT_IMPORTANT             18
-#define MSGCAT_LOG_DELETE_BKVOLS                        19
-#define MSGCAT_LOG_ENTER_Y2_CONFIRM                     20
-#define MSGCAT_LOG_BACKUP_HALTED_BY_USER                21
-#define MSGCAT_LOG_LOGINFO_ARCHIVES_NEEDED_FOR_RESTORE  22
-#define MSGCAT_LOG_LOGINFO_PENDING_ARCHIVES_RELEASED    23
-#define MSGCAT_LOG_LOGINFO_NOTPENDING_ARCHIVE_COMMENT   24
-#define MSGCAT_LOG_LOGINFO_MULT_NOTPENDING_ARCHIVES_COMMENT 25
-#define MSGCAT_LOG_READ_ERROR_DURING_RESTORE            26
-#define MSGCAT_LOG_INPUT_RANGE_ERROR                    27
-#define MSGCAT_LOG_UPTODATE_ERROR                       28
-#define MSGCAT_LOG_LOGINFO_COMMENT_UNUSED_ARCHIVE_NAME	29
-#define MSGCAT_LOG_MAX_ARCHIVES_HAS_BEEN_EXCEEDED	30
+
 
 /*
  * LOG PAGE
