d4d8464a95b10e5eb470e8d186dc64af24e9049b
radudoros <radudoros@gmail.com>
2019-04-11 17:11:03 +0300

    [CBRD-22672] Fix json_table semantic checks (#1525)
    http://jira.cubrid.org/browse/CBRD-22672
    
    Update semantic check for json_table's json expresion.

--- src/query/scan_json_table.cpp
+++ src/query/scan_json_table.cpp
@@ -228,7 +228,6 @@ namespace cubscan
     scanner::open (cubthread::entry *thread_p)
     {
       int error_code = NO_ERROR;
-      const JSON_DOC *document = NULL;
 
       // we need the starting value to expand into a list of records
       DB_VALUE *value_p = NULL;
@@ -254,9 +253,7 @@ namespace cubscan
 
       if (db_value_type (value_p) == DB_TYPE_JSON)
 	{
-	  document = db_get_json_document (value_p);
-
-	  error_code = init_cursor (*document, *m_specp->m_root_node, m_scan_cursor[0]);
+	  error_code = init_cursor (*db_get_json_document (value_p), *m_specp->m_root_node, m_scan_cursor[0]);
 	  if (error_code != NO_ERROR)
 	    {
 	      ASSERT_ERROR ();
@@ -265,22 +262,15 @@ namespace cubscan
 	}
       else
 	{
-	  // we need json
-	  DB_VALUE json_cast_value;
+	  JSON_DOC_STORE document;
 
-	  // we should use explicit coercion, implicit coercion is not allowed between char and json
-	  tp_domain_status status = tp_value_cast (value_p, &json_cast_value, &tp_Json_domain, false);
-	  if (status != DOMAIN_COMPATIBLE)
+	  error_code = db_value_to_json_doc (*value_p, false, document);
+	  if (error_code != NO_ERROR)
 	    {
-	      ASSERT_ERROR_AND_SET (error_code);
+	      ASSERT_ERROR ();
 	      return error_code;
 	    }
-
-	  document = db_get_json_document (&json_cast_value);
-
-	  error_code = init_cursor (*document, *m_specp->m_root_node, m_scan_cursor[0]);
-
-	  pr_clear_value (&json_cast_value);
+	  error_code = init_cursor (*document.get_immutable (), *m_specp->m_root_node, m_scan_cursor[0]);
 	  if (error_code != NO_ERROR)
 	    {
 	      ASSERT_ERROR ();
