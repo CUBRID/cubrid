19eaeef6011d30755250bde0d364a2adbbc7116a
Remzi Ibram <remzi.ibram@arnia.ro>
2019-04-19 17:28:07 +0300

    align record_descriptor with ha_replication

--- src/base/packer.hpp
+++ src/base/packer.hpp
@@ -116,6 +116,9 @@ namespace cubpacking
       const char *get_buffer_end (void);
       bool is_ended (void);
 
+      size_t get_packed_buffer_size (const char *stream, const size_t length, const size_t curr_offset) const;
+      void pack_buffer_with_length (const char *stream, const size_t length);
+
       // template functions to pack objects in bulk
       // note - it requires versions of get_packed_size_overloaded and pack_overloaded
 
@@ -193,6 +196,9 @@ namespace cubpacking
       void unpack_overloaded (db_value &value);
 
       void unpack_overloaded (packable_object &po);
+
+      void peek_unpack_buffer_length (int &value);
+      void unpack_buffer_with_length (char *stream, const size_t max_length);
 
       const char *get_curr_ptr (void);
       void align (const size_t req_alignment);
