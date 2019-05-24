cd7047961157814e39a5f50c265ce45f68319590
alexandru <alexandru.stan@arnia.ro>
2019-05-24 12:51:48 +0300

    [CBRD-22944] Master node GCM (#1581)
    * Master node GCM added
    
    * Fixed previous commit
    
    * Get closed group
    
    * Removed unnecessary flush
    
    * Removed master group file from sa cmake file
    
    * Clean code - review
    
    * Fixed compile
    
    * clean
    
    * Fixed another missed get_last_closed_group
    
    * Slave node GC added
    
    * Revert to 9b342b02c9a879766d7069eb3258d4a6582aa441
    
    * Improve master GC
    
    * Fixed cmake
    
    * Removed unnecessary code
    
    * indent; [skip ci]
    
    * rename; [skip ci]
    
    * formatting; [skip ci]
    
    * typo fix; [skip ci]
    
    * Fixed compile
    
    * Fixed group commit stream position
    
    * review
    
    * Review

--- src/transaction/log_impl.h
+++ src/transaction/log_impl.h
@@ -661,7 +661,6 @@ struct log_global
 #else				/* SERVER_MODE */
   LOG_LSA final_restored_lsa;
 #endif				/* SERVER_MODE */
-
   /* Buffer for log hdr I/O, size : SIZEOF_LOG_PAGE_SIZE */
   LOG_PAGE *loghdr_pgptr;
 
@@ -670,7 +669,9 @@ struct log_global
 
   /* group commit information */
   LOG_GROUP_COMMIT_INFO group_commit_info;
-  tx_complete_manager *m_tran_complete_mgr;
+  // *INDENT-OFF*
+  cubtx::complete_manager *m_tran_complete_mgr;
+  // *INDENT-ON*
 
   /* remote log writer information */
   logwr_info *writer_info;
@@ -885,6 +886,7 @@ extern void logpb_fatal_error_exit_immediately_wo_flush (THREAD_ENTRY * thread_p
 extern int logpb_check_and_reset_temp_lsa (THREAD_ENTRY * thread_p, VOLID volid);
 extern void logpb_initialize_arv_page_info_table (void);
 extern void logpb_initialize_logging_statistics (void);
+extern void logpb_initialize_tran_complete_manager (void);
 extern int logpb_background_archiving (THREAD_ENTRY * thread_p);
 extern void xlogpb_dump_stat (FILE * outfp);
 
