a1e1463f691e8ae383f63d8893d5ea066414986d
radu doros <radudoros@gmail.com>
2019-05-31 17:35:01 +0300

    fix send a valid stream position

--- src/replication/log_consumer.cpp
+++ src/replication/log_consumer.cpp
@@ -319,7 +319,7 @@ namespace cubreplication
       }
 
     entry = se;
-    m_ctrl_chn->send_ack (entry->get_stream_entry_end_position ());
+    m_ctrl_chn->send_ack (entry->get_stream_entry_start_position ());
 
     return err;
   }
