1cfa1f063c7a4a8482a07ee6faeeacef6c62cacf
Remzi Ibram <remzi.ibram@arnia.ro>
2019-05-31 12:26:25 +0300

    fix is_tran_state_undefined [skip ci]

--- src/replication/replication_stream_entry.hpp
+++ src/replication/replication_stream_entry.hpp
@@ -182,7 +182,7 @@ namespace cubreplication
 
       bool is_tran_state_undefined (void) const
       {
-	return m_header.tran_state != stream_entry_header::TRAN_STATE::UNDEFINED;
+	return m_header.tran_state == stream_entry_header::TRAN_STATE::UNDEFINED;
       }
 
       bool check_mvccid_is_valid () const;
