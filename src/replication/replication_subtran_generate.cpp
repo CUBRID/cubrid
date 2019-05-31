67400e9e49e9e329643a124032ffe258f8fb7173
Remzi Ibram <remzi.ibram@arnia.ro>
2019-05-31 10:55:38 +0300

    add missing files [skip ci]

---
+++ src/replication/replication_subtran_generate.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2008 Search Solution Corporation. All rights reserved by Search Solution.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+//
+// generate replication for sub-transactions (changes of serial/click counter)
+//
+
+#include "replication_subtran_generate.hpp"
+
+#include "cubstream.hpp"
+#include "log_generator.hpp"
+#include "log_manager.h"
+#include "thread_manager.hpp"
+
+namespace cubreplication
+{
+  subtran_generate::subtran_generate ()
+    : m_generator (cubreplication::log_generator::get_global_stream ())
+  {
+  }
+
+  void
+  subtran_generate::start ()
+  {
+    log_sysop_start (&cubthread::get_entry ());
+  }
+
+  void
+  subtran_generate::commit ()
+  {
+    cubstream::stream_position start_stream_pos;
+
+    m_generator.on_subtran_commit ();
+    start_stream_pos = m_generator.pack_stream_entry ();
+    log_sysop_commit_replicated (&cubthread::get_entry (), start_stream_pos);
+  }
+
+  void
+  subtran_generate::abort ()
+  {
+    log_sysop_abort (&cubthread::get_entry ());
+  }
+
+  log_generator &
+  subtran_generate::get_repl_generator ()
+  {
+    return m_generator;
+  }
+
+} // namespace cubreplication
---
+++ src/replication/replication_subtran_generate.hpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2008 Search Solution Corporation. All rights reserved by Search Solution.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+//
+// generate replication for sub-transactions (changes of serial/click counter)
+//
+
+#ifndef _REPLICATION_SUBTRAN_GENERATE_HPP_
+#define _REPLICATION_SUBTRAN_GENERATE_HPP_
+
+#include "log_generator.hpp"
+
+namespace cubreplication
+{
+  class subtran_generate
+  {
+    public:
+
+      subtran_generate ();
+      ~subtran_generate () = default;
+
+      void start ();
+      void commit ();
+      void abort ();
+
+      log_generator &get_repl_generator ();
+
+    private:
+      log_generator m_generator;
+  };
+} // namespace cubreplication
+
+#endif // !_REPLICATION_SUBTRAN_GENERATE_HPP_
