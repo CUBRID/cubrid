00aefb8e6f43fa85bfd8cf3eccb0f98a48c7e0e5
Valeri Nistor <nistor.valeri@gmail.com>
2019-06-05 14:37:51 +0300

    do name resolution & semantic checks on select pt node for PT_CREATE_ENTITY node
    Signed-off-by: Valeri Nistor <nistor.valeri@gmail.com>

--- src/parser/semantic_check.c
+++ src/parser/semantic_check.c
@@ -8461,6 +8461,8 @@ pt_check_create_entity (PARSER_CONTEXT * parser, PT_NODE * node)
       select = node->info.create_entity.create_select;
       if (select != NULL)
 	{
+	  select = pt_semantic_check (parser, select);
+
 	  if (pt_has_parameters (parser, select))
 	    {
 	      PT_ERRORmf (parser, select, MSGCAT_SET_PARSER_SEMANTIC, MSGCAT_SEMANTIC_VARIABLE_NOT_ALLOWED, 0);
@@ -15557,7 +15559,7 @@ pt_get_select_list_coll_compat (PARSER_CONTEXT * parser, PT_NODE * query, SEMAN_
  * pt_apply_union_select_list_collation () - scans a UNION parse tree and
  *		sets for each node with collation the collation corresponding
  *		of the column in 'cinfo' array
- *				
+ *
  *   return:  union compatibility status
  *   parser(in): the parser context
  *   query(in): query node
