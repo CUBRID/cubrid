0a39fc1fb5d0f4cfada332a0504cbbd3b5bff66d
Remzi Ibram <ribram@users.noreply.github.com>
2019-07-09 11:35:47 +0300

    [CBRD-22937] pack stream entry state and count separately (#1650)
    http://jira.cubrid.org/browse/CBRD-22937
    
    Separate packing of stream entry tran state and count to avoid bitmask missmatch.

--- src/replication/replication_stream_entry.hpp
+++ src/replication/replication_stream_entry.hpp
@@ -49,12 +49,6 @@ namespace cubreplication
       NEW_MASTER
     } TRAN_STATE;
 
-    const static unsigned STATE_BITS = 3;
-
-    const static unsigned int STATE_MASK = 0xe0000000;
-
-    const static unsigned int COUNT_VALUE_MASK = ~ (STATE_MASK);
-
     cubstream::stream_position prev_record;
     MVCCID mvccid;
     unsigned int count_replication_entries;
@@ -77,6 +71,7 @@ namespace cubreplication
 
       header_size += serializator.get_packed_bigint_size (header_size);
       header_size += serializator.get_packed_bigint_size (header_size);
+      header_size += serializator.get_packed_int_size (header_size);
       header_size += serializator.get_packed_int_size (header_size);
       header_size += serializator.get_packed_int_size (header_size);
 
@@ -184,9 +179,9 @@ namespace cubreplication
 	       || m_header.tran_state > stream_entry_header::NEW_MASTER;
       }
 
-      int pack_stream_entry_header ();
-      int unpack_stream_entry_header ();
-      int get_packable_entry_count_from_header (void);
+      int pack_stream_entry_header () override;
+      int unpack_stream_entry_header () override;
+      int get_packable_entry_count_from_header (void) override;
 
       void stringify (string_buffer &sb, const string_dump_mode mode = short_dump);
 
