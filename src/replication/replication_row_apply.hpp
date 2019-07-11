9794b2c1a538028e44565c04b7f0c38ea6ad329f
Remzi Ibram <ribram@users.noreply.github.com>
2019-07-11 12:02:54 +0300

    [CBRD-22984] replication_row_apply (#1616)
    http://jira.cubrid.org/browse/CBRD-22894
    
    Moved locator_repl_apply_rbr to its own file and separated into different functions for each type of operation. Most functionality is preserved; changes: errors are not accepted (debug assert safe-guards will fail) and operations are not repeated in case of error.
    
    #### replication_row_apply
    
    - implementation of row apply insert/delete/update
    - helper functions:
    
        - prepare_scan: convert class name to class OID and start scan cache
        - overwrite_last_reprid: set last class representation ID to heap record header
        - find_instance_oid: search key in primary key and find instance OID.
        - generate_updated_record: template function to generate the final record version based on a) record descriptor and b) attrids/values.
    
    #### locator_sr
    
    - add insert/delete/update locator functions with the most usual arguments; the current option is locator_attribute_info_force, which covers all functions and has too many arguments, some of them not always used, some used for different purpose (e.g. instance_oid is input for delete/update, but output for insert).
    - also add pruned versions for insert/update locator functions.
    - remove locator_prepare_rbr_apply and locator_repl_apply_rbr
    
    #### other
    
    - record_descriptor::resize_buffer: convert record type from INVALID to NEW.
    - convert arguments in multiple functions to const
    - add or_set_repid to set representation ID directly in a `char *` instead of recdes data.

---
+++ src/replication/replication_row_apply.hpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2008 Search Solution Corporation. All rights reserved by Search Solution.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+//
+// Applying row replication
+//
+
+#ifndef _REPLICATION_ROW_APPLY_HPP_
+#define _REPLICATION_ROW_APPLY_HPP_
+
+#include "dbtype_def.h"
+
+#include <string>
+#include <vector>
+
+// forward declarations
+class record_descriptor;
+
+namespace cubreplication
+{
+  int row_apply_insert (const std::string &classname, const record_descriptor &record);
+  int row_apply_delete (const std::string &classname, const db_value &key_value);
+  int row_apply_update (const std::string &classname, const db_value &key_value, const record_descriptor &record);
+  int row_apply_update (const std::string &classname, const db_value &key_value,
+			const std::vector<int> &attr_ids, const std::vector<db_value> &attr_values);
+
+} // namespace cubreplication
+
+#endif // !_REPLICATION_ROW_APPLY_HPP_
