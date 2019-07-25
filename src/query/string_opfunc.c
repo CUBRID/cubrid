0a7e8f255c45fdd1ed489621324be25a2113af3d
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2019-07-25 15:36:47 +0900

    add omitted codeset incompatbile check

--- src/query/string_opfunc.c
+++ src/query/string_opfunc.c
@@ -4702,9 +4702,15 @@ db_string_regexp_replace (DB_VALUE *result, DB_VALUE *args[], int const num_args
       }
 
     /* codeset compatible check */
+    INTL_CODESET src_codeset = db_get_string_codeset (src);
+    INTL_CODESET pattern_codeset = db_get_string_codeset (pattern);
+    INTL_CODESET repl_codeset = db_get_string_codeset (repl);
     if ((src_category != pattern_category)
 	|| (pattern_category != repl_category)
-	|| (src_category != repl_category))
+	|| (src_category != repl_category)
+  || (src_codeset != pattern_codeset)
+  || (pattern_codeset != repl_codeset)
+  || (src_codeset != repl_codeset))
       {
 	error_status = ER_QSTR_INCOMPATIBLE_CODE_SETS;
 	er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_QSTR_INCOMPATIBLE_CODE_SETS, 0);
