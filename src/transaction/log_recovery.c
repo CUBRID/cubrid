8a31b25f14df91f80e1b43a59d51cc1f6ebafeb2
ESeok Oh <eseok.oh@navercorp.com>
2019-07-29 19:21:11 +0900

    backport log_rv_redo_fix_page change

--- src/transaction/log_recovery.c
+++ src/transaction/log_recovery.c
@@ -6174,6 +6174,18 @@ log_rv_redo_fix_page (THREAD_ENTRY * thread_p, const VPID * vpid_rcv, LOG_RCVIND
    * fix the page as NEW_PAGE and apply its initialization redo log record.
    */
 
+  if (rcvindex == RVPGBUF_COMPENSATE_DEALLOC)
+    {
+      page = pgbuf_fix (thread_p, vpid_rcv, OLD_PAGE_DEALLOCATED, PGBUF_LATCH_WRITE, PGBUF_UNCONDITIONAL_LATCH);
+      if (page == NULL)
+	{
+	  assert_release (false);
+	  return NULL;
+	}
+
+      return page;
+    }
+
   /* let's first try to fix page if it is not deallocated. */
   if (pgbuf_fix_if_not_deallocated (thread_p, vpid_rcv, PGBUF_LATCH_WRITE, PGBUF_UNCONDITIONAL_LATCH, &page)
       != NO_ERROR)
@@ -6181,11 +6193,26 @@ log_rv_redo_fix_page (THREAD_ENTRY * thread_p, const VPID * vpid_rcv, LOG_RCVIND
       ASSERT_ERROR ();
       return NULL;
     }
+
   if (page == NULL && RCV_IS_NEW_PAGE_INIT (rcvindex))
     {
-      /* page is deallocated. however, this is redo of a new page initialization, we still have to apply it.
-       * page must still be reserved, otherwise it means its file was completely destroyed. */
       DISK_ISVALID isvalid;
+
+      /* see case OLD_PAGE_MAYBE_DEALLOCATED of pgbuf_fix
+       * redo recovery may try to fix an immature page, reserved, but which was not initialized
+       * or it was reused (deallocated and allocated again).
+       */
+
+      if (er_errid () == ER_PB_BAD_PAGEID && er_severity () == ER_WARNING_SEVERITY)
+	{
+	  // forget the warning since we are going to fix the page as NEW and don't want it will bother us.
+	  er_clear ();
+	}
+
+      /* page is deallocated. however, this is redo of a new page initialization, we still have to apply it.
+       * page must still be reserved, otherwise it means its file was completely destroyed
+       */
+
       isvalid = disk_is_page_sector_reserved (thread_p, vpid_rcv->volid, vpid_rcv->pageid);
       if (isvalid == DISK_ERROR)
 	{
@@ -6197,7 +6224,9 @@ log_rv_redo_fix_page (THREAD_ENTRY * thread_p, const VPID * vpid_rcv, LOG_RCVIND
 	  /* not reserved */
 	  return NULL;
 	}
+
       assert (isvalid == DISK_VALID);
+
       page = pgbuf_fix (thread_p, vpid_rcv, NEW_PAGE, PGBUF_LATCH_WRITE, PGBUF_UNCONDITIONAL_LATCH);
       if (page == NULL)
 	{
@@ -6205,5 +6234,6 @@ log_rv_redo_fix_page (THREAD_ENTRY * thread_p, const VPID * vpid_rcv, LOG_RCVIND
 	  return NULL;
 	}
     }
+
   return page;
 }
