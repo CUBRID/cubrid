63a1ccdc265c6b54e8848dab4fb7cdb3d5345300
Remzi Ibram <remzi.ibram@arnia.ro>
2019-08-19 13:30:10 +0300

    be conservative and safe-guard m_last_modified_lsa [skip ci]

--- src/transaction/transaction_transient.cpp
+++ src/transaction/transaction_transient.cpp
@@ -126,6 +126,7 @@ tx_transient_class_registry::decache_heap_repr (const LOG_LSA &downto_lsa)
 {
   for (auto &it : m_list)
     {
+      assert (!it.m_last_modified_lsa.is_null ());
       if (downto_lsa.is_null () || it.m_last_modified_lsa > downto_lsa)
 	{
 	  (void) heap_classrepr_decache (NULL, &it.m_class_oid);
