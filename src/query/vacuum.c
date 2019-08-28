5240860278d1b3df4034828c04b08dd3fa140001
Remzi Ibram <remzi.ibram@arnia.ro>
2019-08-28 19:01:11 +0300

    fix compile

--- src/query/vacuum.c
+++ src/query/vacuum.c
@@ -5421,7 +5421,7 @@ vacuum_recover_lost_block_data (THREAD_ENTRY * thread_p)
   assert (!LSA_ISNULL (&mvcc_op_log_lsa));
 
   // reset header; info will be restored if last block is not consumed.
-  logpb_vacuum_reset_log_header_cache (thread_p, log_Gl.hdr);
+  logpb_vacuum_reset_log_header_cache (thread_p, &log_Gl.hdr);
 
   vacuum_er_log (VACUUM_ER_LOG_VACUUM_DATA | VACUUM_ER_LOG_RECOVERY,
 		 "vacuum_recover_lost_block_data, start recovering from %lld|%d ", LSA_AS_ARGS (&mvcc_op_log_lsa));
