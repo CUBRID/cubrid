9adaa552f421240ed9f6c9915663fd0952d90df6
Valeri Nistor <nistor.valeri@gmail.com>
2019-08-29 12:41:12 +0300

    [CBRD-22755] extend log postpone cache to all transactions (#1821)
    http://jira.cubrid.org/browse/CBRD-22755
    
    Extract log postpone cache implementation to separate files and make use of it in all transactions not just for vacuum workers.
    
    renamings:
    struct/enum
    vacuum_cache_postpone_entry -> log_postpone_cache::cache_entry
    vacuum_cache_postpone_status -> log_postpone_cache::cache_status
    
    define
    VACUUM_CACHE_POSTPONE_ENTRIES_MAX_COUNT -> log_postpone_cache::MAX_CACHE_ENTRIES
    
    function
    vacuum_cache_log_postpone_redo_data -> log_postpone_cache::add_redo_data
    vacuum_cache_log_postpone_lsa -> log_postpone_cache::add_lsa
    vacuum_do_postpone_from_cache -> log_postpone_cache::do_postpone

---
+++ src/transaction/log_postpone_cache.hpp
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2008 Search Solution Corporation. All rights reserved by Search Solution.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+/*
+ * log_postpone_cache.hpp - log postpone cache module
+ */
+
+#ifndef _LOG_POSTPONE_CACHE_HPP_
+#define _LOG_POSTPONE_CACHE_HPP_
+
+#include "log_lsa.hpp"
+#include "storage_common.h"
+
+#include <array>
+
+// forward declarations
+struct log_tdes;
+struct log_prior_node;
+
+namespace cubthread
+{
+  class entry;
+}
+
+/**
+ * Caches postpones to avoid reading them from log after commit top operation with postpone.
+ * Otherwise, log critical section may be required which will slow the access on merged index nodes
+ */
+class log_postpone_cache
+{
+  public:
+    log_postpone_cache ()
+      : m_redo_data_buf (NULL)
+      , m_redo_data_ptr (NULL)
+      , m_cache_status (LOG_POSTPONE_CACHE_NO)
+      , m_cache_entries_cursor (0)
+      , m_cache_entries ()
+    {
+      m_redo_data_buf = new char[REDO_DATA_SIZE];
+    }
+
+    log_postpone_cache (log_postpone_cache &&other) = delete;
+    log_postpone_cache (const log_postpone_cache &other) = delete;
+
+    log_postpone_cache &operator= (log_postpone_cache &&other) = delete;
+    log_postpone_cache &operator= (const log_postpone_cache &other) = delete;
+
+    ~log_postpone_cache ()
+    {
+      delete [] m_redo_data_buf;
+    }
+
+    void clear ();
+
+    void add_redo_data (const log_prior_node &node);
+    void add_lsa (const log_lsa &lsa);
+    bool do_postpone (cubthread::entry &thread_ref, const log_lsa &start_postpone_lsa);
+
+  private:
+    static const std::size_t REDO_DATA_SIZE = IO_MAX_PAGE_SIZE;
+    static const std::size_t MAX_CACHE_ENTRIES = 512;
+
+    class cache_entry
+    {
+      public:
+	cache_entry ()
+	  : m_lsa ()
+	  , m_redo_data (NULL)
+	{
+	  m_lsa.set_null ();
+	}
+
+	log_lsa m_lsa;
+	char *m_redo_data;
+    };
+
+    enum cache_status
+    {
+      LOG_POSTPONE_CACHE_NO,
+      LOG_POSTPONE_CACHE_YES,
+      LOG_POSTPONE_CACHE_OVERFLOW
+    };
+
+    char *m_redo_data_buf;
+    char *m_redo_data_ptr;
+
+    cache_status m_cache_status;
+
+    std::size_t m_cache_entries_cursor;
+    std::array<cache_entry, MAX_CACHE_ENTRIES> m_cache_entries;
+};
+
+#endif /* _LOG_POSTPONE_CACHE_HPP_ */
