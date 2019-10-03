558b04a77f8ab258b1cf62d11a173aac25270638
alexstanro <alexandru.stan@arnia.ro>
2019-10-03 19:43:53 +0300

    [CBRD-22467] Cast host variables to expected domain before prepare when recompile

--- src/compat/db_vdb.c
+++ src/compat/db_vdb.c
@@ -683,6 +683,22 @@ db_compile_statement_local (DB_SESSION * session)
    * prepare_and_execute_query(). */
   if (prm_get_integer_value (PRM_ID_XASL_CACHE_MAX_ENTRIES) > 0 && statement->cannot_prepare == 0)
     {
+      if (session->is_subsession_for_prepared)
+	{
+	  /* cast host variables to their expected domain, before XASL generation. */
+	  session->parser->set_host_var = 0;
+	  err = do_cast_host_variables_to_expected_domain (session);
+	  if (err < 0)
+	    {
+	      if (pt_has_error (parser))
+		{
+		  pt_report_to_ersys_with_statement (parser, PT_SEMANTIC, statement);
+		  return er_errid ();
+		}
+	      return err;
+	    }
+	  session->parser->set_host_var = 1;
+	}
 
       /* now, prepare the statement by calling do_prepare_statement() */
       err = do_prepare_statement (parser, statement);
@@ -2717,13 +2733,7 @@ do_recompile_and_execute_prepared_statement (DB_SESSION * session, PT_NODE * sta
       return er_errid ();
     }
 
-  /* cast host variables to their expected domain */
-  err = do_cast_host_variables_to_expected_domain (new_session);
-  if (err != NO_ERROR)
-    {
-      return err;
-    }
-  new_session->parser->set_host_var = 1;
+  assert (new_session->parser->set_host_var == 1);
 
   new_session->parser->is_holdable = session->parser->is_holdable;
   new_session->parser->is_auto_commit = session->parser->is_auto_commit;
