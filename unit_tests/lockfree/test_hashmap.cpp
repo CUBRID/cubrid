bcbc5299d460c7e385d5414c6ca7a98a785e9218
Remzi Ibram <remzi.ibram@arnia.ro>
2019-10-04 11:30:47 +0300

    functional testing continue

--- unit_tests/lockfree/test_hashmap.cpp
+++ unit_tests/lockfree/test_hashmap.cpp
@@ -26,6 +26,8 @@
 #include <array>
 #include <cstddef>
 #include <cstdlib>
+#include <iostream>
+#include <string>
 #include <thread>
 
 using namespace lockfree;
@@ -81,17 +83,18 @@ namespace test_lockfree
   };
 
   using my_hashmap = hashmap<my_key, my_entry>;
-  using my_hash_table = lf_hash_table_cpp<my_key, my_entry>;
+  using my_lf_hash_table = lf_hash_table_cpp<my_key, my_entry>;
 
-  static void init_hash_table (lf_tran_system &transys, int hash_size, my_hash_table &hash);
+  static void init_lf_hash_table (lf_tran_system &transys, int hash_size, my_lf_hash_table &hash);
   static void init_hashmap (tran::system &transys, size_t hash_size, my_hashmap &hash);
 
-  template <class H>
-  testcase_inserts_only (H &hash, tran::index tran_index, size_t insert_count)
+  template <class H, class Tran>
+  void
+  testcase_inserts_only (H &hash, Tran lftran, size_t insert_count)
   {
     my_key k;
-    size_t inserted;
-    size_t rejected;
+    size_t inserted = 0;
+    size_t rejected = 0;
     my_entry *ent;
 
     for (size_t i = 0; i < insert_count; ++i)
@@ -99,7 +102,7 @@ namespace test_lockfree
 	k.m_1 = (unsigned int) std::rand ();
 	k.m_2 = (unsigned int) std::rand ();
 
-	if (hash.insert (tran_index, k, ent))
+	if (hash.insert (lftran, k, ent))
 	  {
 	    ++inserted;
 	  }
@@ -111,13 +114,15 @@ namespace test_lockfree
   }
 
   template <class H, class Tran, size_t ThCnt, typename F, typename ... Args>
+  void
   start_threads (H &hash, std::array<Tran, ThCnt> &tran_array, F &&f, Args &&... args)
   {
     std::thread all_threads[ThCnt];
 
     for (size_t i = 0; i < ThCnt; i++)
       {
-	all_threads[i] = std::thread (f, std::ref (hash), std::ref (tran_array[i]), std::forward (args)...);
+	all_threads[i] = std::thread (std::forward<F> (f), std::ref (hash), std::ref (tran_array[i]),
+				      std::forward<Args> (args)...);
       }
     for (size_t i = 0; i < ThCnt; i++)
       {
@@ -127,17 +132,106 @@ namespace test_lockfree
 
   template <size_t ThCnt, typename F, typename ... Args>
   void
-  test_hashmap_case ()
+  test_hashmap_case (size_t hash_size, F &&f, Args &&... args)
   {
-    // todo...
+    tran::system l_transys { ThCnt };
+
+    std::array<tran::index, ThCnt> l_indexes;
+    for (size_t i = 0; i < ThCnt; i++)
+      {
+	l_indexes[i] = l_transys.assign_index ();
+      }
+
+    my_hashmap l_hash;
+    init_hashmap (transys, hash_size, l_hash);
+
+    start_threads (l_hash, l_indexes, std::forward<F> (f), std::forward<Args> (args)...);
+
+    l_hash.destroy ();
+    for (size_t i = 0; i < ThCnt; ++i)
+      {
+	l_transys.free_index (l_indexes[i]);
+      }
   }
+
+  template <size_t ThCnt, typename F, typename ... Args>
+  void
+  test_lf_hashtable_case (size_t hash_size, F &&f, Args &&... args)
+  {
+    lf_tran_system l_transys;
+    lf_tran_system_init (&l_transys, (int) ThCnt);
+
+    std::array<lf_tran_entry *, ThCnt> l_tran_ents;
+    for (size_t i = 0; i < ThCnt; ++i)
+      {
+	l_tran_ents[i] = lf_tran_request_entry (&l_transys);
+      }
+
+    my_lf_hash_table l_hash;
+    init_lf_hash_table (l_transys, (int) hash_size, l_hash);
+
+    start_threads (l_hash, l_tran_ents, std::forward<F> (f), std::forward<Args> (args)...);
+
+    l_hash.destroy ();
+    for (size_t i = 0; i < ThCnt; ++i)
+      {
+	lf_tran_return_entry (l_tran_ents[i]);
+      }
+    lf_tran_system_destroy (&l_transys);
+  }
+
+  template <size_t ThCnt, typename F, typename ... Args>
+  void
+  test_hashmap_varsizes (const std::string &case_name, F &&f, Args &&... args)
+  {
+    std::array<size_t, 3> hash_sizes = { 1, 100, 10000 };
+    for (size_t i = 0; i < hash_sizes.size (); ++i)
+      {
+	std::cout << "test lockfree_hashmap|";
+	std::cout << case_name << " [tcnt = " << ThCnt << ", hsz = " << hash_sizes[i] << "]\n";
+	test_hashmap_case<ThCnt> (hash_sizes[i], std::forward<F> (f), std::forward<Args> (args)...);
+      }
+  }
+
+  template <size_t ThCnt, typename F, typename ... Args>
+  void
+  test_lf_hash_table_varsizes (const std::string &case_name, F &&f, Args &&... args)
+  {
+    std::array<size_t, 3> hash_sizes = { 1, 100, 10000 };
+    for (size_t i = 0; i < hash_sizes.size (); ++i)
+      {
+	std::cout << "test lf_hash_table_cpp|";
+	std::cout << case_name << " [tcnt = " << ThCnt << ", hsz = " << hash_sizes[i] << "]\n";
+	test_lf_hashtable_case<ThCnt> (hash_sizes[i], std::forward<F> (f), std::forward<Args> (args)...);
+      }
+  }
+
+  template <typename F, typename ... Args>
+  void
+  run_test_hashmap (const std::string &case_name, F &&f, Args &&... args)
+  {
+    test_hashmap_varsizes<1> (case_name, std::forward (f), std::forward (args)...);
+    test_hashmap_varsizes<4> (case_name, std::forward (f), std::forward (args)...);
+    // test_hashmap_varsizes<64> (case_name, std::forward (f), std::forward (args)...);
+  }
+
+  template <typename F, typename ... Args>
+  void
+  run_test_lf_hash_table (const std::string &case_name, F &&f, Args &&... args)
+  {
+    test_lf_hash_table_varsizes<1> (case_name, std::forward<F> (f), std::forward<Args> (args)...);
+    test_lf_hash_table_varsizes<4> (case_name, std::forward<F> (f), std::forward<Args> (args)...);
+    // test_lf_hash_table_varsizes<64> (case_name, std::forward<F> (f), std::forward<Args> (args)...);
+  }
+
+#define TEMPL_LFHT my_lf_hash_table, lf_tran_entry *
+#define TEMPL_HASHMAP my_hashmap, tran::index
 
   int
   test_hashmap_functional ()
   {
-    tran::system sys (10);
-    dummy_int_hashmap hash;
-    hash.init (sys, 10, 10, 10, g_edesc);
+    run_test_lf_hash_table ("inserts_only=10000", testcase_inserts_only<TEMPL_LFHT>, 10000);
+
     return 0;
   }
 
@@ -154,6 +248,7 @@ namespace test_lockfree
   copy_my_key (void *src, void *dest)
   {
     * (my_key *) src = * (my_key *) dest;
+    return 0;
   }
 
   static int
@@ -183,6 +278,7 @@ namespace test_lockfree
   {
     my_entry *e = (my_entry *) p;
     delete e;
+    return 0;
   }
 
   static int
@@ -191,6 +287,7 @@ namespace test_lockfree
     my_entry *e = (my_entry *) p;
     assert (!e->m_init);
     e->m_init = true;
+    return 0;
   }
 
   static int
@@ -199,10 +296,11 @@ namespace test_lockfree
     my_entry *e = (my_entry *) p;
     assert (e->m_init);
     e->m_init = false;
+    return 0;
   }
 
   static void
-  init_hash_table (lf_tran_system &transys, int hash_size, my_hash_table &hash)
+  init_lf_hash_table (lf_tran_system &transys, int hash_size, my_lf_hash_table &hash)
   {
     hash.init (transys, hash_size, 100, 100, g_edesc);
   }
