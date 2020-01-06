42b7217dc5a14fc66d8184145db7aa7455530a81
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2020-01-06 16:17:49 +0900

    avoid FUM when CCI_ER_INVALID_URL occurs

--- src/cci/cci_util.c
+++ src/cci/cci_util.c
@@ -995,56 +995,55 @@ cci_url_match (const char *src, char *token[])
   static int match_idx[] = { 2, 3, 4, 5, 6, 7, -1 };
 
   int error = CCI_ER_NO_ERROR;
+  for (int i = 0; match_idx[i] != -1; i++)
+    {
+      token[i] = NULL;
+    }
 
   // *INDENT-OFF*
   using namespace std::regex_constants;
   try
-  {
-    std::regex reg (pattern, ECMAScript | icase);
+    {
+      std::regex reg (pattern, ECMAScript | icase);
 
-    std::cmatch match;
-    bool searched = std::regex_search (src, match, reg);
-    if (searched)
-      {
-	for (int i = 0; match_idx[i] != -1; i++)
-	  {
-	    token[i] = NULL;
-	  }
-
-	int num_matches = match.size ();
-	for (int i = 0; match_idx[i] != -1 && match_idx[i] < num_matches; i++)
-	  {
-	    std::csub_match sub_by_idx = match[match_idx[i]];
-	    size_t n = sub_by_idx.length ();
-	    token[i] = (char *) MALLOC (n + 1);
-	    if (token[i] == NULL)
-	      {
-		error = CCI_ER_NO_MORE_MEMORY;
-		break;
-	      }
-	    std::string t = sub_by_idx.str ();
-	    strncpy (token[i], t.c_str (), n);
-	    token[i][n] = '\0';
-	  }
-      }
-    else
-      {
-	error = CCI_ER_INVALID_URL;
-      }
-  }
-  catch (std::regex_error & e)
-  {
-    // regex exception
-    if (e.code () == error_stack)
-      {
-	error = CCI_ER_NO_MORE_MEMORY;
-      }
-    else
-      {
-	error = CCI_ER_INVALID_URL;
-      }
-    fprintf (stderr, "regex_error : %s\n", e.what ());
-  }
+      std::cmatch match;
+      bool searched = std::regex_search (src, match, reg);
+      if (searched)
+	{
+	  int num_matches = match.size ();
+	  for (int i = 0; match_idx[i] != -1 && match_idx[i] < num_matches; i++)
+	    {
+	      std::csub_match sub_by_idx = match[match_idx[i]];
+	      size_t n = sub_by_idx.length ();
+	      token[i] = (char *) MALLOC (n + 1);
+	      if (token[i] == NULL)
+		{
+		  error = CCI_ER_NO_MORE_MEMORY;
+		  break;
+		}
+	      std::string t = sub_by_idx.str ();
+	      strncpy (token[i], t.c_str (), n);
+	      token[i][n] = '\0';
+	    }
+	}
+      else
+	{
+	  error = CCI_ER_INVALID_URL;
+	}
+    }
+  catch (std::regex_error &e)
+    {
+      // regex exception
+      if (e.code () == error_stack)
+	{
+	  error = CCI_ER_NO_MORE_MEMORY;
+	}
+      else
+	{
+	  error = CCI_ER_INVALID_URL;
+	}
+      fprintf (stderr, "regex_error : %s\n", e.what ());
+    }
   // *INDENT-ON*
 
   if (error != CCI_ER_NO_ERROR)
