ece08a3dbd6371c482be46d93b399ebd0f68c56f
alexstanro <alexandru.stan@arnia.ro>
2020-01-07 13:46:51 +0200

    Fixed increment class granules

--- src/transaction/lock_manager.c
+++ src/transaction/lock_manager.c
@@ -9881,7 +9881,8 @@ lock_increment_class_granules (LK_ENTRY * class_entry)
     }
 
   class_entry->ngranules++;
-  if (class_entry->class_entry != NULL && !OID_IS_ROOTOID (&class_entry->class_entry->res_head->key.oid))
+  if (class_entry->class_entry != NULL && !OID_IS_ROOTOID (&class_entry->class_entry->res_head->key.oid)
+      && LK_ENTRY_IS_ACTIVE (class_entry->class_entry))
     {
       /* This is a class in a class hierarchy so increment the number of granules for the superclass */
       class_entry->class_entry->ngranules++;
