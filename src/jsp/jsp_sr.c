d984f3d85d22b105dd93941957800a3c24e61fad
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2020-04-14 11:19:02 +0900

    modify to backport into 10.2

--- src/jsp/jsp_sr.c
+++ src/jsp/jsp_sr.c
@@ -38,6 +38,10 @@
 #include <jni.h>
 #include <locale.h>
 #include <assert.h>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iterator>
 
 #include "jsp_sr.h"
 
@@ -340,6 +344,60 @@ jsp_get_create_java_vm_function_ptr (void)
 
 #endif /* !WINDOWS */
 
+
+/*
+ * jsp_create_java_vm
+ *   return: create java vm
+ *
+ * Note:
+ */
+static int
+jsp_create_java_vm (JNIEnv ** env_p, JavaVMInitArgs * vm_arguments)
+{
+  int res;
+#if defined(WINDOWS)
+  __try
+  {
+    res = JNI_CreateJavaVM (&jvm, (void **) env_p, vm_arguments);
+  }
+  __except (delay_load_dll_exception_filter (GetExceptionInformation ()))
+  {
+    res = -1;
+  }
+#else /* WINDOWS */
+  CREATE_VM_FUNC create_vm_func = (CREATE_VM_FUNC) jsp_get_create_java_vm_function_ptr ();
+  if (create_vm_func)
+    {
+      res = (*create_vm_func) (&jvm, (void **) env_p, vm_arguments);
+    }
+  else
+    {
+      res = -1;
+      er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_SP_JVM_LIB_NOT_FOUND, 1, dlerror ());
+    }
+#endif /* WINDOWS */
+  return res;
+}
+
+/*
+ * jsp_tokenize_jvm_options
+ *  return: tokenized array of string
+ *
+ */
+
+// *INDENT-OFF*
+static std::vector <std::string>
+jsp_tokenize_jvm_options (char *opt_str)
+{
+  std::string str (opt_str);
+  std::istringstream iss (str);
+  std::vector <std::string> options;
+  std::copy (std::istream_iterator <std::string> (iss),
+	     std::istream_iterator <std::string> (), std::back_inserter (options));
+  return options;
+}
+// *INDENT-ON*
+
 /*
  * jsp_start_server -
  *   return: Error Code
@@ -359,14 +417,15 @@ jsp_start_server (const char *db_name, const char *path)
   jstring jstr_dbname, jstr_path, jstr_version, jstr_envroot;
   jobjectArray args;
   JavaVMInitArgs vm_arguments;
-  const int vm_n_options = 3;
-  JavaVMOption options[vm_n_options];
+  JavaVMOption *options;
+  int vm_n_options = 3;
   char classpath[PATH_MAX + 32], logging_prop[PATH_MAX + 32];
-  char *loc_p, *locale;
+  char disable_sig_handle[] = "-Xrs";
   const char *envroot;
   char jsp_file_path[PATH_MAX];
-  char optionString2[] = "-Xrs";
-  CREATE_VM_FUNC create_vm_func = NULL;
+  char port[6] = { 0 };
+  char *loc_p, *locale;
+  char *jvm_opt_sysprm = NULL;
 
   if (!prm_get_bool_value (PRM_ID_JAVA_STORED_PROCEDURE))
     {
@@ -391,9 +450,24 @@ jsp_start_server (const char *db_name, const char *path)
   snprintf (logging_prop, sizeof (logging_prop) - 1, "-Djava.util.logging.config.file=%s",
 	    envvar_javadir_file (jsp_file_path, PATH_MAX, "logging.properties"));
 
+  jvm_opt_sysprm = (char *) prm_get_string_value (PRM_ID_JAVA_STORED_PROCEDURE_JVM_OPTIONS);
+  // *INDENT-OFF*
+  std::vector <std::string> opts = jsp_tokenize_jvm_options (jvm_opt_sysprm);
+  // *INDENT-ON*
+  vm_n_options += (int) opts.size ();
+  options = new JavaVMOption[vm_n_options];
+
+  int idx = 3;
   options[0].optionString = classpath;
   options[1].optionString = logging_prop;
-  options[2].optionString = optionString2;
+  options[2].optionString = disable_sig_handle;
+  for (auto it = opts.begin (); it != opts.end (); ++it)
+    {
+      // *INDENT-OFF*
+      options[idx++].optionString = const_cast <char*> (it->c_str ());
+      // *INDENT-ON*
+    }
+
   vm_arguments.version = JNI_VERSION_1_4;
   vm_arguments.options = options;
   vm_arguments.nOptions = vm_n_options;
@@ -406,35 +480,21 @@ jsp_start_server (const char *db_name, const char *path)
       locale = strdup (loc_p);
     }
 
-#if defined(WINDOWS)
+  res = jsp_create_java_vm (&env_p, &vm_arguments);
+  // *INDENT-OFF*
+  delete[] options;
+  // *INDENT-ON*
 
-  __try
-  {
-    res = JNI_CreateJavaVM (&jvm, (void **) &env_p, &vm_arguments);
-  }
-  __except (delay_load_dll_exception_filter (GetExceptionInformation ()))
-  {
-    res = -1;
-  }
-
-#else /* WINDOWS */
-
-  create_vm_func = (CREATE_VM_FUNC) jsp_get_create_java_vm_function_ptr ();
-  if (create_vm_func)
+#if !defined(WINDOWS)
+  if (er_has_error ())
     {
-      res = (*create_vm_func) (&jvm, (void **) &env_p, &vm_arguments);
-    }
-  else
-    {
-      er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_SP_JVM_LIB_NOT_FOUND, 1, dlerror ());
       if (locale != NULL)
 	{
 	  free (locale);
 	}
       return er_errid ();
     }
-
-#endif /* !WINDOWS */
+#endif
 
   setlocale (LC_TIME, locale);
   if (locale != NULL)
