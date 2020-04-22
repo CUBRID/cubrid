de3f7addfdfe70de439566d31e432c71832f23d7
Byungwook Kim <bwkim@cubrid.com>
2020-04-22 12:13:58 +0900

    [APIS-782] Indentation

--- src/jdbc/cubrid/jdbc/jci/UConnection.java
+++ src/jdbc/cubrid/jdbc/jci/UConnection.java
@@ -171,7 +171,7 @@ public class UConnection {
 	public final static int MAX_QUERY_TIMEOUT = 2000000;
 	public final static int MAX_CONNECT_TIMEOUT = 2000000;
 
-  public final static int READ_TIMEOUT = 10000;
+  	public final static int READ_TIMEOUT = 10000;
 
 	public final static int FN_STATUS_NONE = -2;
 	public final static int FN_STATUS_IDLE = -1;
