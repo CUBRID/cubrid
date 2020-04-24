c0ad8c95ef400c6c8a3eec53bcfbb3ba8592cdbf
Kisoo Han <kisoo-han@cubrid.com>
2020-04-24 12:19:33 +0900

    [CBRD-23639] Implementation of Secure Broker/CAS (#2231) (#2275)
    * [CBRD-23639] Implemantation of Secure Borker/CAS (#2231)
    
    * [CBRD-23639] Add magic string for SSL
    
    * [CBRD-22369] Check magic string in cas header including CUBRS
    
    * [CBRD-23639] Fix indent
    
    * [CBRD-23639] Check the connected client and broker are the same type.
    
    * [CBRD-23639] Fix indentation
    
    * [CBRD-23639] Change reject ERROR code when broker & client types are mis-matched
    
    * [CBRD-23639] change length macro to SRV_CON_CLIENT_MAGIC_LEN
    
    * [CBRD-23639] Add self-signed certificate used in CAS
    
    * [CBRD-23369] Change CMakeLists.txt for SSL
    
    * [CBRD-23639] Add README for OPENSSL
    
    * [CBRD-23639] SSL read/write primitives
    
    * [CBRD-23639] Fix indent
    
    * [CBRD-23639] Change for SSL
    
    * [CBRD-23639] Upgrade CAS proto to PROTOCOL_V10
    
    * [CBRD-23639] Correct for Readability

--- src/broker/cas_protocol.h
+++ src/broker/cas_protocol.h
@@ -35,7 +35,11 @@ extern "C"
 #define SRV_CON_CLIENT_INFO_SIZE	10
 #define SRV_CON_CLIENT_MAGIC_LEN	5
 #define SRV_CON_CLIENT_MAGIC_STR	"CUBRK"
+#define SRV_CON_CLIENT_MAGIC_STR_SSL	"CUBRS"
 #define SRV_CON_MSG_IDX_CLIENT_TYPE	5
+
+#define IS_SSL_CLIENT(driver_info) \
+	(strncmp (driver_info, SRV_CON_CLIENT_MAGIC_STR_SSL, SRV_CON_CLIENT_MAGIC_LEN) == 0)
 
 /* 8th and 9th-byte (index 7 and 8) are reserved for backward compatibility.
  * 8.4.0 patch 1 or earlier versions hold minor and patch version on them.
@@ -220,7 +224,8 @@ extern "C"
     PROTOCOL_V7 = 7,		/* timezone types, to pin xasl entry for retry */
     PROTOCOL_V8 = 8,		/* JSON type */
     PROTOCOL_V9 = 9,		/* cas health check: get function status */
-    CURRENT_PROTOCOL = PROTOCOL_V9
+    PROTOCOL_V10 = 10,		/* Secure Broker/CAS using SSL */
+    CURRENT_PROTOCOL = PROTOCOL_V10
   };
   typedef enum t_cas_protocol T_CAS_PROTOCOL;
 
