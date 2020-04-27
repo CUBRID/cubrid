86d332afc1cd194e552d8a7f6860249096f13d41
Kisoo Han <kisoo-han@cubrid.com>
2020-04-27 15:15:02 +0900

    Update cas_ssl.c

--- src/broker/cas_ssl.c
+++ src/broker/cas_ssl.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2008 Search Solution Corporation. All rights reserved by Search Solution.
+ * Copyright (C) 2008 Search Solution Corporation.
+ * Copyright (c) 2020 CUBRID Corp
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
