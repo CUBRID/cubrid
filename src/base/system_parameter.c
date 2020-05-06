f92ab894689eb1b2e2206dfcd2d33c69d310e9ca
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2020-05-06 16:59:34 +0900

    moidfy license comment

--- src/base/system_parameter.c
+++ src/base/system_parameter.c
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) 2008 Search Solution Corporation. All rights reserved by Search Solution.
- *
+ * Copyright (C) 2008 Search Solution Corporation
+ * Copyright (C) 2020 CUBRID Corporation
+ * 
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
