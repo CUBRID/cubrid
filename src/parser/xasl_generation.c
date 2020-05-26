e21d3255a2397b3cb94270900da5d007b46ccb54
shparkcubrid <49504233+shparkcubrid@users.noreply.github.com>
2020-05-26 10:19:18 +0900

    [CBRD-21022] executeBatch returns Cannot communicate with the broker or received invalid packet & CAS down with core dump (#2323)
    http://jira.cubrid.org/browse/CBRD-21022
    
    fix to enable XASL cache for insert statements containing clobs and blobs and do not reuse XASL at XASL generator step

--- src/parser/xasl_generation.c
+++ src/parser/xasl_generation.c
@@ -20906,11 +20906,12 @@ parser_generate_xasl_pre (PARSER_CONTEXT * parser, PT_NODE * node, void *arg, in
     case PT_UNION:
     case PT_DIFFERENCE:
     case PT_INTERSECTION:
-      if (!node->info.query.xasl)
-	{
-	  (void) pt_query_set_reference (parser, node);
-	  pt_push_symbol_info (parser, node);
-	}
+      /* The parser tree can be reused when multiple queries are executed through ux_execute_array (). */
+      /* The XASL object has already been freed at pt_exit_packing_buf (), so only node->info.query.xasl is changed to null. */
+      node->info.query.xasl = NULL;
+
+      (void) pt_query_set_reference (parser, node);
+      pt_push_symbol_info (parser, node);
       break;
 
     default:
