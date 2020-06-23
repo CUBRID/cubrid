2592ed3940aa5662a9b76f366dee59838daafb92
Byungwook Kim <bwkim@cubrid.com>
2020-06-23 14:57:39 +0900

    [CBRD-23684] revert system_param

--- src/base/system_parameter.c
+++ src/base/system_parameter.c
@@ -410,9 +410,9 @@ static const char sysprm_ha_conf_file_name[] = "cubrid_ha.conf";
 
 #define PRM_NAME_JAVA_STORED_PROCEDURE_JVM_OPTIONS "java_stored_procedure_jvm_options"
 
-#define PRM_NAME_JAVA_STORED_PROCEDURE_DEBUG "java_stored_procedure_debug"
-
 #define PRM_NAME_JAVA_STORED_PROCEDURE_RESERVE_01 "java_stored_procedure_reserve_01"
+
+#define PRM_NAME_JAVA_STORED_PROCEDURE_RESERVE_02 "java_stored_procedure_reserve_02"
 
 #define PRM_NAME_COMPAT_PRIMARY_KEY "compat_primary_key"
 
@@ -2282,15 +2282,13 @@ const char *PRM_JAVA_STORED_PROCEDURE_JVM_OPTIONS = "";
 static const char *prm_java_stored_procedure_jvm_options_default = "";
 static unsigned int prm_java_stored_procedure_jvm_options_flag = 0;
 
-int PRM_JAVA_STORED_PROCEDURE_DEBUG = -1;
-static int prm_java_stored_procedure_debug_default = -1;
-static int prm_java_stored_procedure_debug_upper = 65535;
-static int prm_java_stored_procedure_debug_lower = -1;
-static unsigned int prm_java_stored_procedure_debug_flag = 0;
-
 bool PRM_JAVA_STORED_PROCEDURE_RESERVE_01 = false;
 static bool prm_java_stored_procedure_reserve_01_default = false;
 static unsigned int prm_java_stored_procedure_reserve_01_flag = 0;
+
+bool PRM_JAVA_STORED_PROCEDURE_RESERVE_02 = false;
+static bool prm_java_stored_procedure_reserve_02_default = false;
+static unsigned int prm_java_stored_procedure_reserve_02_flag = 0;
 
 typedef int (*DUP_PRM_FUNC) (void *, SYSPRM_DATATYPE, void *, SYSPRM_DATATYPE);
 
@@ -5870,24 +5868,24 @@ static SYSPRM_PARAM prm_Def[] = {
    (char *) NULL,
    (DUP_PRM_FUNC) NULL,
    (DUP_PRM_FUNC) NULL},
-  {PRM_ID_JAVA_STORED_PROCEDURE_DEBUG,
-   PRM_NAME_JAVA_STORED_PROCEDURE_DEBUG,
+  {PRM_ID_JAVA_STORED_PROCEDURE_RESERVE_01,
+   PRM_NAME_JAVA_STORED_PROCEDURE_RESERVE_01,
    (PRM_FOR_SERVER | PRM_HIDDEN),
    PRM_INTEGER,
-   &prm_java_stored_procedure_debug_flag,
-   (void *) &prm_java_stored_procedure_debug_default,
-   (void *) &PRM_JAVA_STORED_PROCEDURE_DEBUG,
+   &prm_java_stored_procedure_reserve_01_flag,
+   (void *) &prm_java_stored_procedure_reserve_01_default,
+   (void *) &PRM_JAVA_STORED_PROCEDURE_RESERVE_01,
    (void *) NULL, (void *) NULL,
    (char *) NULL,
    (DUP_PRM_FUNC) NULL,
    (DUP_PRM_FUNC) NULL},
-  {PRM_ID_JAVA_STORED_PROCEDURE_RESERVE_01,
-   PRM_NAME_JAVA_STORED_PROCEDURE_RESERVE_01,
+  {PRM_ID_JAVA_STORED_PROCEDURE_RESERVE_02,
+   PRM_NAME_JAVA_STORED_PROCEDURE_RESERVE_02,
    (PRM_FOR_SERVER | PRM_HIDDEN),
    PRM_BOOLEAN,
-   &prm_java_stored_procedure_reserve_01_flag,
-   (void *) &prm_java_stored_procedure_reserve_01_default,
-   (void *) &PRM_JAVA_STORED_PROCEDURE_RESERVE_01,
+   &prm_java_stored_procedure_reserve_02_flag,
+   (void *) &prm_java_stored_procedure_reserve_02_default,
+   (void *) &PRM_JAVA_STORED_PROCEDURE_RESERVE_02,
    (void *) NULL, (void *) NULL,
    (char *) NULL,
    (DUP_PRM_FUNC) NULL,
