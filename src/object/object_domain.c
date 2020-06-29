ebcbcfce93f395bd3d7c87a475b0ba0795f94bc5
Byungwook Kim <bwkim@cubrid.com>
2020-06-29 13:32:39 +0900

    [CBRD-23679] add modification for BIT type

--- src/object/object_domain.c
+++ src/object/object_domain.c
@@ -9177,9 +9177,12 @@ tp_value_cast_internal (const DB_VALUE * src, DB_VALUE * dest, const TP_DOMAIN *
 		      {
 			status = DOMAIN_INCOMPATIBLE;
 		      }
-		    else if (data_stat == DATA_STATUS_TRUNCATED && coercion_mode == TP_IMPLICIT_COERCION)
+		    else if (data_stat == DATA_STATUS_TRUNCATED &&
+			     (prm_get_bool_value (PRM_ID_CHECK_TYPE_TRANS_STRICT) == true
+			      || coercion_mode == TP_IMPLICIT_COERCION))
 		      {
 			status = DOMAIN_OVERFLOW;
+			pr_clear_value (target);
 		      }
 		    else
 		      {
@@ -9240,7 +9243,9 @@ tp_value_cast_internal (const DB_VALUE * src, DB_VALUE * dest, const TP_DOMAIN *
 	    {
 	      status = DOMAIN_INCOMPATIBLE;
 	    }
-	  else if (data_stat == DATA_STATUS_TRUNCATED && coercion_mode == TP_IMPLICIT_COERCION)
+	  else if (data_stat == DATA_STATUS_TRUNCATED &&
+		   (prm_get_bool_value (PRM_ID_CHECK_TYPE_TRANS_STRICT) == true
+		    || coercion_mode == TP_IMPLICIT_COERCION))
 	    {
 	      status = DOMAIN_OVERFLOW;
 	      pr_clear_value (target);
@@ -9278,8 +9283,8 @@ tp_value_cast_internal (const DB_VALUE * src, DB_VALUE * dest, const TP_DOMAIN *
 	      status = DOMAIN_INCOMPATIBLE;
 	    }
 	  else if (data_stat == DATA_STATUS_TRUNCATED &&
-              (prm_get_bool_value (PRM_ID_CHECK_TYPE_TRANS_STRICT) == true
-              || coercion_mode == TP_IMPLICIT_COERCION))
+		   (prm_get_bool_value (PRM_ID_CHECK_TYPE_TRANS_STRICT) == true
+		    || coercion_mode == TP_IMPLICIT_COERCION))
 	    {
 	      status = DOMAIN_OVERFLOW;
 	      pr_clear_value (target);
