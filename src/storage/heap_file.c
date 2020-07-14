f5cf33f77d541d39d17a4f1503cf5dd1ca098b27
Byungwook Kim <bwkim@cubrid.com>
2020-07-15 08:53:29 +0900

    [CBRD-23679] allow overflow except for string type

--- src/storage/heap_file.c
+++ src/storage/heap_file.c
@@ -17043,7 +17043,8 @@ heap_object_upgrade_domain (THREAD_ENTRY * thread_p, HEAP_SCANCACHE * upd_scanca
 	  bool set_max_value = false;
 
 	  if (prm_get_bool_value (PRM_ID_ALTER_TABLE_CHANGE_TYPE_STRICT) == true
-	      || prm_get_bool_value (PRM_ID_ALLOW_TRUNCATED_STRING) == false)
+	      || TP_IS_CHAR_TYPE ((TP_DOMAIN_TYPE (dest_dom))
+				  && prm_get_bool_value (PRM_ID_ALLOW_TRUNCATED_STRING) == false))
 	    {
 	      error = ER_ALTER_CHANGE_TRUNC_OVERFLOW_NOT_ALLOWED;
 	      er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, error, 0);
