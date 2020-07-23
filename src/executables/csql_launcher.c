125ad2f1de123b5e7fc2a276585579d51750886b
Mike MyungHwan Oh <mhoh@cubrid.com>
2020-07-23 04:19:34 +0000

    fix indent

--- src/executables/csql_launcher.c
+++ src/executables/csql_launcher.c
@@ -334,10 +334,10 @@ main (int argc, char *argv[])
     {
       /* Don't allow to skip vacuum on CS mode
          goto print_usage;
-    }
-  else if (explicit_single_line && csql_arg.single_line_execution == false)
-    {
-      /* Don't allow both at once. */
+         }
+         else if (explicit_single_line && csql_arg.single_line_execution == false)
+         {
+         /* Don't allow both at once. */
       goto print_usage;
     }
   else if (csql_arg.sa_mode)
