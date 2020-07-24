cfb48d99a20e5875cd4772c573fadf615af602d1
Kang Dooho <airnet@cubrid.com>
2020-07-24 17:23:04 +0900

    [CBRD-23688] add copyright text Copyright (c) 2016 CUBRID Corporation.

--- src/cci/cci_ssl.h
+++ src/cci/cci_ssl.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2008 Search Solution Corporation.
- * Copyright (c) 2020 CUBRID Corp
+ * Copyright (C) 2016 CUBRID Corporation
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
