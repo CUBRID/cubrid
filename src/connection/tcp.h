e2a68f0a363e7f32c6b331248450aa13b3a34238
radu-vtv <65609908+radu-vtv@users.noreply.github.com>
2021-02-10 16:33:31 +0200

    [LETS-18] Separate TCP and UNIX socket open (#2565)

--- src/connection/tcp.h
+++ src/connection/tcp.h
@@ -42,7 +42,8 @@ extern char *css_get_master_domain_path (void);
 
 extern SOCKET css_tcp_client_open (const char *host, int port);
 extern SOCKET css_tcp_client_open_with_retry (const char *host, int port, bool will_retry);
-extern int css_tcp_master_open (int port, SOCKET * sockfd);
+extern int css_tcp_socket_bind_listen (int port, SOCKET & sockfd);
+extern int css_master_open_sockets (int port, SOCKET * sockfd);
 extern bool css_tcp_setup_server_datagram (char *pathname, SOCKET * sockfd);
 extern bool css_tcp_listen_server_datagram (SOCKET sockfd, SOCKET * newfd);
 extern bool css_tcp_master_datagram (char *pathname, SOCKET * sockfd);
