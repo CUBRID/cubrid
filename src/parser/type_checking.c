2ccdee56702046180585b4c3855c19471d24e235
Byungwook Kim <bwkim@cubrid.com>
2021-03-05 16:48:10 +0900

    fix: pt_get_expression_definition

--- src/parser/type_checking.c
+++ src/parser/type_checking.c
@@ -3273,7 +3273,7 @@ pt_get_expression_definition (const PT_OP_TYPE op, EXPRESSION_DEFINITION * def)
 
       def->overloads_count = num;
       break;
-
+#if 0
     case PT_NVL:
     case PT_IFNULL:
     case PT_COALESCE:
@@ -3611,6 +3611,9 @@ pt_get_expression_definition (const PT_OP_TYPE op, EXPRESSION_DEFINITION * def)
       def->overloads_count = num;
 
       break;
+
+#endif
+
     case PT_CONNECT_BY_ROOT:
     case PT_PRIOR:
     case PT_QPRIOR:
