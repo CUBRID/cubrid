4668ef6bfcaa9d1ab5af841d56c8393a6205cd7d
Cristian Arghiroiu <cristi.arghiroiu@gmail.com>
2021-03-05 12:41:03 +0200

    [LETS-27] refactor log recovery redo switch (#2587)
    http://jira.cubrid.org/browse/LETS-27
    
    Refactor log_recovery_redo record type switch cases into a suite of [templated] function calls in order to reduce code duplication to a minimum and allow further development into a hybrid sync/async execution model.
    
    Implementation:
    - added function log_rv_redo_record_sync_or_dispatch_parallel:
      - gathers together common parts of the logic in a case statement in log_recovery_redo,
      - templated on the log record info type;
      - makes use of helper templated functors to extract bits of data from the log record info structure: vpid, log data pointers, redo length, mvccid, offset, the actual data to be applied, apply function pointer
    - log_rv_redo_record_sync_or_dispatch_parallel superseeds the more limited existing function log_rv_redo_rec which is still only used for record type LOG_DBEXTERN_REDO_DATA (could not be refactored because LOG_REC_DBOUT_REDO struct differs too much)
    - small correction along the way: RVVAC_COMPLETE is never used as LOG_MVCC_REDO_DATA so that check and assignment was dropped
    
    Other:
    - some helper macro/template code has been added in debugging/type_helper.hpp to be able to print a friendly name of a template argument (useful in debug-level logging).
    - added a 'scope_exit' helper class to be used to implement a custom RAII-type of functionality with a fair degree of flexibility

---
+++ src/base/scope_exit.hpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2008 Search Solution Corporation. All rights reserved by Search Solution.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+/*
+ * scope_exit.hpp
+ */
+
+#ifndef _SCOPE_EXIT_HPP_
+#define _SCOPE_EXIT_HPP_
+
+// Scope Guard: execute something when it goes out of scope (no extra cost when optimization is enabled)
+#include <type_traits>
+#include <utility>
+
+template<typename Callable>//Callable=function, function address, function pointer, functor, lambda, std::function
+class scope_exit final
+{
+  public:
+    explicit scope_exit (Callable &&callable) noexcept
+      : m_valid (true)
+      , m_callable (std::is_lvalue_reference<decltype (callable)>::value ? callable : std::forward<Callable> (callable))
+    {}
+
+    scope_exit (scope_exit &&sg) noexcept
+      : m_valid (sg.m_valid)
+      , m_callable (sg.m_callable)
+    {
+      sg.m_valid = false;
+    }
+
+    ~scope_exit() noexcept
+    {
+      if (m_valid)
+	{
+	  m_callable();
+	}
+    }
+
+    void release() noexcept
+    {
+      m_valid = false;
+    }
+  private:
+    using func_t = std::function<void (void)>;
+
+    bool m_valid;
+    func_t m_callable;
+
+    scope_exit() = delete;
+    scope_exit (scope_exit &) = delete;
+    scope_exit &operator= (const scope_exit &) = delete;
+    scope_exit &operator= (scope_exit &&) = delete;
+};
+
+#endif /* _SCOPE_EXIT_HPP_ */
