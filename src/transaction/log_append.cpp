78a4cf377f1c1773b5e17f3aa6ad934e760ffe48
Remzi Ibram <ribram@users.noreply.github.com>
2021-03-05 17:00:54 +0200

    [LETS-30] Send/receive log prior lists (#2588)
    http://jira.cubrid.org/browse/LETS-30
    
    Add two new classes, prior_sender and prior_recver, part of the active log transfer process. Their job is to convert the log prior list on the source node into network message and recreate the prior lists on the destination server. The log prior list is the in-memory representation of the log records sequence before it is serialized into log pages.
    
    - prior_sender: convert prior node lists into messages to be sent over network
    - prior_recver: convert messages received over network into prior lists and append them to prior info.
    
    **Implementation details**
    
    `cublog::prior_sender`:
     
    - limited management of receivers as a list of "sinks". add_sink() caller is responsible of creating the function that sends the message argument to a receiver over network. a generic sink system was preferred to allow different type of send/receive situations (ATS->PS, ATS->PTS, PS->PS, PS->PTS) that may be handled slightly differently.
    - send_list(): convert the list of prior nodes into a string message and broadcast the message to all sinks.
    - will be used by the active transaction servers in both SERVER_MODE/SA_MODE
    - a global instance is part of log_Gl
    - logpb_append_prior_lsa_list(), the function that flushes the prior list into log pages, also calls prior_sender.send_list().
    
    `cublog::prior_recver`:
        
    - push_message() is used by request handler to pass a string message containing prior nodes list to the prior receiver
    - internal thread loops and consumes messages, rebuilding the prior nodes list into a prior info (that will be log_Gl.prior_info).
    - will be used by page servers and passive transaction servers only
    
    `log_prior_lsa_info`:
    
    - Default field initialization
    - push_list(): push the argument list at the end of its prior list. used by prior_recver
    
    **Unit testing**
    
      - new project, test_prior_sendrecv, in test_log_transfer
      - two test cases:
      
        1. test one prior sender and one prior receiver
        2. test one prior sender and two prior receivers
    
      - two helper classes:
     
        - test_env: simulates log prior nodes life cycle and encompasses sender/receivers
        - test_wakeup_flush_tracker: hooks into log_wakeup_flush_thread function in order to synchronize with the receivers.
    
      - mockup prior list serialization and log_prior_lsa_info::push_list functions
      
      **Out of scope**
      
     - Indentation changes as a result of merging with scalability_dev

--- src/transaction/log_append.cpp
+++ src/transaction/log_append.cpp
@@ -131,6 +131,49 @@ log_prior_lsa_info::log_prior_lsa_info ()
 }
 
 void
+log_prior_lsa_info::push_list (log_prior_node *&list_head, log_prior_node *&list_tail)
+{
+  if (list_head == nullptr)
+    {
+      return;
+    }
+  assert (list_tail != nullptr);
+
+#if !defined (NDEBUG)
+  // check list input is valid
+  log_prior_node *nodep;
+  for (nodep = list_head; nodep->next != nullptr; nodep = nodep->next)
+    {
+      assert (nodep->next->start_lsa == nodep->log_header.forw_lsa);
+      assert (nodep->next->log_header.back_lsa == nodep->start_lsa);
+    }
+  assert (nodep == list_tail);
+#endif
+
+  std::unique_lock<std::mutex> ulock (log_Gl.prior_info.prior_lsa_mutex);
+
+  assert (list_head->start_lsa == prior_lsa);
+  assert (list_head->log_header.back_lsa == prev_lsa);
+
+  if (prior_list_header == nullptr)
+    {
+      prior_list_header = list_head;
+      prior_list_tail = list_tail;
+    }
+  else
+    {
+      assert (prior_list_tail != nullptr);
+      prior_list_tail->next = list_head;
+      prior_list_tail = list_tail;
+    }
+
+  prior_lsa = list_tail->log_header.forw_lsa;
+  prev_lsa = list_tail->start_lsa;
+
+  list_head = nullptr;
+}
+
+void
 LOG_RESET_APPEND_LSA (const LOG_LSA *lsa)
 {
   // todo - concurrency safe-guard
@@ -1689,19 +1732,20 @@ prior_list_serialize (const log_prior_node *head)
   return serialized;
 }
 
-log_prior_node *
-prior_list_deserialize (const std::string &str)
+void
+prior_list_deserialize (const std::string &str, log_prior_node *&head, log_prior_node *&tail)
 {
   // reversed prior_node_serialize
+
+  head = nullptr;
+  tail = nullptr;
   if (str.empty ())
     {
-      return nullptr;
+      return;
     }
 
   // iterate through str using ptr
   const char *ptr = str.c_str ();
-  log_prior_node *headp = nullptr;
-  log_prior_node *tailp = nullptr;
 
   while (ptr < str.c_str () + str.size ())
     {
@@ -1709,21 +1753,19 @@ prior_list_deserialize (const std::string &str)
       assert (nodep != nullptr);
       ptr = prior_node_deserialize (ptr, *nodep);
 
-      if (headp == nullptr)
+      if (head == nullptr)
 	{
-	  headp = nodep;
-	  tailp = nodep;
+	  head = nodep;
+	  tail = nodep;
 	}
       else
 	{
-	  assert (tailp != nullptr);
-	  tailp->next = nodep;
-	  tailp = nodep;
+	  assert (tail != nullptr);
+	  tail->next = nodep;
+	  tail = nodep;
 	}
     }
   assert (ptr == str.c_str () + str.size ());
-
-  return headp;
 }
 
 /*
