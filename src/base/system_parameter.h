fe1a55fb84114fd313f44ad97e04fb03eb8bffe8
Remzi Ibram <remzi.ibram@gmail.com>
2021-03-11 17:56:04 +0200

    Add debug messages for log prior list send/receive

--- src/base/system_parameter.h
+++ src/base/system_parameter.h
@@ -456,8 +456,9 @@ enum param_id
   PRM_ID_PAGE_SERVER_HOSTS,
   PRM_ID_SERVER_TYPE,
 
+  PRM_ID_ER_LOG_PRIOR_TRANSFER,
   /* change PRM_LAST_ID when adding new system parameters */
-  PRM_LAST_ID = PRM_ID_SERVER_TYPE
+  PRM_LAST_ID = PRM_ID_ER_LOG_PRIOR_TRANSFER
 };
 typedef enum param_id PARAM_ID;
 
