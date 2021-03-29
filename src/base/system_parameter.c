566ab8809d80fa271568c88eb909601717f37151
Cristian Arghiroiu <cristi.arghiroiu@gmail.com>
2021-03-29 15:41:13 +0300

    added system parameter for page server parallel replication

--- src/base/system_parameter.c
+++ src/base/system_parameter.c
@@ -698,12 +698,14 @@ static const char sysprm_ha_conf_file_name[] = "cubrid_ha.conf";
 
 #define PRM_NAME_PAGE_SERVER_HOSTS "page_server_hosts"
 #define PRM_NAME_SERVER_TYPE "server_type"
-#define PRM_NAME_RECOVERY_PARALLEL_COUNT "recovery_parallel_count"
+#define PRM_NAME_PAGE_SERVER_REPLICATION_PARALLEL_COUNT "page_server_replication_parallel_count"
 
 #define PRM_NAME_ER_LOG_PRIOR_TRANSFER "er_log_prior_transfer"
 #define PRM_NAME_ER_LOG_COMM_REQUEST "er_log_comm_request"
 #define PRM_NAME_ER_LOG_COMM_CHANNEL "er_log_comm_channel"
 #define PRM_NAME_ER_LOG_COMMIT_CONFIRM "er_log_commit_confirm"
+
+#define PRM_NAME_RECOVERY_PARALLEL_COUNT "recovery_parallel_count"
 
 #define PRM_VALUE_DEFAULT "DEFAULT"
 #define PRM_VALUE_MAX "MAX"
@@ -2380,6 +2382,12 @@ static int prm_server_type_default = SERVER_TYPE_TRANSACTION;
 static int prm_server_type_lower = SERVER_TYPE_TRANSACTION;
 static int prm_server_type_upper = SERVER_TYPE_PAGE;
 static unsigned int prm_server_type_flag = 0;
+
+static unsigned int prm_page_server_replication_parallel_count_flag = 0;
+static int prm_page_server_replication_parallel_count_default = 8;
+int PRM_PAGE_SERVER_REPLICATION_PARALLEL_COUNT_CURRENT_VALUE = 8;
+static int prm_page_server_replication_parallel_count_upper_value = 32;
+static int prm_page_server_replication_parallel_count_lower_value = 0;
 
 bool PRM_ER_LOG_PRIOR_TRANSFER = false;
 static bool prm_er_log_prior_transfer_default = false;
@@ -6110,6 +6118,18 @@ static SYSPRM_PARAM prm_Def[] = {
    (void *) &PRM_SERVER_TYPE,
    (void *) &prm_server_type_upper,
    (void *) &prm_server_type_lower,
+   (char *) NULL,
+   (DUP_PRM_FUNC) NULL,
+   (DUP_PRM_FUNC) NULL},
+  {PRM_ID_PAGE_SERVER_REPLICATION_PARALLEL_COUNT,
+   PRM_NAME_PAGE_SERVER_REPLICATION_PARALLEL_COUNT,
+   (PRM_FOR_SERVER),
+   PRM_INTEGER,
+   &prm_page_server_replication_parallel_count_flag,
+   (void *) &prm_page_server_replication_parallel_count_default,
+   (void *) &PRM_PAGE_SERVER_REPLICATION_PARALLEL_COUNT_CURRENT_VALUE,
+   (void *) &prm_page_server_replication_parallel_count_upper_value,
+   (void *) &prm_page_server_replication_parallel_count_lower_value,
    (char *) NULL,
    (DUP_PRM_FUNC) NULL,
    (DUP_PRM_FUNC) NULL},
