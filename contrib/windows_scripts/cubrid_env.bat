1c6fb7e3642a6919ea4cf03a376e140823e3225d
Kang Dooho <airnet@cubrid.com>
2021-03-30 10:10:35 +0900

    [CBRD-21446] Change the setting of CUBRID environment variable

--- contrib/windows_scripts/cubrid_env.bat
+++ contrib/windows_scripts/cubrid_env.bat
@@ -5,8 +5,8 @@ rem LOADING CUBRID Environments
 echo Setting CUBRID Environments
 
 
-set CUBRID "C:\CUBRID"
-set CUBRID_DATABASES "%CUBRID%\DATABASES"
+set CUBRID=C:\CUBRID
+set CUBRID_DATABASES=%CUBRID%\databases
 
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\CUBRID\cmserver" /v "ROOT_PATH" /t REG_SZ /d "%CUBRID%\\" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\CUBRID\cmserver" /v "Version" /t REG_SZ /d "10.2" /f
@@ -15,7 +15,7 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\CUBRID\CUBRID" /v "Version" /t REG_SZ /d "1
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\CUBRID\CUBRID" /v "Patch" /t REG_SZ /d "0" /f
 
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "CUBRID" /t REG_SZ /d "%CUBRID%\\" /f
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "CUBRID_DATABASES" /t REG_SZ /d "%CUBRID%\databases" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "CUBRID_DATABASES" /t REG_SZ /d "%CUBRID_DATABASES%" /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "Path" /t REG_SZ /d "%CUBRID%\bin;%PATH%" /f
 
 echo %CUBRID%
