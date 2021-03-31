565bdaed0a7c31b69643c7377d6c9fea57f3e315
hwanyseo <hwanyseo@cubrid.com>
2021-04-01 08:46:20 +0900

    [CBRD-23899] fix license error

--- src/cci/cci_query_execute.c
+++ src/cci/cci_query_execute.c
@@ -1,33 +1,21 @@
 /*
- * Copyright (C) 2008 Search Solution Corporation. 
- * Copyright (c) 2016 CUBRID Corporation.
+ * Copyright 2008 Search Solution Corporation
+ * Copyright 2016 CUBRID Corporation
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * - Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * - Neither the name of the <ORGANIZATION> nor the names of its contributors
- *   may be used to endorse or promote products derived from this software without
- *   specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
- * OF SUCH DAMAGE.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  */
+
 
 
 /*
@@ -3690,7 +3678,7 @@ qe_get_data_int (T_CCI_U_TYPE u_type, char *col_value_p, void *value)
       return CCI_ER_TYPE_CONVERSION;
     }
 
-  *(int *)value = data;
+  *(int *) value = data;
 
   return 0;
 }
@@ -3764,7 +3752,7 @@ qe_get_data_uint (T_CCI_U_TYPE u_type, char *col_value_p, void *value)
       return CCI_ER_TYPE_CONVERSION;
     }
 
-  *(unsigned int *)value = data;
+  *(unsigned int *) value = data;
 
   return 0;
 }
@@ -3847,7 +3835,7 @@ qe_get_data_float (T_CCI_U_TYPE u_type, char *col_value_p, void *value)
       return CCI_ER_TYPE_CONVERSION;
     }
 
-  *(float *)value = data;
+  *(float *) value = data;
   return 0;
 }
 
@@ -3928,7 +3916,7 @@ qe_get_data_double (T_CCI_U_TYPE u_type, char *col_value_p, void *value)
       return CCI_ER_TYPE_CONVERSION;
     }
 
-  *(double *)value = data;
+  *(double *) value = data;
   return 0;
 }
 
@@ -3959,7 +3947,7 @@ qe_get_data_date (T_CCI_U_TYPE u_type, char *col_value_p, void *value)
       return CCI_ER_TYPE_CONVERSION;
     }
 
-  *(T_CCI_DATE *)value = data;
+  *(T_CCI_DATE *) value = data;
   return 0;
 }
 
@@ -3986,7 +3974,7 @@ qe_get_data_date_tz (T_CCI_U_TYPE u_type, char *col_value_p, void *value, int to
       return CCI_ER_TYPE_CONVERSION;
     }
 
-  *(T_CCI_DATE_TZ *)value = data;
+  *(T_CCI_DATE_TZ *) value = data;
   return 0;
 }
 
