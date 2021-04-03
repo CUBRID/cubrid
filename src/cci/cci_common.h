d314d3d7f3ffd452a9b7887c540e5cd871f72e50
Cristian Arghiroiu <cristi.arghiroiu@gmail.com>
2021-04-04 00:45:52 +0300

    revert gratuituous change

--- src/cci/cci_common.h
+++ src/cci/cci_common.h
@@ -185,9 +185,9 @@
 
 #define SET_START_TIME_FOR_QUERY(CON_HANDLE, REQ_HANDLE)            \
   do {                                                              \
-    if (CON_HANDLE != nullptr) {                                    \
+    if (CON_HANDLE) {                                               \
       int time_to_check = 0;                                        \
-      if (REQ_HANDLE != nullptr) {                                  \
+      if (REQ_HANDLE) {                                             \
         time_to_check = ((T_REQ_HANDLE *)(REQ_HANDLE))->query_timeout;\
       }                                                             \
       else {                                                        \
