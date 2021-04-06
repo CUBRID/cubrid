27b159e7d229fe335f9d0334890d8cd4bbe4a229
Cristian Arghiroiu <cristi.arghiroiu@gmail.com>
2021-04-06 10:32:16 +0300

    Revert "temporarily deactivate for testing purposes"
    This reverts commit 5914efd20a8b5dd363b0eeafb27a724e6ae29ef2.

--- src/storage/slotted_page.c
+++ src/storage/slotted_page.c
@@ -5276,6 +5276,6 @@ bool
 spage_is_save_space_disabled ()
 {
   const bool during_crash_recovery = log_is_in_crash_recovery ();
-  //const bool in_page_server (get_server_type () == SERVER_TYPE_PAGE);
-  return during_crash_recovery; // || in_page_server;
+  const bool in_page_server (get_server_type () == SERVER_TYPE_PAGE);
+  return during_crash_recovery || in_page_server;
 }
