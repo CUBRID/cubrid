804ef616d6f7073df8ad44640a4eb7ace837b8ff
Cristian Arghiroiu <cristi.arghiroiu@gmail.com>
2021-04-09 12:13:47 +0300

    [LETS-57] Page Server Parallel Replication (#2672)
    http://jira.cubrid.org/browse/LETS-57
    
    Implementation:
    - introduce a new parameter `page_server_replication_parallel_count` that drives whether replication is to performed in parallel or sequentially; by default it is parallel with `8` tasks; setting the value of the parameter to `0` will render replication working sequentially;
    - the parallel log apply infrastructure has been added in `cublog::replicator` (`log_replication.*`) where it essentially is chained to the `cublog::replicator` daemon task; it comes online before the task and is waited-for-to-finish and stop-destroyed after the daemon has been stopped; additionally, a soft-sync-point has been added on the retire handler of the daemon task (it may be that this is gratuitous and could work without it, but, can't hurt);
    - for the dispatch of jobs, the same rules (read: workarounds) as for parallel log recovery have been added; so, the logic in `replicator::read_and_redo_record` reuses `log_recovery - log_rv_redo_record_sync_or_dispatch_async`
    - `cublog::redo_job_impl`:
      - introduced a new `bool force_each_page_fetch` parameter that allows to drive `log_reader` to always fetch a log page; normally, if the `vpid` is the same, the page is assumed as being already "there" (i.e. up to date) and not fetched again; however, in the recovery case, it is needed to fetch the page with each use; this is, most likely, a workaround that needs a better solution as each fetch is a page-sized `memcpy` that affects performance;
      - in `redo_parallel::redo_job_base` introduced a `log_lsa` - needed for future development in tracking the progress of the asynchronous log replication
      - adapted existing unit tests for the above changes
    
    Testing:
    - manual stress-testing within a container enviroment using a custom testing script that contains: insert/delete/update, drop/create/alter table
    - including start/stop/restart or PS/TS
    - verification of data consistency by means of comparing the result of select queries executed  in stand-alone mode on the transaction server (TS) database and on the page server (PS) database
    
    Co-authored-by: Remzi Ibram <remzi.ibram@gmail.com>

--- src/base/system_parameter.c
+++ src/base/system_parameter.c
@@ -726,6 +726,8 @@ static const char sysprm_ha_conf_file_name[] = "cubrid_ha.conf";
 
 #define PRM_NAME_PL_TRANSACTION_CONTROL "pl_transaction_control"
 
+#define PRM_NAME_RECOVERY_PARALLEL_COUNT "recovery_parallel_count"
+
 #define PRM_VALUE_DEFAULT "DEFAULT"
 #define PRM_VALUE_MAX "MAX"
 #define PRM_VALUE_MIN "MIN"
@@ -2322,12 +2324,6 @@ bool PRM_DDL_AUDIT_LOG = false;
 static bool prm_ddl_audit_log_default = false;
 static unsigned int prm_ddl_audit_log_flag = 0;
 
-static unsigned int prm_recovery_parallel_count_flag = 0;
-static int prm_recovery_parallel_count_default = 8;
-int PRM_RECOVERY_PARALLEL_COUNT_CURRENT_VALUE = 8;
-static int prm_recovery_parallel_count_upper_value = 32;
-static int prm_recovery_parallel_count_lower_value = 0;
-
 UINT64 PRM_DDL_AUDIT_LOG_SIZE = 10485760ULL;
 static UINT64 prm_ddl_audit_log_size_default = 10485760ULL;	/* 10M */
 static UINT64 prm_ddl_audit_log_size_lower = 10485760ULL;	/* 10M */
@@ -2343,6 +2339,12 @@ static unsigned int prm_supplemental_log_flag = 0;
 bool PRM_CDC_LOGGING_DEBUG = false;
 static bool prm_cdc_logging_debug_default = false;
 static unsigned int prm_cdc_logging_debug_flag = 0;
+
+static unsigned int prm_recovery_parallel_count_flag = 0;
+static int prm_recovery_parallel_count_default = 8;
+int PRM_RECOVERY_PARALLEL_COUNT_CURRENT_VALUE = 8;
+static int prm_recovery_parallel_count_upper_value = 32;
+static int prm_recovery_parallel_count_lower_value = 0;
 
 int PRM_RECOVERY_PROGRESS_LOGGING_INTERVAL = 0;
 static int prm_recovery_progress_logging_interval_default = 0;
@@ -6382,7 +6384,7 @@ SYSPRM_PARAM prm_Def[] = {
    (void *) &prm_recovery_parallel_count_lower_value,
    (char *) NULL,
    (DUP_PRM_FUNC) NULL,
-   (DUP_PRM_FUNC) NULL}
+   (DUP_PRM_FUNC) NULL},
 };
 
 static int num_session_parameters = 0;
