32331a06f940c1da2d127c1fedce5b559e52efe7
Joohok <joohokim@cubrid.com>
2021-04-19 09:31:25 +0900

    Add supplemental log parameter

--- src/base/system_parameter.h
+++ src/base/system_parameter.h
@@ -452,8 +452,9 @@ enum param_id
   PRM_ID_IGNORE_TRAILING_SPACE,
   PRM_ID_DDL_AUDIT_LOG,
   PRM_ID_DDL_AUDIT_LOG_SIZE,
+  PRM_ID_SUPPLEMENTAL_LOG,
   /* change PRM_LAST_ID when adding new system parameters */
-  PRM_LAST_ID = PRM_ID_DDL_AUDIT_LOG_SIZE
+  PRM_LAST_ID = PRM_ID_SUPPLEMENTAL_LOG
 };
 typedef enum param_id PARAM_ID;
 
