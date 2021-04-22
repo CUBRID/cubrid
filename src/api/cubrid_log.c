a8ab8816695cb8851011e226f614a645e4d9368a
joonmin83 <hornetmj@cubrid.com>
2021-04-22 16:57:54 +0900

    [CBRD-23920] declare API functions and struct and make skeleton code for these functions

--- src/api/cubrid_log.c
+++ src/api/cubrid_log.c
@@ -24,12 +24,266 @@
 
 #if defined(CS_MODE)
 
+#include <limits.h>
+
 #include "cubrid_log.h"
 
-void
-dummy_fuc (void)
+typedef enum
 {
+  CREATE = 0,
+  ALTER,
+  DROP,
+  RENAME,
+  TRUNCATE
+} DDL_TYPE;
 
+typedef enum
+{
+  INSERT = 0,
+  UPDATE,
+  DELETE
+} DML_TYPE;
+
+typedef enum
+{
+  COMMIT = 0,
+  ROLLBACK
+} DCL_TYPE;
+
+typedef enum
+{
+  TABLE = 0,
+  INDEX,
+  SERIAL,
+  VIEW,
+  FUNCTION,
+  PROCEDURE,
+  TRIGGER
+} OBJ_TYPE;
+
+typedef enum
+{
+  DATA_ITEM_TYPE_DDL = 0,
+  DATA_ITEM_TYPE_DML,
+  DATA_ITEM_TYPE_DCL,
+  DATA_ITEM_TYPE_TIMER
+} DATA_ITEM_TYPE;
+
+/*
+ * cubrid_log_set_connection_timeout () -
+ *   return:
+ *   timeout(in):
+ */
+int
+cubrid_log_set_connection_timeout (int timeout)
+{
+  if (timeout < -1 || timeout > 360)
+    {
+      return CUBRID_LOG_INVALID_CONNECTION_TIMEOUT;
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_set_extraction_timeout () -
+ *   return:
+ *   timeout(in):
+ */
+int
+cubrid_log_set_extraction_timeout (int timeout)
+{
+  if (timeout < -1 || timeout > 360)
+    {
+      return CUBRID_LOG_INVALID_EXTRACTION_TIMEOUT;
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_set_tracelog () -
+ *   return:
+ *   path(in):
+ *   level(in):
+ *   filesize(in):
+ */
+int
+cubrid_log_set_tracelog (char *path, int level, int filesize)
+{
+  if (level < 0 || level > 2)
+    {
+      return CUBRID_LOG_INVALID_LEVEL;
+    }
+
+  if (filesize < 8 || filesize > 512)
+    {
+      if (filesize == -1)
+	{
+	  filesize = 10;
+	}
+      else
+	{
+	  return CUBRID_LOG_INVALID_FILESIZE;
+	}
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_set_max_log_item () -
+ *   return:
+ *   max_log_item(in):
+ */
+int
+cubrid_log_set_max_log_item (int max_log_item)
+{
+  if (max_log_item < 1 || max_log_item > 1024)
+    {
+      return CUBRID_LOG_INVALID_MAX_LOG_ITEM;
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_set_all_in_cond () -
+ *   return:
+ *   retrieve_all(in):
+ */
+int
+cubrid_log_set_all_in_cond (int retrieve_all)
+{
+  if (retrieve_all < 0 || retrieve_all > 1)
+    {
+      return CUBRID_LOG_INVALID_RETRIEVE_ALL;
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_set_extraction_table () -
+ *   return:
+ *   classoid_arr(in):
+ *   arr_size(in):
+ */
+int
+cubrid_log_set_extraction_table (uint64_t * classoid_arr, int arr_size)
+{
+  if ((classoid_arr == NULL && arr_size != 0) || (classoid_arr != NULL && arr_size == 0))
+    {
+      return CUBRID_LOG_INVALID_CLASSOID_ARR_SIZE;
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_set_extraction_user () -
+ *   return:
+ *   user_arr(in):
+ *   arr_size(in):
+ */
+int
+cubrid_log_set_extraction_user (char **user_arr, int arr_size)
+{
+  if ((user_arr == NULL && arr_size != 0) || (user_arr != NULL && arr_size == 0))
+    {
+      return CUBRID_LOG_INVALID_USER_ARR_SIZE;
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_connect_server () -
+ *   return:
+ *   host(in):
+ *   port(in):
+ *   dbname(in):
+ */
+int
+cubrid_log_connect_server (char *host, int port, char *dbname)
+{
+  if (host == NULL)
+    {
+      return CUBRID_LOG_INVALID_HOST;
+    }
+
+  if (port > USHRT_MAX)
+    {
+      return CUBRID_LOG_INVALID_PORT;
+    }
+
+  if (dbname == NULL)
+    {
+      return CUBRID_LOG_INVALID_DBNAME;
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_find_lsa () -
+ *   return:
+ *   timestamp(in):
+ *   lsa(out):
+ */
+int
+cubrid_log_find_lsa (time_t timestamp, uint64_t * lsa)
+{
+  if (lsa == NULL)
+    {
+      return CUBRID_LOG_INVALID_OUT_PARAM;
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_extract () -
+ *   return:
+ *   lsa(in/out):
+ *   log_item_list(out):
+ *   list_size(out):
+ */
+int
+cubrid_log_extract (uint64_t * lsa, CUBRID_LOG_ITEM ** log_item_list, int *list_size)
+{
+  if (lsa == NULL || log_item_list == NULL || list_size == NULL)
+    {
+      return CUBRID_LOG_INVALID_OUT_PARAM;
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_clear_log_item () -
+ *   return:
+ *   log_item_list(in):
+ */
+int
+cubrid_log_clear_log_item (CUBRID_LOG_ITEM * log_item_list)
+{
+  if (log_item_list == NULL)
+    {
+      return CUBRID_LOG_INVALID_LOGITEM_LIST;
+    }
+
+  return CUBRID_LOG_SUCCESS;
+}
+
+/*
+ * cubrid_log_finalize () -
+ *   return:
+ */
+int
+cubrid_log_finalize (void)
+{
+  return CUBRID_LOG_SUCCESS;
 }
 
 #endif /* CS_MODE */
