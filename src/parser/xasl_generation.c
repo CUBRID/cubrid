2fc86590d3a30d34fc04f2722bbbb356426c123f
CT.Shim <ctshim@cubrid.com>
2021-04-22 01:37:25 +0000

    modify indent -2

--- src/parser/xasl_generation.c
+++ src/parser/xasl_generation.c
@@ -12782,10 +12782,10 @@ pt_to_cublink_table_spec_list (PARSER_CONTEXT * parser, PT_NODE * spec, PT_NODE 
 
   access = pt_make_dblink_access_spec (access_method, regu_attributes_rest,
 				       (char *) pCublink->__cts_url->info.value.data_value.str->bytes /*dblink_url */ ,
-				       (char *) pCublink->__cts_user->info.value.data_value.str->
-				       bytes /* dblink_user */ ,
-				       (char *) pCublink->__cts_pwd->info.value.data_value.str->
-				       bytes /* dblink_passowrd */ ,
+				       (char *) pCublink->__cts_user->info.value.data_value.
+				       str->bytes /* dblink_user */ ,
+				       (char *) pCublink->__cts_pwd->info.value.data_value.
+				       str->bytes /* dblink_passowrd */ ,
 				       (char *) pCublink->qstr->info.value.data_value.str->bytes /*dblink_sql */ );
 
   return access;
