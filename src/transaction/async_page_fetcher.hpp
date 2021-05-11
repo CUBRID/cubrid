392456f9f8077151d370ad8c7d37a7272e5716ad
Ilie Danila <ilie.danila@arnia.ro>
2021-05-11 19:56:38 +0300

    test for indent

--- src/transaction/async_page_fetcher.hpp
+++ src/transaction/async_page_fetcher.hpp
@@ -46,7 +46,6 @@ namespace cublog
       // seed the worker pool threads with a non-null transaction and a valid thread
       // identity as to properly identify these agains perf logging
       std::unique_ptr<cubthread::entry_manager> m_worker_pool_context_manager;
-      
   };
 
 }
