26bc256d68f9a510bfffba7d3d5d734bd74690ea
Remzi Ibram <remzi.ibram@gmail.com>
2021-05-27 12:18:31 +0300

    Comments and small adjustments

--- src/transaction/log_recovery_context.hpp
+++ src/transaction/log_recovery_context.hpp
@@ -25,7 +25,12 @@
 
 class log_recovery_context
 {
+    //////////////////////////////////////////////////////////////////////////
     //
+    // Recovery contextual information that is either passed around between different recovery phases or that changes
+    // the behavior of recovery.
+    //
+    //////////////////////////////////////////////////////////////////////////
 
   public:
     log_recovery_context ();
@@ -34,29 +39,32 @@ class log_recovery_context
     log_recovery_context (log_recovery_context &&) = delete;
 
     // Accessors
-    void set_end_redo_lsa (const log_lsa &end_redo_lsa);
-    void set_start_redo_lsa (const log_lsa &start_redo_lsa);
     const log_lsa &get_checkpoint_lsa () const;
     const log_lsa &get_start_redo_lsa () const;
     const log_lsa &get_end_redo_lsa () const;
+    void set_end_redo_lsa (const log_lsa &end_redo_lsa);
+    void set_start_redo_lsa (const log_lsa &start_redo_lsa);
 
     // Restore related functions
-    void init_for_restore (const time_t *stopat_p);   // Init recovery context for restore
-    bool is_restore_from_backup () const;
-    bool is_restore_incomplete () const;              // Returns true if restore is set to stop before end of log
-    void force_stop_restore_at (time_t stopat);       // Force incomplete restoration and change stop time
-    bool does_restore_stop_before_time (time_t complete_time);
-    void set_incomplete_restore ();
+    void init_for_restore (const time_t *stopat_p);               // Init recovery context for restore
+    bool is_restore_from_backup () const;                         // see m_is_restore_from_backup
+    bool is_restore_incomplete () const;                          // see m_is_restore_incomplete
+    void set_incomplete_restore ();                               // set m_is_restore_incomplete to true
+    void force_stop_restore_at (time_t stopat);                   // Force incomplete restoration and change stop time
+    bool does_restore_stop_before_time (time_t complete_time);    // Is restore stopped before time argument
 
   private:
-    time_t m_restore_stop_point = 0;	      // no stop point if the value is zero
-    bool m_is_restore_from_backup = false;
-    bool m_is_restore_incomplete = false;
+    // Restore related members
+    time_t m_restore_stop_point = 0;	      // restore stop point. no stop point if the value is zero
+    bool m_is_restore_from_backup = false;    // true if server is being restored restore from backup
+    // false if server is recovering after forced stop
+    bool m_is_restore_incomplete = false;     // true if restore is stopped before end of log
+    // false if full restore is executed
 
-    bool m_is_page_server = false;
+    bool m_is_page_server = false;            // true for page server, false for transaction server
 
-    log_lsa m_checkpoint_lsa = NULL_LSA;
-    log_lsa m_start_redo_lsa = NULL_LSA;
-    log_lsa m_end_redo_lsa = NULL_LSA;
+    log_lsa m_checkpoint_lsa = NULL_LSA;      // the initial checkpoint LSA, starting point for recovery analysis
+    log_lsa m_start_redo_lsa = NULL_LSA;      // starting point for recovery redo
+    log_lsa m_end_redo_lsa = NULL_LSA;        // end point for recovery redo
 };
 #endif // !_LOG_RECOVERY_CONTEXT_HPP_
