457f836de673dedda99de939460e4f704511381c
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2021-06-03 17:40:00 +0900

    [skip ci] fix license check

--- src/query/method_scan.cpp
+++ src/query/method_scan.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 Search Solution Corporation
+ *
  * Copyright 2016 CUBRID Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +11,7 @@
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and1
+ *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
  */
