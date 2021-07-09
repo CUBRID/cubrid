52c4a51cf80d609db54acf00731d98cc55e24ea4
adrianbrab <Adrian Brabete>
2021-07-09 11:09:43 +0300

    fix indent and argument usage

--- src/connection/server_support.c
+++ src/connection/server_support.c
@@ -2646,9 +2646,9 @@ css_process_server_server_connect (SOCKET master_fd)
     {
     case cubcomm::server_server::CONNECT_ACTIVE_TRAN_TO_PAGE_SERVER:
       // *INDENT-OFF*
-      if (!chn.send_int (static_cast < int >(cubcomm::server_server::CONNECT_ACTIVE_TRAN_TO_PAGE_SERVER)))
+      if (!chn.send_int (static_cast <int>(cubcomm::server_server::CONNECT_ACTIVE_TRAN_TO_PAGE_SERVER)))
 	{
-	  er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_NET_TRANSACTION_SERVER_CONNECTION, 1, chn.get_channel_id ());
+	  er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_NET_TRANSACTION_SERVER_CONNECTION, 0);
 	  break;
 	}
       // *INDENT-ON*
