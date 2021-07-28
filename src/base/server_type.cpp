26a97cb021d0861f16836c7a17387a1059237242
adrianbrab <Adrian Brabete>
2021-07-28 15:30:19 +0300

    changed finalize function for UNKNOWN server

--- src/base/server_type.cpp
+++ src/base/server_type.cpp
@@ -94,7 +94,7 @@ void finalize_server_type ()
     {
       ats_Gl.disconnect_page_server ();
     }
-  else
+  if (get_server_type() == SERVER_TYPE_PAGE)
     {
       assert (get_server_type () == SERVER_TYPE_PAGE);
       ps_Gl.disconnect_active_tran_server ();
