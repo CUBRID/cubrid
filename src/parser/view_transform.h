eb7eabdda54cf3093bb918c3e1b1f483db701e26
Byungwook Kim <bwkim@cubrid.com>
2021-07-30 09:51:24 +0900

    useless line

--- src/parser/view_transform.h
+++ src/parser/view_transform.h
@@ -96,5 +96,4 @@ extern PT_NODE *mq_reset_ids_in_methods (PARSER_CONTEXT * parser, PT_NODE * stat
 extern PT_NODE *mq_rewrite_aggregate_as_derived (PARSER_CONTEXT * parser, PT_NODE * agg_sel);
 
 extern PT_NODE *mq_rewrite_query_as_derived (PARSER_CONTEXT * parser, PT_NODE * query);
-
 #endif /* _VIEW_TRANSFORM_H_ */
