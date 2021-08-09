253241ef00059941f16dee5ba8af849481a5750a
Jaeeun Kim (bigs) <jekim@cubrid.com>
2021-08-09 16:20:08 +0900

    [CBRD-23954] Use BIGINT to get the result of SELECT COUNT (#2954)
    http://jira.cubrid.org/browse/CBRD-23954
    
    [regression] Change the data type for the result of select count which has been changed in #2876.

--- src/object/schema_class_truncator.cpp
+++ src/object/schema_class_truncator.cpp
@@ -529,7 +529,7 @@ namespace cubschema
 	DB_VALUE value;
 	char select_query[DB_MAX_IDENTIFIER_LENGTH + 256] = { 0 };
 	constexpr int CNT_CATCLS_OBJECTS = 5;
-	int cnt_refers = CNT_CATCLS_OBJECTS + 1;
+	DB_BIGINT cnt_refers = CNT_CATCLS_OBJECTS + 1;
 
 	const char *class_name = db_get_class_name (m_mop);
 	if (class_name == NULL)
@@ -595,7 +595,7 @@ namespace cubschema
 	    return error;
 	  }
 
-	cnt_refers = db_get_int (&value);
+	cnt_refers = db_get_bigint (&value);
 
 	db_query_end (result);
 	db_close_session (session);
