cff710f7fa00a1361f6a423fdd373db5de519882
Cristian Arghiroiu <cristi.arghiroiu@gmail.com>
2021-08-12 17:59:29 +0300

    alignas

--- src/transaction/log_tran_table.c
+++ src/transaction/log_tran_table.c
@@ -3541,13 +3541,11 @@ logtb_tran_update_unique_stats (THREAD_ENTRY * thread_p, const BTID * btid, int 
   if (write_to_log)
     {
       /* log statistics */
-      constexpr size_t data_size (OR_BTID_ALIGNED_SIZE + (3 * OR_INT_SIZE));
+      constexpr size_t DATA_SIZE (OR_BTID_ALIGNED_SIZE + (3 * OR_INT_SIZE));
 
-      char buf[data_size + MAX_ALIGNMENT];
-      char *const data = PTR_ALIGN (buf, MAX_ALIGNMENT);
+      alignas (MAX_ALIGNMENT) char data[DATA_SIZE];
       size_t data_written_size = 0;
-
-      btree_rv_data_pack_btid_and_stats (btid, -n_nulls, -n_oids, -n_keys, data, data_size, data_written_size);
+      btree_rv_data_pack_btid_and_stats (btid, -n_nulls, -n_oids, -n_keys, data, DATA_SIZE, data_written_size);
 
       log_append_undo_data2 (thread_p, RVBT_MVCC_INCREMENTS_UPD, NULL, NULL, NULL_OFFSET, data_written_size, data);
     }
@@ -4895,25 +4893,20 @@ logtb_update_global_unique_stats_by_delta (THREAD_ENTRY * thread_p, BTID * btid,
       /* although we don't change the btree header, we still need to log here the new values of statistics so that they
        * can be recovered at recover stage. For undo purposes we log the increments. */
 
-      constexpr size_t data_size (OR_BTID_ALIGNED_SIZE + (3 * OR_INT_SIZE));
+      constexpr size_t DATA_SIZE (OR_BTID_ALIGNED_SIZE + (3 * OR_INT_SIZE));
 
-      char undo_buf[data_size + BTREE_MAX_ALIGN];
-      char *const undo_data = PTR_ALIGN (undo_buf, BTREE_MAX_ALIGN);
+      alignas (MAX_ALIGNMENT) char undo_data[DATA_SIZE];
       size_t undo_data_written_size = 0;
-
-      btree_rv_data_pack_btid_and_stats (btid, null_delta, oid_delta, key_delta, undo_data, data_size,
+      btree_rv_data_pack_btid_and_stats (btid, null_delta, oid_delta, key_delta, undo_data, DATA_SIZE,
 					 undo_data_written_size);
 
-      char redo_buf[data_size + BTREE_MAX_ALIGN];
-      char *const redo_data = PTR_ALIGN (redo_buf, BTREE_MAX_ALIGN);
+      alignas (MAX_ALIGNMENT) char redo_data[DATA_SIZE];
       size_t redo_data_written_size = 0;
-
-      btree_rv_data_pack_btid_and_stats (btid, num_nulls, num_oids, num_keys, redo_data, data_size,
+      btree_rv_data_pack_btid_and_stats (btid, num_nulls, num_oids, num_keys, redo_data, DATA_SIZE,
 					 redo_data_written_size);
 
       log_append_undoredo_data2 (thread_p, RVBT_LOG_GLOBAL_UNIQUE_STATS_COMMIT, NULL, NULL, HEADER,
 				 undo_data_written_size, redo_data_written_size, undo_data, redo_data);
-      //undo_rec.length, redo_rec.length, undo_rec.data, redo_rec.data);
       LSA_COPY (&stats->last_log_lsa, &tdes->tail_lsa);
     }
   else if (!LSA_ISNULL (&log_Gl.unique_stats_table.curr_rcv_rec_lsa))
