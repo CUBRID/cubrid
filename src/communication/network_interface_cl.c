24d4e24d0c82d569290934282f9778fdeb77146f
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2021-09-27 18:26:58 +0900

    remove moved file, indent

--- src/communication/network_interface_cl.c
+++ src/communication/network_interface_cl.c
@@ -10456,10 +10456,9 @@ method_invoke_fold_constants (method_sig_list & sig_list, std::vector < DB_VALUE
   OR_ALIGNED_BUF (OR_INT_SIZE * 3) a_reply;
   char *reply = OR_ALIGNED_BUF_START (a_reply);
 
-  int req_error =
-    net_client_request_method_callback (NET_SERVER_METHOD_FOLD_CONSTANTS, eb.get_ptr (),
-					(int) packer.get_current_size (), reply,
-					OR_ALIGNED_BUF_SIZE (a_reply), &data_reply, &data_reply_size);
+  int req_error = net_client_request_method_callback (NET_SERVER_METHOD_FOLD_CONSTANTS, eb.get_ptr (),
+						      (int) packer.get_current_size (), reply,
+						      OR_ALIGNED_BUF_SIZE (a_reply), &data_reply, &data_reply_size);
   if (req_error != NO_ERROR)
     {
       return req_error;
