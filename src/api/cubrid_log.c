30f84e768317887f928fc7db37bbbfb32ec8d180
Joohok <joohok@github.com>
2021-09-30 21:57:19 +0900

    login function added

--- src/api/cubrid_log.c
+++ src/api/cubrid_log.c
@@ -34,6 +34,7 @@
 #include <assert.h>
 #include <limits.h>
 
+#include "authenticate.h"
 #include "connection_cl.h"
 #include "connection_list_cl.h"
 #include "cubrid_log.h"
@@ -41,6 +42,7 @@
 #include "network.h"
 #include "object_representation.h"
 #include "dbi.h"
+#include "dbtype_def.h"
 
 #define CUBRID_LOG_ERROR_HANDLING(e, v) \
   do\
@@ -514,6 +516,42 @@ cubrid_log_error:
   return err_code;
 }
 
+static int
+cubrid_log_db_login (char *dbname, char *id, char *password)
+{
+  MOP user;
+  int error = NO_ERROR;
+
+  error = db_restart ("cubrid_log_api", TRUE, dbname);
+  if (error != NO_ERROR)
+    {
+      return CUBRID_LOG_FAILED_LOGIN;
+    }
+
+  user = au_find_user (id);
+  if (user == NULL)
+    {
+      error = CUBRID_LOG_FAILED_LOGIN;
+      goto error;
+    }
+
+  if (!au_is_dba_group_member (user))
+    {
+      error = CUBRID_LOG_FAILED_LOGIN;
+      goto error;
+    }
+
+  db_shutdown ();
+
+  return CUBRID_LOG_SUCCESS;
+
+error:
+
+  db_shutdown ();
+
+  return error;
+}
+
 /*
  * cubrid_log_connect_server () -
  *   return:
@@ -573,7 +611,7 @@ cubrid_log_connect_server (char *host, int port, char *dbname, char *id, char *p
       CUBRID_LOG_ERROR_HANDLING (CUBRID_LOG_INVALID_PASSWORD, trace_errbuf);
     }
 
-  if (db_login (id, password) != NO_ERROR)
+  if (cubrid_log_db_login (dbname, id, password) != CUBRID_LOG_SUCCESS)
     {
       CUBRID_LOG_ERROR_HANDLING (CUBRID_LOG_FAILED_LOGIN, trace_errbuf);
     }
