b2798d0ef24abb0836a663a47cce89431af16e58
jaeeunkim <jekim@cubrid.com>
2021-10-06 13:24:25 +0900

    Remove the detailed info from the analysis start

--- src/transaction/log_recovery.c
+++ src/transaction/log_recovery.c
@@ -730,8 +730,7 @@ log_recovery (THREAD_ENTRY * thread_p, int ismedia_crash, time_t * stopat)
 
   log_Gl.rcv_phase = LOG_RECOVERY_ANALYSIS_PHASE;
 
-  er_set (ER_NOTIFICATION_SEVERITY, ARG_FILE_LINE, ER_LOG_RECOVERY_ANALYSIS_STARTED, 1,
-	  LSA_DIFF_IN_PAGE (&log_Gl.hdr.eof_lsa, &rcv_lsa));
+  er_set (ER_NOTIFICATION_SEVERITY, ARG_FILE_LINE, ER_LOG_RECOVERY_ANALYSIS_STARTED, 0);
   log_recovery_analysis (thread_p, &rcv_lsa, &start_redolsa, &end_redo_lsa, ismedia_crash, stopat, &did_incom_recovery,
 			 &num_redo_log_records);
   er_set (ER_NOTIFICATION_SEVERITY, ARG_FILE_LINE, ER_LOG_RECOVERY_PHASE_FINISHED, 1, "ANALYSIS");
