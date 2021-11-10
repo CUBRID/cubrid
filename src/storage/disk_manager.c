6c1b21d9a13c4bbfca71b59b49094ade26af5e84
Mike MyungHwan Oh <mhoh@cubrid.com>
2021-11-10 04:25:34 +0000

    - temp modify

--- src/storage/disk_manager.c
+++ src/storage/disk_manager.c
@@ -1695,22 +1695,24 @@ disk_extend (THREAD_ENTRY * thread_p, DISK_EXTEND_INFO * extend_info, DISK_RESER
   assert (disk_Cache->owner_extend == thread_get_entry_index (thread_p));
 
   /* expand */
-  if (voltype == DB_TEMPORARY_VOLTYPE)
-    {
-      nsect_extend = intention;
-    }
-  else
-    {
-      /* what is the desired remaining free after expand? */
-      target_free = MAX ((DKNSECTS) (total * 0.01), DISK_MIN_VOLUME_SECTS);
-      /* what is the desired expansion? do not expand less than intention. */
-      nsect_extend = MAX (target_free - free, 0) + intention;
-    }
+  /* what is the desired remaining free after expand? */
+  target_free = MAX ((DKNSECTS) (total * 0.01), DISK_MIN_VOLUME_SECTS);
+  /* what is the desired expansion? do not expand less than intention. */
+  nsect_extend = MAX (target_free - free, 0) + intention;
 
   if (nsect_extend <= 0)
     {
       /* no expand needed */
       return NO_ERROR;
+    }
+
+  if (voltype == DB_TEMPORARY_VOLTYPE && total + nsect_extend >= disk_Temp_max_sects)
+    {
+      /* too much temporary space */
+      assert (false);
+      er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_BO_MAXTEMP_SPACE_HAS_BEEN_EXCEEDED, 1,
+	  (long) disk_Temp_max_sects * DISK_SECTOR_NPAGES);
+      return ER_BO_MAXTEMP_SPACE_HAS_BEEN_EXCEEDED;
     }
 
   disk_log ("disk_extend", "extend %s disk by %d sectors.", disk_type_to_string (extend_info->voltype), nsect_extend);
@@ -4413,6 +4415,8 @@ disk_reserve_from_cache (THREAD_ENTRY * thread_p, DISK_RESERVE_CONTEXT * context
       return ER_FAILED;
     }
 
+  extend_info = &disk_Cache->perm_purpose_info.extend_info;
+
   disk_cache_lock_reserve_for_purpose (context->purpose);
   if (context->purpose == DB_TEMPORARY_DATA_PURPOSE)
     {
@@ -4432,8 +4436,7 @@ disk_reserve_from_cache (THREAD_ENTRY * thread_p, DISK_RESERVE_CONTEXT * context
 	}
 
       /* reserve sectors from temporary volumes */
-      extend_info = &disk_Cache->temp_purpose_info.extend_info;
-      if (extend_info->nsect_free >= context->n_cache_reserve_remaining)
+      if (extend_info->nsect_free > 0)
 	{
 	  disk_reserve_from_cache_vols (DB_TEMPORARY_VOLTYPE, context);
 	  if (context->n_cache_reserve_remaining <= 0)
@@ -4445,6 +4448,7 @@ disk_reserve_from_cache (THREAD_ENTRY * thread_p, DISK_RESERVE_CONTEXT * context
 	    }
 	}
 
+#if 0
       if (extend_info->nsect_total + context->n_cache_reserve_remaining >= disk_Temp_max_sects)
 	{
 	  /* too much temporary space */
@@ -4454,27 +4458,26 @@ disk_reserve_from_cache (THREAD_ENTRY * thread_p, DISK_RESERVE_CONTEXT * context
 	  disk_cache_unlock_reserve_for_purpose (context->purpose);
 	  return ER_BO_MAXTEMP_SPACE_HAS_BEEN_EXCEEDED;
 	}
+#endif
 
       /* fall through */
     }
   else
     {
-      extend_info = &disk_Cache->perm_purpose_info.extend_info;
-      /* fall through */
-    }
+      /* permanent volume */
+//  assert (context->n_cache_reserve_remaining > 0);
+//  assert (extend_info->owner_reserve == thread_get_entry_index (thread_p));
 
-  assert (context->n_cache_reserve_remaining > 0);
-  assert (extend_info->owner_reserve == thread_get_entry_index (thread_p));
-
-  if (extend_info->nsect_free > context->n_cache_reserve_remaining)
-    {
-      disk_reserve_from_cache_vols (extend_info->voltype, context);
-      if (context->n_cache_reserve_remaining <= 0)
+      if (extend_info->nsect_free > context->n_cache_reserve_remaining)
 	{
-	  /* found enough sectors */
-	  assert (context->n_cache_reserve_remaining == 0);
-	  disk_cache_unlock_reserve (extend_info);
-	  return NO_ERROR;
+	  disk_reserve_from_cache_vols (extend_info->voltype, context);
+	  if (context->n_cache_reserve_remaining <= 0)
+	    {
+	      /* found enough sectors */
+	      assert (context->n_cache_reserve_remaining == 0);
+	      disk_cache_unlock_reserve (extend_info);
+	      return NO_ERROR;
+	    }
 	}
     }
 
@@ -4960,9 +4963,9 @@ disk_manager_init (THREAD_ENTRY * thread_p, bool load_from_disk)
     }
   else
     {
-      disk_Temp_max_sects = MAX (DISK_MIN_VOLUME_SECTS * DISK_SECTOR_NPAGES, disk_Temp_max_sects);
-      disk_Temp_max_sects = ceil (disk_Temp_max_sects / (double) DISK_SECTOR_NPAGES);
-      disk_Temp_max_sects = ceil (disk_Temp_max_sects / (double) DISK_MIN_VOLUME_SECTS) * DISK_MIN_VOLUME_SECTS;
+      disk_Temp_max_sects = MAX ((DISK_MIN_VOLUME_SECTS + 1) * DISK_SECTOR_NPAGES, disk_Temp_max_sects);
+      disk_Temp_max_sects = CEIL_PTVDIV(disk_Temp_max_sects, DISK_SECTOR_NPAGES);
+      disk_Temp_max_sects = DISK_SECTS_ROUND_UP(disk_Temp_max_sects);
     }
 
   disk_Logging = prm_get_bool_value (PRM_ID_DISK_LOGGING);
