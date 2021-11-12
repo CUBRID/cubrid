191cb50c729dd9427524d2bb49ec05f3468a2b4e
Remzi Ibram <remzi.ibram@gmail.com>
2021-11-12 15:24:10 +0200

    Remove unused option; add error for unacceptable option

--- src/executables/util_sa.c
+++ src/executables/util_sa.c
@@ -341,19 +341,6 @@ createdb_with_remote_storage (UTIL_FUNCTION_ARG * arg)
       return usage_error ();
     }
 
-  const char *output_file_name = utility_get_option_string_value (arg_map, CREATE_OUTPUT_FILE_S, 0);
-  FILE *output_file = stdout;
-  if (output_file_name != nullptr && output_file_name[0] != '\0')
-    {
-      output_file = fopen (output_file_name, "w");
-    }
-  if (output_file == NULL)
-    {
-      PRINT_AND_LOG_ERR_MSG (msgcat_message (MSGCAT_CATALOG_UTILS, MSGCAT_UTIL_SET_CREATEDB, CREATEDB_MSG_BAD_OUTPUT),
-			     output_file_name);
-      return EXIT_FAILURE;
-    }
-
   const char *log_path = utility_get_option_string_value (arg_map, CREATE_LOG_PATH_S, 0);
   const char *lob_path = utility_get_option_string_value (arg_map, CREATE_LOB_PATH_S, 0);
   const char *host_name = utility_get_option_string_value (arg_map, CREATE_SERVER_NAME_S, 0);
@@ -367,6 +354,15 @@ createdb_with_remote_storage (UTIL_FUNCTION_ARG * arg)
   db_path_info.lob_path = lob_path;
   db_path_info.db_host = host_name;
   db_path_info.db_comments = comment;
+
+  if (utility_get_option_bool_value (arg_map, CREATE_REPLACE_S))
+    {
+      // Cannot overwrite database
+      PRINT_AND_LOG_ERR_MSG (msgcat_message
+			     (MSGCAT_CATALOG_UTILS, MSGCAT_UTIL_SET_CREATEDB, CREATEDB_INCOMPATIBLE_OPTIONS),
+			     CREATE_REPLACE_S, CREATE_REMOTE_STORAGE_S);
+      return EXIT_FAILURE;
+    }
 
   int error_code = boot_initialize_remote_storage_client (database_name, db_path_info);
   if (error_code != NO_ERROR)
