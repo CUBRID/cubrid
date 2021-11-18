976d325d709a53dc9d02c6c501bfdb02d6b5f2a3
Remzi Ibram <remzi.ibram@gmail.com>
2021-11-18 10:36:16 +0200

    Add missing argument

--- src/transaction/log_recovery_analysis.cpp
+++ src/transaction/log_recovery_analysis.cpp
@@ -1671,7 +1671,7 @@ log_rv_analysis_record_on_tran_server (THREAD_ENTRY *thread_p, LOG_RECTYPE log_t
     case LOG_UNDO_DATA:
     case LOG_REDO_DATA:
     case LOG_DBEXTERN_REDO_DATA:
-      (void) log_rv_analysis_undo_redo (thread_p, tran_id, log_lsa);
+      (void) log_rv_analysis_undo_redo (thread_p, tran_id, log_lsa, is_mvcc);
       break;
 
     case LOG_DUMMY_HEAD_POSTPONE:
