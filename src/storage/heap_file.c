4e1c1b6a4960c492852dbc9c95fa534198a5564b
adrianbrab <Adrian Brabete>
2021-11-19 13:42:53 +0200

    added start/end logs

--- src/storage/heap_file.c
+++ src/storage/heap_file.c
@@ -21926,6 +21926,7 @@ heap_update_bigone (THREAD_ENTRY * thread_p, HEAP_OPERATION_CONTEXT * context, b
 
   HEAP_PERF_TRACK_PREPARE (thread_p, context);
 
+  log_append_empty_record (thread_p, LOG_START_ATOMIC_REPL, NULL);
   if (is_mvcc_op)
     {
       /* log old overflow record and set prev version lsa */
@@ -22098,6 +22099,7 @@ heap_update_bigone (THREAD_ENTRY * thread_p, HEAP_OPERATION_CONTEXT * context, b
   /* Fall through to exit. */
 
 exit:
+  log_append_empty_record (thread_p, LOG_END_ATOMIC_REPL, NULL);
   return error_code;
 }
 
