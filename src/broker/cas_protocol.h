331126120600fc70dff65c45d0a431f2ade0518f
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2021-12-10 17:57:29 +0900

    set CURRENT_PROTOCOL to V11

--- src/broker/cas_protocol.h
+++ src/broker/cas_protocol.h
@@ -226,7 +226,7 @@ extern "C"
     PROTOCOL_V9 = 9,		/* cas health check: get function status */
     PROTOCOL_V10 = 10,		/* Secure Broker/CAS using SSL */
     PROTOCOL_V11 = 11,		/* make out resultset */
-    CURRENT_PROTOCOL = PROTOCOL_V10
+    CURRENT_PROTOCOL = PROTOCOL_V11
   };
   typedef enum t_cas_protocol T_CAS_PROTOCOL;
 
