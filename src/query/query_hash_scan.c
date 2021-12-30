f8f90801acd1acd3e01f1543b66be9351d9b0d34
SE park <shpark@cubrid.com>
2021-12-30 09:44:00 +0900

    Update src/query/query_hash_scan.c

--- src/query/query_hash_scan.c
+++ src/query/query_hash_scan.c
@@ -1924,7 +1924,7 @@ fhs_binary_search_bucket (THREAD_ENTRY * thread_p, PAGE_PTR bucket_page_p, PGSLO
 	}
       else
 	{
-	  middle += (flag > 0) ? flag - 1 : 0; /* go to last slot using flag */
+	  middle += (flag > 0) ? flag - 1 : 0;	/* go to last slot using flag */
 	  low = middle + 1;
 	}
     }
