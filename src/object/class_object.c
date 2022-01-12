e4a082e8bb2921384292ff8f40364806ac7b3157
Byungwook Kim <bwkim@cubrid.com>
2022-01-12 17:05:30 +0900

    dummy

--- src/object/class_object.c
+++ src/object/class_object.c
@@ -4110,7 +4110,12 @@ classobj_find_constraint_by_attrs (SM_CLASS_CONSTRAINT * cons_list, DB_CONSTRAIN
 			  continue;
 			}
 
-		      if (!filter_predicate->pred_string || !cons->filter_predicate->pred_string)
+		      if (!cons->filter_predicate->pred_string)
+			{
+			  continue;
+			}
+
+		      if (!filter_predicate->pred_string)
 			{
 			  continue;
 			}
