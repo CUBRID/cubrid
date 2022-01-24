5961658c8ad945c175740e9eacaebfc05a17828b
keonwoo98 <keonwoo98@naver.com>
2022-01-24 14:49:53 +0900

    [CBRD-24168] Fixed the problem that the critical section is not returned when an error occurs after entering it in function logpb_copy_page.

--- src/transaction/log_page_buffer.c
+++ src/transaction/log_page_buffer.c
@@ -1913,12 +1913,8 @@ logpb_copy_page (THREAD_ENTRY * thread_p, LOG_PAGEID pageid, LOG_CS_ACCESS_MODE 
   else
     {
       er_set (ER_FATAL_ERROR_SEVERITY, ARG_FILE_LINE, ER_LOG_PAGE_CORRUPTED, 1, pageid);
-      if (log_csect_entered)
-        {
-          rv = ER_LOG_PAGE_CORRUPTED;
-          goto exit;
-        }
-      return ER_LOG_PAGE_CORRUPTED;
+      rv = ER_LOG_PAGE_CORRUPTED;
+      goto exit;
     }
 
   if (log_bufptr->pageid == pageid)
