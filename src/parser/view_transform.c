f7787295ca51aacf1ca5476d2c332aeb53e80099
shparkcubrid <shpark@cubrid.com>
2022-02-10 18:20:03 +0900

    remove save_data_type

--- src/parser/view_transform.c
+++ src/parser/view_transform.c
@@ -1892,7 +1892,7 @@ mq_update_order_by (PARSER_CONTEXT * parser, PT_NODE * statement, PT_NODE * quer
   PT_NODE *order, *val;
   PT_NODE *attributes, *attr, *prev_order;
   PT_NODE *node, *result, *order_by;
-  PT_NODE *save_data_type, *free_node = NULL, *save_next;
+  PT_NODE *free_node = NULL, *save_next;
   int attr_count;
   int i;
   UINTPTR spec_id;
@@ -1950,9 +1950,6 @@ mq_update_order_by (PARSER_CONTEXT * parser, PT_NODE * statement, PT_NODE * quer
 	      attr = attr->next;
 	    }
 
-	  save_data_type = attr->data_type;
-	  attr->data_type = NULL;
-
 	  for (i = 1, node = statement->info.query.q.select.list; node != NULL; node = node->next)
 	    {
 	      i++;
@@ -1969,9 +1966,6 @@ mq_update_order_by (PARSER_CONTEXT * parser, PT_NODE * statement, PT_NODE * quer
 	    }
 
 	  assert (attr != NULL && attr->node_type == PT_NAME);
-	  save_data_type = attr->data_type;
-	  attr->data_type = NULL;
-
 	  for (node = statement->info.query.q.select.list, i = 1; node != NULL; node = node->next, i++)
 	    {
 	      /* 3 check whether attr is found in output list */
@@ -2032,8 +2026,6 @@ mq_update_order_by (PARSER_CONTEXT * parser, PT_NODE * statement, PT_NODE * quer
 	{
 	  prev_order = order;
 	}
-
-      attr->data_type = save_data_type;
     }
 
   statement->info.query.order_by = parser_append_node (order_by, statement->info.query.order_by);
@@ -2315,7 +2307,8 @@ mq_substitute_subquery_in_statement (PARSER_CONTEXT * parser, PT_NODE * statemen
 	  bool is_removable_select_list = PT_IS_SELECT (tmp_result) && PT_IS_SELECT (query_spec);
 
 	  /* rewrite vclass spec */
-	  class_spec = mq_rewrite_vclass_spec_as_derived (parser, tmp_result, class_spec, query_spec, is_removable_select_list);
+	  class_spec =
+	    mq_rewrite_vclass_spec_as_derived (parser, tmp_result, class_spec, query_spec, is_removable_select_list);
 
 	  /* get derived expending spec node */
 	  if (!class_spec || !(derived_table = class_spec->info.spec.derived_table)
