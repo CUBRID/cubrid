65a9fe645d9d319f9ac2316bcad8f55317136217
adrianbrab <77616614+adrianbrab@users.noreply.github.com>
2022-02-23 12:15:09 +0200

    [LETS-397] During ATS recovery PS must serve heap pages with specific fetch mode (#3372)
    Changed ATS inside pgbuf_request_data_page_from_page_server to also send the desired fetch mode to the PS when requesting a data page.
    
    The fetch mode that is send depends on the stage of recovery and avoids problems with de-allocated pages.

--- src/storage/page_buffer.c
+++ src/storage/page_buffer.c
@@ -8186,6 +8186,7 @@ pgbuf_read_page_from_file_or_page_server (THREAD_ENTRY * thread_p, const VPID * 
 	  std::unique_ptr<char []> buffer_uptr = std::make_unique<char []> (io_page_size);
 	  FILEIO_PAGE *second_io_page = reinterpret_cast<FILEIO_PAGE *> (buffer_uptr.get ());
 	  // *INDENT-ON*
+	  // The page returned can be null during recovery in the case that the page was already deallocated on PS
 	  error_code = pgbuf_request_data_page_from_page_server (vpid, target_repl_lsa, second_io_page);
 	  if (error_code != NO_ERROR)
 	    {
@@ -8326,12 +8327,23 @@ pgbuf_request_data_page_from_page_server (const VPID * vpid, log_lsa target_repl
 
   size += cublog::lsa_utils::get_packed_size (pac, size);
   size += vpid_utils::get_packed_size (pac, size);
+  size += pac.get_packed_int_size (size);
   std::unique_ptr<char[]> buffer (new char[size]);
 
   pac.set_buffer (buffer.get (), size);
   vpid_utils::pack (pac, *vpid);
   cublog::lsa_utils::pack (pac, target_repl_lsa);
 
+  PAGE_FETCH_MODE fetch_mode;
+  if (log_is_in_crash_recovery_and_not_yet_completes_redo ())
+    {
+      fetch_mode = RECOVERY_PAGE;
+    }
+    else
+    {
+      fetch_mode = OLD_PAGE;
+    }
+  pac.pack_int (fetch_mode);
   std::string request_message (buffer.get (), size);
 
   if (prm_get_bool_value (PRM_ID_ER_LOG_READ_DATA_PAGE))
@@ -8391,12 +8403,14 @@ pgbuf_respond_data_fetch_page_request (THREAD_ENTRY &thread_r, std::string &payl
 
   // Unpack the message data
   cubpacking::unpacker message_upk (payload_in_out.c_str (), payload_in_out.size ());
-
   VPID vpid;
   vpid_utils::unpack (message_upk, vpid);
 
   LOG_LSA target_repl_lsa;
   cublog::lsa_utils::unpack (message_upk, target_repl_lsa);
+  int packed_fetch_mode;
+  message_upk.unpack_int (packed_fetch_mode);
+  const PAGE_FETCH_MODE fetch_mode = static_cast<PAGE_FETCH_MODE> (packed_fetch_mode);
 
   // Fetch data page. But first make sure that replication hits its target LSA
   if (!target_repl_lsa.is_null ())
@@ -8408,8 +8422,21 @@ pgbuf_respond_data_fetch_page_request (THREAD_ENTRY &thread_r, std::string &payl
     }
 
   int error = NO_ERROR;
-  PAGE_PTR page_ptr = pgbuf_fix (&thread_r, &vpid, OLD_PAGE, PGBUF_LATCH_READ, PGBUF_UNCONDITIONAL_LATCH);
-  if (page_ptr == nullptr)
+  PAGE_PTR page_ptr = pgbuf_fix (&thread_r, &vpid, fetch_mode, PGBUF_LATCH_READ, PGBUF_UNCONDITIONAL_LATCH);
+  if (page_ptr == nullptr && fetch_mode == RECOVERY_PAGE)
+    {
+      ASSERT_NO_ERROR ();
+      //The found page was deallocated already
+      error = ER_PB_BAD_PAGEID;
+      payload_in_out = { reinterpret_cast<const char *> (&error), sizeof (error) };
+      if (prm_get_bool_value (PRM_ID_ER_LOG_READ_DATA_PAGE))
+        {
+          _er_log_debug (ARG_FILE_LINE,
+                         "[READ DATA] Read on deallocated page with VPID = %d|%d, target repl LSA = %lld|%d\n",
+                         error, VPID_AS_ARGS (&vpid), LSA_AS_ARGS (&target_repl_lsa));
+        }
+    }
+  else if (page_ptr == nullptr)
     {
       ASSERT_ERROR_AND_SET (error);
       // respond with the error
