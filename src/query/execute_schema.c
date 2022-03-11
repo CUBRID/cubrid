11aa9ed69eb32c4723450801d666ce58cc0aeccd
Byungwook Kim <bwkim@cubrid.com>
2022-03-11 12:43:46 +0900

    [skip ci] for merge

--- src/query/execute_schema.c
+++ src/query/execute_schema.c
@@ -10655,7 +10655,7 @@ do_change_att_schema_only (PARSER_CONTEXT * parser, DB_CTMPL * ctemplate, PT_NOD
 
   if (is_att_prop_set (attr_chg_prop->p[P_COMMENT], ATT_CHG_PROPERTY_DIFF))
     {
-      /* free before assign new */
+      /* free old before assign new */
       if (found_att->comment)
 	{
 	  ws_free_string (found_att->comment);
