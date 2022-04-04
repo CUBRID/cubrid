86a78c581ce3c0fc16642f94c4bacde785801663
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2022-04-04 16:30:52 +0900

    remove temporary added demo programs for javasp

--- demo/make_cubrid_demo.sh
+++ demo/make_cubrid_demo.sh
@@ -23,12 +23,4 @@ fi
 cubrid createdb --db-volume-size=100M --log-volume-size=100M demodb en_US.utf8  > /dev/null 2>&1
 cubrid loaddb -u dba -s $CUBRID/demo/demodb_schema -d $CUBRID/demo/demodb_objects demodb > /dev/null 2>&1
 
-"$JAVA_HOME/bin/javac" -cp $CUBRID/jdbc/cubrid_jdbc.jar:$CUBRID/java/jspserver.jar $CUBRID/demo/test/*.java 
-mkdir -p $CUBRID/demo/java/test
-
-for clz in $(ls -d $CUBRID/demo/test/*.class); do
-    cp $clz $CUBRID/demo/java/test
-	# loadjava demodb $clz > /dev/null 2>&1
-done
-
 cubrid loaddb -u dba -s $CUBRID/demo/test/testsp_schema demodb > /dev/null 2>&1
