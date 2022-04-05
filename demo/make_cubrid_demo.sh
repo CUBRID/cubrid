080ef73abba118d12f2d39628ac99607bd4c5da1
youngjinj <youngjinj@cubrid.com>
2022-04-05 10:46:46 +0900

    Add --no-user-specified-name option to script that automatically creates demodb.

--- demo/make_cubrid_demo.sh
+++ demo/make_cubrid_demo.sh
@@ -5,4 +5,4 @@ if [ -z "$CUBRID" ]; then
 fi
 
 cubrid createdb --db-volume-size=100M --log-volume-size=100M demodb en_US.utf8  > /dev/null 2>&1
-cubrid loaddb -u dba -s $CUBRID/demo/demodb_schema -d $CUBRID/demo/demodb_objects demodb > /dev/null 2>&1
+cubrid loaddb -u dba -s $CUBRID/demo/demodb_schema -d $CUBRID/demo/demodb_objects --no-user-specified-name demodb > /dev/null 2>&1
