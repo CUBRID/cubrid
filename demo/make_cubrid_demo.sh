2f953be8be7ec1711f389111ba991c60bc7d5661
Joo Youngjin <68979537+youngjinj@users.noreply.github.com>
2022-04-05 19:31:35 +0900

    [CBRD-24266] Add --no-user-specified-name option to script that automatically creates demodb. (#3477)
    http://jira.cubrid.org/browse/CBRD-24266
    
    Add --no-user-specified-name option to script that automatically creates demodb.

--- demo/make_cubrid_demo.sh
+++ demo/make_cubrid_demo.sh
@@ -1,3 +1,20 @@
+# 
+# Copyright 2008 Search Solution Corporation
+# Copyright 2016 CUBRID Corporation
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
 #!/bin/sh
 
 if [ -z "$CUBRID" ]; then
@@ -5,4 +22,4 @@ if [ -z "$CUBRID" ]; then
 fi
 
 cubrid createdb --db-volume-size=100M --log-volume-size=100M demodb en_US.utf8  > /dev/null 2>&1
-cubrid loaddb -u dba -s $CUBRID/demo/demodb_schema -d $CUBRID/demo/demodb_objects demodb > /dev/null 2>&1
+cubrid loaddb -u dba -s $CUBRID/demo/demodb_schema -d $CUBRID/demo/demodb_objects --no-user-specified-name demodb > /dev/null 2>&1
