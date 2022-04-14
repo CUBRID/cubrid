48ebe3d9040b83d8c08d5b2d8926a79b901e2712
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2022-04-14 16:43:33 +0900

    add comment, block calling method when constant folding

--- src/parser/parse_evaluate.c
+++ src/parser/parse_evaluate.c
@@ -1355,7 +1355,17 @@ pt_evaluate_tree_internal (PARSER_CONTEXT * parser, PT_NODE * tree, DB_VALUE * d
       break;
 
     case PT_METHOD_CALL:
-      error = do_call_method (parser, tree);
+      if (db_query_get_plan_dump_file () != NULL)
+	{
+	  // It is for the get_query_info.
+	  // Do not call method by constant folding
+	  db_make_null (db_values);
+	  error = NO_ERROR;
+	}
+      else
+	{
+	  error = do_call_method (parser, tree);
+	}
       if (error >= NO_ERROR)
 	{
 	  if ((val = (DB_VALUE *) (tree->etc)) != NULL)
