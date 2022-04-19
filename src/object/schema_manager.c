52c64654d116cb7bfc7a39b2fc440e933f1b3900
Joo Youngjin <68979537+youngjinj@users.noreply.github.com>
2022-04-19 16:10:19 +0900

    [CBRD-24286] [Regression] [isolation] It is not possible to create a db with a name that starts with the system class	. (#3510)
    Fixed an issue where non-DBA users could not use names that start with system class names.

--- src/object/schema_manager.c
+++ src/object/schema_manager.c
@@ -131,9 +131,89 @@ typedef struct schema_def
 
 static SCHEMA_DEF Current_Schema = { {'\0'}, NULL, NULL, NULL, NULL };
 
+typedef struct system_class_def
+{
+  const char *name;
+  int len;
+} SYSTEM_CLASS_DEF;
 
+// *INDENT-OFF*
+static SYSTEM_CLASS_DEF system_classes[] = {
+  {ROOTCLASS_NAME, strlen (ROOTCLASS_NAME)},			// "Rootclass"
+  {CT_DUAL_NAME, strlen (CT_DUAL_NAME)},			// "dual"
 
+  /*
+   * authorization classes
+   *
+   * AU_ROOT_CLASS_NAME     = CT_ROOT_NAME
+   * AU_OLD_ROOT_CLASS_NAME = CT_AUTHORIZATIONS_NAME
+   * AU_USER_CLASS_NAME     = CT_USER_NAME
+   * AU_PASSWORD_CLASS_NAME = CT_PASSWORD_NAME
+   * AU_AUTH_CLASS_NAME     = CT_AUTHORIZATION_NAME
+   * AU_GRANT_CLASS_NAME
+   */
+  {AU_ROOT_CLASS_NAME, strlen (AU_ROOT_CLASS_NAME)},		// "db_root"
+  {AU_USER_CLASS_NAME, strlen (AU_USER_CLASS_NAME)},		// "db_user"
+  {AU_PASSWORD_CLASS_NAME, strlen (AU_PASSWORD_CLASS_NAME)},	// "db_password"
+  {AU_AUTH_CLASS_NAME, strlen (AU_AUTH_CLASS_NAME)},		// "db_authorization"
+  {AU_OLD_ROOT_CLASS_NAME, strlen (AU_OLD_ROOT_CLASS_NAME)},	// "db_authorizations"
 
+  /* currently, not implemented */
+  {AU_GRANT_CLASS_NAME, strlen (AU_GRANT_CLASS_NAME)},		// "db_grant"
+
+  /* 
+   * catalog classes
+   */
+  {CT_CLASS_NAME, strlen (CT_CLASS_NAME)},			// "_db_class"
+  {CT_ATTRIBUTE_NAME, strlen (CT_ATTRIBUTE_NAME)}, 		// "_db_attribute"
+  {CT_DOMAIN_NAME, strlen (CT_DOMAIN_NAME)},			// "_db_domain"
+  {CT_METHOD_NAME, strlen (CT_METHOD_NAME)},			// "_db_method"
+  {CT_METHSIG_NAME, strlen (CT_METHSIG_NAME)},			// "_db_meth_sig"
+  {CT_METHARG_NAME, strlen (CT_METHARG_NAME)},			// "_db_meth_arg"
+  {CT_METHFILE_NAME, strlen (CT_METHFILE_NAME)},		// "_db_meth_file"
+  {CT_QUERYSPEC_NAME, strlen (CT_QUERYSPEC_NAME)},		// "_db_query_spec"
+  {CT_INDEX_NAME, strlen (CT_INDEX_NAME)},			// "_db_index"
+  {CT_INDEXKEY_NAME, strlen (CT_INDEXKEY_NAME)},		// "_db_index_key"
+  {CT_DATATYPE_NAME, strlen (CT_DATATYPE_NAME)},		// "_db_data_type"
+  {CT_CLASSAUTH_NAME, strlen (CT_CLASSAUTH_NAME)},		// "_db_auth"
+  {CT_PARTITION_NAME, strlen (CT_PARTITION_NAME)},		// "_db_partition"
+  {CT_STORED_PROC_NAME, strlen (CT_STORED_PROC_NAME)},		// "_db_stored_procedure"
+  {CT_STORED_PROC_ARGS_NAME, strlen (CT_STORED_PROC_ARGS_NAME)},	// "_db_stored_procedure_args"
+  {CT_SERIAL_NAME, strlen (CT_SERIAL_NAME)},			// "db_serial"
+  {CT_HA_APPLY_INFO_NAME, strlen (CT_HA_APPLY_INFO_NAME)},	// "db_ha_apply_info"
+  {CT_COLLATION_NAME, strlen (CT_COLLATION_NAME)},		// "_db_collation"
+  {CT_CHARSET_NAME, strlen (CT_CHARSET_NAME)},			// "_db_charset"
+  {CT_DB_SERVER_NAME, strlen (CT_DB_SERVER_NAME)},		// "_db_server"
+
+  {CT_TRIGGER_NAME, strlen (CT_TRIGGER_NAME)},			// "db_trigger"
+
+  /* currently, not implemented */
+  {CT_RESOLUTION_NAME, strlen (CT_RESOLUTION_NAME)},		// "_db_resolution"
+
+  /*
+   * catalog vclasses
+   */
+  {CTV_CLASS_NAME, strlen (CTV_CLASS_NAME)},			// "db_class"
+  {CTV_SUPER_CLASS_NAME, strlen (CTV_SUPER_CLASS_NAME)},	// "db_direct_super_class"
+  {CTV_VCLASS_NAME, strlen (CTV_VCLASS_NAME)},			// "db_vclass"
+  {CTV_ATTRIBUTE_NAME, strlen (CTV_ATTRIBUTE_NAME)},		// "db_attribute"
+  {CTV_ATTR_SD_NAME, strlen (CTV_ATTR_SD_NAME)},		// "db_attr_setdomain_elm"
+  {CTV_METHOD_NAME, strlen (CTV_METHOD_NAME)},			// "db_method"
+  {CTV_METHARG_NAME, strlen (CTV_METHARG_NAME)},		// "db_meth_arg"
+  {CTV_METHARG_SD_NAME, strlen (CTV_METHARG_SD_NAME)},		// "db_meth_arg_setdomain_elm"
+  {CTV_METHFILE_NAME, strlen (CTV_METHFILE_NAME)},		// "db_meth_file"
+  {CTV_INDEX_NAME, strlen (CTV_INDEX_NAME)},			// "db_index"
+  {CTV_INDEXKEY_NAME, strlen (CTV_INDEXKEY_NAME)},		// "db_index_key"
+  {CTV_AUTH_NAME, strlen (CTV_AUTH_NAME)},			// "db_auth"
+  {CTV_TRIGGER_NAME, strlen (CTV_TRIGGER_NAME)},		// "db_trig"
+  {CTV_PARTITION_NAME, strlen (CTV_PARTITION_NAME)},		// "db_partition"
+  {CTV_STORED_PROC_NAME, strlen (CTV_STORED_PROC_NAME)},	// "db_stored_procedure"
+  {CTV_STORED_PROC_ARGS_NAME, strlen (CTV_STORED_PROC_ARGS_NAME)},	// "db_stored_procedure_args"
+  {CTV_DB_COLLATION_NAME, strlen (CTV_DB_COLLATION_NAME)},	// "db_collation"
+  {CTV_DB_CHARSET_NAME, strlen (CTV_DB_CHARSET_NAME)},		// "db_charset"
+  {CTV_DB_SERVER_NAME, strlen (CTV_DB_SERVER_NAME)}		// "db_server"
+};
+// *INDENT-ON*
 
 #define WC_PERIOD L'.'
 
@@ -3197,6 +3277,20 @@ sm_is_system_class (MOP op)
   return sm_get_class_flag (op, SM_CLASSFLAG_SYSTEM);
 }
 
+static int
+system_class_def_compare (const void *a, const void *b)
+{
+  const SYSTEM_CLASS_DEF *sa = STATIC_CAST (const SYSTEM_CLASS_DEF *, a);
+  const SYSTEM_CLASS_DEF *sb = STATIC_CAST (const SYSTEM_CLASS_DEF *, b);
+
+  if (sa->len != sb->len)
+    {
+      return sa->len - sb->len;
+    }
+
+  return strcmp (sa->name, sb->name);
+}
+
 /*
  * sm_check_system_class_by_name () - Checks whether the class name is
  *    the same as the system class name.
@@ -3206,95 +3300,24 @@ sm_is_system_class (MOP op)
 bool
 sm_check_system_class_by_name (const char *name)
 {
-  // *INDENT-OFF*
-  static
-  struct system_class_list
-  {
-    const char *name;
-    int len;
-  }
-  system_classes[] =
-  {
-    /* 
-     * authorization classes
-     *
-     * AU_ROOT_CLASS_NAME     = CT_ROOT_NAME
-     * AU_OLD_ROOT_CLASS_NAME = CT_AUTHORIZATIONS_NAME
-     * AU_USER_CLASS_NAME     = CT_USER_NAME
-     * AU_PASSWORD_CLASS_NAME = CT_PASSWORD_NAME
-     * AU_AUTH_CLASS_NAME     = CT_AUTHORIZATION_NAME
-     * AU_GRANT_CLASS_NAME
-     */
-    {AU_ROOT_CLASS_NAME, strlen (AU_ROOT_CLASS_NAME)},		// "db_root"
-    {AU_USER_CLASS_NAME, strlen (AU_USER_CLASS_NAME)},		// "db_user"
-    {AU_PASSWORD_CLASS_NAME, strlen (AU_PASSWORD_CLASS_NAME)},	// "db_password"
-    {AU_AUTH_CLASS_NAME, strlen (AU_AUTH_CLASS_NAME)},		// "db_authorization"
-    {AU_OLD_ROOT_CLASS_NAME, strlen (AU_OLD_ROOT_CLASS_NAME)},	// "db_authorizations"
+  static int was_initialized = FALSE;
+  static int count = sizeof (system_classes) / sizeof (system_classes[0]);
 
-    /* currently, not implemented */
-    {AU_GRANT_CLASS_NAME, strlen (AU_GRANT_CLASS_NAME)},	// "db_grant"
-
-    /* 
-     * catalog classes
-     */
-    {CT_CLASS_NAME, strlen (CT_CLASS_NAME)},			// "_db_class"
-    {CT_ATTRIBUTE_NAME, strlen (CT_ATTRIBUTE_NAME)}, 		// "_db_attribute"
-    {CT_DOMAIN_NAME, strlen (CT_DOMAIN_NAME)},			// "_db_domain"
-    {CT_METHOD_NAME, strlen (CT_METHOD_NAME)},			// "_db_method"
-    {CT_METHSIG_NAME, strlen (CT_METHSIG_NAME)},		// "_db_meth_sig"
-    {CT_METHARG_NAME, strlen (CT_METHARG_NAME)},		// "_db_meth_arg"
-    {CT_METHFILE_NAME, strlen (CT_METHFILE_NAME)},		// "_db_meth_file"
-    {CT_QUERYSPEC_NAME, strlen (CT_QUERYSPEC_NAME)},		// "_db_query_spec"
-    {CT_INDEX_NAME, strlen (CT_INDEX_NAME)},			// "_db_index"
-    {CT_INDEXKEY_NAME, strlen (CT_INDEXKEY_NAME)},		// "_db_index_key"
-    {CT_DATATYPE_NAME, strlen (CT_DATATYPE_NAME)},		// "_db_data_type"
-    {CT_CLASSAUTH_NAME, strlen (CT_CLASSAUTH_NAME)},		// "_db_auth"
-    {CT_PARTITION_NAME, strlen (CT_PARTITION_NAME)},		// "_db_partition"
-    {CT_STORED_PROC_NAME, strlen (CT_STORED_PROC_NAME)},	// "_db_stored_procedure"
-    {CT_STORED_PROC_ARGS_NAME, strlen (CT_STORED_PROC_ARGS_NAME)},	// "_db_stored_procedure_args"
-    {CT_SERIAL_NAME, strlen (CT_SERIAL_NAME)},			// "db_serial"
-    {CT_HA_APPLY_INFO_NAME, strlen (CT_HA_APPLY_INFO_NAME)},	// "db_ha_apply_info"
-    {CT_COLLATION_NAME, strlen (CT_COLLATION_NAME)},		// "_db_collation"
-    {CT_CHARSET_NAME, strlen (CT_CHARSET_NAME)},		// "_db_charset"
-    {CT_DB_SERVER_NAME, strlen (CT_DB_SERVER_NAME)},		// "_db_server"
-
-    {CT_TRIGGER_NAME, strlen (CT_TRIGGER_NAME)},		// "db_trigger"
-
-    /* currently, not implemented */
-    {CT_RESOLUTION_NAME, strlen (CT_RESOLUTION_NAME)},		// "_db_resolution"
-
-    /*
-     * catalog vclasses
-     */
-    {CTV_CLASS_NAME, strlen (CTV_CLASS_NAME)},			// "db_class"
-    {CTV_SUPER_CLASS_NAME, strlen (CTV_SUPER_CLASS_NAME)},	// "db_direct_super_class"
-    {CTV_VCLASS_NAME, strlen (CTV_VCLASS_NAME)},		// "db_vclass"
-    {CTV_ATTRIBUTE_NAME, strlen (CTV_ATTRIBUTE_NAME)},		// "db_attribute"
-    {CTV_ATTR_SD_NAME, strlen (CTV_ATTR_SD_NAME)},		// "db_attr_setdomain_elm"
-    {CTV_METHOD_NAME, strlen (CTV_METHOD_NAME)},		// "db_method"
-    {CTV_METHARG_NAME, strlen (CTV_METHARG_NAME)},		// "db_meth_arg"
-    {CTV_METHARG_SD_NAME, strlen (CTV_METHARG_SD_NAME)},	// "db_meth_arg_setdomain_elm"
-    {CTV_METHFILE_NAME, strlen (CTV_METHFILE_NAME)},		// "db_meth_file"
-    {CTV_INDEX_NAME, strlen (CTV_INDEX_NAME)},			// "db_index"
-    {CTV_INDEXKEY_NAME, strlen (CTV_INDEXKEY_NAME)},		// "db_index_key"
-    {CTV_AUTH_NAME, strlen (CTV_AUTH_NAME)},			// "db_auth"
-    {CTV_TRIGGER_NAME, strlen (CTV_TRIGGER_NAME)},		// "db_trig"
-    {CTV_PARTITION_NAME, strlen (CTV_PARTITION_NAME)},		// "db_partition"
-    {CTV_STORED_PROC_NAME, strlen (CTV_STORED_PROC_NAME)},	// "db_stored_procedure"
-    {CTV_STORED_PROC_ARGS_NAME, strlen (CTV_STORED_PROC_ARGS_NAME)},	// "db_stored_procedure_args"
-    {CTV_DB_COLLATION_NAME, strlen (CTV_DB_COLLATION_NAME)},	// "db_collation"
-    {CTV_DB_CHARSET_NAME, strlen (CTV_DB_CHARSET_NAME)},	// "db_charset"
-    {CTV_DB_SERVER_NAME, strlen (CTV_DB_SERVER_NAME)}		// "db_server"
-  };
-  // *INDENT-ON*
-
+  SYSTEM_CLASS_DEF sa;
   char downcase_name[SM_MAX_IDENTIFIER_LENGTH - SM_MAX_USER_LENGTH] = { '\0' };
-  int count = 0;
+  int len = 0;
+  int cmp = 0;
   int i = 0;
 
   if (name == NULL || name[0] == '\0')
     {
       return false;
+    }
+
+  if (!was_initialized)
+    {
+      qsort (system_classes, count, sizeof (system_classes[0]), system_class_def_compare);
+      was_initialized = TRUE;
     }
 
   /* The user-specified name is not a system class name. */
@@ -3304,28 +3327,20 @@ sm_check_system_class_by_name (const char *name)
     }
 
   sm_downcase_name (name, downcase_name, SM_MAX_IDENTIFIER_LENGTH - SM_MAX_USER_LENGTH);
+  sa.name = downcase_name;
+  sa.len = strlen (downcase_name);
 
-  if (strncmp (downcase_name, ROOTCLASS_NAME, strlen (ROOTCLASS_NAME)) == 0)
-    {
-      return true;
-    }
-
-  if (strncmp (downcase_name, CT_DUAL_NAME, strlen (CT_DUAL_NAME)) == 0)
-    {
-      return true;
-    }
-
-  if (strncmp (downcase_name, "_db_", 4) != 0 && strncmp (downcase_name, "db_", 3) != 0)
+  if (sa.len > system_classes[count - 1].len)
     {
       return false;
     }
 
-  count = sizeof (system_classes) / sizeof (system_classes[0]);
   for (i = 0; i < count; i++)
     {
-      if (strncmp (downcase_name, system_classes[i].name, system_classes[i].len) == 0)
+      cmp = system_class_def_compare (&sa, system_classes + i);
+      if (cmp <= 0)
 	{
-	  return true;
+	  return (cmp == 0);
 	}
     }
 
