52dc69df529f393b15ef0bf48bed68b2609b7079
Cristian Arghiroiu <cristi.arghiroiu@gmail.com>
2022-05-06 23:11:00 +0300

    try to fix scenario where tdes rcv atomic_sysop_start_lsa remains set after the last [atomic] sysop has been merged to outer sysop/transaction

--- src/transaction/log_manager.c
+++ src/transaction/log_manager.c
@@ -4439,6 +4439,7 @@ log_sysop_attach_to_outer (THREAD_ENTRY * thread_p)
   /* Attach to outer: transfer postpone LSA. Not much to do really :) */
   if (tdes->topops.last - 1 >= 0)
     {
+      /* At least one more outer sysop */
       if (LSA_ISNULL (&tdes->topops.stack[tdes->topops.last - 1].posp_lsa))
 	{
 	  LSA_COPY (&tdes->topops.stack[tdes->topops.last - 1].posp_lsa,
@@ -4447,10 +4448,20 @@ log_sysop_attach_to_outer (THREAD_ENTRY * thread_p)
     }
   else
     {
+      /* No outer sysop present */
+      assert (tdes->topops.last == 0);
       if (LSA_ISNULL (&tdes->posp_nxlsa))
 	{
 	  LSA_COPY (&tdes->posp_nxlsa, &tdes->topops.stack[tdes->topops.last].posp_lsa);
 	}
+
+      // - if the last sysop has been assigned to the parent transaction, meaning that, effectively
+      //  there are no sysop's present in the transaction anymore; clean traces of atomic sysops as well;
+      // - this occurs when there are nested non-atomic sysops and atomic sysops (in whichever order, not sure
+      //  which)
+      // - similar logic to this exists in 'log_sysop_commit_internal'
+      // TODO: this might be a workaround for an issue whose root cause is elsewhere
+      LSA_SET_NULL (&tdes->rcv.atomic_sysop_start_lsa);
     }
 
   log_sysop_end_final (thread_p, tdes);
