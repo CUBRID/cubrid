f773328e6bd447b18df1c97731038bc11ad95be8
Constantin-Adrian BRABETE (101569) <constantin.brabete@stud.acs.upb.ro>
2022-05-10 17:19:08 +0300

    fixed last comment

--- src/transaction/atomic_replication_helper.cpp
+++ src/transaction/atomic_replication_helper.cpp
@@ -145,15 +145,12 @@ namespace cublog
 	rcv.pgptr = m_watcher.pgptr;
       }
 
-    if (!log_rv_fix_page_and_check_redo_is_needed (thread_p, m_vpid, rcv.pgptr, record_info.m_start_lsa,
+    if (log_rv_fix_page_and_check_redo_is_needed (thread_p, m_vpid, rcv.pgptr, record_info.m_start_lsa,
 	redo_context.m_end_redo_lsa, redo_context.m_page_fetch_mode))
       {
-	/* nothing else needs to be done, see explanation in function */
-	assert (rcv.pgptr == nullptr);
-	return;
+	rcv.reference_lsa = m_record_lsa;
+	log_rv_redo_record_sync_apply (thread_p,redo_context, record_info, m_vpid, rcv);
       }
-    rcv.reference_lsa = m_record_lsa;
-    log_rv_redo_record_sync_apply (thread_p,redo_context, record_info, m_vpid, rcv);
   }
 
   int atomic_replication_helper::atomic_replication_unit::fix_page (THREAD_ENTRY *thread_p)
