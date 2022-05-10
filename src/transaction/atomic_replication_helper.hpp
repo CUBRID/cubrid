45ed2cb666ae313088429b9121e92dbf2393331c
Constantin-Adrian BRABETE (101569) <constantin.brabete@stud.acs.upb.ro>
2022-05-10 17:28:13 +0300

    fixed merge

--- src/transaction/atomic_replication_helper.hpp
+++ src/transaction/atomic_replication_helper.hpp
@@ -25,6 +25,7 @@
 
 #include "log_lsa.hpp"
 #include "log_record.hpp"
+#include "log_recovery.h"
 #include "log_recovery_redo.hpp"
 #include "page_buffer.h"
 #include "thread_entry.hpp"
@@ -141,8 +142,13 @@ namespace cublog
 	assert (m_watcher.page_was_unfixed == 0);
 	rcv.pgptr = m_watcher.pgptr;
       }
-    rcv.reference_lsa = m_record_lsa;
-    log_rv_redo_record_sync_apply (thread_p,redo_context, record_info, m_vpid, rcv);
+
+    if (log_rv_fix_page_and_check_redo_is_needed (thread_p, m_vpid, rcv.pgptr, record_info.m_start_lsa,
+	redo_context.m_end_redo_lsa, redo_context.m_page_fetch_mode))
+      {
+	rcv.reference_lsa = m_record_lsa;
+	log_rv_redo_record_sync_apply (thread_p,redo_context, record_info, m_vpid, rcv);
+      }
   }
 
 }
