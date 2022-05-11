fe154332a501d7cae7de7a12e0aa2cb57cbecc89
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2022-05-11 10:28:32 +0900

    [CBRD-23846] Slip closing socket that connecting with ExecuteThread in invalid state (#3587)
    http://jira.cubrid.org/browse/CBRD-23846
    
    It is a trivial fix, but It can cause exhausting PIDs on the system. so a new socket can not be opened.

--- src/method/method_connection_pool.cpp
+++ src/method/method_connection_pool.cpp
@@ -72,6 +72,7 @@ namespace cubmethod
 	// test socket
 	if (conn->is_valid() == false)
 	  {
+	    jsp_disconnect_server (conn->m_socket); // disconnect connecting with ExecuteThread in invalid state
 	    conn->m_socket = jsp_connect_server (boot_db_name (), jsp_server_port ());
 	  }
 
