b8ba0f6f5c5926baa3201341c830afa04b447d3c
youngjinj <youngjinj@cubrid.com>
2022-07-26 10:19:20 +0900

    The comments were changed.

--- src/query/execute_schema.c
+++ src/query/execute_schema.c
@@ -9194,7 +9194,7 @@ do_recreate_renamed_class_indexes (const PARSER_CONTEXT * parser, const char *co
     }
 
   /* We can come here by renaming a view. If there are no constraints, NO_ERROR is returned before.
-   * However, views can have shared columns with the not null constraint. Renaming does not affect
+   * However, views can have shared columns with a not null constraint. Renaming does not affect
    * the not null constraint. So, even if the view has a not null constraint, change it so that an error
    * does not occur here. I do not think there is a view with a constraint other than a not null constraint.
    * Change the return value from ER_OBJ_NOT_A_CLASS to NO_ERROR. (In CBRD-24435) */
