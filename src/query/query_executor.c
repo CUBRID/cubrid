b3f76595105d0ec99160076e7bf0d36ab5ad0213
SE park <shpark@cubrid.com>
2022-09-07 12:06:07 +0900

    [CBRD-24467] Sort-limit optimization does not work when host variable is used in limit clause. (#3793)
    http://jira.cubrid.org/browse/CBRD-24467
    
    A problem occurred when the XASL header that stores the sort-limit optimization status was not properly fetched.
    I fixed it (xqmgr_prepare_query)
    
    A problem occurs because TYPE_INARITH is excluded when checking whether ntopsort(IMS) optimization is possible.
    An arithmetic expression was added to the limit clause, but it seems that the related routine has not been modified together.
    (qexec_get_orderbynum_upper_bound)

--- src/query/query_executor.c
+++ src/query/query_executor.c
@@ -24489,7 +24489,7 @@ qexec_get_orderbynum_upper_bound (THREAD_ENTRY * thread_p, PRED_EXPR * pred, VAL
       op = pred->pe.m_eval_term.et.et_comp.rel_op;
       if (lhs->type != TYPE_CONSTANT)
 	{
-	  if (lhs->type != TYPE_POS_VALUE && lhs->type != TYPE_DBVAL)
+	  if (lhs->type != TYPE_POS_VALUE && lhs->type != TYPE_DBVAL && lhs->type != TYPE_INARITH)
 	    {
 	      goto cleanup;
 	    }
@@ -24520,7 +24520,7 @@ qexec_get_orderbynum_upper_bound (THREAD_ENTRY * thread_p, PRED_EXPR * pred, VAL
 	      goto cleanup;
 	    }
 	}
-      if (rhs->type != TYPE_POS_VALUE && rhs->type != TYPE_DBVAL)
+      if (rhs->type != TYPE_POS_VALUE && rhs->type != TYPE_DBVAL && rhs->type != TYPE_INARITH)
 	{
 	  goto cleanup;
 	}
