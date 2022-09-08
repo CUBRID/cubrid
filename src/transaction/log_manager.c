85756c9cd990c44a5785f9bdf1ee1cfc011484f0
jaeeunkim <jekim@cubrid.com>
2022-09-08 18:12:51 +0900

    [CBRD-24200] make LOG_WILL_COMMIT obsolete and delete corresponding codes

--- src/transaction/log_manager.c
+++ src/transaction/log_manager.c
@@ -385,9 +385,6 @@ log_to_string (LOG_RECTYPE type)
     case LOG_COMPENSATE:
       return "LOG_COMPENSATE";
 
-    case LOG_WILL_COMMIT:
-      return "LOG_WILL_COMMIT";
-
     case LOG_COMMIT_WITH_POSTPONE:
       return "LOG_COMMIT_WITH_POSTPONE";
 
@@ -5280,8 +5277,8 @@ log_clear_lob_locator_list (THREAD_ENTRY * thread_p, LOG_TDES * tdes, bool at_co
 									 "LOB_TRANSIENT_DELETED"
 									 : ((entry->top->state ==
 									     LOB_PERMANENT_CREATED) ?
-									    "LOB_PERMANENT_CREATED" : (entry->
-												       top->state ==
+									    "LOB_PERMANENT_CREATED" : (entry->top->
+												       state ==
 												       LOB_PERMANENT_DELETED)
 									    ? "LOB_PERMANENT_DELETED" : "LOB_UNKNOWN")),
 		    entry->top->savept_lsa.pageid, entry->top->savept_lsa.offset);
@@ -7256,10 +7253,6 @@ log_dump_record (THREAD_ENTRY * thread_p, FILE * out_fp, LOG_RECTYPE record_type
       log_page_p = log_dump_record_commit_postpone (thread_p, out_fp, log_lsa, log_page_p);
       break;
 
-    case LOG_WILL_COMMIT:
-      fprintf (out_fp, "\n");
-      break;
-
     case LOG_COMMIT:
     case LOG_ABORT:
       log_page_p = log_dump_record_transaction_finish (thread_p, out_fp, log_lsa, log_page_p);
@@ -8242,7 +8235,6 @@ log_rollback (THREAD_ENTRY * thread_p, LOG_TDES * tdes, const LOG_LSA * upto_lsa
 	      LSA_SET_NULL (&prev_tranlsa);
 	      break;
 
-	    case LOG_WILL_COMMIT:
 	    case LOG_COMMIT:
 	    case LOG_ABORT:
 	    case LOG_2PC_COMMIT_DECISION:
@@ -8669,7 +8661,6 @@ log_do_postpone (THREAD_ENTRY * thread_p, LOG_TDES * tdes, LOG_LSA * start_postp
 		      /* TODO: consider to add FI here */
 		      break;
 
-		    case LOG_WILL_COMMIT:
 		    case LOG_COMMIT_WITH_POSTPONE:
 		    case LOG_SYSOP_START_POSTPONE:
 		    case LOG_2PC_PREPARE:
