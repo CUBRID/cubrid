5f85269de8a503c7723fd94c05a90bfd4c9d47e0
Cristian Arghiroiu <cristi.arghiroiu@gmail.com>
2022-10-05 14:30:00 +0300

    comment

--- src/transaction/atomic_replication_helper.cpp
+++ src/transaction/atomic_replication_helper.cpp
@@ -220,7 +220,8 @@ namespace cublog
 	return;
       }
 
-    sequence_it->second.apply_and_unfix_sequence (thread_p);
+    atomic_log_sequence &sequence = sequence_it->second;
+    sequence.apply_and_unfix_sequence (thread_p);
     m_sequences_map.erase (sequence_it);
 
 #if !defined (NDEBUG)
@@ -369,14 +370,20 @@ namespace cublog
   {
     // Applying the log right after the fix could lead to problems as the records are fixed one by one as
     // they come to be read by the PTS and some might be unfixed and refixed after the apply procedure
-    // leading to inconsistency. To avoid this situation we sequentially apply each log redo of the sequence
-    // when the end sequence log appears and the entire sequence is fixed
+    // leading to inconsistency.
+    // To avoid this situation each log redo of the sequence is applied when the end sequence log appears
+    // and the entire sequence is already fixed.
+    // Right after applying, unfix and ref-count-down each page. The bookkeeping mechanism will take care
+    // of either unfixing the page or retaining it for a subsequent unfix.
     for (const auto &log_entry : m_log_vec)
       {
 	log_entry.apply_log_redo (thread_p, m_redo_context);
 	// bookkeeping actually will either unfix the page or just decrease its reference count
 	m_page_ptr_bookkeeping.unfix_page (thread_p, log_entry.m_vpid);
       }
+
+    // clear the vector of log records; page pts's might be, at this point, dangling pointers as the page ptr
+    // bookkeeping mechanims might have already unfixed the page
     m_log_vec.clear ();
   }
 
