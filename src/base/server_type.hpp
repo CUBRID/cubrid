911f00319cf8e77fa9d5fd8b98f0cc54da968594
jaeeunkim <jekim@cubrid.com>
2022-10-13 13:16:31 +0900

    license

--- src/base/server_type.hpp
+++ src/base/server_type.hpp
@@ -1,6 +1,6 @@
 /*
  * Copyright 2008 Search Solution Corporation
- * Copyright 2021 CUBRID Corporation
+ * Copyright 2016 CUBRID Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
