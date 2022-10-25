4b926ed74591db1b83b98d70a6f1afa32c021d93
Byungwook Kim <bwkim@cubrid.com>
2022-10-25 14:39:21 +0900

    fix: info.flag_agg_optimize condition

--- src/parser/xasl_generation.c
+++ src/parser/xasl_generation.c
@@ -4727,7 +4727,7 @@ pt_to_aggregate (PARSER_CONTEXT * parser, PT_NODE * select_node, OUTPTR_LIST * o
 	  if (from->info.spec.entity_name)
 	    {
 	      info.class_name = from->info.spec.entity_name->info.name.original;
-	      //info.flag_agg_optimize = true;
+	      info.flag_agg_optimize = true;
 	    }
 	}
     }
@@ -16229,7 +16229,9 @@ pt_to_buildlist_proc (PARSER_CONTEXT * parser, PT_NODE * select_node, QO_PLAN * 
 	pt_to_aggregate (parser, select_node, xasl->outptr_list, buildlist->g_val_list, buildlist->g_regu_list,
 			 buildlist->g_scan_regu_list, group_out_list, &buildlist->g_grbynum_val);
 
-      if (aggregate && aggregate->flag_agg_optimize == true)
+      if (aggregate
+	  && (aggregate->function == PT_COUNT_STAR || aggregate->function == PT_MAX || aggregate->function == PT_MIN)
+	  && aggregate->flag_agg_optimize == true)
 	{
 	  qo_plan->plan_un.scan.scan_method = QO_SCANMETHOD_INDEX_SCAN_OPTIMIZED;
 	}
