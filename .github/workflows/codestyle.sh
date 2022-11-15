388a07037482b8665c5d17a867bc5aaebef92836
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2022-11-15 17:05:17 +0900

    modify not to use goolge-java-format action

--- .github/workflows/codestyle.sh
+++ .github/workflows/codestyle.sh
@@ -9,4 +9,7 @@ case $ext in
 .cpp|.hpp|.ipp)
   astyle --style=gnu --mode=c --indent-namespaces --indent=spaces=2 -xT8 -xt4 --add-brackets --max-code-length=120 --align-pointer=name --indent-classes --pad-header --pad-first-paren-out ${f}
 ;;
+.java)
+  java -jar google-java-format-1.7-all-deps.jar -a -r ${f}
+;;
 esac
