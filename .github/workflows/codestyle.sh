f246d4ff8d5f3081d7a80f7320b93c6cfa5bb707
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2022-11-15 17:59:29 +0900

    fix path

--- .github/workflows/codestyle.sh
+++ .github/workflows/codestyle.sh
@@ -28,6 +28,6 @@ case $ext in
   astyle --style=gnu --mode=c --indent-namespaces --indent=spaces=2 -xT8 -xt4 --add-brackets --max-code-length=120 --align-pointer=name --indent-classes --pad-header --pad-first-paren-out ${f}
 ;;
 .java)
-  java -jar google-java-format-1.7-all-deps.jar -a -r ${f}
+  java -jar .github/workflows/google-java-format-1.7-all-deps.jar -a -r ${f}
 ;;
 esac
