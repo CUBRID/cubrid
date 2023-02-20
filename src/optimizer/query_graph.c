227dfc72b9a2b861e0548e8c051019394bdeae1c
shparkcubrid <shpark@cubrid.com>
2023-02-20 16:38:00 +0900

    indent

--- src/optimizer/query_graph.c
+++ src/optimizer/query_graph.c
@@ -6079,11 +6079,13 @@ qo_discover_edges (QO_ENV * env)
 	    {
 	      qo_exchange (term1, term2);
 	    }
-	  else if ((QO_TERM_PRED_ORDER (term1) == QO_TERM_PRED_ORDER (term2)) && (QO_TERM_SELECTIVITY (term1) < QO_TERM_SELECTIVITY (term2)))
+	  else if ((QO_TERM_PRED_ORDER (term1) == QO_TERM_PRED_ORDER (term2))
+		   && (QO_TERM_SELECTIVITY (term1) < QO_TERM_SELECTIVITY (term2)))
 	    {
 	      qo_exchange (term1, term2);
 	    }
-	  else if ((QO_TERM_PRED_ORDER (term1) == QO_TERM_PRED_ORDER (term2)) && (QO_TERM_SELECTIVITY (term1) == QO_TERM_SELECTIVITY (term2))
+	  else if ((QO_TERM_PRED_ORDER (term1) == QO_TERM_PRED_ORDER (term2))
+		   && (QO_TERM_SELECTIVITY (term1) == QO_TERM_SELECTIVITY (term2))
 		   && QO_TERM_RANK (term1) < QO_TERM_RANK (term1))
 	    {
 	      qo_exchange (term1, term2);
