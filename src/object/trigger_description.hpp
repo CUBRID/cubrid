29c69ef3228bfec0b83028b67acb10b39d6d3774
Kang Dooho <airnet@cubrid.com>
2023-02-23 15:35:55 +0900

    fix style

--- src/object/trigger_description.hpp
+++ src/object/trigger_description.hpp
@@ -63,7 +63,7 @@ struct trigger_description
   void fprint (FILE *file);
 };
 
-int tr_dump_trigger (extract_context & ctxt, print_output &output_ctx, DB_OBJECT *trigger_object);
-int tr_dump_selective_triggers (extract_context & ctxt, print_output &output_ctx, DB_OBJLIST *classes);
+int tr_dump_trigger (extract_context &ctxt, print_output &output_ctx, DB_OBJECT *trigger_object);
+int tr_dump_selective_triggers (extract_context &ctxt, print_output &output_ctx, DB_OBJLIST *classes);
 
 #endif // _TRIGGER_DESCRIPTION_HPP_
