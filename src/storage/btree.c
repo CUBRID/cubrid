71ae8e7fd3bc164a1101a57d99b0c256bd847333
yahong <yechan0815@naver.com>
2023-02-27 16:28:33 +0900

    [CBRD-24681] Change the lock mode and add a comment.

--- src/storage/btree.c
+++ src/storage/btree.c
@@ -8258,9 +8258,10 @@ btree_repair_prev_link (THREAD_ENTRY * thread_p, OID * oid, BTID * index_btid, b
 	  break;
 	}
     }
+  /* if the break statement occurs due to an error, the lock acquired immediately before should be released. */
   if (!OID_ISNULL (&class_oid))
     {
-      lock_unlock_object (thread_p, &class_oid, oid_Root_class_oid, SCH_S_LOCK, true);
+      lock_unlock_object (thread_p, &class_oid, oid_Root_class_oid, FILE_GET_TRACKER_LOCK_MODE (FILE_BTREE), true);
     }
 
   return valid;
