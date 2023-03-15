416b3364712d01b266abeab8dbe4da6e7de5fe90
Jaeeun Kim (bigs) <jekim@cubrid.com>
2023-03-15 13:41:14 +0900

    [LETS-660] Stop out-going msg generators outside explicitly. (#4174)
    http://jira.cubrid.org/browse/LETS-660
    
    - Cause
    The cause of the regression issue is calling the virtual function stop_outgoing_page_server_messages inside the connection_handler's destructor. It results in: ~tran_server() -> disconnect_all_page_servers() -> ~connection_handler() -> stop_outgoing_page_server_messages() so it ends up calling a virtual function in its destructor, which is problematic.
    
    - Solution
    Call stop_outgoing_page_server_messages() not in the dtor, but explicitly outside before calling disconnect_all_page_servers().
    
    It's also better regarding code consistency because other threads are explicitly destroyed in this way, such as finish_replication_during_shutdown() and finalize_request_responder().

--- src/server/tran_server.cpp
+++ src/server/tran_server.cpp
@@ -341,8 +341,6 @@ tran_server::disconnect_all_page_servers ()
 {
   assert_is_tran_server ();
 
-  stop_outgoing_page_server_messages ();
-
   for (const auto &conn : m_page_server_conn_vec)
     {
       er_log_debug (ARG_FILE_LINE, "Transaction server disconnected from page server with channel id: %s.\n",
