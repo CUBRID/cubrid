4f158a92e6d253f5f8a25c2bf3905a10b53c38ab
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2023-03-16 14:19:17 +0900

    remove package related

--- src/executables/loadjava.cpp
+++ src/executables/loadjava.cpp
@@ -26,7 +26,6 @@
 
 #include <cassert>
 #include <string>
-#include <regex>
 #include <filesystem>
 
 #include "cubrid_getopt.h"
@@ -39,7 +38,6 @@
 #endif /* WINDOWS */
 
 namespace fs = std::filesystem;
-using namespace std::regex_constants;
 
 #define JAVA_DIR                "java"
 #if defined(WINDOWS)
@@ -48,16 +46,11 @@ using namespace std::regex_constants;
 #define SEPERATOR               "/"
 #endif /* !WINDOWS */
 
-static const std::string JAVA_PACKAGE_PATTERN = "^([a-z_]{1}[a-z0-9_]*(\\.[a-z_]{1}[a-z0-9_]*)*)$";
-static const std::string SEPARATOR_STRING (SEPERATOR);
-
 static char *Program_name = NULL;
 static char *Dbname = NULL;
 std::string Src_class;
-std::regex Java_package_reg (JAVA_PACKAGE_PATTERN, ECMAScript | icase | optimize);
 
 static int Force_overwrite = false;
-static std::string package_path;
 
 static void
 usage (void)
@@ -71,14 +64,13 @@ parse_argument (int argc, char *argv[])
   struct option loadjava_option[] =
   {
     {"overwrite", 0, 0, 'y'},
-    {"package", 0, 0, 'p'},
     {0, 0, 0, 0}
   };
 
   while (1)
     {
       int option_index = 0;
-      int option_key = getopt_long (argc, argv, "y:p:h", loadjava_option, &option_index);
+      int option_key = getopt_long (argc, argv, "yh", loadjava_option, &option_index);
       if (option_key == -1)
 	{
 	  break;
@@ -89,24 +81,6 @@ parse_argument (int argc, char *argv[])
 	case 'y':
 	  Force_overwrite = true;
 	  break;
-	case 'p':
-	{
-	  // check valid package name
-	  std::string package_name (optarg);
-	  if (!package_name.empty())
-	    {
-	      bool is_matched = std::regex_search (package_name, Java_package_reg);
-	      if (!is_matched)
-		{
-		  fprintf (stderr, "invalid java package name\n");
-		  return ER_FAILED;
-		}
-	      // replace all for package name's dot to SEPARATER
-	      // e.g. org.cubrid.abc => org/cubrid/abc
-	      package_path = std::regex_replace (package_name, std::regex ("\\."), SEPARATOR_STRING);
-	    }
-	}
-	break;
 	case 'h':
 	/* fall through */
 	default:
@@ -128,27 +102,6 @@ parse_argument (int argc, char *argv[])
 
   Program_name = argv[0];
 
-  return NO_ERROR;
-}
-
-static int
-create_package_directories (const fs::path &java_dir_path)
-{
-  try
-    {
-      if (fs::exists (java_dir_path) == false)
-	{
-	  fs::create_directories (java_dir_path);
-	  fs::permissions (java_dir_path,
-			   fs::perms::owner_all | fs::perms::group_read | fs::perms::others_read,
-			   fs::perm_options::add);	// mkdir (java_dir_path, 0744)
-	}
-    }
-  catch (fs::filesystem_error &e)
-    {
-      fprintf (stderr, "can't create directory: %s. %s\n", java_dir_path.generic_string ().c_str (), e.what ());
-      return ER_FAILED;
-    }
   return NO_ERROR;
 }
 
@@ -221,14 +174,6 @@ main (int argc, char *argv[])
 
   // e.g. $CUBRID/demodb/java
   java_dir_path.append (JAVA_DIR);
-
-  // e.g. $CUBRID/demodb/java/org/cubrid/path/
-  java_dir_path.append (package_path);
-
-  if (create_package_directories (java_dir_path) != NO_ERROR)
-    {
-      goto error;
-    }
 
   if (copy_file (java_dir_path) != NO_ERROR)
     {
