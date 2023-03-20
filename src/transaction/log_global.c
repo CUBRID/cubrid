ad364d592c760d284457d66b6aaf156841ba63f2
jaeeunkim <jekim@cubrid.com>
2023-03-20 10:10:37 +0900

    Revert the location of func2

--- src/transaction/log_global.c
+++ src/transaction/log_global.c
@@ -106,18 +106,6 @@ log_global::~log_global ()
 
 #if defined (SERVER_MODE)
 void
-log_global::wait_flushed_lsa (const log_lsa &flush_lsa)
-{
-  std::unique_lock<std::mutex> lock (m_ps_lsa_mutex);
-  if (m_ps_consensus_flushed_lsa >= flush_lsa)
-    {
-      // already flushed
-      return;
-    }
-  m_ps_lsa_cv.wait (lock, [flush_lsa, this] { return m_ps_consensus_flushed_lsa >= flush_lsa; });
-}
-
-void
 log_global::update_ps_consensus_flushed_lsa ()
 {
   assert (is_active_transaction_server ());
@@ -139,6 +127,18 @@ log_global::update_ps_consensus_flushed_lsa ()
   }
 
   m_ps_lsa_cv.notify_all ();
+}
+
+void
+log_global::wait_flushed_lsa (const log_lsa &flush_lsa)
+{
+  std::unique_lock<std::mutex> lock (m_ps_lsa_mutex);
+  if (m_ps_consensus_flushed_lsa >= flush_lsa)
+    {
+      // already flushed
+      return;
+    }
+  m_ps_lsa_cv.wait (lock, [flush_lsa, this] { return m_ps_consensus_flushed_lsa >= flush_lsa; });
 }
 
 void
