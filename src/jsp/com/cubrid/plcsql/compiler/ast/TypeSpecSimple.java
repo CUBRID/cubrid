17e243f4b715ab2dd0d74665fb07adeb39d64e39
Na <hina@cubrid.com>
2023-03-27 17:27:11 +0900

    bugfix: NPE due to null names of Null and Cursor internal types

--- src/jsp/com/cubrid/plcsql/compiler/ast/TypeSpecSimple.java
+++ src/jsp/com/cubrid/plcsql/compiler/ast/TypeSpecSimple.java
@@ -56,8 +56,8 @@ public class TypeSpecSimple extends TypeSpec {
     }
 
     // the following two are not actual Java types but only for internal type checking
-    public static TypeSpecSimple NULL = new TypeSpecSimple(null, null);
-    public static TypeSpecSimple CURSOR = new TypeSpecSimple(null, null);
+    public static TypeSpecSimple NULL = new TypeSpecSimple("Null", null);
+    public static TypeSpecSimple CURSOR = new TypeSpecSimple("Cursor", null);
 
     // (1) used as an argument type of some operators in SpLib
     // (2) used as an expression type when a specific Java type cannot be given
