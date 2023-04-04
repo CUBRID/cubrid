a365984dc0591895f221e0ea0a3637d04f747ab6
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2023-04-04 18:10:23 +0900

    split global_semantics demo program

--- demo/plcsql/demo_global_semantics.sql
+++
@@ -1,9 +0,0 @@
-create or replace function demo_global_semantics() return varchar as
-i numeric;
-m varchar;
-begin
-    demo_hello ();
-    m := demo_hello_ret ();
-    i := demo_pl_serial.NEXT_VALUE;
-    return m || ' :' || i;
-end;
