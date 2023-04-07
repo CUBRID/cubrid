c38a7a85411d467f2a85c9de7d5f6fb6c58ccb3b
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2023-04-08 00:11:11 +0900

    revise rollback demo

--- demo/plcsql/test_tcl_rollback.sql
+++ demo/plcsql/test_tcl_rollback.sql
@@ -1,16 +1,28 @@
 /*
+=====[PREAPRE SCHEMA]=======================================
 DROP TABLE IF EXISTS test_tcl_tbl2;
 CREATE TABLE test_tcl_tbl2 (code INT, name STRING);
 
+=====[TEST]=================================================
 ;set autocommit off
 ;server-output on
-TRUNCATE test_tcl_tbl2;
-CALL test_tcl_rollbackk ();
 
+TRUNCATE test_tcl_tbl2;
+CALL test_tcl_rollback ();
 COMMIT;
 SELECT * FROM test_tcl_tbl2;
-*/
 
+=====[Expected]=============================================
+    Expected:
+    === <Result of SELECT Command in Line 1> ===
+
+            code  name                
+    ===================================
+                1  'aaa'               
+                2  'bbb'               
+                3  'ccc'               
+                4  'ddd'
+*/
 create or replace procedure test_tcl_rollback () as
     i int;
     n varchar;
@@ -23,7 +35,6 @@ begin
 
     INSERT INTO test_tcl_tbl2 VALUES (6,'daf');
     INSERT INTO test_tcl_tbl2 VALUES (7,'qwe');
-
     ROLLBACK;
 
     for r in (SELECT code, name FROM test_tcl_tbl2 WHERE code > 2) loop
