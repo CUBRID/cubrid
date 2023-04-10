7dd75ce60e9ff1dd51acda19101877736a0a50e1
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2023-04-10 19:56:38 +0900

    fix restoring custom print

--- src/parser/parse_tree_cl.c
+++ src/parser/parse_tree_cl.c
@@ -15343,7 +15343,6 @@ pt_print_update (PARSER_CONTEXT * parser, PT_NODE * p)
       parser->custom_print &= ~PT_PRINT_ALIAS;
       r1 = pt_print_bytes_spec_list (parser, p->info.update.spec);
       b = pt_append_varchar (parser, b, r1);
-      parser->custom_print = save_custom;
     }
   else
     {
@@ -15351,6 +15350,7 @@ pt_print_update (PARSER_CONTEXT * parser, PT_NODE * p)
       b = pt_append_nulstring (parser, b, "object ");
       b = pt_append_varchar (parser, b, r1);
     }
+  parser->custom_print = save_custom;
 
   r1 = pt_print_bytes_l (parser, p->info.update.assignment);
   b = pt_append_nulstring (parser, b, " set ");
