327c2a2b67b0dac7711439a273e7ed3a0676717e
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2023-04-25 20:16:42 +0900

    indent

--- src/jsp/com/cubrid/jsp/ExecuteThread.java
+++ src/jsp/com/cubrid/jsp/ExecuteThread.java
@@ -340,7 +340,9 @@ public class ExecuteThread extends Thread {
                 }
             }
         } catch (Exception e) {
-            info = new CompileInfo(-1, -1, e.getMessage().isEmpty() ? "unknown error" : e.getMessage());
+            info =
+                    new CompileInfo(
+                            -1, -1, e.getMessage().isEmpty() ? "unknown error" : e.getMessage());
             throw new RuntimeException(e);
         } finally {
             CUBRIDPacker packer = new CUBRIDPacker(ByteBuffer.allocate(1024));
