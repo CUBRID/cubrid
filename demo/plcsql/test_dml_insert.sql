844004b2ad64c7b7d19575acdc60ca9751546eed
Na, Hyunik <67700070+hyunikn@users.noreply.github.com>
2023-04-28 19:00:44 +0900

    [CBRD-24782] extracting Static SQL texts without SqlParser.g4 (#4332)
    * extracting Static SQL texts without SqlParser.g4
    
    * revise demo
    
    ---------
    
    Co-authored-by: Na <hina@cubrid.com>
    Co-authored-by: Hyung-Gyu Ryoo <hgryoo@cubrid.com>

--- demo/plcsql/test_dml_insert.sql
+++ demo/plcsql/test_dml_insert.sql
@@ -9,27 +9,41 @@ create or replace procedure test_dml_insert() as
     p varchar;
 
     new_table VARCHAR := 'a_tbl1';
+    temp VARCHAR := '';
 begin
 -- 0)
 --insert default values with DEFAULT keyword before VALUES
-    EXECUTE IMMEDIATE 'INSERT INTO a_tbl1 DEFAULT VALUES';
+    INSERT INTO a_tbl1 DEFAULT VALUES;
 
 --insert multiple rows
-    EXECUTE IMMEDIATE 'INSERT INTO a_tbl1 VALUES (1,''aaa'', DEFAULT),(2,''bbb'', DEFAULT)';
+    INSERT INTO a_tbl1 VALUES (1,'aaa', DEFAULT),(2,'bbb', DEFAULT);
 
 --insert a single row specifying column values for all
     INSERT INTO a_tbl1 VALUES (3,'ccc', '333-3333');
 
 --insert two rows specifying column values for only
-    EXECUTE IMMEDIATE 'INSERT INTO a_tbl1(id) VALUES (4), (5)';
+    INSERT INTO a_tbl1(id) VALUES (4), (5);
 
 --insert a single row with SET clauses
-    EXECUTE IMMEDIATE 'INSERT INTO a_tbl1 SET id=6, name=''eee'';';
-    EXECUTE IMMEDIATE 'INSERT INTO a_tbl1 SET id=7, phone=''777-7777'';';
+
+    temp := temp || '/* ((';
+    temp := temp || '(((( */';
+    
+    temp := temp || 'INSERT INTO a_tbl1 SET id=6, name=''eee'';';
+
+    temp := temp || '/* ((';
+    temp := temp || '(((( */';
+
+    put_line (temp);
+
+    EXECUTE IMMEDIATE temp;
+    put_line(temp || ' is succeed');
+
+    INSERT INTO a_tbl1 SET id=7, phone='777-7777';
 
 -- 1)
     PUT_LINE('[Test 1] =====================================================================');
-    EXECUTE IMMEDIATE 'INSERT INTO a_tbl1 SET id=6, phone=''000-0000'' ON DUPLICATE KEY UPDATE phone=''666-6666'';';
+    INSERT INTO a_tbl1 SET id=6, phone='000-0000' ON DUPLICATE KEY UPDATE phone='666-6666';
 
 -- 2)
     PUT_LINE('Expected: ');
@@ -54,7 +68,7 @@ begin
 =========================================================
             7  ggg                 777-7777');
 
-    EXECUTE IMMEDIATE 'INSERT INTO a_tbl1 SELECT * FROM a_tbl1 WHERE id=7 ON DUPLICATE KEY UPDATE name=''ggg'';';
+    INSERT INTO a_tbl1 SELECT * FROM a_tbl1 WHERE id=7 ON DUPLICATE KEY UPDATE name='ggg';
 
     PUT_LINE('Actual');
     for r in (SELECT id, name, phone FROM a_tbl1 WHERE id=7) loop
