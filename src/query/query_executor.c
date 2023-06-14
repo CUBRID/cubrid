4d0a0c02ae4cd7a7cc578e8ef59577ab90c3877c
beyondykk <bwkim@cubrid.com>
2023-06-14 10:11:14 +0900

    [CBRD-24516] multi-aggregate-optimize: replace the information which is printed in "show trace" from aggregate function optimization

--- src/query/query_executor.c
+++ src/query/query_executor.c
@@ -1803,9 +1803,15 @@ qexec_clear_access_spec_list (THREAD_ENTRY * thread_p, XASL_NODE * xasl_p, ACCES
   for (p = list; p; p = p->next)
     {
       /* aggregate optimize related should be free */
-      if (p->s_id.scan_stats.agg_index_name != NULL)
+      if (p->s_id.scan_stats.agl)
 	{
-	  free (p->s_id.scan_stats.agg_index_name);
+	  SCAN_AGL *agl;
+
+	  for (agl = p->s_id.scan_stats.agl; agl; agl = agl->next)
+	    {
+	      free (agl->agg_index_name);
+	      free (agl);
+	    }
 	}
 
       memset (&p->s_id.scan_stats, 0, sizeof (SCAN_STATS));
@@ -23837,7 +23843,7 @@ qexec_evaluate_aggregates_optimize (THREAD_ENTRY * thread_p, AGGREGATE_TYPE * ag
 	{
 	  /* scan is needed for this aggregate */
 	  *is_scan_needed = true;
-	  break;
+	  continue;
 	}
 
       /* If we deal with a count optimization and the snapshot wasn't already taken then prepare current class for
@@ -23851,7 +23857,7 @@ qexec_evaluate_aggregates_optimize (THREAD_ENTRY * thread_p, AGGREGATE_TYPE * ag
 	    {
 	      agg_ptr->flag_agg_optimize = false;
 	      *is_scan_needed = true;
-	      break;
+	      continue;
 	    }
 	  if (tdes->mvccinfo.snapshot.valid)
 	    {
@@ -23859,7 +23865,7 @@ qexec_evaluate_aggregates_optimize (THREAD_ENTRY * thread_p, AGGREGATE_TYPE * ag
 		{
 		  agg_ptr->flag_agg_optimize = false;
 		  *is_scan_needed = true;
-		  break;
+		  continue;
 		}
 	    }
 	  else
@@ -23868,7 +23874,7 @@ qexec_evaluate_aggregates_optimize (THREAD_ENTRY * thread_p, AGGREGATE_TYPE * ag
 		{
 		  agg_ptr->flag_agg_optimize = false;
 		  *is_scan_needed = true;
-		  break;
+		  continue;
 		}
 	      class_cos->count_state = COS_TO_LOAD;
 
@@ -23879,16 +23885,45 @@ qexec_evaluate_aggregates_optimize (THREAD_ENTRY * thread_p, AGGREGATE_TYPE * ag
 		}
 
 	    }
+	}
 
-	  if (thread_is_on_trace (thread_p))
+      if (thread_is_on_trace (thread_p))
+	{
+	  char *agg_index_name;
+	  SCAN_AGL *agl;
+
+	  error = heap_get_indexinfo_of_btid (thread_p, &ACCESS_SPEC_CLS_OID (spec), &agg_ptr->btid,
+					      NULL, NULL, NULL, NULL, &agg_index_name, NULL);
+	  if (error != NO_ERROR)
 	    {
-	      /* agg_index_name is set to show it in the trace information */
-	      error = heap_get_indexinfo_of_btid (thread_p, &ACCESS_SPEC_CLS_OID (spec), &agg_ptr->btid,
-						  NULL, NULL, NULL, NULL, &spec->s_id.scan_stats.agg_index_name, NULL);
-	      if (error != NO_ERROR)
+	      return error;
+	    }
+
+	  for (agl = spec->s_id.scan_stats.agl; agl; agl = agl->next)
+	    {
+	      if (strcmp (agl->agg_index_name, agg_index_name) == 0)
 		{
-		  return error;
+		  /* same index name found */
+		  break;
 		}
+	    }
+
+	  if (agl == NULL)
+	    {
+	      agl = (SCAN_AGL *) malloc (sizeof (SCAN_AGL));
+	      if (agl == NULL)
+		{
+		  return ER_FAILED;
+		}
+
+	      agl->agg_index_name = agg_index_name;
+	      agl->next = spec->s_id.scan_stats.agl;
+	      spec->s_id.scan_stats.agl = agl;
+	    }
+	  else
+	    {
+	      /* same index name found */
+	      free (agg_index_name);
 	    }
 	}
     }
@@ -23925,6 +23960,8 @@ qexec_evaluate_aggregates_optimize (THREAD_ENTRY * thread_p, AGGREGATE_TYPE * ag
 	    }
 	}
     }
+
+  spec->s_id.scan_stats.noscan = (*is_scan_needed) ? false : true;
 
   return error;
 }
