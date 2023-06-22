e60ac134cd44f60173609af691cf2a1db504bca4
joohok <joohokim@cubrid.com>
2023-06-22 14:50:44 +0900

    Rename the member variable

--- src/executables/master_heartbeat.h
+++ src/executables/master_heartbeat.h
@@ -207,8 +207,8 @@ struct hb_node_entry
   HB_NODE_STATE_TYPE state;
   short score;
   short heartbeat_gap;
-  bool has_transaction_server;
-  bool has_page_server;
+  bool is_tran_server_alive;
+  bool is_page_server_alive;
 
   struct timeval last_recv_hbtime;	/* last received heartbeat time */
 };
