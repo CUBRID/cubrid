ff018630ab12aa4e2aaf935bbda8a66736d650f0
beyondykk <bwkim@cubrid.com>
2023-06-27 10:37:46 +0900

    [CBRD-24684] revised: divide operation result into NUMERIC type

--- src/parser/parser_support.c
+++ src/parser/parser_support.c
@@ -4456,7 +4456,9 @@ pt_limit_to_numbering_expr (PARSER_CONTEXT * parser, PT_NODE * limit, PT_OP_TYPE
 
       sum->data_type->type_enum = PT_TYPE_NUMERIC;
       sum->data_type->info.data_type.precision = DB_MAX_NUMERIC_PRECISION;
-      sum->data_type->info.data_type.dec_precision = 0;
+
+      /* It is necessary to prepare for the division operation in advance.*/
+      sum->data_type->info.data_type.dec_precision = DB_DEFAULT_NUMERIC_DIVISION_SCALE; /* 9 */
 
       sum->info.expr.arg1 = parser_copy_tree (parser, limit);
       sum->info.expr.arg2 = parser_copy_tree (parser, limit->next);
