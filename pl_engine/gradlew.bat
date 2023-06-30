9c8889dbf240443577d329db7b614e75b9a84f29
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2023-06-30 14:14:10 +0900

    update license

--- pl_engine/gradlew.bat
+++ pl_engine/gradlew.bat
@@ -1,5 +1,8 @@
+@echo off
+
 @rem
-@rem Copyright 2015 the original author or authors.
+@rem
+@rem Copyright 2016 CUBRID Corporation
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
