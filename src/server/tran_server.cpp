0a33da779078527b792d6826f4d8f78d0988fe9a
jaeeunkim <jekim@cubrid.com>
2023-07-07 15:51:29 +0900

    Call on_connecting without lock

--- src/server/tran_server.cpp
+++ src/server/tran_server.cpp
@@ -330,48 +330,53 @@ tran_server::connection_handler::connect ()
 
   assert_is_tran_server ();
 
-  auto lockg_state = std::lock_guard<std::shared_mutex> { m_state_mtx };
-  assert (m_state == state::IDLE);
+  {
+    auto lockg_state = std::lock_guard<std::shared_mutex> { m_state_mtx };
+    assert (m_state == state::IDLE);
 
-  m_state = state::CONNECTING;
+    m_state = state::CONNECTING;
 
-  // connect to page server
-  constexpr int CHANNEL_POLL_TIMEOUT = 1000;    // 1000 milliseconds = 1 second
-  cubcomm::server_channel srv_chn (m_ts.m_server_name.c_str (), SERVER_TYPE_PAGE, CHANNEL_POLL_TIMEOUT);
+    // connect to page server
+    constexpr int CHANNEL_POLL_TIMEOUT = 1000;    // 1000 milliseconds = 1 second
+    cubcomm::server_channel srv_chn (m_ts.m_server_name.c_str (), SERVER_TYPE_PAGE, CHANNEL_POLL_TIMEOUT);
 
-  srv_chn.set_channel_name ("TS_PS_comm");
+    srv_chn.set_channel_name ("TS_PS_comm");
 
-  css_error_code comm_error_code = srv_chn.connect (m_node.get_host ().c_str (), m_node.get_port (),
-				   CMD_SERVER_SERVER_CONNECT);
-  if (comm_error_code != css_error_code::NO_ERRORS)
-    {
-      return ps_conn_error_lambda (lockg_state);
-    }
+    css_error_code comm_error_code = srv_chn.connect (m_node.get_host ().c_str (), m_node.get_port (),
+				     CMD_SERVER_SERVER_CONNECT);
+    if (comm_error_code != css_error_code::NO_ERRORS)
+      {
+	return ps_conn_error_lambda (lockg_state);
+      }
 
-  if (srv_chn.send_int (static_cast<int> (m_ts.m_conn_type)) != NO_ERRORS)
-    {
-      return ps_conn_error_lambda (lockg_state);
-    }
+    if (srv_chn.send_int (static_cast<int> (m_ts.m_conn_type)) != NO_ERRORS)
+      {
+	return ps_conn_error_lambda (lockg_state);
+      }
 
-  int returned_code;
-  if (srv_chn.recv_int (returned_code) != css_error_code::NO_ERRORS)
-    {
-      return ps_conn_error_lambda (lockg_state);
-    }
-  if (returned_code != static_cast<int> (m_ts.m_conn_type))
-    {
-      return ps_conn_error_lambda (lockg_state);
-    }
+    int returned_code;
+    if (srv_chn.recv_int (returned_code) != css_error_code::NO_ERRORS)
+      {
+	return ps_conn_error_lambda (lockg_state);
+      }
+    if (returned_code != static_cast<int> (m_ts.m_conn_type))
+      {
+	return ps_conn_error_lambda (lockg_state);
+      }
 
-  set_connection (std::move (srv_chn));
+    set_connection (std::move (srv_chn));
+
+    er_log_debug (ARG_FILE_LINE, "Transaction server successfully connected to the page server. Channel id: %s.\n",
+		  srv_chn.get_channel_id ().c_str ());
+  }
 
   // Do the preliminary jobs depending on the server type before opening the conneciton to the outisde.
   on_connecting ();
 
-  m_state = state::CONNECTED;
+  auto lockg_state = std::lock_guard<std::shared_mutex> { m_state_mtx };
+  assert (m_state == state::CONNECTING);
 
-  er_log_debug (ARG_FILE_LINE, "Transaction server successfully connected to the page server. Channel id: %s.\n",
-		srv_chn.get_channel_id ().c_str ());
+  m_state = state::CONNECTED;
 
   return NO_ERROR;
 }
