e9c11ffa1ce1a12eba57598f0b4907e5021fad5e
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2023-08-07 16:18:12 +0900

    move system_classes array to separate file

--- src/executables/util_common.c
+++ src/executables/util_common.c
@@ -38,6 +38,7 @@
 #include "log_writer.h"
 #include "mprec.h"
 #include "system_parameter.h"
+#include "schema_system_class.hpp"
 #include "environment_variable.h"
 #include "heartbeat.h"
 #if defined (WINDOWS)
@@ -51,93 +52,6 @@ typedef enum
   EXISTING_DATABASE,
   NEW_DATABASE
 } DATABASE_NAME;
-
-typedef struct system_class_def SYSTEM_CLASS_DEF;
-struct system_class_def
-{
-  const char *name;
-  int len;
-};
-
-// *INDENT-OFF*
-static SYSTEM_CLASS_DEF system_classes[] = {
-  {"Rootclass", strlen ("Rootclass")},					// ROOTCLASS_NAME
-  {"dual", strlen ("dual")},						// CT_DUAL_NAME
-
-  /*
-   * authorization classes
-   *
-   * AU_ROOT_CLASS_NAME     = CT_ROOT_NAME
-   * AU_OLD_ROOT_CLASS_NAME = CT_AUTHORIZATIONS_NAME
-   * AU_USER_CLASS_NAME     = CT_USER_NAME
-   * AU_PASSWORD_CLASS_NAME = CT_PASSWORD_NAME
-   * AU_AUTH_CLASS_NAME     = CT_AUTHORIZATION_NAME
-   * AU_GRANT_CLASS_NAME
-   */
-  {"db_root", strlen ("db_root")},					// AU_ROOT_CLASS_NAME
-  {"db_user", strlen ("db_user")},					// AU_USER_CLASS_NAME
-  {"db_password", strlen ("db_password")},				// AU_PASSWORD_CLASS_NAME
-  {"db_authorization", strlen ("db_authorization")},			// AU_AUTH_CLASS_NAME
-  {"db_authorizations", strlen ("db_authorizations")},			// "db_authorizations"
-
-  /* currently, not implemented */
-  {"db_grant", strlen ("db_grant")},					// AU_GRANT_CLASS_NAME
-
-  /* 
-   * catalog classes
-   */
-  {"_db_class", strlen ("_db_class")},					// CT_CLASS_NAME
-  {"_db_attribute", strlen ("_db_attribute")}, 				// CT_ATTRIBUTE_NAME
-  {"_db_domain", strlen ("_db_domain")},				// CT_DOMAIN_NAME
-  {"_db_method", strlen ("_db_method")},				// CT_METHOD_NAME
-  {"_db_meth_sig", strlen ("_db_meth_sig")},				// CT_METHSIG_NAME
-  {"_db_meth_arg", strlen ("_db_meth_arg")},				// CT_METHARG_NAME
-  {"_db_meth_file", strlen ("_db_meth_file")},				// CT_METHFILE_NAME
-  {"_db_query_spec", strlen ("_db_query_spec")},			// CT_QUERYSPEC_NAME
-  {"_db_index", strlen ("_db_index")},					// CT_INDEX_NAME
-  {"_db_index_key", strlen ("_db_index_key")},				// CT_INDEXKEY_NAME
-  {"_db_data_type", strlen ("_db_data_type")},				// CT_DATATYPE_NAME
-  {"_db_auth", strlen ("_db_auth")},					// CT_CLASSAUTH_NAME
-  {"_db_partition", strlen ("_db_partition")},				// CT_PARTITION_NAME
-  {"_db_stored_procedure", strlen ("_db_stored_procedure")},		// CT_STORED_PROC_NAME
-  {"_db_stored_procedure_args", strlen ("_db_stored_procedure_args")},	// CT_STORED_PROC_ARGS_NAME
-  {"db_serial", strlen ("db_serial")},					// CT_SERIAL_NAME
-  {"db_ha_apply_info", strlen ("db_ha_apply_info")},			// CT_HA_APPLY_INFO_NAME
-  {"_db_collation", strlen ("_db_collation")},				// CT_COLLATION_NAME
-  {"_db_charset", strlen ("_db_charset")},				// CT_CHARSET_NAME
-  {"_db_server", strlen ("_db_server")},				// CT_DB_SERVER_NAME
-  {"_db_synonym", strlen ("_db_synonym")},				// CT_SYNONYM_NAME
-
-  {"db_trigger", strlen ("db_trigger")},				// CT_TRIGGER_NAME
-
-  /* currently, not implemented */
-  {"_db_resolution", strlen ("_db_resolution")},			// CT_RESOLUTION_NAME
-
-  /*
-   * catalog vclasses
-   */
-  {"db_class", strlen ("db_class")},					// CTV_CLASS_NAME
-  {"db_direct_super_class", strlen ("db_direct_super_class")},		// CTV_SUPER_CLASS_NAME
-  {"db_vclass", strlen ("db_vclass")},					// CTV_VCLASS_NAME
-  {"db_attribute", strlen ("db_attribute")},				// CTV_ATTRIBUTE_NAME
-  {"db_attr_setdomain_elm", strlen ("db_attr_setdomain_elm")},		// CTV_ATTR_SD_NAME
-  {"db_method", strlen ("db_method")},					// CTV_METHOD_NAME
-  {"db_meth_arg", strlen ("db_meth_arg")},				// CTV_METHARG_NAME
-  {"db_meth_arg_setdomain_elm", strlen ("db_meth_arg_setdomain_elm")},	// CTV_METHARG_SD_NAME
-  {"db_meth_file", strlen ("db_meth_file")},				// CTV_METHFILE_NAME
-  {"db_index", strlen ("db_index")},					// CTV_INDEX_NAME
-  {"db_index_key", strlen ("db_index_key")},				// CTV_INDEXKEY_NAME
-  {"db_auth", strlen ("db_auth")},					// CTV_AUTH_NAME
-  {"db_trig", strlen ("db_trig")},					// CTV_TRIGGER_NAME
-  {"db_partition", strlen ("db_partition")},				// CTV_PARTITION_NAME
-  {"db_stored_procedure", strlen ("db_stored_procedure")},		// CTV_STORED_PROC_NAME
-  {"db_stored_procedure_args", strlen ("db_stored_procedure_args")},	// CTV_STORED_PROC_ARGS_NAME
-  {"db_collation", strlen ("db_collation")},				// CTV_DB_COLLATION_NAME
-  {"db_charset", strlen ("db_charset")},				// CTV_DB_CHARSET_NAME
-  {"db_server", strlen ("db_server")},					// CTV_DB_SERVER_NAME
-  {"db_synonym", strlen ("db_synonym")}					// CTV_SYNONYM_NAME
-};
-// *INDENT-ON*
 
 static int utility_get_option_index (UTIL_ARG_MAP * arg_map, int arg_ch);
 static int check_database_name_local (const char *name, int existing_or_new_db);
@@ -1488,7 +1402,7 @@ bool
 utility_check_system_class_name (const char *class_name)
 {
   static int was_initialized = FALSE;
-  static int count = sizeof (system_classes) / sizeof (system_classes[0]);
+  static int count = get_system_classes_size ();
 
   SYSTEM_CLASS_DEF sa;
   int cmp = 0;
---
+++ src/object/schema_system_class.cpp
@@ -0,0 +1,113 @@
+/*
+ *
+ * Copyright 2016 CUBRID Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#include "schema_system_class.hpp"
+
+#include "authenticate.h"
+#include "oid.h"
+#include "transform.h"
+
+// *INDENT-OFF*
+static SYSTEM_CLASS_DEF system_classes[] = {
+  {ROOTCLASS_NAME, strlen (ROOTCLASS_NAME)},			// "Rootclass"
+  {CT_DUAL_NAME, strlen (CT_DUAL_NAME)},			// "dual"
+
+  /*
+   * authorization classes
+   *
+   * AU_ROOT_CLASS_NAME     = CT_ROOT_NAME
+   * AU_OLD_ROOT_CLASS_NAME = CT_AUTHORIZATIONS_NAME
+   * AU_USER_CLASS_NAME     = CT_USER_NAME
+   * AU_PASSWORD_CLASS_NAME = CT_PASSWORD_NAME
+   * AU_AUTH_CLASS_NAME     = CT_AUTHORIZATION_NAME
+   * AU_GRANT_CLASS_NAME
+   */
+  {AU_ROOT_CLASS_NAME, strlen (AU_ROOT_CLASS_NAME)},		// "db_root"
+  {AU_USER_CLASS_NAME, strlen (AU_USER_CLASS_NAME)},		// "db_user"
+  {AU_PASSWORD_CLASS_NAME, strlen (AU_PASSWORD_CLASS_NAME)},	// "db_password"
+  {AU_AUTH_CLASS_NAME, strlen (AU_AUTH_CLASS_NAME)},		// "db_authorization"
+  {AU_OLD_ROOT_CLASS_NAME, strlen (AU_OLD_ROOT_CLASS_NAME)},	// "db_authorizations"
+
+  /* currently, not implemented */
+  {AU_GRANT_CLASS_NAME, strlen (AU_GRANT_CLASS_NAME)},		// "db_grant"
+
+  /* 
+   * catalog classes
+   */
+  {CT_CLASS_NAME, strlen (CT_CLASS_NAME)},			// "_db_class"
+  {CT_ATTRIBUTE_NAME, strlen (CT_ATTRIBUTE_NAME)}, 		// "_db_attribute"
+  {CT_DOMAIN_NAME, strlen (CT_DOMAIN_NAME)},			// "_db_domain"
+  {CT_METHOD_NAME, strlen (CT_METHOD_NAME)},			// "_db_method"
+  {CT_METHSIG_NAME, strlen (CT_METHSIG_NAME)},			// "_db_meth_sig"
+  {CT_METHARG_NAME, strlen (CT_METHARG_NAME)},			// "_db_meth_arg"
+  {CT_METHFILE_NAME, strlen (CT_METHFILE_NAME)},		// "_db_meth_file"
+  {CT_QUERYSPEC_NAME, strlen (CT_QUERYSPEC_NAME)},		// "_db_query_spec"
+  {CT_INDEX_NAME, strlen (CT_INDEX_NAME)},			// "_db_index"
+  {CT_INDEXKEY_NAME, strlen (CT_INDEXKEY_NAME)},		// "_db_index_key"
+  {CT_DATATYPE_NAME, strlen (CT_DATATYPE_NAME)},		// "_db_data_type"
+  {CT_CLASSAUTH_NAME, strlen (CT_CLASSAUTH_NAME)},		// "_db_auth"
+  {CT_PARTITION_NAME, strlen (CT_PARTITION_NAME)},		// "_db_partition"
+  {CT_STORED_PROC_NAME, strlen (CT_STORED_PROC_NAME)},		// "_db_stored_procedure"
+  {CT_STORED_PROC_ARGS_NAME, strlen (CT_STORED_PROC_ARGS_NAME)},	// "_db_stored_procedure_args"
+  {CT_SERIAL_NAME, strlen (CT_SERIAL_NAME)},			// "db_serial"
+  {CT_HA_APPLY_INFO_NAME, strlen (CT_HA_APPLY_INFO_NAME)},	// "db_ha_apply_info"
+  {CT_COLLATION_NAME, strlen (CT_COLLATION_NAME)},		// "_db_collation"
+  {CT_CHARSET_NAME, strlen (CT_CHARSET_NAME)},			// "_db_charset"
+  {CT_DB_SERVER_NAME, strlen (CT_DB_SERVER_NAME)},		// "_db_server"
+  {CT_SYNONYM_NAME, strlen (CT_SYNONYM_NAME)},			// "_db_synonym"
+
+  {CT_TRIGGER_NAME, strlen (CT_TRIGGER_NAME)},			// "db_trigger"
+
+  /* currently, not implemented */
+  {CT_RESOLUTION_NAME, strlen (CT_RESOLUTION_NAME)},		// "_db_resolution"
+
+  /*
+   * catalog vclasses
+   */
+  {CTV_CLASS_NAME, strlen (CTV_CLASS_NAME)},			// "db_class"
+  {CTV_SUPER_CLASS_NAME, strlen (CTV_SUPER_CLASS_NAME)},	// "db_direct_super_class"
+  {CTV_VCLASS_NAME, strlen (CTV_VCLASS_NAME)},			// "db_vclass"
+  {CTV_ATTRIBUTE_NAME, strlen (CTV_ATTRIBUTE_NAME)},		// "db_attribute"
+  {CTV_ATTR_SD_NAME, strlen (CTV_ATTR_SD_NAME)},		// "db_attr_setdomain_elm"
+  {CTV_METHOD_NAME, strlen (CTV_METHOD_NAME)},			// "db_method"
+  {CTV_METHARG_NAME, strlen (CTV_METHARG_NAME)},		// "db_meth_arg"
+  {CTV_METHARG_SD_NAME, strlen (CTV_METHARG_SD_NAME)},		// "db_meth_arg_setdomain_elm"
+  {CTV_METHFILE_NAME, strlen (CTV_METHFILE_NAME)},		// "db_meth_file"
+  {CTV_INDEX_NAME, strlen (CTV_INDEX_NAME)},			// "db_index"
+  {CTV_INDEXKEY_NAME, strlen (CTV_INDEXKEY_NAME)},		// "db_index_key"
+  {CTV_AUTH_NAME, strlen (CTV_AUTH_NAME)},			// "db_auth"
+  {CTV_TRIGGER_NAME, strlen (CTV_TRIGGER_NAME)},		// "db_trig"
+  {CTV_PARTITION_NAME, strlen (CTV_PARTITION_NAME)},		// "db_partition"
+  {CTV_STORED_PROC_NAME, strlen (CTV_STORED_PROC_NAME)},	// "db_stored_procedure"
+  {CTV_STORED_PROC_ARGS_NAME, strlen (CTV_STORED_PROC_ARGS_NAME)},	// "db_stored_procedure_args"
+  {CTV_DB_COLLATION_NAME, strlen (CTV_DB_COLLATION_NAME)},	// "db_collation"
+  {CTV_DB_CHARSET_NAME, strlen (CTV_DB_CHARSET_NAME)},		// "db_charset"
+  {CTV_DB_SERVER_NAME, strlen (CTV_DB_SERVER_NAME)},		// "db_server"
+  {CTV_SYNONYM_NAME, strlen (CTV_SYNONYM_NAME)}			// "db_synonym"
+};
+// *INDENT-ON*
+
+SYSTEM_CLASS_DEF get_system_class (int idx)
+{
+  return system_classes [idx];
+}
+
+int get_system_classes_size ()
+{
+  return sizeof (system_classes) / sizeof (system_classes[0]);
+}
