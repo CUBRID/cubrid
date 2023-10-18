c21af764ae15fc6f74614dedda3fe96e1e135b17
Na, Hyunik <hyunikn@gmail.com>
2023-10-18 17:59:17 +0900

    (1) set default session timezone :09:00 (2) set default locale en_US for printing date, time values

--- pl_engine/pl_server/src/main/java/com/cubrid/plcsql/predefined/sp/SpLib.java
+++ pl_engine/pl_server/src/main/java/com/cubrid/plcsql/predefined/sp/SpLib.java
@@ -55,6 +55,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Stack;
+import java.util.Locale;
 import java.util.regex.PatternSyntaxException;
 
 public class SpLib {
