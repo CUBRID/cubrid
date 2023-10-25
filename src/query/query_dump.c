717e1556a094fa74335bbe7e922b15671c07ab6f
shparkcubrid <shpark@cubrid.com>
2023-10-25 16:06:20 +0900

    slip

--- src/query/query_dump.c
+++ src/query/query_dump.c
@@ -3219,7 +3219,7 @@ qdump_print_stats_text (FILE * fp, xasl_node * xasl_p, int indent)
     case BUILD_SCHEMA_PROC:
       fprintf (fp, "%s (time: %d, fetch: %lld, ioread: %lld\n", qdump_xasl_type_string (xasl_p),
 	       TO_MSEC (xasl_p->xasl_stats.elapsed_time), (long long int) xasl_p->xasl_stats.fetches,
-	       (long long int) xasl_p->xasl_stats.ioreads,);
+	       (long long int) xasl_p->xasl_stats.ioreads);
       indent += 2;
       break;
 
