2bca70b3eaf79b5be02b472909467c566724064b
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2023-11-21 14:46:26 +0800

    free symbols allocated by backtrace_symbol()

--- src/executables/javasp.cpp
+++ src/executables/javasp.cpp
@@ -361,7 +361,8 @@ static void javasp_signal_handler (int sig)
 	    }
 	}
       er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_SP_SERVER_CRASHED, 1, err_msg.c_str ());
-
+      free(symbols);
+      er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_SP_SERVER_CRASHED, 1, err_msg.c_str ());
       pid = fork ();
       if (pid == 0) // child
 	{
