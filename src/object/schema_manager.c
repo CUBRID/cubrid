e4003d06532ffcad6e29a52747ae08576755cb6a
chuntaek.shim <ctshim@cubrid.com>
2024-01-05 12:50:58 +0900

    remove assert

--- src/object/schema_manager.c
+++ src/object/schema_manager.c
@@ -10127,9 +10127,6 @@ fixup_self_domain (TP_DOMAIN * domain, MOP self)
   for (d = domain; d != NULL; d = d->next)
     {
       /* PR_TYPE is changeable only for transient domain. */
-      // assert (d->type != tp_Type_null || !d->is_cached); assert ( !(d->type == tp_Type_null && d->is_cached));
-      //assert (d->type != tp_Type_null || (d->type == tp_Type_null && d->is_cached));
-      assert ((d->type != tp_Type_null && !d->is_cached) || (d->type == tp_Type_null && d->is_cached));
       if (d->type == tp_Type_null && !d->is_cached)
 	{
 	  d->type = tp_Type_object;
