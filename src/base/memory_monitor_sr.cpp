2e678d8f4ce129362b000df6dbedd02bcc1fda03
Rudeus <hyunsoo_cho@cubrid.com>
2024-01-29 09:30:52 +0900

    Add internal functions of memory_monitor class

---
+++ src/base/memory_monitor_sr.cpp
@@ -0,0 +1,63 @@
+/*
+ *
+ * Copyright 2016 CUBRID Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+/*
+ * memory_monitor_sr.cpp - Implementation of memory monitor module
+ */
+
+#include <cstring>
+#include <algorithm>
+
+#include "openssl/md5.h"
+#include "memory_monitor_sr.hpp"
+
+namespace cubmem
+{
+  memory_monitor::memory_monitor (const char *server_name)
+    : m_server_name {server_name} {}
+
+  std::string memory_monitor::make_tag_name (const char *file, const int line)
+  {
+    std::string filecopy (file);
+    std::string ret;
+
+    // Find the last occurrence of "src" in the path
+    size_t pos = filecopy.rfind ("src");
+
+    if (pos != std::string::npos)
+      {
+        filecopy = filecopy.substr (pos);
+      }
+
+    ret = filecopy + ':' + std::to_string (line);
+    return ret;
+  }
+
+  int memory_monitor::generate_checksum (int tag_id, uint64_t size)
+  {
+    char input[32]; // INT_MAX digits 10 +  ULLONG_MAX digits 20
+    unsigned char digest[MD5_DIGEST_LENGTH];
+    int ret;
+
+    memset (input, 0, sizeof (input));
+    memset (digest, 0, sizeof (digest));
+    sprintf (input, "%d%lu", tag_id, size);
+    (void) MD5 (reinterpret_cast<const unsigned char *> (input), strlen (input), digest);
+    memcpy (&ret, digest, sizeof (int));
+    return ret;
+  }
---
+++ src/base/memory_monitor_sr.hpp
@@ -0,0 +1,46 @@
+/*
+ *
+ * Copyright 2016 CUBRID Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+/*
+ * memory_monitor_sr.hpp - Declaration of APIs and structures, classes
+ *                         for memory monitoring module
+ */
+
+#ifndef _MEMORY_MONITOR_SR_HPP_
+#define _MEMORY_MONITOR_SR_HPP_
+
+#include <string>
+
+namespace cubmem
+{
+  class memory_monitor
+  {
+    public:
+      memory_monitor (const char *server_name);
+      ~memory_monitor () {}
+
+    private:
+      std::string make_tag_name (const char *file, const int line);
+      int generate_checksum (int tag_id, uint64_t size);
+
+    private:
+      std::string m_server_name;
+  };
+} //namespace cubmem
+
+#endif // _MEMORY_MONITOR_SR_HPP_
