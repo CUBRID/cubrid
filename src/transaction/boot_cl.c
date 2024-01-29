118a72cb30eec0a76accb202dde88899c57ddf3b
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2024-01-29 15:34:03 +0900

    move sp_builtin_install ()

--- src/transaction/boot_cl.c
+++ src/transaction/boot_cl.c
@@ -565,11 +565,11 @@ boot_initialize_client (BOOT_CLIENT_CREDENTIAL * client_credential, BOOT_DB_PATH
 		  sm_mark_system_classes ();
 		  error_code = tran_commit (false);
 		}
-	    }
 
-	  if (error_code == NO_ERROR)
-	    {
-	      error_code = sp_builtin_install ();
+	      if (error_code == NO_ERROR)
+		{
+		  error_code = sp_builtin_install ();
+		}
 	    }
 	}
     }
