25d8d08139ed39a1363eeff43e14f9738bf7b5bc
Byungwook Kim <bwkim@cubrid.com>
2024-01-29 16:51:18 +0900

    [CBRD-25195] A core dump occurs when multiple tables and serial functions are used simultaneously in one query. (#4897)
    http://jira.cubrid.org/browse/CBRD-25195

--- src/parser/xasl_generation.c
+++ src/parser/xasl_generation.c
@@ -17999,6 +17999,11 @@ error:
       free_and_init (*oid_listp);
     }
 
+  if (*lock_listp)
+    {
+      free_and_init (*lock_listp);
+    }
+
   if (*tcard_listp)
     {
       free_and_init (*tcard_listp);
@@ -18142,6 +18147,7 @@ pt_serial_to_xasl_class_oid_list (PARSER_CONTEXT * parser, const PT_NODE * seria
   *nump = o_num;
   *sizep = o_size;
   *oid_listp = o_list;
+  *lock_listp = lck_list;
   *tcard_listp = t_list;
 
   return o_num;
@@ -18150,6 +18156,11 @@ error:
   if (*oid_listp)
     {
       free_and_init (*oid_listp);
+    }
+
+  if (*lock_listp)
+    {
+      free_and_init (*lock_listp);
     }
 
   if (*tcard_listp)
