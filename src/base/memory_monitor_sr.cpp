623850bc0648cc4c671e0acd315a0be41a2ac4da
Rudeus <hyunsoo_cho@cubrid.com>
2024-01-30 15:26:46 +0900

    [CBRD-25201] Add 'memory_monitor' class with implementing internal functions of add/sub_stat() (#4905)
    http://jira.cubrid.org/browse/CBRD-25201
    
    Add 'memory_monitor' class with implementing internal function of add/sub_stat()
    
     - Create memory_monitor class
     - add m_server_name for registering server name to memory_monitor when cub_server starts.
     - Implement internal functions 'make_tag_name()'.

---
+++ src/base/memory_monitor_sr.cpp
@@ -0,0 +1,53 @@
+/*
+ *
+ * Copyright 2016 CUBRID Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+/*
+ * memory_monitor_sr.cpp - Implementation of memory monitor module
+ */
+
+#include <cstring>
+#include <algorithm>
+#include <cassert>
+
+#include "memory_monitor_sr.hpp"
+
+namespace cubmem
+{
+  memory_monitor::memory_monitor (const char *server_name)
+    : m_server_name {server_name} {}
+
+  static std::string memory_monitor::make_tag_name (const char *file, const int line)
+  {
+    std::string filecopy (file);
+    std::string target ("/src/");
+
+    // Find the last occurrence of "src" in the path
+    size_t pos = filecopy.rfind (target);
+#if !defined (NDEBUG)
+    size_t lpos = filecopy.find (target);
+    assert (pos == lpos);
+#endif // NDEBUG
+
+    if (pos != std::string::npos)
+      {
+	filecopy = filecopy.substr (pos + target.length ());
+      }
+
+    return filecopy + ':' + std::to_string (line);
+  }
+}
