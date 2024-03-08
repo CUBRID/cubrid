987d3d535a982bd86bbaa2ba28697affb53ea778
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2024-03-08 15:04:21 +0800

    [CBRD-25248] Server crash for Java SPs running a SELECT statement with 'WHERE 0 <> 0' clause (#5017)
    http://jira.cubrid.org/browse/CBRD-25248
    
    For false query such as 'SELECT * FROM db_class WHERE 0 <> 0', dummy query id is set.
    I've changed to set the query_id as NULL_QUERY_ID if a query is not executed (tuple_count <= 0).

--- src/method/method_query_handler.cpp
+++ src/method/method_query_handler.cpp
@@ -523,7 +523,14 @@ namespace cubmethod
 
     if (qres && qres->type == T_SELECT)
       {
-	result_info.query_id = qres->res.s.query_id;
+	if (qresult.tuple_count > 0)
+	  {
+	    result_info.query_id = qres->res.s.query_id;
+	  }
+	else
+	  {
+	    result_info.query_id = NULL_QUERY_ID; // initialized value
+	  }
       }
     return error;
   }
