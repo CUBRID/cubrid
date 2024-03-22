d39f1407ca878d6ccab9362da0e2edf20508c605
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2024-03-22 17:14:22 +0900

    slip

--- src/object/authenticate.c
+++ src/object/authenticate.c
@@ -3874,7 +3874,7 @@ check_grant_option (MOP classop, SM_CLASS * sm_class, DB_AUTH type)
    * this potentially can be called during initialization when we don't
    * actually have any users yet.  If so, assume its ok
    */
-  if (au_get_cache_index ())
+  if (au_get_cache_index () < 0)
     {
       return NO_ERROR;
     }
