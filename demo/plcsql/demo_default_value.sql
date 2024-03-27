3002b21bcaf4bc831a3a1e6bbddd15a7230b80bc
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2024-03-27 16:09:20 +0900

    add demo code

--- demo/plcsql/demo_default_value.sql
+++ demo/plcsql/demo_default_value.sql
@@ -1,9 +1,12 @@
+-- all arguments
 create or replace procedure demo_default_value (a int := 1) as
 begin
     DBMS_OUTPUT.put_line(a);
 end;
 
--- all arguments
+CALL demo_default_value (3);
+CALL demo_default_value ();
+
 create or replace procedure demo_default_value2 (
         a varchar := 'a', 
         b varchar := 'b'
@@ -12,6 +15,10 @@ begin
     DBMS_OUTPUT.put_line(a);
     DBMS_OUTPUT.put_line(b);
 end;
+
+CALL demo_default_value2 ();
+CALL demo_default_value2 ('c');
+CALL demo_default_value2 ('c', 'd');
 
 -- trailing arguments
 create or replace procedure demo_default_value3 (
@@ -23,6 +30,9 @@ begin
     DBMS_OUTPUT.put_line(b);
 end;
 
+CALL demo_default_value3 ('k');
+CALL demo_default_value3 ('j', 'c');
+
 -- expression
 create or replace procedure demo_default_value7 (
         a varchar := TO_CHAR(12345,'S999999')
@@ -31,8 +41,24 @@ begin
     DBMS_OUTPUT.put_line(a);
 end;
 
+CALL demo_default_value7 ();
+CALL demo_default_value7 ('cubrid');
 
--- Error
+-- null args
+create or replace procedure demo_default_value8 (
+        a varchar, 
+        b varchar := NULL
+) as
+begin
+    DBMS_OUTPUT.put_line(a);
+    DBMS_OUTPUT.put_line(b);
+end;
+
+CALL demo_default_value8 ('a');
+CALL demo_default_value8 ('a', NULL);
+CALL demo_default_value8 ('a', 'b');
+
+-- Error 1) no trailing arguments
 create or replace procedure demo_default_value4 (
         a varchar := 'a', 
         b varchar
@@ -42,18 +68,23 @@ begin
     DBMS_OUTPUT.put_line(b);
 end;
 
+CALL demo_default_value4 ('c');
+CALL demo_default_value4 ('c', 'd');
+
+-- Error) not same type
 create or replace procedure demo_default_value5 (
-        a varchar := 1, 
-        b varchar
+        a varchar, 
+        b varchar := 1
 ) as
 begin
     DBMS_OUTPUT.put_line(a);
     DBMS_OUTPUT.put_line(b);
 end;
 
+-- Error) type incompatbile
 create or replace procedure demo_default_value6 (
-        a varchar := 1, 
-        b varchar
+        a varchar, 
+        b integer := 'a'
 ) as
 begin
     DBMS_OUTPUT.put_line(a);
