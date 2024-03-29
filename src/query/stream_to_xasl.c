8d6cafd0c1fac49fd10f73c4205c27d99726182d
xmilex-git <xmilex@naver.com>
2024-03-29 13:11:52 +0900

    redundant initialization

--- src/query/stream_to_xasl.c
+++ src/query/stream_to_xasl.c
@@ -242,7 +242,6 @@ stx_map_stream_to_xasl (THREAD_ENTRY * thread_p, xasl_node ** xasl_tree, bool us
   xasl->class_oid_list = NULL;
   xasl->class_locks = NULL;
   xasl->tcard_list = NULL;
-  xasl->sq_cache_flag = 0;
 
   /* initialize the query in progress flag to FALSE.  Note that this flag is not packed/unpacked.  It is strictly a
    * server side flag. */
