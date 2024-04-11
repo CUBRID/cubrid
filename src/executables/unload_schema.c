1b9054bba7dec55aeb942f615998f9d73656111a
airnet73 <61675061+airnet73@users.noreply.github.com>
2024-04-12 08:28:26 +0900

    [CBRD-25300] When unloaddb, the comment of reverse unique index is mi… (#5121)
    http://jira.cubrid.org/browse/CBRD-25300
    
    When unloaddb, the comment of reverse unique index is missing
    Added comment to reverse unique

--- src/executables/unload_schema.c
+++ src/executables/unload_schema.c
@@ -3295,9 +3295,17 @@ emit_reverse_unique_def (extract_context & ctxt, print_output & output_ctx, DB_O
 
 	      // reverse unique does not care for direction of the column.
 	    }
-	  output_ctx (");\n");
+
+	  output_ctx (")");
+
+	  if (constraint->comment != NULL && constraint->comment[0] != '\0')
+	    {
+	      output_ctx (" ");
+	      help_print_describe_comment (output_ctx, constraint->comment);
+	    }
 	}
     }
+  output_ctx (";\n");
 }
 
 
@@ -5572,6 +5580,7 @@ emit_unique_key (extract_context & ctxt, print_output & output_ctx, DB_OBJLIST *
 
       if (reverse_unique_flag)
 	{
+	  output_ctx ("\n");
 	  emit_reverse_unique_def (ctxt, output_ctx, cl->op);
 	}
 
