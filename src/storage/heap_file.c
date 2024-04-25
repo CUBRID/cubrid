67eb3667ac50f1406023d84f8d2b4add9eaf89e5
yjlee <yjlee@cubrid.com>
2024-04-25 18:33:28 +0900

    add assertion to check HFID exists

--- src/storage/heap_file.c
+++ src/storage/heap_file.c
@@ -14534,10 +14534,11 @@ heap_dump_heap_file (THREAD_ENTRY * thread_p, FILE * fp, bool dump_records, cons
   error_code = heap_hfid_cache_get (thread_p, &class_oid, &hfid, NULL, NULL);
   if (error_code != NO_ERROR)
     {
+      ASSERT_ERROR ();
       return;
     }
-  heap_dump (thread_p, fp, &hfid, dump_records);
 
+  heap_dump (thread_p, fp, &hfid, dump_records);
 }
 #endif
 
