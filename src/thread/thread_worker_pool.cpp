dd10c26a7256701370f111e1823fdf3f1e253b79
Rudeus <danniss@naver.com>
2024-05-09 02:55:26 +0900

    Update src/thread/thread_worker_pool.cpp

--- src/thread/thread_worker_pool.cpp
+++ src/thread/thread_worker_pool.cpp
@@ -61,7 +61,7 @@ namespace cubthread
     cubperf::stat_definition (Wpstat_retire_task, cubperf::stat_definition::COUNTER_AND_TIMER,
 			      "Counter_retire_task", "Timer_retire_task"),
     cubperf::stat_definition (Wpstat_found_in_queue, cubperf::stat_definition::COUNTER_AND_TIMER,
-    "Counter_found_task_in_queue", "Timer_found_task_in_queue"),
+			      "Counter_found_task_in_queue", "Timer_found_task_in_queue"),
     cubperf::stat_definition (Wpstat_wakeup_with_task, cubperf::stat_definition::COUNTER_AND_TIMER,
     "Counter_wakeup_with_task", "Timer_wakeup_with_task"),
     cubperf::stat_definition (Wpstat_recycle_context, cubperf::stat_definition::COUNTER_AND_TIMER,
