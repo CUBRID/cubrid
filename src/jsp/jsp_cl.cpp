f78bea821b781a7ca4e6ad0230d88aa9e815823e
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2024-05-13 14:32:59 +0900

    slip

--- src/jsp/jsp_cl.cpp
+++ src/jsp/jsp_cl.cpp
@@ -1148,7 +1148,7 @@ drop_stored_procedure (const char *name, SP_TYPE_ENUM expected_type)
 	}
 
       target = db_get_string (&target_val);
-      class_name = get_class_name (target);
+      class_name = get_class_name (std::string (target));
 
       err = drop_stored_procedure_code (class_name.c_str ());
       if (err != NO_ERROR)
