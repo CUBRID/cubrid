6b57c4f048e1b9de91aedefef85b1a45a6022450
Kisoo Han <kisoo-han@cubrid.com>
2024-05-21 10:55:41 +0900

    [CBRD-25369] check buf_size, it is valid for 16K, 32K, 48K and 64K

--- src/broker/broker_config.c
+++ src/broker/broker_config.c
@@ -123,6 +123,7 @@ static void read_conf_cache (int cid, bool * acl, int *num_br, int *shm_id, char
 static void write_conf_cache (char *file, bool * acl_flag, int *num_broker, int *shm_id, char *alog,
 			      T_BROKER_INFO * br_info, time_t bf_mtime);
 static void clear_conf_cache_entry (int cid);
+static bool is_invalid_buf_size (int size);
 
 static T_CONF_TABLE tbl_appl_server[] = {
   {APPL_SERVER_CAS_TYPE_NAME, APPL_SERVER_CAS},
@@ -669,7 +670,7 @@ broker_config_read_internal (const char *conf_file, T_BROKER_INFO * br_info, int
       strncpy_bufsize (size_str, s);
       br_info[num_brs].net_buf_size = (int) ut_size_string_to_kbyte (size_str, "K");
 
-      if (br_info[num_brs].net_buf_size < 0)
+      if (is_invalid_buf_size (br_info[num_brs].net_buf_size))
 	{
 	  errcode = PARAM_BAD_RANGE;
 	  goto conf_error;
@@ -1844,4 +1845,25 @@ int
 conf_get_value_proxy_log_mode (const char *value)
 {
   return (get_conf_value (value, tbl_proxy_log_mode));
+}
+
+static bool
+is_invalid_buf_size (int size)
+{
+  bool ret = false;
+
+  // The unit of size is KB.
+  switch (size)
+  {
+    case 16:
+    case 32:
+    case 48:
+    case 64:
+      break;
+    default:
+      ret = true;
+      break;
+  }
+
+  return ret;
 }
