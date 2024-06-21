ff177adc96fc3e4c6ad80c79eda4dc49fa14deff
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2024-06-21 17:38:44 +0900

    slip: license, make header idempotent

--- src/jsp/sp_constants.hpp
+++ src/jsp/sp_constants.hpp
@@ -1,3 +1,23 @@
+/*
+ *
+ * Copyright 2016 CUBRID Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#ifndef _SP_CONSTASNTS_HPP_
+#define _SP_CONSTASNTS_HPP_
 
 #define SP_CLASS_NAME           "_db_stored_procedure"
 #define SP_ARG_CLASS_NAME       "_db_stored_procedure_args"
@@ -50,3 +70,5 @@ typedef enum
   SP_LANG_PLCSQL = 0,
   SP_LANG_JAVA = 1
 } SP_LANG_ENUM;
+
+#endif // _SP_CONSTASNTS_HPP_
