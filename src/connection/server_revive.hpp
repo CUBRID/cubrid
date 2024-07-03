dc02a08e5989dfb47b6bee2f0b85c53651708709
yjlee <yjlee@cubrid.com>
2024-07-03 18:19:33 +0900

    modify licence

--- src/connection/server_revive.hpp
+++ src/connection/server_revive.hpp
@@ -1,4 +1,5 @@
 /*
+ *
  * Copyright 2016 CUBRID Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +15,6 @@
  *  limitations under the License.
  *
  */
-
 
 /*
  * server_revive.hpp -
