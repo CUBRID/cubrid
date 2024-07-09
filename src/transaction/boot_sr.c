c4ced00dcd77134adfae390f77b737f41022c6f6
joohok <joohokim@cubrid.com>
2024-07-09 20:02:25 +0900

    Apply code review

--- src/transaction/boot_sr.c
+++ src/transaction/boot_sr.c
@@ -146,7 +146,9 @@ extern void boot_client_all_finalize (bool is_er_final);
 
 BOOT_SERVER_STATUS boot_Server_status = BOOT_SERVER_DOWN;
 
+#if defined(SERVER_MODE)
 bool boot_Enabled_flush_daemons = false;
+#endif /* SERVER_MODE */
 
 #if defined(SERVER_MODE)
 /* boot_cl.c:boot_Host_name[] if CS_MODE and SA_MODE */
@@ -2481,8 +2483,8 @@ boot_restart_server (THREAD_ENTRY * thread_p, bool print_restart, const char *db
     }
 
 #if defined(SERVER_MODE)
-  dwb_daemons_init ();
   pgbuf_daemons_init ();
+  dwb_daemons_init ();
 #endif /* SERVER_MODE */
 
   /*
@@ -2500,6 +2502,7 @@ boot_restart_server (THREAD_ENTRY * thread_p, bool print_restart, const char *db
 
 #if defined(SERVER_MODE)
   BO_ENABLE_FLUSH_DAEMONS ();
+
   cdc_daemons_init ();
 #endif /* SERVER_MODE */
 
@@ -2814,9 +2817,9 @@ error:
 #if defined(SERVER_MODE)
   cdc_daemons_destroy ();
 
-  assert (BO_IS_FLUSH_DAEMON_AVAILABLE ());
-  dwb_daemons_destroy ();
+  BO_DISABLE_FLUSH_DAEMONS ();
   pgbuf_daemons_destroy ();
+  dwb_daemons_destroy ();
 #endif
 
   log_final (thread_p);
