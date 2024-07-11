aca51936978d373715ec4430488672abfbd633a8
jongmin-won <kceeck@gamil.com>
2024-07-11 16:28:30 +0900

    name_resolution 파일에 오타 수정

--- src/parser/name_resolution.c
+++ src/parser/name_resolution.c
@@ -3440,18 +3440,8 @@ pt_bind_names (PARSER_CONTEXT * parser, PT_NODE * node, void *arg, int *continue
     case PT_FUNCTION:
       if (node->info.function.function_type == PT_GENERIC)
 	{
-	  const char *dot = NULL;
 	  const char *current_schema_name = NULL;
 	  char buffer[SM_MAX_IDENTIFIER_LENGTH];
-
-	  dot = strchr (node->info.function.generic_name, '.');
-	  if (dot == NULL)
-	    {
-	      current_schema_name = sc_current_schema_name ();
-	      sprintf (buffer, "%s.%s", current_schema_name, node->info.function.generic_name);
-	      node->info.function.generic_name = strdup (buffer);
-	    }
-
 	  node->info.function.function_type = pt_find_function_type (node->info.function.generic_name);
 
 	  if (node->info.function.function_type == PT_GENERIC)
@@ -3463,7 +3453,10 @@ pt_bind_names (PARSER_CONTEXT * parser, PT_NODE * node, void *arg, int *continue
 	       */
 	      if (jsp_is_exist_stored_procedure (node->info.function.generic_name))
 		{
-		  node->info.function.generic_name = strdup (node->info.function.generic_name);
+		  current_schema_name = sc_current_schema_name ();
+		  sprintf (buffer, "%s.%s", current_schema_name, node->info.function.generic_name);
+		  node->info.function.generic_name = strdup (buffer);
+
 		  node1 = pt_resolve_stored_procedure (parser, node, bind_arg);
 		}
 	      else
