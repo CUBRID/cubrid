4acddd0a762a3440684493fc6310ef5344af621e
vimkim <kimdhyungg@gmail.com>
2024-07-22 14:38:21 +0900

    fix: att_name in emit_class_alter_serial query

--- src/executables/unload_schema.c
+++ src/executables/unload_schema.c
@@ -904,12 +904,12 @@ emit_class_alter_serial (extract_context & ctxt, print_output & output_ctx)
   const char *query_all =
     "select [unique_name], [name], [owner].[name], [current_val], [increment_val], [max_val], [min_val], "
     "[cyclic], [started], [cached_num], [class_name], [comment] "
-    "from [db_serial] where [class_name] is not null and [att_name] is not null";
+    "from [db_serial] where [class_name] is not null and [attr_name] is not null";
 
   const char *query_user =
     "select [unique_name], [name], [owner].[name], [class_name], [current_val], [increment_val], [max_val], [min_val], "
     "[cyclic], [started], [cached_num], [class_name], [comment] "
-    "from [db_serial] where [class_name] is not null and [att_name] is not null and owner.name='%s'";
+    "from [db_serial] where [class_name] is not null and [attr_name] is not null and owner.name='%s'";
 
   if (ctxt.is_dba_user == false && ctxt.is_dba_group_member == false)
     {
