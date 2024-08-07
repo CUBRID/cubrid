d9866de67aa25ae495d1ea58b21c6ad85d4317e3
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2024-08-07 17:27:49 +0900

    [CBRD-25498] Pass SP's owner to PL Compiler when creating PL/CSQL (#5382)
    http://jira.cubrid.org/browse/CBRD-25498
    
    To handle CREATE PROCEDURE with [user name].[sp_name] properly, the owner name should be passed to PL compiler to know which user is going to have to own the procedure.

--- src/method/method_compile_def.cpp
+++ src/method/method_compile_def.cpp
@@ -29,7 +29,40 @@ namespace cubmethod
 //////////////////////////////////////////////////////////////////////////
 // compile info
 //////////////////////////////////////////////////////////////////////////
-  compile_info::compile_info ()
+
+#define COMPILE_REQUEST_PACKER_ARGS() \
+  code, owner, mode
+
+  compile_request::compile_request ()
+    : code {}
+    , owner {}
+    , mode {}
+  {
+    //
+  }
+
+  void
+  compile_request::pack (cubpacking::packer &serializator) const
+  {
+    serializator.pack_all (COMPILE_REQUEST_PACKER_ARGS ());
+  }
+
+  size_t
+  compile_request::get_packed_size (cubpacking::packer &serializator, std::size_t start_offset) const
+  {
+    return serializator.get_all_packed_size_starting_offset (start_offset,
+	   COMPILE_REQUEST_PACKER_ARGS ());
+  }
+
+  void
+  compile_request::unpack (cubpacking::unpacker &deserializator)
+  {
+    deserializator.unpack_all (COMPILE_REQUEST_PACKER_ARGS ());
+  }
+
+//
+
+  compile_response::compile_response ()
     : err_code (-1)
     , err_line (0)
     , err_column (0)
@@ -38,7 +71,7 @@ namespace cubmethod
   }
 
   void
-  compile_info::pack (cubpacking::packer &serializator) const
+  compile_response::pack (cubpacking::packer &serializator) const
   {
     serializator.pack_int (err_code);
     if (err_code < 0)
@@ -63,7 +96,7 @@ namespace cubmethod
   }
 
   size_t
-  compile_info::get_packed_size (cubpacking::packer &serializator, std::size_t start_offset) const
+  compile_response::get_packed_size (cubpacking::packer &serializator, std::size_t start_offset) const
   {
     size_t size = serializator.get_packed_int_size (start_offset); // err_code
 
@@ -91,7 +124,7 @@ namespace cubmethod
   }
 
   void
-  compile_info::unpack (cubpacking::unpacker &deserializator)
+  compile_response::unpack (cubpacking::unpacker &deserializator)
   {
     deserializator.unpack_int (err_code);
     if (err_code < 0)
