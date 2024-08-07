626ea5ab5ea775d1a2fbc769c3d23e7678b6297d
yahong <yechan0815@naver.com>
2024-08-07 23:40:07 +0900

    [CBRD-25502] the page must be unfixed before the function heap_page_prev ends.

--- src/storage/heap_file.c
+++ src/storage/heap_file.c
@@ -18758,6 +18758,7 @@ heap_page_prev (THREAD_ENTRY * thread_p, const OID * class_oid, const HFID * hfi
       if (OID_ISNULL (prev_vpid))
 	{
 	  /* no more pages to scan */
+	  pgbuf_ordered_unfix (thread_p, &pg_watcher);
 	  return S_END;
 	}
       /* get next page */
@@ -18772,7 +18773,6 @@ heap_page_prev (THREAD_ENTRY * thread_p, const OID * class_oid, const HFID * hfi
     }
   if (pg_watcher.pgptr == NULL)
     {
-      pgbuf_ordered_unfix (thread_p, &pg_watcher);
       return S_ERROR;
     }
 
