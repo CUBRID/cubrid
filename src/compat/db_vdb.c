ef00f398dcdeba0be6ad667064b2002d62737b54
Byungwook-Kim <bwkim@cubrid.com>
2024-08-14 14:34:35 +0900

    slip

--- src/compat/db_vdb.c
+++ src/compat/db_vdb.c
@@ -1910,8 +1910,7 @@ db_execute_and_keep_statement_local (DB_SESSION * session, int stmt_ndx, DB_QUER
 	    }
 
 	  cls_status = pt_has_modified_class (parser, statement);
-	  if (err == ER_QPROC_RESULT_CACHE_INVALID
-	      || (cls_status == DB_CLASS_NOT_MODIFIED)
+	  if (err == ER_QPROC_RESULT_CACHE_INVALID || cls_status == DB_CLASS_NOT_MODIFIED)
 	    {
 	      /* forget all errors */
 	      er_clear ();
