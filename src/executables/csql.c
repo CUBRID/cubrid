c8c06411ce5f184752a22bfb9d2e2f9e49c6ba0d
Na, Hyunik <hyunikn@gmail.com>
2024-08-26 13:30:10 +0900

    code style

--- src/executables/csql.c
+++ src/executables/csql.c
@@ -1860,11 +1860,12 @@ csql_print_server_output (const CSQL_ARGUMENT * csql_arg)
 	      DB_VALUE *str_val = pt_find_value_of_label ("pl_output_str");
 	      if (str_val)
 		{
-                  if (print_header) {
-                        print_header = false;
-                        fprintf(csql_Output_fp, "<DBMS_OUTPUT>\n");
-                        fprintf(csql_Output_fp, "====\n");
-                  }
+		  if (print_header)
+		    {
+		      print_header = false;
+		      fprintf (csql_Output_fp, "<DBMS_OUTPUT>\n");
+		      fprintf (csql_Output_fp, "====\n");
+		    }
 		  const char *str = db_get_string (str_val);
 		  fprintf (csql_Output_fp, "%s\n", str);
 		}
@@ -1881,10 +1882,11 @@ csql_print_server_output (const CSQL_ARGUMENT * csql_arg)
     }
   while (status == 0);
 
-  if (!print_header) {
-        // print_header is false if there was at least one server-output line.
-        fprintf(csql_Output_fp, "\n");
-  }
+  if (!print_header)
+    {
+      // print_header is false if there was at least one server-output line.
+      fprintf (csql_Output_fp, "\n");
+    }
 }
 
 /*
