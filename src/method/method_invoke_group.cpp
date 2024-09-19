311d40ce7ac5c9ef7e0f1ff218cd10d4f7033b37
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2024-09-19 15:58:56 +0900

    [CBRD-25586] Fix the problem that query entries with no result sets are not freed (#5489) (#5490)
    http://jira.cubrid.org/browse/CBRD-25586
    
    backport of #5489

--- src/method/method_invoke_group.cpp
+++ src/method/method_invoke_group.cpp
@@ -426,6 +426,7 @@ namespace cubmethod
     if (query_id == NULL_QUERY_ID || query_id >= SHRT_MAX)
       {
 	// false query e.g) SELECT * FROM db_class WHERE 0 <> 0
+	assert (query_id == NULL_QUERY_ID);
 	return nullptr;
       }
 
