22959e3bf51540c02b49f9544f8ec0ffb85a57aa
InChijun <chijun@cubrid.com>
2024-11-08 16:44:15 +0900

    [CBRD-25541] Fix: Reverted changes to code on line 2777

--- src/connection/connection_support.c
+++ src/connection/connection_support.c
@@ -2774,7 +2774,7 @@ css_make_access_status_exist_user (THREAD_ENTRY * thread_p, OID * class_oid, LAS
 
   while (true)
     {
-      scan = heap_next (thread_p, &hfid, class_oid, &inst_oid, &recdes, &scan_cache, PEEK);
+      scan = heap_next (thread_p, &hfid, NULL, &inst_oid, &recdes, &scan_cache, PEEK);
       if (scan == S_SUCCESS)
 	{
 	  error = heap_attrinfo_read_dbvalues (thread_p, &inst_oid, &recdes, &attr_info);
