e324f5a91d90c0c446a5901acbf58b1628a44635
chuntaek.shim <ctshim@cubrid.com>
2024-12-02 15:09:51 +0900

    code-style

--- src/parser/parse_tree_cl.c
+++ src/parser/parse_tree_cl.c
@@ -942,13 +942,13 @@ pt_walk_private (PARSER_CONTEXT * parser, PT_NODE * node, void *void_arg)
 	  /* this is an optimization to remove a procedure call per node from the recursion path. It is the same as
 	   * calling pt_apply. */
 	  node_type = node->node_type;
- 
-          assert (node_type >= PT_NODE_NONE);
-          if (node_type == PT_NODE_NONE)
+
+	  assert (node_type >= PT_NODE_NONE);
+	  if (node_type == PT_NODE_NONE)
 	    {
-              assert (pt_has_error (parser));
+	      assert (pt_has_error (parser));
 	      return NULL;
-	    }	  
+	    }
 
 	  if (node_type >= PT_LAST_NODE_NUMBER || !(apply = pt_apply_f[node_type]))
 	    {
