228da2494f04d9f64fc3564127570c09456fa022
chuntaek.shim <ctshim@cubrid.com>
2024-12-05 17:03:07 +0900

    indent

--- src/compat/db_vdb.c
+++ src/compat/db_vdb.c
@@ -3026,7 +3026,6 @@ do_recompile_and_execute_prepared_statement (DB_SESSION * session, PT_NODE * sta
 
   new_session->parser->flag.is_holdable = session->parser->flag.is_holdable;
   new_session->parser->flag.is_auto_commit = session->parser->flag.is_auto_commit;
-
   return db_execute_and_keep_statement_local (new_session, 1, result);
 }
 
