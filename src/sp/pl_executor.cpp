4d9ea44e9a1d3f6ec760f5a7c4407ce5aeb6ea55
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2024-12-16 17:25:30 +0900

    slip

--- src/sp/pl_executor.cpp
+++ src/sp/pl_executor.cpp
@@ -337,7 +337,7 @@ exit:
     prepare_args prepare_arg ((std::uint64_t) this, tid, METHOD_TYPE_PLCSQL, m_args);
     invoke_java invoke_arg ((std::uint64_t) this, tid, &m_sig, prm_get_bool_value (PRM_ID_PL_TRANSACTION_CONTROL));
 
-    error = m_stack->send_data_to_java (prepare_arg, session_params, invoke_arg);
+    error = m_stack->send_data_to_java (session_params, prepare_arg, invoke_arg);
     return error;
   }
 
