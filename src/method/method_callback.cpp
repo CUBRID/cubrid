11d6e62aea754fccc4123f0537422928efc2fcbb
Na, Hyunik <hyunikn@gmail.com>
2024-12-30 13:13:59 +0900

    typo

--- src/method/method_callback.cpp
+++ src/method/method_callback.cpp
@@ -658,7 +658,7 @@ namespace cubmethod
 		      param_info.type = db_get_int (&arg_type);
 		    }
 
-		  if (db_get (arg_mop_p, SP_ATTR_DATA_TYPE, &has_default) == NO_ERROR)
+		  if (db_get (arg_mop_p, SP_ATTR_DEFAULT_VALUE, &has_default) == NO_ERROR)
 		    {
 		      param_info.has_default = DB_IS_NULL (&has_default) ? 0 : 1;
 		    }
