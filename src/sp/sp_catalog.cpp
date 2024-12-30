1137d48356f066b8eec09a121492c7d29b52961d
Na, Hyunik <hyunikn@gmail.com>
2024-12-30 15:03:23 +0900

    clear a DB_VALUE after its use

--- src/sp/sp_catalog.cpp
+++ src/sp/sp_catalog.cpp
@@ -115,10 +115,11 @@ static int sp_builtin_init ()
   a.arg_name = "s";
   a.data_type = DB_TYPE_INTEGER;
   a.mode = SP_MODE_IN;
-  db_make_int (&a.default_value, 20000); // Oracle compatibility (no need to call pr_clear_value() ?)
+  db_make_int (&a.default_value, 20000); // Oracle compatibility
   a.comment  = "";
 
   v.args.push_back (a);
+  pr_clear_value(&a.default_value);
 
   //
   sp_builtin_definition.push_back (v);
