a702553470f93eacb432b95106e6b25f48f0bbbc
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-01-02 17:26:27 +0900

    wait for PL server ready when DB server started

--- src/sp/pl_sr.cpp
+++ src/sp/pl_sr.cpp
@@ -540,6 +540,8 @@ pl_server_init (const char *db_name)
 
   pl_server_manager = new cubpl::server_manager (db_name);
   pl_server_manager->start ();
+
+  pl_server_manager->wait_for_server_ready (); // wait until PL server ready at first
 }
 
 void
