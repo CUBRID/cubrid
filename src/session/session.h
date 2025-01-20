d851d897f63f897a6b007a3202664f02e143febe
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-01-20 17:40:28 +0900

    [CBRD-25835] Check if pl_session exists in logtb_set_tran_index_interrupt and then handle the interrupt (#5828)
    http://jira.cubrid.org/browse/CBRD-25835
    
    This PR prevents cubpl::get_session() from creating an empty pl_session and entering an unnecessary interrupt handling routine
    # 제목: 변경 요약 (50자 이내)  (refactor, hotfix, feature)
    
    # 본문: 변경 사항에 대한 자세한 설명
    # - 주요 변경 사항 1
    # - 주요 변경 사항 2
    
    # 참고: 이 라인 아래의 내용은 커밋 메시지에서 제거됩니다.

--- src/session/session.h
+++ src/session/session.h
@@ -92,6 +92,7 @@ extern int session_set_load_session (THREAD_ENTRY * thread_p, load_session * loa
 extern int session_get_load_session (THREAD_ENTRY * thread_p, REFPTR (load_session, load_session_ref_ptr));
 
 extern int session_get_pl_session (THREAD_ENTRY * thread_p, REFPTR (PL_SESSION, pl_session_ref_ptr));
+extern bool session_has_pl_session (THREAD_ENTRY * thread_p);
 #if defined (SERVER_MODE)
 extern void session_notify_pl_task_completion (const struct session_state *session_arg);
 #endif
