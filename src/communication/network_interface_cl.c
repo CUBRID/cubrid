9991c92c2518890be850f10db1ff1bf12d95da7e
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-01-21 14:06:42 +0900

    null checks

--- src/communication/network_interface_cl.c
+++ src/communication/network_interface_cl.c
@@ -11057,7 +11057,12 @@ error:
       }
     else
       {
-	std::string err_msg = executor.get_stack ()->get_error_message ();
+	std::string err_msg;
+	if (executor.get_stack ())
+	  {
+	    err_msg = executor.get_stack ()->get_error_message ();
+	  }
+
 	if (err_msg.empty () && req_error != ER_SP_EXECUTE_ERROR)
 	  {
 	    err_msg.assign (er_msg ());
