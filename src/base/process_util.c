f2cc75660295a455913e8b634f21433033e6324d
chuntaek.shim <ctshim@cubrid.com>
2025-01-23 18:48:23 +0900

    slip

--- src/base/process_util.c
+++ src/base/process_util.c
@@ -171,7 +171,7 @@ create_child_process (const char *path, const char *const argv[], int wait_flag,
 	  return 1;
 	}
     }
-  if (stderr_file && (StdErr != INVALID_HANDLE_VALUE))
+  if (stderr_file && (hStdErr != INVALID_HANDLE_VALUE))
     {
       rc = CloseHandle (hStdErr);
       if (rc == FALSE)
