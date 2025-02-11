446b205b8083c3d665a5839c622ac3a9daebe35b
Daehyun Kim <kimdhyungg@gmail.com>
2025-02-11 12:42:28 +0900

    minor refactor(xasl_generation): move F_L2_DISTANCE down

--- src/parser/xasl_generation.c
+++ src/parser/xasl_generation.c
@@ -6785,9 +6785,9 @@ pt_function_to_regu (PARSER_CONTEXT * parser, PT_NODE * function)
 	case F_REGEXP_COUNT:
 	case F_REGEXP_INSTR:
 	case F_REGEXP_LIKE:
-	case F_L2_DISTANCE:
 	case F_REGEXP_REPLACE:
 	case F_REGEXP_SUBSTR:
+	case F_L2_DISTANCE:
 	  result_type = pt_node_to_db_type (function);
 	  break;
 	case F_BENCHMARK:
