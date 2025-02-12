193edc5cf243b1ffc2fadf35c1f55fd9a0917623
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-02-12 21:38:47 +0900

    Revert "clear interrupt when starting a new query"
    This reverts commit 44013f5bfb62f1c81340874c5780b9e3fa1993b3.

--- src/sp/pl_session.hpp
+++ src/sp/pl_session.hpp
@@ -122,7 +122,6 @@ namespace cubpl
       bool is_interrupted ();
       int get_interrupt_id ();
       std::string get_interrupt_msg ();
-      void clear_interrupt ();
 
       void wait_for_interrupt ();
       void set_local_error_for_interrupt (); // set interrupt on thread local error manager
