2746bb3f66272f1d0bf5c00b83b0df246341b1d5
InChijun <chijun@cubrid.com>
2025-02-12 10:04:43 +0900

    [CBRD-25772] Use perror instead of PRINT_AND_LOG_ERR_MSG for error logging

--- src/executables/util_sa.c
+++ src/executables/util_sa.c
@@ -1575,7 +1575,8 @@ diagdb (UTIL_FUNCTION_ARG * arg)
 
   if (class_name && class_list_file)
     {
-      PRINT_AND_LOG_ERR_MSG ("Error: The -n and -i options cannot be used together. Please choose only one option.\n");
+      errno = EINVAL;
+      perror ("The -n and -i options cannot be used together.");
       goto error_exit;
     }
 
