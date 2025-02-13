564c6306eb36910c9b3f061dc9070935d8e0b91f
Daehyun Kim <kimdhyungg@gmail.com>
2025-02-13 13:36:06 +0900

    style(vector_opfunc.hpp): space between function name and parens

--- src/query/vector_opfunc.hpp
+++ src/query/vector_opfunc.hpp
@@ -27,7 +27,7 @@
 #include "dbtype_def.h"
 
 int
-vector_l2_distance(DB_VALUE *result, DB_VALUE *args[], int num_args);
+vector_l2_distance (DB_VALUE *result, DB_VALUE *args[], int num_args);
 
 
 #endif /* _VECTOR_OPFUNC_H_ */
