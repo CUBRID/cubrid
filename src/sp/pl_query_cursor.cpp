b9e70487cbf363d988631722fd8f1b24c5b476d8
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-02-14 13:47:29 +0900

    slip

--- src/sp/pl_query_cursor.cpp
+++ src/sp/pl_query_cursor.cpp
@@ -72,7 +72,7 @@ namespace cubpl
   {
     if (m_is_opened == false)
       {
-	if (reset () && qfile_open_list_scan (m_query_entry->list_id, &m_scan_id) == NO_ERROR)
+	if (reset () == NO_ERROR && qfile_open_list_scan (m_query_entry->list_id, &m_scan_id) == NO_ERROR)
 	  {
 	    m_is_opened = true;
 	  }
