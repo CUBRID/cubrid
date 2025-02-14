bec08fd79de3eab2e31a8608b1a90477653e990f
Daehyun Kim <18080546+vimkim@users.noreply.github.com>
2025-02-14 20:22:09 +0900

    fix format
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/query/vector_opfunc.cpp
+++ src/query/vector_opfunc.cpp
@@ -26,7 +26,7 @@ static std::vector<float> db_value_get_stdvector_float (const DB_VALUE *value)
 {
   assert (value != nullptr && DB_VALUE_TYPE (value) == DB_TYPE_VECTOR);
 
-  DB_SET* set_ref = db_get_set (value);
+  DB_SET *set_ref = db_get_set (value);
 
   int size = db_set_size (set_ref);
 
