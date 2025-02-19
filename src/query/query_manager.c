a609ef69202a9f919b3c500b1580b8919ea834cb
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-02-19 17:57:27 +0900

    revert removed code

--- src/query/query_manager.c
+++ src/query/query_manager.c
@@ -153,6 +153,7 @@ static void qmgr_deallocate_query_entries (QMGR_QUERY_ENTRY * q_ptr);
 static void qmgr_deallocate_oid_blocks (OID_BLOCK_LIST * oid_block);
 static void qmgr_add_query_entry (THREAD_ENTRY * thread_p, QMGR_QUERY_ENTRY * q_ptr, int trans_ind);
 static QMGR_QUERY_ENTRY *qmgr_find_query_entry (QMGR_QUERY_ENTRY * query_list_p, QUERY_ID query_id);
+static void qmgr_delete_query_entry (THREAD_ENTRY * thread_p, QUERY_ID query_id, int trans_ind);
 static void qmgr_free_tran_entries (void);
 
 static void qmgr_clear_relative_cache_entries (THREAD_ENTRY * thread_p, QMGR_TRAN_ENTRY * tran_entry_p);
