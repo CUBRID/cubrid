352a22793b7c7f21db8cd7269e8fd874bb5c88d4
xmilex-git <xmilex@naver.com>
2025-02-25 11:47:19 +0900

    apply code review

--- src/parser/xasl_generation.c
+++ src/parser/xasl_generation.c
@@ -12356,6 +12356,11 @@ pt_to_class_spec_list (PARSER_CONTEXT * parser, PT_NODE * spec, PT_NODE * where_
 					   NULL, where, NULL, NULL, regu_attributes_pred, regu_attributes_rest, NULL,
 					   output_val_list, regu_var_list, NULL, cache_pred, cache_rest,
 					   NULL, NO_SCHEMA, db_values_array_p, regu_attributes_reserved);
+	      if (access_method == ACCESS_METHOD_SEQUENTIAL
+		  && PT_IS_SPEC_FLAG_SET (spec, PT_SPEC_FLAG_NO_PARALLEL_HEAP_SCAN))
+		{
+		  access->flags = (ACCESS_SPEC_FLAG) (access->flags | ACCESS_SPEC_FLAG_NO_PARALLEL_HEAP_SCAN);
+		}
 
 	    }
 	  else if (PT_SPEC_SPECIAL_INDEX_SCAN (spec))
@@ -12585,10 +12590,6 @@ pt_to_class_spec_list (PARSER_CONTEXT * parser, PT_NODE * spec, PT_NODE * where_
 	      if (spec->info.spec.flag & PT_SPEC_FLAG_FOR_UPDATE_CLAUSE)
 		{
 		  access->flags = (ACCESS_SPEC_FLAG) (access->flags | ACCESS_SPEC_FLAG_FOR_UPDATE);
-		}
-	      if (PT_IS_SPEC_FLAG_SET (spec, PT_SPEC_FLAG_NO_PARALLEL_HEAP_SCAN))
-		{
-		  access->flags = (ACCESS_SPEC_FLAG) (access->flags | ACCESS_SPEC_FLAG_NO_PARALLEL_HEAP_SCAN);
 		}
 
 	      access->next = access_list;
--- src/query/query_dump.c
+++ src/query/query_dump.c
@@ -555,7 +555,7 @@ qdump_print_access_spec (ACCESS_SPEC_TYPE * spec_list_p)
 
   if (spec_list_p->flags & ACCESS_SPEC_FLAG_NO_PARALLEL_HEAP_SCAN)
     {
-      fprintf (foutput, ",NO_PARALLEL_HEAP_SCAN");
+      fprintf (foutput, ",no_parallel_heap_scan");
     }
 
   if (IS_ANY_INDEX_ACCESS (spec_list_p->access))
