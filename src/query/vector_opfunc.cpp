895a8c2041858edf5ed92166262f4c9b461d8ad4
Daehyun Kim <kimdhyungg@gmail.com>
2025-02-26 13:41:05 +0900

    refactor(vector_opfunc.cpp): remove comments

--- src/query/vector_opfunc.cpp
+++ src/query/vector_opfunc.cpp
@@ -74,13 +74,10 @@ static std::vector<float> db_value_get_stdvector_float (const DB_VALUE *value)
  */
 int vector_distance (DB_VALUE *result, DB_VALUE *args[], int num_args)
 {
-  // Ensure we have the correct number of arguments.
   assert (num_args == 3);
 
-  // Extract vectors from the arguments.
   const std::vector<float> vec1 = db_value_get_stdvector_float (args[0]);
   const std::vector<float> vec2 = db_value_get_stdvector_float (args[1]);
-  // Check that vectors are non-empty and of equal size.
   assert (!vec1.empty() && !vec2.empty());
   assert (vec1.size() == vec2.size());
 
