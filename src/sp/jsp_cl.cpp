78cf55530abd503e37d3a323d317a7bf33263e32
jongmin-won <kceeck@gamil.com>
2025-02-26 15:54:08 +0900

    Reflects the code review content.

--- src/sp/jsp_cl.cpp
+++ src/sp/jsp_cl.cpp
@@ -1493,15 +1493,8 @@ jsp_check_stored_procedure_name (const char *str)
 
   if (strncasecmp (str, "dbms_output.", dbms_output_len) == 0)
     {
-      const int str_len = strlen (str);
-
-      if (str_len < dbms_output_len)
-	{
-	  return name;
-	}
-
       sprintf (buffer, "public.dbms_output.%s",
-	       sm_downcase_name (str + dbms_output_len, tmp, (str_len + dbms_output_len + 1)));
+	       sm_downcase_name (str + dbms_output_len, tmp, SM_MAX_IDENTIFIER_LENGTH + 2));
     }
   else
     {
