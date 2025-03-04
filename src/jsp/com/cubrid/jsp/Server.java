b0601508d5d68032087e631c5ee2b061227f9d29
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-03-04 22:00:17 +0900

    [CBRD-25940] Fix JVM's ListenerThread could terminate abnormally due to a system error (#5958)
    http://jira.cubrid.org/browse/CBRD-25940

--- src/jsp/com/cubrid/jsp/Server.java
+++ src/jsp/com/cubrid/jsp/Server.java
@@ -33,13 +33,13 @@ package com.cubrid.jsp;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 import java.net.ServerSocket;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.newsclub.net.unix.AFUNIXServerSocket;
 import org.newsclub.net.unix.AFUNIXSocketAddress;
@@ -63,6 +63,8 @@ public class Server {
 
     private static Server serverInstance = null;
 
+    private static LoggingThread loggingThread = null;
+
     private Server(
             String name, String path, String version, String rPath, String uPath, String port)
             throws IOException, ClassNotFoundException {
@@ -75,6 +77,16 @@ public class Server {
         ServerSocket serverSocket = null;
         int port_number = Integer.parseInt(port);
         try {
+            String logFilePath =
+                    rootPath
+                            + File.separatorChar
+                            + LOG_DIR
+                            + File.separatorChar
+                            + serverName
+                            + "_java.log";
+            loggingThread = new LoggingThread(logFilePath);
+            loggingThread.start();
+
             tmpPath = System.getenv("CUBRID_TMP");
             if (tmpPath == null) {
                 tmpPath = rootPath + File.separator + "tmp";
@@ -182,6 +194,7 @@ public class Server {
     public static void stop(int status) {
         getServer().setShutdown();
         getServer().stopSocketListener();
+        loggingThread.interrupt();
         System.exit(status);
     }
 
@@ -189,31 +202,19 @@ public class Server {
         Server.start(args);
     }
 
-    public static void log(Throwable ex) {
-        FileHandler logHandler = null;
+    public static void log(String str) {
+        loggingThread.log(str);
+    }
 
-        try {
-            logHandler =
-                    new FileHandler(
-                            rootPath
-                                    + File.separatorChar
-                                    + LOG_DIR
-                                    + File.separatorChar
-                                    + serverName
-                                    + "_java.log",
-                            true);
-            logger.addHandler(logHandler);
-            logger.log(Level.SEVERE, "", ex);
-        } catch (Throwable e) {
-        } finally {
-            if (logHandler != null) {
-                try {
-                    logHandler.close();
-                    logger.removeHandler(logHandler);
-                } catch (Throwable e) {
-                }
-            }
-        }
+    public static void log(Throwable ex) {
+        StringWriter sw = new StringWriter();
+        ex.printStackTrace(new PrintWriter(sw));
+        String exceptionAsString = sw.toString();
+        loggingThread.log(exceptionAsString);
+    }
+
+    public static void flushLog() {
+        loggingThread.flush();
     }
 
     public void setShutdown() {
