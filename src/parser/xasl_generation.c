3e406563d567edb09930de0f2e246601c960ea53
Byungwook-Kim <bwkim@cubrid.com>
2025-03-05 17:40:47 +0900

    [CBRD-25951] PL/CSQL Static SQL Loop 내 CTE에서 SP 사용 시 Core Dump 발생

--- src/parser/xasl_generation.c
+++ src/parser/xasl_generation.c
@@ -21979,8 +21979,10 @@ parser_generate_xasl_post (PARSER_CONTEXT * parser, PT_NODE * node, void *arg, i
       break;
 
     case PT_CTE:
-      assert (node->info.cte.xasl == NULL);
-
+      if (node->info.cte.xasl != NULL)
+	{
+	  assert (parser->host_var_count == 0 && parser->auto_param_count == 0);
+	}
       xasl = parser_generate_xasl_proc (parser, node, info->query_list);
       break;
 
