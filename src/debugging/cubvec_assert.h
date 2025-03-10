e548e61ce338df79741343a32ac0a8a3a8ffa9f3
Daehyun Kim <kimdhyungg@gmail.com>
2025-03-10 13:40:24 +0900

    fix(cubvec_assert.h): add license and other team members

--- src/debugging/cubvec_assert.h
+++ src/debugging/cubvec_assert.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2008 Search Solution Corporation
+ * Copyright 2016 CUBRID Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+/*
+ * cubvec_assert.h - Utility macros for debugging CUBVEC
+ */
+
 #ifndef CUBVEC_ASSERT_H
 #define CUBVEC_ASSERT_H
 
@@ -7,6 +29,30 @@
 #define ASSERT_CUBVEC(expr) assert(expr)
 #else
 #define ASSERT_CUBVEC(expr) ((void)0)
+#endif
+
+#ifdef MHOH3963
+#define ASSERT_MHOH3963(expr) assert(expr)
+#else
+#define ASSERT_MHOH3963(expr) ((void)0)
+#endif
+
+#ifdef HORNETMJ
+#define ASSERT_HORNETMJ(expr) assert(expr)
+#else
+#define ASSERT_HORNETMJ(expr) ((void)0)
+#endif
+
+#ifdef HGRYOO
+#define ASSERT_HGRYOO(expr) assert(expr)
+#else
+#define ASSERT_HGRYOO(expr) ((void)0)
+#endif
+
+#ifdef YEUNJUNLEE
+#define ASSERT_YEUNJUNLEE(expr) assert(expr)
+#else
+#define ASSERT_YEUNJUNLEE(expr) ((void)0)
 #endif
 
 #ifdef VIMKIM
