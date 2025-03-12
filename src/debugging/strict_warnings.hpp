af87e6f72082ceec02fe01bb6563567e702324a3
Daehyun Kim <kimdhyungg@gmail.com>
2025-03-12 18:18:22 +0900

    feat!: add missing warnings

--- src/debugging/strict_warnings.hpp
+++ src/debugging/strict_warnings.hpp
@@ -28,14 +28,13 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic error "-Wall"
 #pragma GCC diagnostic error "-Wextra"
-#pragma GCC diagnostic error "-Wconversion"
 
 #elif defined(__GNUC__)
 // GCC does not support turning -Wall and -Wextra into errors directly.
 // Instead, we list each warning that those groups enable individually.
 #pragma GCC diagnostic push
 
-// Warnings from -Wall:
+// Warnings from -Wall
 #pragma GCC diagnostic error "-Waddress"
 #pragma GCC diagnostic error "-Warray-bounds"
 #pragma GCC diagnostic error "-Wbool-compare"
@@ -68,17 +67,77 @@
 #pragma GCC diagnostic error "-Wunused-variable"
 #pragma GCC diagnostic error "-Wunused-parameter"
 #pragma GCC diagnostic error "-Wvarargs"
+#pragma GCC diagnostic error "-Warray-compare"
+#pragma GCC diagnostic error "-Warray-parameter=2"
+#pragma GCC diagnostic error "-Wbool-operation"
+#pragma GCC diagnostic error "-Wc++11-compat"
+#pragma GCC diagnostic error "-Wc++14-compat"
+#pragma GCC diagnostic error "-Wc++17-compat"
+#pragma GCC diagnostic error "-Wc++20-compat"
+#pragma GCC diagnostic error "-Wdangling-else"
+#pragma GCC diagnostic error "-Wdangling-pointer=2"
+#pragma GCC diagnostic error "-Wenum-compare"
+#pragma GCC diagnostic error "-Wformat=1"
+#pragma GCC diagnostic error "-Wformat-contains-nul"
+#pragma GCC diagnostic error "-Wformat-diag"
+#pragma GCC diagnostic error "-Wformat-extra-args"
+#pragma GCC diagnostic error "-Wformat-overflow=1"
+#pragma GCC diagnostic error "-Wformat-truncation=1"
+#pragma GCC diagnostic error "-Wformat-zero-length"
+#pragma GCC diagnostic error "-Wframe-address"
+#pragma GCC diagnostic error "-Wimplicit"
+#pragma GCC diagnostic error "-Wimplicit-function-declaration"
+#pragma GCC diagnostic error "-Wimplicit-int"
+#pragma GCC diagnostic error "-Winfinite-recursion"
+#pragma GCC diagnostic error "-Wint-in-bool-context"
+#pragma GCC diagnostic error "-Wmemset-elt-size"
+#pragma GCC diagnostic error "-Wmemset-transposed-args"
+#pragma GCC diagnostic error "-Wmisleading-indentation"
+#pragma GCC diagnostic error "-Wmismatched-dealloc"
+#pragma GCC diagnostic error "-Wmissing-attributes"
+#pragma GCC diagnostic error "-Wmissing-braces"
+#pragma GCC diagnostic error "-Wmultistatement-macros"
+#pragma GCC diagnostic error "-Wnonnull"
+#pragma GCC diagnostic error "-Wnonnull-compare"
+#pragma GCC diagnostic error "-Wopenmp-simd"
+#pragma GCC diagnostic error "-Wrestrict"
+#pragma GCC diagnostic error "-Wsizeof-array-div"
+#pragma GCC diagnostic error "-Wsizeof-pointer-div"
+#pragma GCC diagnostic error "-Wsizeof-pointer-memaccess"
+#pragma GCC diagnostic error "-Wstrict-aliasing"
+#pragma GCC diagnostic error "-Wtautological-compare"
+#pragma GCC diagnostic error "-Wzero-length-bounds"
+#pragma GCC diagnostic error "-Wunused-but-set-variable"
+#pragma GCC diagnostic error "-Wunused-const-variable=1"
+#pragma GCC diagnostic error "-Wunused-local-typedefs"
+#pragma GCC diagnostic error "-Wunused-value"
+#pragma GCC diagnostic error "-Wuse-after-free=2"
+#pragma GCC diagnostic error "-Wvla-parameter"
+#pragma GCC diagnostic error "-Wvolatile-register-var"
 
 // Language-specific warnings:
 #ifdef __cplusplus
-// C++ specific
+// C++ specific warnings may be already included above.
+#pragma GCC diagnostic error "-Wdelete-non-virtual-dtor"    // (C++ only)
+#pragma GCC diagnostic error "-Wcatch-value"                // (C++ only)
+#pragma GCC diagnostic error "-Wclass-memaccess"            // (C++ only)
+#pragma GCC diagnostic error "-Wmismatched-new-delete"      // (C++ only)
+#pragma GCC diagnostic error "-Woverloaded-virtual=1"       // (C++ only)
+#pragma GCC diagnostic error "-Wpessimizing-move"            // (C++ only)
+#pragma GCC diagnostic error "-Wrange-loop-construct"        // (C++ only)
+#pragma GCC diagnostic error "-Wreorder"                     // (C++ only)
+#pragma GCC diagnostic error "-Wself-move"                 // (C++ only)
+#pragma GCC diagnostic error "-Wnarrowing"                 // (C++ only)
+#pragma GCC diagnostic error "-Waligned-new"                // (C++ only)
 #else
-// C-specific
+// C-specific warnings:
+#pragma GCC diagnostic error "-Wduplicate-decl-specifier"
+#pragma GCC diagnostic error "-Wenum-int-mismatch"
 #pragma GCC diagnostic error "-Wmissing-parameter-type"
 #pragma GCC diagnostic error "-Wpointer-sign"
 #endif
 
-// Extra warnings from -Wextra:
+// Extra warnings from -Wextra
 #pragma GCC diagnostic error "-Wclobbered"
 #pragma GCC diagnostic error "-Wdeprecated"
 #pragma GCC diagnostic error "-Wfloat-conversion"
@@ -90,15 +149,28 @@
 #pragma GCC diagnostic error "-Wswitch-default"
 #pragma GCC diagnostic error "-Wundef"
 #pragma GCC diagnostic error "-Wuseless-cast"
+#pragma GCC diagnostic error "-Wabsolute-value"
+#pragma GCC diagnostic error "-Walloc-size"
+#pragma GCC diagnostic error "-Wcalloc-transposed-args"
+#pragma GCC diagnostic error "-Wcast-function-type"
+#pragma GCC diagnostic error "-Wempty-body"
+#pragma GCC diagnostic error "-Wexpansion-to-defined"
+#pragma GCC diagnostic error "-Wshift-negative-value"
+#pragma GCC diagnostic error "-Wstring-compare"
+#pragma GCC diagnostic error "-Wtype-limits"
+#pragma GCC diagnostic error "-Wunterminated-string-initialization"
+#pragma GCC diagnostic error "-Wunused-but-set-parameter"
 
-// Language-specific warnings:
 #ifdef __cplusplus
-// C++ specific
-#pragma GCC diagnostic error "-Wold-style-cast"
-#pragma GCC diagnostic error "-Woverloaded-virtual"
-#pragma GCC diagnostic error "-Wzero-as-null-pointer-constant"
+#pragma GCC diagnostic error "-Wdangling-reference"
+#pragma GCC diagnostic error "-Wdeprecated-copy"
+#pragma GCC diagnostic error "-Wredundant-move"
+#pragma GCC diagnostic error "-Wsized-deallocation"
 #else
-// C-specific
+#pragma GCC diagnostic error "-Wenum-conversion"
+#pragma GCC diagnostic error "-Wmissing-parameter-name"
+#pragma GCC diagnostic error "-Wold-style-declaration"
+#pragma GCC diagnostic error "-Woverride-init"
 #endif
 
 #else
