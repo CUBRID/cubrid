490e7c2a43420e6f1b4ccb5a2a95f1122bf718ea
Daehyun Kim <kimdhyungg@gmail.com>
2025-03-12 19:30:37 +0900

    recover perf.cpp

--- src/base/perf.cpp
+++ src/base/perf.cpp
@@ -29,7 +29,6 @@
 #include <cstring>
 // XXX: SHOULD BE THE LAST INCLUDE HEADER
 #include "memory_wrapper.hpp"
-#include "strict_warnings.hpp"
 
 namespace cubperf
 {
@@ -190,5 +189,3 @@ namespace cubperf
   }
 
 } // namespace cubperf
-
-#include "strict_warnings_off.hpp"
