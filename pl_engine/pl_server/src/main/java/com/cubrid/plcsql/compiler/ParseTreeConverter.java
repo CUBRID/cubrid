1abe6e6df5bffed8e230e6df41625f1809425b9c
Na, Hyunik <hyunikn@gmail.com>
2025-03-14 11:22:46 +0900

    minor blank

--- pl_engine/pl_server/src/main/java/com/cubrid/plcsql/compiler/ParseTreeConverter.java
+++ pl_engine/pl_server/src/main/java/com/cubrid/plcsql/compiler/ParseTreeConverter.java
@@ -2750,7 +2750,7 @@ public class ParseTreeConverter extends PlcParserBaseVisitor<AstNode> {
         }
 
         if (ret == null && throwIfNotFound) {
-            throw new UndeclaredId(Misc.getLineColumnOf(ctx), "undeclared id" + name);
+            throw new UndeclaredId(Misc.getLineColumnOf(ctx), "undeclared id " + name);
         } else {
             return ret;
         }
