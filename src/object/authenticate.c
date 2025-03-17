df7a9c7935b0932f65dd6452894dc5b70700d583
Kang Dooho <airnet@cubrid.com>
2025-03-17 10:42:56 +0900

    [CBRD-25934] Backport from develop to 11.2 - Skip errors due to ownership change in unloaddb.

--- src/object/authenticate.c
+++ src/object/authenticate.c
@@ -8381,7 +8381,7 @@ au_export_grants (print_output & output_ctx, MOP class_mop)
   int error = NO_ERROR;
   CLASS_AUTH cl_auth;
   CLASS_USER *u;
-  int statements, ecount;
+  int statements;
   char *uname;
 
   cl_auth.class_mop = class_mop;
@@ -8400,7 +8400,7 @@ au_export_grants (print_output & output_ctx, MOP class_mop)
       while ((statements = class_grant_loop (output_ctx, &cl_auth)))
 	;
 
-      for (u = cl_auth.users, ecount = 0; u != NULL; u = u->next)
+      for (u = cl_auth.users; u != NULL; u = u->next)
 	{
 	  if (u->grants != NULL)
 	    {
@@ -8415,13 +8415,7 @@ au_export_grants (print_output & output_ctx, MOP class_mop)
 					  MSGCAT_AUTH_GRANT_DUMP_ERROR), uname);
 	      output_ctx ("*/\n");
 	      ws_free_string (uname);
-	      ecount++;
 	    }
-	}
-      if (ecount)
-	{
-	  error = ER_GENERIC_ERROR;
-	  er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, error, 0);
 	}
     }
 
