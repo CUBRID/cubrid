b6713da527bdf1cca9635fff2c7d5aecbbada22d
Daehyun Kim <kimdhyungg@gmail.com>
2025-03-17 12:51:44 +0900

    remove PT in ddl_log

--- src/base/ddl_log.c
+++ src/base/ddl_log.c
@@ -51,7 +51,6 @@
 #include "util_func.h"
 #include "hide_password.h"
 #include "db.h"
-#include "cubvec_assert.h"
 
 #define DDL_LOG_MSG 	            (256)
 #define DDL_LOG_PATH    	    "log/ddl_audit"
@@ -1421,11 +1420,6 @@ logddl_is_ddl_type (int node_type, PT_NODE * node)
     case PT_ALTER_TRIGGER:
     case PT_CREATE_ENTITY:
     case PT_CREATE_INDEX:
-    case PT_CREATE_VECTOR_INDEX:
-      if (node_type == PT_CREATE_VECTOR_INDEX)
-	{
-	  ASSERT_CUBVEC (false);	// analysis required
-	}
     case PT_CREATE_SERIAL:
     case PT_CREATE_STORED_PROCEDURE:
     case PT_CREATE_SYNONYM:
