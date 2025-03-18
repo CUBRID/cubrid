7cdefa34349bac3c4f2b1a152d1036189a957484
Daehyun Kim <kimdhyungg@gmail.com>
2025-03-18 19:48:18 +0900

    fix: feedback - make ASSERT_CUBVEC work in circleci release mode build

--- src/debugging/cubvec_assert.h
+++ src/debugging/cubvec_assert.h
@@ -26,9 +26,21 @@
 #include <assert.h>
 
 #ifdef CUBVEC_TEAM
-#define ASSERT_CUBVEC(expr) assert(expr)
+  #ifdef NDEBUG
+    // Custom assertion that works even in release mode
+    #define ASSERT_CUBVEC(expr) \
+      do { \
+        if (!(expr)) { \
+          fprintf(stderr, "Assertion failed: %s, file %s, line %d\n", #expr, __FILE__, __LINE__); \
+          abort(); \
+        } \
+      } while (0)
+  #else
+    // In debug mode, use the standard assert
+    #define ASSERT_CUBVEC(expr) assert(expr)
+  #endif
 #else
-#define ASSERT_CUBVEC(expr) ((void)0)
+  #define ASSERT_CUBVEC(expr) ((void)0)
 #endif
 
 #ifdef HORNETMJ
