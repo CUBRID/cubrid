a0a6ffc2e68f7433ad07784dafd0678c3c262f8c
Kang Dooho <airnet@cubrid.com>
2025-03-21 10:34:23 +0900

    [CBRD-25737] Backport from develop to 11.3 - Add user schema to SELECT NEXT_VALUE statement

--- src/executables/unload_schema.c
+++ src/executables/unload_schema.c
@@ -851,7 +851,8 @@ export_serial (extract_context & ctxt, print_output & output_ctx)
 
 	  if (db_get_int (&values[SERIAL_STARTED]) == 1)
 	    {
-	      output_ctx ("SELECT %s%s%s.NEXT_VALUE;\n", PRINT_IDENTIFIER (db_get_string (&values[SERIAL_NAME])));
+	      output_ctx ("SELECT %s%s%s%s.NEXT_VALUE;\n", output_owner,
+			  PRINT_IDENTIFIER (db_get_string (&values[SERIAL_NAME])));
 	    }
 
 	  db_value_clear (&diff_value);
