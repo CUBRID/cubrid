45e460a6855b9e9a6a2efd608e4acddde501eafe
Byungwook-Kim <bwkim@cubrid.com>
2025-03-25 09:30:25 +0900

    free (replydata_listid)

--- src/communication/network_interface_cl.c
+++ src/communication/network_interface_cl.c
@@ -7279,9 +7279,14 @@ qmgr_execute_query (const XASL_ID * xasl_id, QUERY_ID * query_idp, int dbval_cnt
 
 	  tran_set_latest_query_status (end_query_result, tran_state, should_conn_reset);
 
+	  /* check if dblink transaction is aborted */
 	  if (tran_state == TRAN_UNACTIVE_ABORTED_INFORMING_PARTICIPANTS)
 	    {
-	      /* dblink transaction is aborted */
+	      if (replydata_listid)
+		{
+		  free_and_init (replydata_listid);
+		}
+
 	      return NULL;
 	    }
 	}
