c3531290b99ce6422e412c7c0435cfd9b04664c4
xmilex-git <xmilex@naver.com>
2025-04-01 20:08:58 +0900

    feat: add callable task in parallel query namespace

---
+++ src/query/parallel/px_callable_task.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 CUBRID Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+/*
+ * px_callable_task.cpp
+ */
+
+#include "px_callable_task.hpp"
+#include "px_worker_manager.hpp"
+
+namespace parallel_query
+{
+  void callable_task::set_worker_manager (worker_manager *manager_p)
+  {
+    m_worker_manager_p = manager_p;
+  }
+
+  void callable_task::execute (cubthread::entry &context)
+  {
+    m_exec_f (context);
+  }
+
+  void callable_task::retire ()
+  {
+    if (m_worker_manager_p != nullptr)
+      {
+	m_worker_manager_p->pop_task ();
+	m_worker_manager_p=nullptr;
+      }
+
+    m_retire_f ();
+  }
+} // namespace parallel_query
