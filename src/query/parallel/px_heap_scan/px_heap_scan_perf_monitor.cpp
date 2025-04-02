063845154baaaf2d8ab094d745121b46f98b0ae8
xmilex-git <xmilex@naver.com>
2025-04-02 14:43:22 +0900

    refactor: min..max trace

--- src/query/parallel/px_heap_scan/px_heap_scan_perf_monitor.cpp
+++ src/query/parallel/px_heap_scan/px_heap_scan_perf_monitor.cpp
@@ -46,45 +46,64 @@ namespace parallel_heap_scan
 
   void perf_monitor::print_text (FILE *fp, int indent, char *class_name, bool is_list_merge)
   {
-    UINT64 avg_elapsed_scan = 0;
-    UINT64 avg_read_rows = 0;
-    UINT64 avg_qualified_rows = 0;
+    UINT64 min_elapsed_scan = std::numeric_limits<UINT64>::max();
+    UINT64 max_elapsed_scan = 0;
+    UINT64 min_read_rows = std::numeric_limits<UINT64>::max();
+    UINT64 max_read_rows = 0;
+    UINT64 min_qualified_rows = std::numeric_limits<UINT64>::max();
+    UINT64 max_qualified_rows = 0;
 
     for (std::size_t i = 0; i < m_parallelism; i++)
       {
-	avg_elapsed_scan += (UINT64) (TO_MSEC (m_scan_stats[i].elapsed_scan));
-	avg_read_rows += (UINT64) m_scan_stats[i].read_rows;
-	avg_qualified_rows += (UINT64) m_scan_stats[i].qualified_rows;
+	min_elapsed_scan = std::min (min_elapsed_scan, (UINT64) (TO_MSEC (m_scan_stats[i].elapsed_scan)));
+	max_elapsed_scan = std::max (max_elapsed_scan, (UINT64) (TO_MSEC (m_scan_stats[i].elapsed_scan)));
+	min_read_rows = std::min (min_read_rows, (UINT64) m_scan_stats[i].read_rows);
+	max_read_rows = std::max (max_read_rows, (UINT64) m_scan_stats[i].read_rows);
+	min_qualified_rows = std::min (min_qualified_rows, (UINT64) m_scan_stats[i].qualified_rows);
+	max_qualified_rows = std::max (max_qualified_rows, (UINT64) m_scan_stats[i].qualified_rows);
       }
-    avg_elapsed_scan /= m_parallelism;
-    avg_read_rows /= m_parallelism;
-    avg_qualified_rows /= m_parallelism;
+
     fprintf (fp,
-	     "\n%*c(table: %s), (parallel workers: %zu, avg heap time: %llu, avg readrows: %llu, avg rows: %llu, gather: %s)",
+	     "\n%*c(parallel workers: %zu, heap time: %lu..%lu, readrows: %lu..%lu, rows: %lu..%lu, gather: %s)",
 	     indent,
-	     ' ', class_name, m_parallelism, avg_elapsed_scan, avg_read_rows, avg_qualified_rows,
+	     ' ', class_name, m_parallelism, min_elapsed_scan, max_elapsed_scan, min_read_rows, max_read_rows, min_qualified_rows,
+	     max_qualified_rows,
 	     is_list_merge ? "mergable list" : "row by row");
   }
 
   void perf_monitor::print_json (json_t *scan, char *class_name, bool is_list_merge)
   {
-    UINT64 avg_elapsed_scan = 0;
-    UINT64 avg_read_rows = 0;
-    UINT64 avg_qualified_rows = 0;
+    UINT64 min_elapsed_scan = 0;
+    UINT64 max_elapsed_scan = 0;
+    UINT64 min_read_rows = 0;
+    UINT64 max_read_rows = 0;
+    UINT64 min_qualified_rows = 0;
+    UINT64 max_qualified_rows = 0;
 
     for (std::size_t i = 0; i < m_parallelism; i++)
       {
-	avg_elapsed_scan += (UINT64) (TO_MSEC (m_scan_stats[i].elapsed_scan));
-	avg_read_rows += (UINT64) m_scan_stats[i].read_rows;
-	avg_qualified_rows += (UINT64) m_scan_stats[i].qualified_rows;
+	min_elapsed_scan = std::min (min_elapsed_scan, (UINT64) (TO_MSEC (m_scan_stats[i].elapsed_scan)));
+	max_elapsed_scan = std::max (max_elapsed_scan, (UINT64) (TO_MSEC (m_scan_stats[i].elapsed_scan)));
+	min_read_rows = std::min (min_read_rows, (UINT64) m_scan_stats[i].read_rows);
+	max_read_rows = std::max (max_read_rows, (UINT64) m_scan_stats[i].read_rows);
+	min_qualified_rows = std::min (min_qualified_rows, (UINT64) m_scan_stats[i].qualified_rows);
+	max_qualified_rows = std::max (max_qualified_rows, (UINT64) m_scan_stats[i].qualified_rows);
       }
-    avg_elapsed_scan /= m_parallelism;
-    avg_read_rows /= m_parallelism;
-    avg_qualified_rows /= m_parallelism;
 
-    json_t *parallel_obj = json_pack ("{s:I, s:I, s:I, s:I, s:s}", "parallel_workers", m_parallelism, "time",
-				      avg_elapsed_scan, "readrows", avg_read_rows,
-				      "rows", avg_qualified_rows, "gather", is_list_merge ? "mergable list" : "row by row");
+    char time_buf[64];
+    char readrows_buf[64];
+    char rows_buf[64];
+
+    snprintf (time_buf, sizeof (time_buf), "%lu..%lu", min_elapsed_scan, max_elapsed_scan);
+    snprintf (readrows_buf, sizeof (readrows_buf), "%lu..%lu", min_read_rows, max_read_rows);
+    snprintf (rows_buf, sizeof (rows_buf), "%lu..%lu", min_qualified_rows, max_qualified_rows);
+
+    json_t *parallel_obj = json_pack ("{s:I, s:s, s:s, s:s, s:s}",
+				      "parallel_workers", m_parallelism,
+				      "time", time_buf,
+				      "readrows", readrows_buf,
+				      "rows", rows_buf,
+				      "gather", is_list_merge ? "mergable list" : "row by row");
     json_object_set_new (scan, "parallel heap", parallel_obj);
   }
 
