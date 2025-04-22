00fc87a83b1433255af572221b815e1f111b344d
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-04-22 16:06:23 +0900

    slip

--- src/session/session.c
+++ src/session/session.c
@@ -3348,7 +3348,7 @@ session_stop_attached_threads (THREAD_ENTRY * thread_p, void *session_arg)
 
   if (session->pl_session_p != NULL)
     {
-      if (thread_p->type == TT_WORKER)
+      if (thread_p && thread_p->type == TT_WORKER)
 	{
 	  session->pl_session_p->set_interrupt (er_errid ());
 	  session->pl_session_p->wait_for_interrupt ();
