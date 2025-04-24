77142f8aa34de143866f15a0db6def02c737bc96
Daehyun Kim <kimdhyungg@gmail.com>
2025-04-24 18:33:29 +0900

    style: fix

--- src/executables/csql_result.c
+++ src/executables/csql_result.c
@@ -562,7 +562,6 @@ get_current_result (int **lengths, const CUR_RESULT_INFO * result_info, const CS
 	      || value_type == result_info->attr_types[i]
 	      || (TP_IS_CHAR_TYPE (value_type) && TP_IS_CHAR_TYPE (result_info->attr_types[i])));
 
-
       switch (value_type)
 	{
 	case DB_TYPE_NULL:	/* null value */
