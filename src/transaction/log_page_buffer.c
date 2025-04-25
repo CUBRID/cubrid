c98fa9156fbd2e79987f8d5fd3262526bc5f4cc2
InChijun <chijun@cubrid.com>
2025-04-25 17:23:40 +0900

    [CBRD-26027] [regression] Set the core count to 1

--- src/transaction/log_page_buffer.c
+++ src/transaction/log_page_buffer.c
@@ -7532,18 +7532,13 @@ cubthread::entry_workpool * g_backup_read_worker_pool = NULL;
 void
 logpb_create_backup_read_worker_pool (size_t thread_count)
 {
-  size_t core_count = thread_count / 3;
   if (thread_count == 0)
     {
-      core_count = thread_count = 1;
-    }
-  if (core_count == 0)
-    {
-      core_count = 1;
+      thread_count = 1;
     }
   g_backup_read_worker_pool =
     cubthread::get_manager ()->create_worker_pool (thread_count, thread_count, "backup read workers", NULL,
-						   core_count, false, true);
+						   1, false, true);
 }
 
 void
