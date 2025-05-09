9448f7628aead1ca450bf6f0c6c73b51ebefdb38
Byungwook-Kim <bwkim@cubrid.com>
2025-05-09 10:45:38 +0900

    er_clear in case of dblink

--- src/parser/name_resolution.c
+++ src/parser/name_resolution.c
@@ -969,7 +969,14 @@ pt_bind_name_or_path_in_scope (PARSER_CONTEXT * parser, PT_BIND_NAMES_ARG * bind
 	    {
 	      if (er_errid () != NO_ERROR)
 		{
-		  PT_ERRORc (parser, in_node, er_msg ());
+		  if (scopes && scopes->specs && scopes->specs->info.spec.remote_server_name)
+		    {
+		      er_clear ();
+		    }
+		  else
+		    {
+		      PT_ERRORc (parser, in_node, er_msg ());
+		    }
 		}
 	      else
 		{
