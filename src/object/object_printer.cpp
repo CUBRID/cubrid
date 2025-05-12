e8196670d72e332542956b8189fe030717c1580a
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-05-12 18:19:14 +0900

    print VECTOR type's domain through ;sc command

--- src/object/object_printer.cpp
+++ src/object/object_printer.cpp
@@ -320,7 +320,7 @@ void object_printer::describe_domain (/*const*/tp_domain &domain, class_descript
 
 	case DB_TYPE_VECTOR:
 	  strcpy (temp_buffer, temp_domain->type->name);
-	  m_buf ("%s", ustr_upper (temp_buffer));
+	  m_buf ("%s(%d)", ustr_upper (temp_buffer), temp_domain->precision);
 	  break;
 
 	case DB_TYPE_ENUMERATION:
