815b6a3ce79251857427e46b06ee5b30e4430616
Hamkua <heoja0123@naver.com>
2025-05-14 15:48:56 +0900

    [CBRD-25776] modify set operators to use the hint from the left-side SELECT statement

--- src/parser/view_transform.c
+++ src/parser/view_transform.c
@@ -5090,9 +5090,9 @@ mq_check_cte_inline_or_materialize (PARSER_CONTEXT * parser, PT_NODE * node)
       (void) parser_walk_tree (parser, cte->info.cte.non_recursive_part,
 			       mq_check_rewrite_cte, &is_inlinable, NULL, NULL);
 
-      if (is_inlinable && PT_IS_SELECT (cte->info.cte.non_recursive_part))
+      if (is_inlinable)
 	{
-	  hint = cte->info.cte.non_recursive_part->info.query.q.select.hint;
+	  hint = pt_get_hint_from_query (parser, cte->info.cte.non_recursive_part);
 
 	  if (hint & (PT_HINT_MATERIALIZE_CTE | PT_HINT_SELECT_BTREE_NODE_INFO | PT_HINT_SELECT_KEY_INFO))
 	    {
