6d5a1e7d5b94ac810b85a3d5cb89784d558038dc
Yeunjun Lee <yjlee@cubrid.com>
2025-05-14 13:25:56 +0900

    [CUBVEC-77] Modify HNSW index vector ID from incremental ID to OID (#6167)
    http://jira.cubrid.org/browse/CUBVEC-77
    
    Modify HNSW index vector ID from incremental ID to OID

--- src/storage/hnsw.hpp
+++ src/storage/hnsw.hpp
@@ -34,12 +34,12 @@
 #include "dbtype_def.h"
 #include "thread_compat.hpp"
 #include "faiss/IndexHNSW.h"
+#include "faiss/IndexIDMap.h"
 
 BTID *xhnsw_add_index (THREAD_ENTRY *thread_p, BTID *btid, int dimension, int hnsw_M, int hnsw_efConstruction,
 		       enum faiss::MetricType metric_type);
 int xhnsw_delete_index (THREAD_ENTRY *thread_p, BTID *btid);
 int hnsw_print_index_info (BTID *btid);
-int hnsw_add_element (BTID *btid, DB_VALUE *key_dbvalue);
-
+int hnsw_add_element (BTID *btid, OID *oid, DB_VALUE *key_dbvalue);
 
 #endif
