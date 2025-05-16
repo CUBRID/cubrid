61e599acaa74a9b2d087960a07debce4cb3e62ea
Daehyun Kim <kimdhyungg@gmail.com>
2025-05-16 19:23:14 +0900

    feat(schema_manager): convert cubrid metric enum to faiss metric enum

--- src/object/schema_manager.c
+++ src/object/schema_manager.c
@@ -10831,17 +10831,35 @@ allocate_index (MOP classop, SM_CLASS * class_, DB_OBJLIST * subclasses, SM_CLAS
 	      return error;
 	    }
 
-#if !defined(NDEBUG)
-	  {
-	    auto m = hnsw::m;
-	    auto ef_construction = hnsw::ef_construction;
-	    auto metric = hnsw::metric;
+	  auto m = hnsw::m;
+	  auto ef_construction = hnsw::ef_construction;
+	  auto metric = hnsw::metric;
 
-	    vimkim_log ("m, ef_construction, metric: %d, %d, %d\n", m, ef_construction, metric);
-	  }
-#endif
+	  vimkim_log ("m, ef_construction, metric: %d, %d, %d\n", m, ef_construction, metric);
 
-	  error = hnsw_add_index (index, domain->precision, 32, 100, faiss::METRIC_L2);
+	  faiss::MetricType faiss_metric;
+	  switch (metric)
+	    {
+	    case METRIC_UNKNOWN:
+	    case METRIC_COSINE:
+	    case METRIC_DOT:
+	      // TODO (CUBVEC): Faiss does not support cosine distance directly?
+	      faiss_metric = faiss::METRIC_INNER_PRODUCT;
+	      break;
+
+	    case METRIC_EUCLIDEAN:
+	      faiss_metric = faiss::METRIC_L2;
+	      break;
+
+	    case METRIC_MANHATTAN:
+	      faiss_metric = faiss::METRIC_L1;
+	      break;
+
+	    default:
+	      ASSERT_CUBVEC (false);
+	    }
+
+	  error = hnsw_add_index (index, domain->precision, m, ef_construction, faiss_metric);
 	}
       else
 	{
