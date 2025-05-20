44187dbb824cf3db9316be15de8a56325b895aa3
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-05-20 13:18:41 +0900

    indent

--- src/xasl/access_spec.hpp
+++ src/xasl/access_spec.hpp
@@ -50,7 +50,7 @@ typedef enum			/* range search option */
   LT_GE,			/* key < v1 || key >= v2 or NOT (v1 <= key < v2) */
   LT_GT,			/* key < v1 || key > v2 or NOT (v1 <= key <= v2) */
   NEQ_NA,			/* key != v1 */
-  K_NN,			/* key for K-NN search */
+  K_NN,			        /* key for K-NN search */
 } RANGE;
 
 typedef struct key_val_range KEY_VAL_RANGE;
