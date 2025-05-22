a3af3bce414bf8dda5fbba6eb23abf8c8e724b42
xmilex-git <xmilex@naver.com>
2025-05-22 10:46:32 +0900

    empty commit : pr title problem

--- src/query/parallel/px_heap_scan/px_heap_scan_manager.cpp
+++ src/query/parallel/px_heap_scan/px_heap_scan_manager.cpp
@@ -539,7 +539,7 @@ scan_open_parallel_heap_scan (THREAD_ENTRY *thread_p, SCAN_ID *scan_id,
   else
     {
       if (result_get_method == parallel_heap_scan::RESULT_GET_METHOD::LIST_MERGE && xasl->list_id->tfile_vfid != NULL
-	  && !VPID_ISNULL (&xasl->list_id->first_vpid))
+	  )
 	{
 	  qfile_reopen_list_as_append_mode (thread_p, xasl->list_id);
 	}
