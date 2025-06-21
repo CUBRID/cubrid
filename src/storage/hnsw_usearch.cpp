c6f666bb1811d7804c8d67e09461e2af6e6091b1
Yeunjun Lee <yjlee@cubrid.com>
2025-06-21 12:32:09 +0900

    [CUBVEC-85] Fix assertion when insert vectors in table after rebooting server (#6248)
    http://jira.cubrid.org/browse/CUBVEC-85

--- src/storage/hnsw_usearch.cpp
+++ src/storage/hnsw_usearch.cpp
@@ -126,15 +126,6 @@ int xhnsw_delete_index (THREAD_ENTRY *thread_p, BTID *btid)
 {
   char filepath[PATH_MAX];
 
-  if (!hnsw_index_directory_created)
-    {
-      if (create_hnsw_index_directory () != NO_ERROR)
-	{
-	  er_log_debug (ARG_FILE_LINE, "Failed to create HNSW Index directory");
-	  return ER_FAILED;
-	}
-    }
-
   if (!btid)
     {
       assert (false);
@@ -224,6 +215,28 @@ void dump_all_hnsw_indices_to_files ()
 	  assert (false);
 	  _er_log_debug (ARG_FILE_LINE, "Failed to dump HNSW Index with ID %d", hnsw_id);
 	}
+    }
+}
+
+void init_hnsw_index_path ()
+{
+  char db_path[PATH_MAX];
+  fileio_get_directory_path (db_path, boot_db_full_name());
+  int written = snprintf (hnsw_index_directory, PATH_MAX, "%s%cvindex", db_path, PATH_SEPARATOR);
+  if (written < 0 || written >= PATH_MAX)
+    {
+      assert (false);
+      _er_log_debug (ARG_FILE_LINE, "Failed to create path for HNSW Index directory since path is too long");
+      return;
+    }
+
+  if (std::filesystem::exists (hnsw_index_directory))
+    {
+      hnsw_index_directory_created = true;
+    }
+  else
+    {
+      hnsw_index_directory_created = false;
     }
 }
 
