e7bb830f69364e052cf6063679ec83d701a8d8aa
chuntaek.shim <ctshim@cubrid.com>
2025-06-26 09:47:33 +0900

    fix indent

--- src/heaplayers/malloc_2_8_3.c
+++ src/heaplayers/malloc_2_8_3.c
@@ -1236,52 +1236,55 @@ extern void *sbrk (ptrdiff_t);
 #endif /* LACKS_UNISTD_H */
 #endif /* HAVE_MMAP */
 
+
+// *INDENT-OFF* 
 #ifndef WIN32
 #ifndef malloc_getpagesize
-#ifdef _SC_PAGESIZE		/* some SVR4 systems omit an underscore */
-#ifndef _SC_PAGE_SIZE
-#define _SC_PAGE_SIZE _SC_PAGESIZE
-#endif
-#endif
-#ifdef _SC_PAGE_SIZE
-#define malloc_getpagesize sysconf(_SC_PAGE_SIZE)
-#else
-#if defined(BSD) || defined(DGUX) || defined(HAVE_GETPAGESIZE)
+#  ifdef _SC_PAGESIZE		/* some SVR4 systems omit an underscore */
+#    ifndef _SC_PAGE_SIZE
+#      define _SC_PAGE_SIZE _SC_PAGESIZE
+#    endif
+#  endif
+#  ifdef _SC_PAGE_SIZE
+#    define malloc_getpagesize sysconf(_SC_PAGE_SIZE)
+#  else
+#    if defined(BSD) || defined(DGUX) || defined(HAVE_GETPAGESIZE)
 extern size_t getpagesize ();
-#define malloc_getpagesize getpagesize()
-#else
-#ifdef WIN32			/* use supplied emulation of getpagesize */
-#define malloc_getpagesize getpagesize()
-#else
-#ifndef LACKS_SYS_PARAM_H
-#include <sys/param.h>
+#      define malloc_getpagesize getpagesize()
+#    else
+#      ifdef WIN32		/* use supplied emulation of getpagesize */
+#        define malloc_getpagesize getpagesize()
+#      else
+#        ifndef LACKS_SYS_PARAM_H
+#          include <sys/param.h>
+#        endif
+#        ifdef EXEC_PAGESIZE
+#          define malloc_getpagesize EXEC_PAGESIZE
+#        else
+#          ifdef NBPG
+#            ifndef CLSIZE
+#              define malloc_getpagesize NBPG
+#            else
+#              define malloc_getpagesize (NBPG * CLSIZE)
+#            endif
+#          else
+#            ifdef NBPC
+#              define malloc_getpagesize NBPC
+#            else
+#              ifdef PAGESIZE
+#                define malloc_getpagesize PAGESIZE
+#              else /* just guess */
+#                define malloc_getpagesize ((size_t)4096U)
+#              endif
+#            endif
+#          endif
+#        endif
+#      endif
+#    endif
+#  endif
 #endif
-#ifdef EXEC_PAGESIZE
-#define malloc_getpagesize EXEC_PAGESIZE
-#else
-#ifdef NBPG
-#ifndef CLSIZE
-#define malloc_getpagesize NBPG
-#else
-#define malloc_getpagesize (NBPG * CLSIZE)
 #endif
-#else
-#ifdef NBPC
-#define malloc_getpagesize NBPC
-#else
-#ifdef PAGESIZE
-#define malloc_getpagesize PAGESIZE
-#else /* just guess */
-#define malloc_getpagesize ((size_t)4096U)
-#endif
-#endif
-#endif
-#endif
-#endif
-#endif
-#endif
-#endif
-#endif
+// *INDENT-ON* 
 
 /* ------------------- size_t and alignment properties -------------------- */
 
