acbf900aa1f6d3b28ba9b1ef64c0807bdbba3729
hyahong <yechan0815@naver.com>
2025-07-31 11:11:15 +0000

    Double Mapped Ring Buffer (Single Producer Single Consumer) for CSS module.

---
+++ src/base/DMRB_SPSC.hpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008 Search Solution Corporation
+ * Copyright 2016 CUBRID Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+/*
+ * DMRB_SPSC.hpp
+ */
+
+#ifndef _DMRB_SPSC_HPP_
+#define _DMRB_SPSC_HPP_
+
+#ident "$Id$"
+
+#include "DMRB.hpp"
+
+#include <cstring>
+#include <cstddef>
+
+namespace cubbase
+{
+  /* single producer single consumer */
+  class DMRB_SPSC : public DMRB
+  {
+  public:
+    DMRB_SPSC (std::size_t capacity);
+    ~DMRB_SPSC ();
+
+    cubbase::span<std::byte> reserve (std::size_t length);
+    void consume (std::size_t length);
+
+    cubbase::span<const std::byte> peek () const;
+  };
+}
+
+#endif
