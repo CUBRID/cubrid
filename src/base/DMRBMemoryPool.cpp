6a220a519de7efada80d462462a0531dae4b7467
hyahong <yechan0815@naver.com>
2025-08-13 07:50:35 +0000

    code style

--- src/base/DMRBMemoryPool.cpp
+++ src/base/DMRBMemoryPool.cpp
@@ -58,47 +58,48 @@ namespace cubbase
     std::uint64_t order, size;
     std::map<std::uint64_t, std::uint64_t>::iterator it, prev;
 
-    order = m_tail + ((m_size - (m_tail & m_mask) + ((reinterpret_cast<std::uintptr_t> (mem.data ()) - reinterpret_cast<std::uintptr_t> (m_base)) & m_mask)) & m_mask);
+    order = m_tail + ((m_size - (m_tail & m_mask) + ((reinterpret_cast<std::uintptr_t> (mem.data ()) -
+		       reinterpret_cast<std::uintptr_t> (m_base)) & m_mask)) & m_mask);
     size = mem.size ();
 
 #if !defined (NDEBUG)
     it = m_free.lower_bound (order);
     if (it != m_free.begin ())
-    {
-      prev = std::prev (it);
-      assert (prev->first + prev->second <= order);
-    }
+      {
+	prev = std::prev (it);
+	assert (prev->first + prev->second <= order);
+      }
     if (it != m_free.end ())
-    {
-      assert (order + size <= it->first);
-    }
+      {
+	assert (order + size <= it->first);
+      }
 #endif
 
     if (order != m_tail)
-    {
-      m_free.emplace (order, size);
-      return ;
-    }
+      {
+	m_free.emplace (order, size);
+	return ;
+      }
 
     assert (order == m_tail);
 
     for (it = m_free.begin (); it != m_free.end (); )
-    {
-      if (it->first != order + size)
       {
-	break;
+	if (it->first != order + size)
+	  {
+	    break;
+	  }
+	size += it->second;
+	it = m_free.erase (it);
       }
-      size += it->second;
-      it = m_free.erase (it);
-    }
-    
+
     this->consume (size);
   }
 
   bool DMRBMemoryPool::is_in (cubbase::span<std::byte> &mem)
   {
     std::uintptr_t base, addr;
-    
+
     base = reinterpret_cast<std::uintptr_t> (m_base);
     addr = reinterpret_cast<std::uintptr_t> (mem.data ());
 
