15aaba889a2fc61427540ee221b4c3ecc315831e
hyahong <yechan0815@naver.com>
2025-08-19 05:58:59 +0000

    add memory monitor header

--- src/base/DMRBMemoryPool.cpp
+++ src/base/DMRBMemoryPool.cpp
@@ -25,6 +25,9 @@
 #include <cstring>
 #include <cstddef>
 
+// XXX: SHOULD BE THE LAST INCLUDE HEADER
+#include "memory_wrapper.hpp"
+
 namespace cubbase
 {
   DMRBMemoryPool::DMRBMemoryPool (std::size_t capacity) :
