fe7bd73ac906d4fb91a568d51a54b383b9239fd1
hyahong <yechan0815@naver.com>
2025-09-02 11:26:26 +0000

    clear for alignment

--- src/connection/connection_sr.c
+++ src/connection/connection_sr.c
@@ -1954,15 +1954,7 @@ css_add_queue_entry (CSS_CONN_ENTRY * conn, CSS_LIST * list, unsigned short requ
   CSS_QUEUE_ENTRY *p;
   int r;
 
-  /* for alignment */
-  /* TODO: remove this and handle this problem in other way */
-  char *buf;
-  buf = (char *) new std::byte[buffer_size];
-  std::memcpy (buf, buffer, buffer_size);
-  conn->release_packet_origin (buffer, buffer_size);
-  p = css_make_queue_entry (conn, request_id, buf, buffer_size, rc, transid, invalidate_snapshot, db_error);
-
-//  p = css_make_queue_entry (conn, request_id, buffer, buffer_size, rc, transid, invalidate_snapshot, db_error);
+  p = css_make_queue_entry (conn, request_id, buffer, buffer_size, rc, transid, invalidate_snapshot, db_error);
   if (p == NULL)
     {
       return CANT_ALLOC_BUFFER;
