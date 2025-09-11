236be7d36df76ae4814fa71db82abf197847f9e0
hyahong <yechan0815@naver.com>
2025-09-11 08:40:37 +0000

    give a warning messages.

--- src/base/hardware_topology.cpp
+++ src/base/hardware_topology.cpp
@@ -89,14 +89,14 @@ namespace cubbase
     ifname = cubbase::ifsys::auto_select_primary_iface ();
     if (ifname.empty ())
       {
-	fprintf (stderr, "[ERR] cannot auto select iface\n");
+	fprintf (stderr, "warning: cannot auto select iface\n");
 	return ;
       }
 
     /* channel */
     if (!this->set_nic_channels (ifname, m_selected.size ()))
       {
-	fprintf (stderr, "[WARN] set_nic_channels_combined failed (driver may limit)\n");
+	fprintf (stderr, "warning: set_nic_channels_combined failed (driver may limit)\n");
       }
     /* wait until applied */
     usleep (1000 * 1000);
@@ -111,7 +111,7 @@ namespace cubbase
       }
     else
       {
-	fprintf (stderr, "[WARN] no IRQ lines found for %s in /proc/interrupts\n", ifname.c_str ());
+	fprintf (stderr, "warning: no IRQ lines found for %s in /proc/interrupts\n", ifname.c_str ());
       }
     free (qs.v);
 
