d3f967b6938c30be448497d116def287b0b9f468
hyahong <yechan0815@naver.com>
2025-09-25 12:23:39 +0000

    add error code

--- src/connection/connection_worker.cpp
+++ src/connection/connection_worker.cpp
@@ -834,10 +834,10 @@ namespace cubconn
     /* entry */
     m_entry = cubthread::get_manager ()->claim_entry ();
     if (m_entry == nullptr)
-    {
-      _er_log_debug (__FILE__, __LINE__, "connection_worker->initialize: claim_entry failed\n");
-      assert_release (false);
-    }
+      {
+	_er_log_debug (__FILE__, __LINE__, "connection_worker->initialize: claim_entry failed\n");
+	assert_release (false);
+      }
     m_entry->register_id ();
     m_entry->index = m_index;
     m_entry->type = TT_SERVER;
