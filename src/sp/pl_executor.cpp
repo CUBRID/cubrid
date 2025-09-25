8d2d4f8493097b079155554709eb42a0f7d038b5
Na, Hyunik <hyunikn@gmail.com>
2025-09-25 18:34:04 +0900

    . restore the assertion . close all cursors of the session in the callback for ROLLBACK

--- src/sp/pl_executor.cpp
+++ src/sp/pl_executor.cpp
@@ -741,7 +741,6 @@ exit:
     query_cursor *cursor = m_stack->get_cursor (qid);
     if (cursor == nullptr)
       {
-	assert (false);
 	cubmem::block b = std::move (pack_data_block (METHOD_RESPONSE_ERROR, ER_FAILED, std::string ("no cursor for the query"),
 				     ARG_FILE_LINE));
 	error = m_stack->send_data_to_java (b);
@@ -953,9 +952,17 @@ exit:
   {
     int error = NO_ERROR;
     int code = METHOD_CALLBACK_END_TRANSACTION;
-    int command; // commit or abort
+    int command; // commit=1 or abort=2
 
     unpacker.unpack_all (command);
+    if (command == 2) {
+        cubpl::session *pl_session = cubpl::get_session ();
+        if (pl_session) {
+            pl_session->destroy_all_cursors();
+        } else {
+            return ER_SES_SESSION_EXPIRED;
+        }
+    }
 
     auto java_lambda = [&] (const cubmem::block & b)
     {
