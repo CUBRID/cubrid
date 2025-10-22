b15fa647e4421cef56f414450ae0e0202ac0e291
xmilex-git <xmilex@naver.com>
2025-10-22 14:09:02 +0900

    가끔 error가 interrupted로 나오는 현상 해결

--- src/query/parallel/px_query_execute/px_query_executor.cpp
+++ src/query/parallel/px_query_execute/px_query_executor.cpp
@@ -187,8 +187,22 @@ namespace parallel_query_execute
 		    is_interrupt = logtb_is_interrupted_tran (thread_p, true, &continue_checking, thread_p->tran_index);
 		  }
 	      }
-	    cuberr::context::get_thread_local_context ().get_current_error_level ().swap (*m_error_messages.m_error_messages.at (
-			0));
+	    std::lock_guard<std::mutex> lock (m_error_messages.m_mutex);
+	    bool found_error = false;
+	    for (auto *msg : m_error_messages.m_error_messages)
+	      {
+		if (msg->err_id != NO_ERROR && msg->err_id != ER_INTERRUPTED)
+		  {
+		    cuberr::context::get_thread_local_context ().get_current_error_level ().swap (*msg);
+		    found_error = true;
+		    break;
+		  }
+	      }
+	    if (!found_error)
+	      {
+		cuberr::context::get_thread_local_context ().get_current_error_level ().swap (*m_error_messages.m_error_messages.at (
+			    0));
+	      }
 	    err_code = ER_FAILED;
 	  }
 	  break;
