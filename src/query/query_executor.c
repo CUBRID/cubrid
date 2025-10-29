831c69b2a41b3e11b1712449c53ac4599c32aeeb
HyunukLee <hulee@cubrid.com>
2025-10-29 14:53:27 +0900

    [fix] Merge join 수행 시 정상적으로 Num_query_mjoins 카운터 증가하도록 수정, MERGELIST_PROC 일 때 Num_query_selects 카운터가 증가하는 문제 수정

--- src/query/query_executor.c
+++ src/query/query_executor.c
@@ -14246,6 +14246,8 @@ qexec_end_mainblock_iterations (THREAD_ENTRY * thread_p, XASL_NODE * xasl, XASL_
 	{
 	  GOTO_EXIT_ON_ERROR;
 	}
+      /* monitor */
+      perfmon_inc_stat (thread_p, PSTAT_QM_NUM_MJOINS);
       break;
 
     case HASHJOIN_PROC:
@@ -15573,8 +15575,10 @@ qexec_execute_mainblock_internal (THREAD_ENTRY * thread_p, XASL_NODE * xasl, XAS
 	    }
 	}
 
-      /* monitor */
-      perfmon_inc_stat (thread_p, PSTAT_QM_NUM_SELECTS);
+      if (xasl->type != MERGELIST_PROC)
+	{
+	  perfmon_inc_stat (thread_p, PSTAT_QM_NUM_SELECTS);
+	}
       break;
     }
 
