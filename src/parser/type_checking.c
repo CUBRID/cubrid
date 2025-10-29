57b91c70b005def9c0bdb267f4811f19aea89ff1
Hamkua <heoja0123@naver.com>
2025-10-29 18:30:21 +0900

    누락된 수정사항 추가 - auto paramterized host variable이 재작성되지 않도록 수정

--- src/parser/type_checking.c
+++ src/parser/type_checking.c
@@ -17578,7 +17578,8 @@ pt_fold_const_expr (PARSER_CONTEXT * parser, PT_NODE * expr, void *arg)
     }
 
   save_set_host_var = parser->flag.set_host_var;
-  parser->flag.set_host_var = PT_EXPR_INFO_IS_FLAGED (expr, PT_EXPR_INFO_REMOVABLE) ? 1 : save_set_host_var;
+  parser->flag.set_host_var = (prm_get_bool_value (PRM_ID_HOSTVAR_PEEKING)
+			       && PT_EXPR_INFO_IS_FLAGED (expr, PT_EXPR_INFO_REMOVABLE)) ? 1 : save_set_host_var;
 
   location = expr->info.expr.location;
 
@@ -17853,7 +17854,7 @@ pt_fold_const_expr (PARSER_CONTEXT * parser, PT_NODE * expr, void *arg)
       goto end;
     }
 
-  if (opd1 && opd1->node_type == PT_HOST_VAR)
+  if (opd1 && opd1->node_type == PT_HOST_VAR && opd1->info.host_var.index < parser->host_var_count)
     {
       arg1 = pt_value_to_db (parser, opd1);
       if (!DB_IS_NULL (arg1))
@@ -17890,7 +17891,7 @@ pt_fold_const_expr (PARSER_CONTEXT * parser, PT_NODE * expr, void *arg)
       opd2 = expr->info.expr.arg2;
     }
 
-  if (opd2 && opd2->node_type == PT_HOST_VAR)
+  if (opd2 && opd2->node_type == PT_HOST_VAR && opd2->info.host_var.index < parser->host_var_count)
     {
       arg2 = pt_value_to_db (parser, opd2);
       if (!DB_IS_NULL (arg2))
@@ -18134,7 +18135,7 @@ pt_fold_const_expr (PARSER_CONTEXT * parser, PT_NODE * expr, void *arg)
       opd3 = expr->info.expr.arg3;
     }
 
-  if (opd3 && opd3->node_type == PT_HOST_VAR)
+  if (opd3 && opd3->node_type == PT_HOST_VAR && opd3->info.host_var.index < parser->host_var_count)
     {
       arg3 = pt_value_to_db (parser, opd3);
       if (!DB_IS_NULL (arg3))
