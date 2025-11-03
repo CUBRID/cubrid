c6e9c56cd61af0c26323cd54985c4745f57b7f73
Daehyun Kim <kimdhyungg@gmail.com>
2025-11-03 09:55:13 +0900

    make unit tests fail intentionally

--- src/storage/oos_file.cpp
+++ src/storage/oos_file.cpp
@@ -6,24 +6,26 @@ oos_create (const THREAD_ENTRY *thread_entry, const HFID &hfid)
   printf ("oos_create called\n");
   fflush (stdout);
   OosCreateResult result{};
-  result.error_code = 0;
+
+  result.error_code = 1;
   return result;
+
 }
 
 int oos_destroy (const THREAD_ENTRY *thread_entry, const HFID &hfid)
 {
-  return 0;
+  return 1;
 }
 
 [[nodiscard]] OosInsertResult
 oos_insert (const THREAD_ENTRY *thread_entry, const VFID &oos_vfid, const std::vector<RECDES> &db_values)
 {
-  return OosInsertResult{0, {}};
+  return OosInsertResult{1, {}};
 }
 
 [[nodiscard]] int
 oos_get (const THREAD_ENTRY *threnad_entry, const VFID &oos_vfid, const OID &oid, RECDES &recdes)
 {
-  return 0;
+  return 1;
 }
 
