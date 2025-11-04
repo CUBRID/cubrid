ca495e5e3732c9538fe0edc2074a6f854abb5a7e
InChijun <chijun@cubrid.com>
2025-11-04 04:58:44 +0000

    Add boot_lob_remove_temp_dir() in boot_sr.c module

--- src/transaction/boot_sr.c
+++ src/transaction/boot_sr.c
@@ -219,6 +219,8 @@ static int boot_after_copydb (THREAD_ENTRY * thread_p);
 
 static int boot_generate_tde_keys (THREAD_ENTRY * thread_p);
 
+static int boot_lob_remove_temp_dir ();
+
 /*
  * bo_server) -set server's status, UP or DOWN
  *   return: void
@@ -2569,7 +2571,7 @@ boot_restart_server (THREAD_ENTRY * thread_p, bool print_restart, const char *db
 	}
 
       /* remove lob ces temp dir */
-      error_code = fileio_lob_remove_dir ((char *) LOB_TEMPDIR_PREFIX);
+      error_code = boot_lob_remove_temp_dir ();
 
       if (error_code != NO_ERROR)
 	{
@@ -3069,7 +3071,7 @@ xboot_shutdown_server (REFPTR (THREAD_ENTRY, thread_p), ER_FINAL_CODE is_er_fina
   (void) boot_remove_all_temp_volumes (thread_p, REMOVE_TEMP_VOL_DEFAULT_ACTION);
 
   /* remove lob ces temp dir */
-  (void) fileio_lob_remove_dir ((char *) LOB_TEMPDIR_PREFIX);
+  (void) boot_lob_remove_temp_dir ();
 
   // ha delays are registered and logged, and must be stopped before vacuum master
   log_stop_ha_delay_registration ();
@@ -5256,7 +5258,7 @@ boot_remove_all_volumes (THREAD_ENTRY * thread_p, const char *db_fullname, const
     }
 
   /* remove lob ces temp dir */
-  error_code = fileio_lob_remove_dir ((char *) LOB_TEMPDIR_PREFIX);
+  error_code = boot_lob_remove_temp_dir ();
   if (error_code != NO_ERROR)
     {
       goto error_rem_allvols;
@@ -6173,4 +6175,9 @@ boot_after_copydb (THREAD_ENTRY * thread_p)
   er_log_debug (ARG_FILE_LINE, "Complete boot_after_copydb \n");
 
   return NO_ERROR;
+}
+
+static int boot_lob_remove_temp_dir ()
+{
+  return fileio_lob_remove_dir ((char *) LOB_TEMPDIR_PREFIX);
 }
