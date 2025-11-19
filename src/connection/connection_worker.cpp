77da8157bdd53b81c17a89acad54fa70597ba30e
hyahong <yechan0815@naver.com>
2025-11-19 12:29:33 +0000

    reduce the delay

--- src/connection/connection_worker.cpp
+++ src/connection/connection_worker.cpp
@@ -499,7 +499,7 @@ namespace cubconn
     end = std::chrono::steady_clock::now ();
     m_stats.add (stats::BLOCKED_RMUTEX, std::chrono::duration_cast<std::chrono::microseconds> (end - start).count ());
 
-    thread_sleep (1000);
+    thread_sleep (1);
 
     /* close the socket */
     css_shutdown_socket (ctx->m_conn->fd);
