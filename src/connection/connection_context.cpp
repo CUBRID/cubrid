18c665dd2c7657d8b3fceb924a850da602ea41a7
hyahong <yechan0815@naver.com>
2025-12-01 12:49:07 +0000

    add memory header

--- src/connection/connection_context.cpp
+++ src/connection/connection_context.cpp
@@ -22,6 +22,9 @@
 
 #include "connection_context.hpp"
 
+// XXX: SHOULD BE THE LAST INCLUDE HEADER
+#include "memory_wrapper.hpp"
+
 namespace cubconn
 {
   /* --------------------------------------------------------------------------- */
