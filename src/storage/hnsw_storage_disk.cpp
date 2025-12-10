20ab620d7b2da11f55f24955461492b6a061c90e
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2025-12-10 14:36:39 +0900

    add license

--- src/storage/hnsw_storage_disk.cpp
+++ src/storage/hnsw_storage_disk.cpp
@@ -1,11 +1,24 @@
+/*
+ *
+ * Copyright 2016 CUBRID Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 #include "hnsw_storage_disk.hpp"
 
-#include "memory_alloc.h"
-#include "oid.h"
-
 #include "file_manager.h" // FILE_DESCRIPTORS
-#include "overflow_file.h"
-#include "btree.h" // btree_initialize_new_page
 
 namespace cubhnsw
 {
