99cc06d02a15864a1437459e1174ac572c8a5993
chuntaek.shim <ctshim@cubrid.com>
2025-12-11 10:46:15 +0900

    [CBRD-26427] incorrect result when large record-size in plcsql

--- src/sp/pl_query_cursor.cpp
+++ src/sp/pl_query_cursor.cpp
@@ -184,7 +184,7 @@ namespace cubpl
 
 		or_init (&buf, ptr, length);
 
-		if (pr_type->data_readval (&buf, value, domain, -1, false /* Don't copy */, NULL, 0) != NO_ERROR)
+		if (pr_type->data_readval (&buf, value, domain, -1, true, NULL, 0) != NO_ERROR)
 		  {
 		    scan_code = S_ERROR;
 		    break;
