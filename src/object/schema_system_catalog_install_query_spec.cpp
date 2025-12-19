02d09bbed69d4326a91c3c7207d02a6530087801
childyouth <childyouth@cubrid.com>
2025-12-19 15:34:10 +0900

    db_attribute use _db_attribute's flags with DB_ATTOPT_INVISIBLE_COLUMN

--- src/object/schema_system_catalog_install_query_spec.cpp
+++ src/object/schema_system_catalog_install_query_spec.cpp
@@ -311,7 +311,7 @@ sm_define_view_attribute_spec (void)
 	  "CAST ([d].[class_of].[owner].[name] AS VARCHAR(255)) AS [domain_owner_name], " /* string -> varchar(255) */
 	  "[a].[default_value] AS [default_value], "
 	  "CASE WHEN [a].[is_nullable] = 1 THEN 'YES' ELSE 'NO' END AS [is_nullable], "
-	  "CASE WHEN ([a].[flags] & 1) = 1 THEN 'YES' ELSE 'NO' END AS [is_invisible], "
+	  "CASE WHEN ([a].[flags] & %d) = %d THEN 'YES' ELSE 'NO' END AS [is_invisible], "
 	  "[a].[comment] AS [comment] "
 	"FROM "
 	  /* CT_CLASS_NAME */
@@ -366,6 +366,8 @@ sm_define_view_attribute_spec (void)
 	    ")",
 	CT_CHARSET_NAME,
 	CT_COLLATION_NAME,
+	DB_ATTOPT_INVISIBLE_COLUMN,
+	DB_ATTOPT_INVISIBLE_COLUMN,
 	CT_CLASS_NAME,
 	CT_ATTRIBUTE_NAME,
 	CT_DOMAIN_NAME,
