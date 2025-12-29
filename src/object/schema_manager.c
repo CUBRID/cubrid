0647fa36bd23a44b15e1f0a504794a293706f2d1
InChijun <chijun@cubrid.com>
2025-12-29 05:08:13 +0000

    Refactor sm_lob_handling_attr_in_truncate()

--- src/object/schema_manager.c
+++ src/object/schema_manager.c
@@ -395,7 +395,7 @@ static int sm_load_online_index (MOP classmop, const char *constraint_name);
 
 static const char *sm_locate_method_file (SM_CLASS * class_, const char *function);
 
-static int sm_lob_handling_attr_in_truncate (SM_CLASS *class_, HFID *prev_hfid, HFID *new_hfid);
+static int sm_lob_handling_attr_in_truncate (SM_CLASS * class_, HFID * prev_hfid, HFID * new_hfid);
 
 #if defined (WINDOWS)
 static void sm_method_final (void);
@@ -15707,13 +15707,9 @@ sm_truncate_using_destroy_heap (MOP class_mop)
   SM_CLASS *class_ = NULL;
   OID *oid = NULL;
   DB_OBJLIST *subs;
-  // SM_ATTRIBUTE *attr;
   bool reuse_oid = false;
-  int error = NO_ERROR;
   int partition_type = DB_NOT_PARTITIONED_CLASS;
-  // int *lob_alloc_attrid_arr = NULL;
-  // int lob_local_attrid_arr[2];
-  // int lob_attrid_arr_length = 0;
+  int error = NO_ERROR;
 
   oid = ws_oid (class_mop);
   assert (!OID_ISTEMP (oid));
@@ -15790,12 +15786,13 @@ end:
 }
 
 static int
-sm_lob_handling_attr_in_truncate (SM_CLASS *class_, HFID *prev_hfid, HFID *new_hfid)
+sm_lob_handling_attr_in_truncate (SM_CLASS * class_, HFID * prev_hfid, HFID * new_hfid)
 {
   SM_ATTRIBUTE *attr;
   int lob_attrid_arr_length = 0;
   int lob_local_attrid_arr[2];
   int *lob_alloc_attrid_arr = NULL;
+  int *lob_attrid_arr;
   int error = NO_ERROR;
 
 
@@ -15805,18 +15802,15 @@ sm_lob_handling_attr_in_truncate (SM_CLASS *class_, HFID *prev_hfid, HFID *new_h
 
       if (TP_IS_LOB_TYPE (attr->type->id))
 	{
-	  if (lob_attrid_arr_length >= 2)
-	    {
-	      lob_attrid_arr_length++;
-	    }
-	  else
-	    {
-	      lob_local_attrid_arr[lob_attrid_arr_length++] = attr->id;
-	    }
+	  lob_attrid_arr_length++;
 	}
     }
 
-  if (lob_attrid_arr_length > 2)
+  if (lob_attrid_arr_length == 0)
+    {
+      goto end;
+    }
+  else if (lob_attrid_arr_length > 2)
     {
       int index = 0;
 
@@ -15836,15 +15830,28 @@ sm_lob_handling_attr_in_truncate (SM_CLASS *class_, HFID *prev_hfid, HFID *new_h
 	      lob_alloc_attrid_arr[index++] = attr->id;
 	    }
 	}
+      lob_attrid_arr = lob_alloc_attrid_arr;
+    }
+  else
+    {
+      int index = 0;
+
+      for (int i = 0; i < class_->att_count; i++)
+	{
+	  attr = &class_->attributes[i];
+
+	  if (TP_IS_LOB_TYPE (attr->type->id))
+	    {
+	      lob_local_attrid_arr[index++] = attr->id;
+	    }
+	}
+      lob_attrid_arr = lob_local_attrid_arr;
     }
 
   /* Destroy and Create the lob dir if need */
   if (lob_attrid_arr_length)
     {
-      error =
-	locator_lob_create_or_remove_dir (prev_hfid, new_hfid,
-					  lob_alloc_attrid_arr ? lob_alloc_attrid_arr : lob_local_attrid_arr,
-					  lob_attrid_arr_length);
+      error = locator_lob_create_or_remove_dir (prev_hfid, new_hfid, lob_attrid_arr, lob_attrid_arr_length);
     }
   if (error != NO_ERROR)
     {
