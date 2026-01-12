17bb98a24ffa7d13e855d4a77d0c92b856cf6e2b
Daehyun Kim <kimdhyungg@gmail.com>
2026-01-12 18:43:58 +0900

    minor(): static_cast -> c cast for indent

--- src/object/object_representation.c
+++ src/object/object_representation.c
@@ -5752,8 +5752,7 @@ or_header_size (char *ptr)
   int mvcc_flag = OR_GET_MVCC_FLAG (ptr);
   int idx = mvcc_flag & 0x07;	// 0x07 to get last 3 bits. the 4th bit is for "has_oos"
 
-  assert (0 <= idx
-	  && idx < static_cast < int >(sizeof (mvcc_header_size_lookup) / sizeof (mvcc_header_size_lookup[0])));
+  assert (0 <= idx && idx < (int) (sizeof (mvcc_header_size_lookup) / sizeof (mvcc_header_size_lookup[0])));
 
   return mvcc_header_size_lookup[idx];
 }
