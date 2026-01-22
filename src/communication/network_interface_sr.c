19520384e341fede620c2499c090d2447d1972c8
InChijun <chijun@cubrid.com>
2026-01-22 08:20:29 +0000

    refactor add line

--- src/communication/network_interface_sr.c
+++ src/communication/network_interface_sr.c
@@ -11566,6 +11566,7 @@ end:
   db_private_free_and_init (thread_p, attrid_arr);
 
   ptr = or_pack_errcode (reply, error);
+
   css_send_data_to_client (thread_p->conn_entry, rid, reply, OR_ALIGNED_BUF_SIZE (a_reply));
 
   return;
