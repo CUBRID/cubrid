2df6f40f258609b8484c2e5dbd6063268a327355
InChijun <chijun@cubrid.com>
2026-01-27 01:10:18 +0000

    remove unnecessary code.

--- src/communication/network_interface_cl.c
+++ src/communication/network_interface_cl.c
@@ -11404,7 +11404,6 @@ lob_create_dir (HFID * hfid, int *attrid_arr, int lob_attrid_arr_length)
     }
   else
     {
-      request = request_local;
       request = (char *) malloc (request_size);
       if (request == NULL)
 	{
