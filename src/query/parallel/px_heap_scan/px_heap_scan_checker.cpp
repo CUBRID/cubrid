453858ee71bf3e074a418a9381f6b1d38b62f492
xmilex-git <xmilex@naver.com>
2026-02-02 12:21:07 +0900

    enable dptr index scan

--- src/query/parallel/px_heap_scan/px_heap_scan_checker.cpp
+++ src/query/parallel/px_heap_scan/px_heap_scan_checker.cpp
@@ -558,7 +558,7 @@ namespace parallel_heap_scan
 
     for (XASL_NODE *xaslp = arg->dptr_list; xaslp; xaslp = xaslp->next)
       {
-	temp = check<false> (xaslp);
+	temp = sibling_check<false> (xaslp);
 	if (is_flag_set (temp, CANNOT_PARALLEL_HEAP_SCAN))
 	  {
 	    set_flag (result, CANNOT_PARALLEL_HEAP_SCAN);
