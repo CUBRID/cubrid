24dd401a674c1d2916cb26917c147bf30207d15c
xmilex-git <xmilex@naver.com>
2026-02-27 14:18:40 +0900

    code style

--- src/query/memoize.cpp
+++ src/query/memoize.cpp
@@ -1103,7 +1103,7 @@ extern "C"
 	  {
 	    xasl->memoize_storage->stop_timer();
 	  }
-	  clear_memoize_storage (thread_p, xasl);
+	clear_memoize_storage (thread_p, xasl);
 	return NO_ERROR;
 	break;
 
@@ -1155,7 +1155,7 @@ extern "C"
 	  {
 	    xasl->memoize_storage->stop_timer();
 	  }
-	  clear_memoize_storage (thread_p, xasl);
+	clear_memoize_storage (thread_p, xasl);
 	return NO_ERROR;
 	break;
 
@@ -1203,7 +1203,7 @@ extern "C"
 	  {
 	    xasl->memoize_storage->stop_timer();
 	  }
-	  clear_memoize_storage (thread_p, xasl);
+	clear_memoize_storage (thread_p, xasl);
 	return NO_ERROR;
 	break;
 
