a3f31c041fec10771d2c6ead20a966d331341ebd
shparkcubrid <shpark@cubrid.com>
2026-03-13 11:25:16 +0900

    indent

--- src/optimizer/query_planner.c
+++ src/optimizer/query_planner.c
@@ -3373,10 +3373,9 @@ qo_nljoin_cost (QO_PLAN * planp)
       else if (outer->plan_type == QO_PLANTYPE_JOIN)
 	{
 	  guessed_result_cardinality = outer->limit_nljoin_guessed_card;
-          outer_card = ((outer->info)->cardinality == 0) ? 1 : (outer->info)->cardinality;
-          /* result = outer_guessed * (inner_card * selectivity) = outer_guessed * (plan_card/outer_card). */
-	  planp->limit_nljoin_guessed_card =
-	    guessed_result_cardinality * ((planp->info)->cardinality / outer_card);
+	  outer_card = ((outer->info)->cardinality == 0) ? 1 : (outer->info)->cardinality;
+	  /* result = outer_guessed * (inner_card * selectivity) = outer_guessed * (plan_card/outer_card). */
+	  planp->limit_nljoin_guessed_card = guessed_result_cardinality * ((planp->info)->cardinality / outer_card);
 	}
       else
 	{
