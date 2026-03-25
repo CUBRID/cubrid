57d29224543a36b5aa830065b4e675ebb9f01c43
Daehyun Kim <dhkim@cubrid.com>
2026-03-25 18:12:18 +0900

    [CBRD-26637] Refactor OOS error handling to use er_set and ASSERT_ERROR (#6940)
    http://jira.cubrid.org/browse/CBRD-26637
    
    [CBRD-26565] Refactor OOS error handling to use er_set and ASSERT_ERROR

--- src/storage/oos_file.cpp
+++ src/storage/oos_file.cpp
@@ -18,6 +18,7 @@
 
 #include <cassert>
 #include "error_code.h"
+#include "error_manager.h"
 #include "file_manager.h"
 #include "memory_alloc.h"
 #include "page_buffer.h"
@@ -86,7 +87,7 @@ static std::unordered_map<VFID, VPID, VFIDHash, VFIDEq> oos_recently_inserted_oo
 
 // ****************************************************************************
 
-using namespace oos_log;
+
 
 // review point: should it be MAX_ALIGNMENT?
 static constexpr int OOS_ALIGNMENT = MAX_ALIGNMENT;
@@ -101,6 +102,8 @@ oos_file_create (THREAD_ENTRY *thread_p, VFID &oos_vfid)
   if (err != NO_ERROR)
     {
       oos_error ("file_create_with_npages failed");
+      assert_release_error (er_errid () != NO_ERROR);
+      assert (false);
       return err;
     }
 
@@ -128,6 +131,9 @@ oos_make_oos_recdes (RECDES &rec_in, const OOS_RECORD_HEADER &oos_header, RECDES
   err = recdes_allocate_data_area (&rec_out, rec_in.length + (int)sizeof (OOS_RECORD_HEADER));
   if (err != NO_ERROR)
     {
+      oos_error ("recdes_allocate_data_area failed in oos_make_oos_recdes");
+      assert_release_error (er_errid () != NO_ERROR);
+      assert (false);
       return err;
     }
 
@@ -153,6 +159,8 @@ oos_pop_record_header (RECDES &rec_in, OOS_RECORD_HEADER &header_out, RECDES &re
   if (err != NO_ERROR)
     {
       oos_error ("recdes_allocate_data_area failed");
+      assert_release_error (er_errid () != NO_ERROR);
+      assert (false);
       return err;
     }
 
@@ -234,6 +242,8 @@ oos_insert_across_pages (THREAD_ENTRY *thread_p, const VFID &oos_vfid, RECDES &r
       if (err != NO_ERROR)
 	{
 	  oos_error ("could not insert chunk index=%d of length %d.", i, chunk_recdes.length);
+	  assert_release_error (er_errid () != NO_ERROR);
+	  assert (false);
 	  // TODO: free partially inserted chunks.
 	  // Currently, already inserted chunks to slotted pages will remain as garbage.
 	  return err;
@@ -271,6 +281,8 @@ oos_insert_within_page (THREAD_ENTRY *thread_p, const VFID &oos_vfid, RECDES &re
     if (err != NO_ERROR)
       {
 	oos_error ("oos_prepend_record_header failed");
+	assert_release_error (er_errid () != NO_ERROR);
+	assert (false);
 	return err;
       }
 
@@ -287,7 +299,8 @@ oos_insert_within_page (THREAD_ENTRY *thread_p, const VFID &oos_vfid, RECDES &re
     if (sp_status != SP_SUCCESS)
       {
 	oos_error ("spage_insert failed with status %d", sp_status);
-	return ER_FAILED;
+	er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_GENERIC_ERROR, 0);
+	return ER_GENERIC_ERROR;
       }
 
     assert (slotid != NULL_SLOTID);
@@ -299,12 +312,14 @@ oos_insert_within_page (THREAD_ENTRY *thread_p, const VFID &oos_vfid, RECDES &re
     catch (const std::bad_alloc &)
       {
 	oos_error ("memory allocation failed while inserting into map");
-	return ER_FAILED;
+	er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_GENERIC_ERROR, 0);
+	return ER_GENERIC_ERROR;
       }
     catch (const std::exception &e)
       {
 	oos_error ("exception while inserting into map: %s", e.what());
-	return ER_FAILED;
+	er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_GENERIC_ERROR, 0);
+	return ER_GENERIC_ERROR;
       }
 
     oid.pageid = vpid.pageid;
@@ -333,6 +348,9 @@ oos_read_across_pages (THREAD_ENTRY *thread_p, const OID &oid,
   err = recdes_allocate_data_area (&recdes, total_size);
   if (err != NO_ERROR)
     {
+      oos_error ("recdes_allocate_data_area failed in oos_read_across_pages, total_size=%d", total_size);
+      assert_release_error (er_errid () != NO_ERROR);
+      assert (false);
       return err;
     }
 
@@ -352,6 +370,8 @@ oos_read_across_pages (THREAD_ENTRY *thread_p, const OID &oid,
 	if (err != NO_ERROR)
 	  {
 	    oos_error ("oos_read_within_page failed for chunk index=%d", idx);
+	    assert_release_error (er_errid () != NO_ERROR);
+	    assert (false);
 	    recdes_free_data_area (&recdes);
 	    return err;
 	  }
@@ -394,7 +414,9 @@ oos_read_within_page (THREAD_ENTRY *thread_p, const OID &oid, RECDES &recdes,
   if (page_ptr == nullptr)
     {
       oos_error ("oos_read_within_page: pgbuf_fix failed for volid=%d, pageid=%d", volid, pageid);
-      return ER_FAILED;
+      assert_release_error (er_errid () != NO_ERROR);
+      assert (false);
+      return er_errid ();
     }
 
   scope_exit page_unfixer ([&]()
@@ -408,7 +430,8 @@ oos_read_within_page (THREAD_ENTRY *thread_p, const OID &oid, RECDES &recdes,
     {
       oos_error ("oos_read_within_page: spage_get_record failed for volid=%d, pageid=%d, slotid=%d",
 		 volid, pageid, slotid);
-      return ER_FAILED;
+      er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_GENERIC_ERROR, 0);
+      return ER_GENERIC_ERROR;
     }
 
   // TODO: Ensure OOS_RECORD_HEADER always fits within a single page
@@ -418,6 +441,8 @@ oos_read_within_page (THREAD_ENTRY *thread_p, const OID &oid, RECDES &recdes,
     {
       oos_error ("oos_pop_record_header failed for volid=%d, pageid=%d, slotid=%d",
 		 volid, pageid, slotid);
+      assert_release_error (er_errid () != NO_ERROR);
+      assert (false);
       return err;
     }
 
@@ -452,6 +477,8 @@ oos_read (THREAD_ENTRY *thread_p, const OID &oid, RECDES &recdes)
   if (err != NO_ERROR)
     {
       oos_error ("oos_read_within_page failed");
+      assert_release_error (er_errid () != NO_ERROR);
+      assert (false);
       return err;
     }
 
@@ -464,6 +491,8 @@ oos_read (THREAD_ENTRY *thread_p, const OID &oid, RECDES &recdes)
       if (err != NO_ERROR)
 	{
 	  oos_error ("oos_read_across_pages failed");
+	  assert_release_error (er_errid () != NO_ERROR);
+	  assert (false);
 	  return err;
 	}
     }
@@ -505,9 +534,11 @@ oos_file_alloc_new (THREAD_ENTRY *thread_p, const VFID &oos_vfid,
 
   log_sysop_start (thread_p);
   err = file_alloc (thread_p, &oos_vfid, oos_vpid_init_new, &page_type, &vpid_out, nullptr);
-  if (err)
+  if (err != NO_ERROR)
     {
       oos_error ("file_alloc failed");
+      assert_release_error (er_errid () != NO_ERROR);
+      assert (false);
       log_sysop_abort (thread_p);
       return nullptr;
     }
@@ -676,6 +707,8 @@ oos_get_length (THREAD_ENTRY *thread_p, const OID &oid)
   if (page_ptr == nullptr)
     {
       oos_error ("oos_get_length: pgbuf_fix failed for volid=%d, pageid=%d", volid, pageid);
+      assert_release_error (er_errid () != NO_ERROR);
+      assert (false);
       return -1;
     }
 
@@ -690,6 +723,7 @@ oos_get_length (THREAD_ENTRY *thread_p, const OID &oid)
     {
       oos_error ("oos_get_length: spage_get_record failed for volid=%d, pageid=%d, slotid=%d",
 		 volid, pageid, slotid);
+      er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_GENERIC_ERROR, 0);
       return -1;
     }
 
