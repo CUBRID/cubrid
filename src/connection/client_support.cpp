039b988e6d37cbe7f9bcd9e762f93e68b7809862
chuntaek.shim <ctshim@cubrid.com>
2026-03-27 17:09:50 +0900

    fix error code

--- src/connection/client_support.cpp
+++ src/connection/client_support.cpp
@@ -174,7 +174,7 @@ client_support::css_client_init (int sockid, const char *server_name, const char
       if (map == NULL)
 	{
 	  css_free_conn (conn);
-	  error = ER_CSS_ALLOC;
+	  error = ER_NET_CANT_CONNECT_SERVER;
 	}
 #if defined(MULTI_CONN_TO_A_SERVER)
       else
@@ -211,7 +211,7 @@ client_support::css_client_sub_init (const char *server_name, const char *host_n
       if (map == NULL)
 	{
 	  css_free_conn (conn);
-	  error = ER_FAILED;
+	  error = ER_NET_CANT_CONNECT_SERVER;
 	}
       else
 	{
@@ -224,7 +224,7 @@ client_support::css_client_sub_init (const char *server_name, const char *host_n
       error = er_errid ();
       if (error == NO_ERROR)
 	{
-	  error = ER_FAILED;
+	  error = ER_NET_CANT_CONNECT_SERVER;
 	}
     }
 
