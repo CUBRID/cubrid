5a08f527386976e2f9b4f6d332dbc9c9da161ef6
hyahong <yechan0815@naver.com>
2026-04-03 07:21:45 +0000

    Fix format

--- src/base/system_parameter.c
+++ src/base/system_parameter.c
@@ -2457,6 +2457,12 @@ bool PRM_STATDUMP_FORCE_ADD_INT_MAX = false;
 static bool prm_statdump_force_add_int_max_default = false;
 static unsigned int prm_statdump_force_add_int_max_flag = 0;
 
+int PRM_LOG_POSTPONE_CACHE_SIZE = 512;
+static int prm_log_postpone_cache_size_default = 512;
+static int prm_log_postpone_cache_size_upper = 4096;
+static int prm_log_postpone_cache_size_lower = 4;
+static unsigned int prm_log_postpone_cache_size_flag = 0;
+
 typedef int (*DUP_PRM_FUNC) (void *, SYSPRM_DATATYPE, void *, SYSPRM_DATATYPE);
 
 static int prm_size_to_io_pages (void *out_val, SYSPRM_DATATYPE out_type, void *in_val, SYSPRM_DATATYPE in_type);
@@ -6307,11 +6313,11 @@ static SYSPRM_PARAM prm_Def[] = {
    PRM_NAME_LOG_POSTPONE_CACHE_SIZE,
    (PRM_FOR_SERVER | PRM_HIDDEN),
    PRM_INTEGER,
-   PRM_CLEAR_DYNAMIC_FLAG,
-   {false, {.i = 512}},
-   {false, {.i = 512}},
-   {false, {.i = 4096}},
-   {false, {.i = 4}},
+   &prm_log_postpone_cache_size_flag,
+   (void *) &prm_log_postpone_cache_size_default,
+   (void *) &PRM_LOG_POSTPONE_CACHE_SIZE,
+   (void *) &prm_log_postpone_cache_size_upper,
+   (void *) &prm_log_postpone_cache_size_lower,
    (char *) NULL,
    (DUP_PRM_FUNC) NULL,
    (DUP_PRM_FUNC) NULL},
