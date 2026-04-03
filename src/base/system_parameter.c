8deef80baf156adedc0da312fb2cc3a1ade31893
Yeoun <yechan0815@naver.com>
2026-04-03 19:13:02 +0900

    [CBRD-26527] Prevent resetting the entire postpone cache inside nested loops. (#7007)
    * Add partial reset
    * Add hidden parameter postpone_cache_entry

--- src/base/system_parameter.c
+++ src/base/system_parameter.c
@@ -706,6 +706,8 @@ static const char sysprm_ha_conf_file_name[] = "cubrid_ha.conf";
 
 #define PRM_NAME_STATDUMP_FORCE_ADD_INT_MAX "statdump_force_add_int_max"
 
+#define PRM_NAME_LOG_POSTPONE_CACHE_SIZE "postpone_cache_size"
+
 /*
  * Note about ERROR_LIST and INTEGER_LIST type
  * ERROR_LIST type is an array of bool type with the size of -(ER_LAST_ERROR)
@@ -2381,6 +2383,12 @@ static int prm_page_latch_timeout_default = 300;
 static int prm_page_latch_timeout_upper = 3000;
 static int prm_page_latch_timeout_lower = 0;
 static unsigned int prm_page_latch_timeout_flag = 0;
+
+int PRM_LOG_POSTPONE_CACHE_SIZE = 512;
+static int prm_log_postpone_cache_size_default = 512;
+static int prm_log_postpone_cache_size_upper = 4096;
+static int prm_log_postpone_cache_size_lower = 4;
+static unsigned int prm_log_postpone_cache_size_flag = 0;
 
 typedef int (*DUP_PRM_FUNC) (void *, SYSPRM_DATATYPE, void *, SYSPRM_DATATYPE);
 
@@ -6132,7 +6140,19 @@ static SYSPRM_PARAM prm_Def[] = {
    (void *) &prm_page_latch_timeout_lower,
    (char *) NULL,
    (DUP_PRM_FUNC) NULL,
-   (DUP_PRM_FUNC) NULL}
+   (DUP_PRM_FUNC) NULL},
+  {PRM_ID_LOG_POSTPONE_CACHE_SIZE,
+   PRM_NAME_LOG_POSTPONE_CACHE_SIZE,
+   (PRM_FOR_SERVER | PRM_HIDDEN),
+   PRM_INTEGER,
+   &prm_log_postpone_cache_size_flag,
+   (void *) &prm_log_postpone_cache_size_default,
+   (void *) &PRM_LOG_POSTPONE_CACHE_SIZE,
+   (void *) &prm_log_postpone_cache_size_upper,
+   (void *) &prm_log_postpone_cache_size_lower,
+   (char *) NULL,
+   (DUP_PRM_FUNC) NULL,
+   (DUP_PRM_FUNC) NULL},
 };
 
 #define NUM_PRM ((int)(sizeof(prm_Def)/sizeof(prm_Def[0])))
