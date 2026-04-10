a012ac70cf6dfa626afcafd728e8a903506f35cc
childyouth <childyouth@cubrid.com>
2026-04-10 17:18:42 +0900

    remove unnecessary modification

--- src/parser/xasl_generation.c
+++ src/parser/xasl_generation.c
@@ -22261,8 +22261,6 @@ pt_append_omitted_on_update_expr_assignments (PARSER_CONTEXT * parser, PT_NODE *
 	  new_lhs_of_assign->info.name.resolved = cls->header.ch_name;
 	  new_lhs_of_assign->info.name.spec_id = spec_id;
 
-	  //   PT_OP_TYPE op = pt_op_type_from_default_expr_type (att->on_update_default_expr);
-	  //   PT_NODE *new_rhs_of_assign = parser_make_expression (parser, op, NULL, NULL, NULL);
 	  PT_NODE *new_rhs_of_assign = pt_make_expression_default_expr (parser, NULL, att->on_update_default_expr);
 
 	  if (new_rhs_of_assign == NULL)
