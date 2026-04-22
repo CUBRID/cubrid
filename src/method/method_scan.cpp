7d786ea73beba1b4d16b84b3f0bcf2e057186708
mhoh3963 <mhoh@cubrid.com>
2026-04-23 08:18:15 +0900

    - add clear and swap code for method args related variables

--- src/method/method_scan.cpp
+++ src/method/method_scan.cpp
@@ -105,6 +105,16 @@ namespace cubscan
     {
       close_value_array ();
       pr_clear_value_vector (m_arg_vector);
+      m_arg_vector.clear ();
+      m_arg_use_vector.clear ();
+      m_arg_dom_vector.clear ();
+
+      if (is_final)
+        {
+          std::vector<TP_DOMAIN *>().swap(m_arg_dom_vector);
+          std::vector<DB_VALUE>().swap(m_arg_vector);
+          std::vector<bool>().swap(m_arg_use_vector);
+        }
 
       if (is_final && m_method_group)
 	{
