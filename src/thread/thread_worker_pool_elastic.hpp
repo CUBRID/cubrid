1d17b29c6170dbf21adbe0c591c94617cf4a5196
hyahong <yechan0815@naver.com>
2026-04-23 00:35:24 +0000

    Implement a basic elastic worker pool.

---
+++ src/thread/thread_worker_pool_elastic.hpp
@@ -0,0 +1,122 @@
+/*
+ *
+ * Copyright 2016 CUBRID Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+/*
+ * thread_worker_pool_elastic.hpp
+ */
+
+#ifndef _THREAD_WORKER_POOL_ELASTIC_HPP_
+#define _THREAD_WORKER_POOL_ELASTIC_HPP_
+
+#if !defined (SERVER_MODE)
+#error Wrong module
+#endif // not SERVER_MODE
+
+// same module include
+#include "thread_worker_pool_impl.hpp"
+
+// cubrid includes
+#include "error_manager.h"
+
+// system includes
+#include <chrono>
+
+namespace cubthread
+{
+  // elastic_worker_pool<Stats>
+  //
+  // description
+  //    worker pool that maintains target concurrency by spawning
+  //    additional workers when existing workers enter a known wait
+  //    (e.g., blocked on a transaction lock).
+  template <bool Stats>
+  class elastic_worker_pool : public worker_pool_impl<Stats>
+  {
+    public:
+      // forward definition
+      class core_elastic;
+
+      ~elastic_worker_pool ();
+
+    protected:
+      elastic_worker_pool (std::size_t pool_size, std::size_t core_count, const char *name, entry_manager &entry_mgr,
+			   bool pool_threads = false, wait_seconds idle_timeout = std::chrono::seconds (5));
+
+      std::unique_ptr<worker_pool::core> allocate_core (bool pool_threads) override;
+  };
+
+  // elastic_worker_pool<Stats>::core_elastic
+  //
+  // description
+  //    TODO: add description
+  //
+  template <bool Stats>
+  class elastic_worker_pool<Stats>::core_elastic : public worker_pool_impl<Stats>::core_impl
+  {
+    public:
+      ~core_elastic ();
+
+    protected:
+      core_elastic (bool pool_threads);
+  };
+
+} // namespace cubthread
+
+namespace cubthread
+{
+  //////////////////////////////////////////////////////////////////////////
+  // elastic_worker_pool<Stats>
+  //////////////////////////////////////////////////////////////////////////
+
+  template <bool Stats>
+  elastic_worker_pool<Stats>::elastic_worker_pool (std::size_t pool_size, std::size_t core_count, const char *name,
+      entry_manager &entry_mgr, bool pool_threads, wait_seconds idle_timeout)
+    : worker_pool_impl<Stats> (pool_size, core_count, name, entry_mgr, pool_threads, idle_timeout)
+  {
+  }
+
+  template <bool Stats>
+  elastic_worker_pool<Stats>::~elastic_worker_pool ()
+  {
+  }
+
+  template <bool Stats>
+  std::unique_ptr<typename worker_pool::core>
+  elastic_worker_pool<Stats>::allocate_core (bool pool_threads)
+  {
+    return std::unique_ptr<worker_pool::core> (new core_elastic (pool_threads));
+  }
+
+  //////////////////////////////////////////////////////////////////////////
+  // elastic_worker_pool<Stats>::core_elastic
+  //////////////////////////////////////////////////////////////////////////
+
+  template <bool Stats>
+  elastic_worker_pool<Stats>::core_elastic::core_elastic (bool pool_threads)
+    : worker_pool_impl<Stats>::core_impl (pool_threads)
+  {
+  }
+
+  template <bool Stats>
+  elastic_worker_pool<Stats>::core_elastic::~core_elastic ()
+  {
+  }
+
+}
+
+#endif
