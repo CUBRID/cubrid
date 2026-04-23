680f392c7b6c04a092cfe5ded7cdd8953d0a8c3a
mhoh3963 <mhoh@cubrid.com>
2026-04-23 16:02:53 +0900

    - fix error occurred by executin regression testing case

--- src/method/method_scan.cpp
+++ src/method/method_scan.cpp
@@ -106,28 +106,25 @@ namespace cubscan
       close_value_array ();
       pr_clear_value_vector (m_arg_vector);
 
-      m_arg_vector.clear ();
-      m_arg_use_vector.clear ();
-      m_arg_dom_vector.clear ();
-
-      if (is_final)
+      if (is_final && m_method_group)
 	{
+	  m_method_group->reset (true);
+	  m_method_group->end ();
+
+	  cubmethod::runtime_context *rctx = cubmethod::get_rctx (m_thread_p);
+	  if (rctx)
+	    {
+	      rctx->pop_stack (m_thread_p, m_method_group);
+	    }
+	  m_method_group = nullptr; // will be destroyed by cubmethod::runtime_context
+
+	  m_arg_vector.clear ();
+	  m_arg_use_vector.clear ();
+	  m_arg_dom_vector.clear ();
+
 	  std::vector<TP_DOMAIN *>().swap (m_arg_dom_vector);
 	  std::vector<DB_VALUE>().swap (m_arg_vector);
 	  std::vector<bool>().swap (m_arg_use_vector);
-
-	  if (m_method_group)
-	    {
-	      m_method_group->reset (true);
-	      m_method_group->end ();
-
-	      cubmethod::runtime_context *rctx = cubmethod::get_rctx (m_thread_p);
-	      if (rctx)
-		{
-		  rctx->pop_stack (m_thread_p, m_method_group);
-		}
-	      m_method_group = nullptr; // will be destroyed by cubmethod::runtime_context
-	    }
 	}
     }
 
