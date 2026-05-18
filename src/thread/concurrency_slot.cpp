f9d626381a1d5d45256736b76c34a531f14e237d
hyahong <yechan0815@naver.com>
2026-05-18 05:03:58 +0000

    Implement the slot daemon

--- src/thread/concurrency_slot.cpp
+++ src/thread/concurrency_slot.cpp
@@ -26,6 +26,9 @@
 #include "system_parameter.h"
 #include "error_manager.h"
 
+#include <algorithm>
+#include <utility>
+
 // XXX: SHOULD BE THE LAST INCLUDE HEADER
 #include "memory_wrapper.hpp"
 
@@ -156,6 +159,23 @@ namespace cubthread
   }
 
   void
+  concurrency_slot::return_to_pool (std::unique_ptr<concurrency_slot> slot)
+  {
+    assert (m_owner_pool && m_holder_pool);
+    assert (slot.get () == this);
+
+    auto returned_from_owner = m_owner_pool->return_slot (std::move (slot), false);
+    if (returned_from_owner)
+      {
+	auto returned_from_holder = m_holder_pool->return_slot (std::move (returned_from_owner), false);
+	if (returned_from_holder)
+	  {
+	    m_owner_pool->return_slot (std::move (returned_from_holder), true);
+	  }
+      }
+  }
+
+  void
   concurrency_slot::start_waiting ()
   {
     m_wait = true;
@@ -168,13 +188,23 @@ namespace cubthread
     m_wait = false;
   }
 
+  bool
+  concurrency_slot::has_wait_expired (const std::chrono::time_point<std::chrono::steady_clock> &now)
+  {
+    constexpr auto threshold = std::chrono::milliseconds (50);
+
+    return m_wait && (now - m_wait_since >= threshold);
+  }
+
   //////////////////////////////////////////////////////////////////////////
   // concurrency_slot_pool
   //////////////////////////////////////////////////////////////////////////
 
-  concurrency_slot_pool::concurrency_slot_pool (std::mutex &mtx)
+  concurrency_slot_pool::concurrency_slot_pool (void *parent, std::mutex &mtx)
     : concurrency_slot_subscriber (concurrency_slot_daemon::get_publisher ())
+    , m_parent (parent)
     , m_available_slots ()
+    , m_surplus (false)
     , m_surplus_since ()
     , m_slot_count (0)
     , m_target_count (0)
@@ -199,6 +229,8 @@ namespace cubthread
     m_slot_count = concurrency;
     m_target_count = concurrency;
 
+    check_surplus_slots ();
+
     // attach to the daemon
     concurrency_slot_subscriber::activate (identifier);
   }
@@ -214,6 +246,7 @@ namespace cubthread
   void
   concurrency_slot_pool::adjust_concurrency (std::size_t concurrency, std::unique_lock<std::mutex> &ulock)
   {
+    std::vector<std::unique_ptr<concurrency_slot>> bucket;
     std::size_t i;
 
     m_target_count = concurrency;
@@ -233,10 +266,23 @@ namespace cubthread
 	  {
 	    auto slot = std::move (m_available_slots.front ());
 	    m_available_slots.pop ();
-	    slot.reset ();
+	    if (slot->m_owner_pool == this)
+	      {
+		slot.reset ();
 
-	    m_slot_count--;
+		m_slot_count--;
+	      }
+	    else
+	      {
+		bucket.emplace_back (std::move (slot));
+	      }
 	  }
+	for (auto &slot : bucket)
+	  {
+	    m_available_slots.push (std::move (slot));
+	  }
+
+	check_surplus_slots ();
       }
   }
 
@@ -264,6 +310,8 @@ namespace cubthread
     assert (slot);
     slot->set_holder_pool (this);
     assert (slot->get_owner_pool () && slot->get_holder_pool ());
+
+    check_surplus_slots ();
 
     return slot;
   }
@@ -294,6 +342,7 @@ namespace cubthread
 	thread_p->m_slot->set_holder_pool (this);
 	assert (thread_p->m_slot->get_owner_pool () && thread_p->m_slot->get_holder_pool ());
 
+	check_surplus_slots ();
 	return true;
       }
 
@@ -378,11 +427,13 @@ namespace cubthread
     // reset
     slot->reset ();
 
-    if (m_target_count < m_slot_count)
+    if (slot->m_owner_pool == this && m_target_count < m_slot_count)
       {
 	slot.reset ();
 
 	m_slot_count--;
+
+	check_surplus_slots ();
 	return;
       }
 
@@ -420,18 +471,87 @@ namespace cubthread
 	waiter->unlock ();
 	ulock.lock ();
       }
+
+    check_surplus_slots ();
   }
 
-  bool
-  concurrency_slot_pool::borrow_surplus_slots ()
+  std::unique_ptr<concurrency_slot>
+  concurrency_slot_pool::return_slot (std::unique_ptr<concurrency_slot> slot, bool force)
   {
-    return false;
+    assert (slot);
+
+    std::unique_lock<std::mutex> ulock (*m_mutex);
+
+    if ((m_available_slots.empty () && !m_wait_queue.empty ()) ||
+	(slot->m_owner_pool == this && m_slot_count > m_target_count) ||
+	force)
+      {
+	release_slot (std::move (slot), ulock);
+
+	return nullptr;
+      }
+    return slot;
+  }
+
+  std::vector<std::unique_ptr<concurrency_slot>>
+      concurrency_slot_pool::borrow_surplus_slots (const std::chrono::time_point<std::chrono::steady_clock> &now)
+  {
+    constexpr auto threshold = std::chrono::milliseconds (2000);
+    std::vector<std::unique_ptr<concurrency_slot>> slots;
+
+    std::unique_lock<std::mutex> ulock (*m_mutex);
+
+    if (m_surplus &&
+	now - m_surplus_since > threshold &&
+	m_available_slots.size () >= SLOT_SURPLUS_THRESHOLD)
+      {
+	while (m_available_slots.size () >= SLOT_SURPLUS_THRESHOLD)
+	  {
+	    slots.emplace_back (std::move (m_available_slots.front ()));
+	    m_available_slots.pop ();
+	  }
+
+	check_surplus_slots ();
+      }
+
+    return slots;
   }
 
   std::size_t
   concurrency_slot_pool::available_slots ()
   {
     return m_available_slots.size ();
+  }
+
+  const void *
+  concurrency_slot_pool::get_parent ()
+  {
+    return m_parent;
+  }
+
+  float
+  concurrency_slot_pool::get_score ()
+  {
+    std::lock_guard<std::mutex> lock (*m_mutex);
+
+    return -static_cast<float> (m_available_slots.size ()) + m_wait_queue.size () * 2.0f;
+  }
+
+  void
+  concurrency_slot_pool::check_surplus_slots ()
+  {
+    if (m_available_slots.size () >= SLOT_SURPLUS_THRESHOLD)
+      {
+	if (!m_surplus)
+	  {
+	    m_surplus_since = std::chrono::steady_clock::now ();
+	    m_surplus = true;
+	  }
+      }
+    else
+      {
+	m_surplus = false;
+      }
   }
 
   //////////////////////////////////////////////////////////////////////////
@@ -441,14 +561,25 @@ namespace cubthread
   class concurrency_slot_daemon_task : public entry_task
   {
     public:
-      concurrency_slot_daemon_task ();
+      concurrency_slot_daemon_task (concurrency_slot_publisher *publisher);
       ~concurrency_slot_daemon_task () = default;
 
       void execute (entry &thread_ref) override;
 
     private:
+      void steal_from_entries_if_excess (std::vector<std::unique_ptr<concurrency_slot>> &slots, void *identifier);
+      void steal_from_cores_if_excess (std::vector<std::unique_ptr<concurrency_slot>> &slots, void *identifier,
+				       std::vector<concurrency_slot_subscriber *> &subs);
+
+      void distribute_slots (std::vector<std::unique_ptr<concurrency_slot>> &slots,
+			     std::vector<concurrency_slot_subscriber *> &subs);
+
+      void wakeup_workers (void *identifier, std::vector<concurrency_slot_subscriber *> &subs);
+
       void tune_parameters (std::size_t &max_transaction_concurrency, std::size_t &max_transaction_worker);
       void check_and_propagate_parameters ();
+
+      concurrency_slot_publisher *m_publisher;
 
       std::size_t m_max_transaction_concurrency;
       std::size_t m_max_transaction_worker;
@@ -458,8 +589,9 @@ namespace cubthread
   // concurrency_slot_daemon task definition
   //////////////////////////////////////////////////////////////////////////
 
-  concurrency_slot_daemon_task::concurrency_slot_daemon_task ()
-    : m_max_transaction_concurrency (static_cast<std::size_t> (prm_get_integer_value (PRM_ID_MAX_TRANSACTION_CONCURRENCY)))
+  concurrency_slot_daemon_task::concurrency_slot_daemon_task (concurrency_slot_publisher *publisher)
+    : m_publisher (publisher)
+    , m_max_transaction_concurrency (static_cast<std::size_t> (prm_get_integer_value (PRM_ID_MAX_TRANSACTION_CONCURRENCY)))
     , m_max_transaction_worker (static_cast<std::size_t> (prm_get_integer_value (PRM_ID_MAX_TRANSACTION_WORKER)))
   {
   }
@@ -467,12 +599,145 @@ namespace cubthread
   void
   concurrency_slot_daemon_task::execute (entry &thread_ref)
   {
-    // 1. traverse all entries and steal the concurrency slot from any entry
+    // 1. apply concurrency parameter changes at runtime.
+    // 2. traverse all entries and steal the concurrency slot from any entry
     //    whose wait time exceeds the threshold. (identifier = worker pool pointer)
-    // 2. rebalance slots across cores.
-    // 3. apply concurrency parameter changes at runtime.
+    // 3. take the slots from cores which has them over threshold time.
+    // 4. rebalance slots across cores.
+    // 5. wake the workers in each core if there are the tasks in wait queue
 
+    // 1.
     check_and_propagate_parameters ();
+
+    static_cast<concurrency_slot_daemon *> (m_publisher)->traverse_subscribers ([this] (void *identifier, auto subs)
+    {
+      std::vector<std::unique_ptr<concurrency_slot>> slots;
+
+      assert (!subs.empty ());
+
+      // 2.
+      steal_from_entries_if_excess (slots, identifier);
+
+      // 3.
+      steal_from_cores_if_excess (slots, identifier, subs);
+
+      // 4.
+      distribute_slots (slots, subs);
+      assert (slots.empty ());
+
+      // 5.
+      wakeup_workers (identifier, subs);
+    });
+  }
+
+  void
+  concurrency_slot_daemon_task::steal_from_entries_if_excess (std::vector<std::unique_ptr<concurrency_slot>> &slots,
+      void *identifier)
+  {
+    std::chrono::time_point<std::chrono::steady_clock> now = std::chrono::steady_clock::now ();
+    auto func = [&slots, &now] (entry &thread_ref, bool &stop)
+    {
+      thread_ref.lock ();
+
+      if (thread_ref.m_status == cubthread::entry::status::TS_WAIT &&
+	  thread_ref.m_slot && thread_ref.m_slot->has_wait_expired (now))
+	{
+	  slots.emplace_back (std::move (thread_ref.m_slot));
+	  thread_ref.m_slot = nullptr;
+
+	  thread_ref.unlock ();
+	}
+      else
+	{
+	  thread_ref.unlock ();
+	}
+    };
+
+    auto *base = static_cast<worker_pool *> (identifier);
+    if (auto *pool = dynamic_cast<worker_pool_elastic<stats_t::on> *> (base))
+      {
+	pool->map_running_contexts (func);
+      }
+    else if (auto *pool = dynamic_cast<worker_pool_elastic<stats_t::off> *> (base))
+      {
+	pool->map_running_contexts (func);
+      }
+  }
+
+  void
+  concurrency_slot_daemon_task::steal_from_cores_if_excess (std::vector<std::unique_ptr<concurrency_slot>> &slots,
+      void *identifier, std::vector<concurrency_slot_subscriber *> &subs)
+  {
+    std::chrono::time_point<std::chrono::steady_clock> now = std::chrono::steady_clock::now ();
+
+    for (auto &sub : subs)
+      {
+	auto surplus_slots = static_cast<concurrency_slot_pool *> (sub)->borrow_surplus_slots (now);
+	slots.insert (
+		slots.end (),
+		std::make_move_iterator (surplus_slots.begin ()),
+		std::make_move_iterator (surplus_slots.end ())
+	);
+      }
+  }
+
+  void
+  concurrency_slot_daemon_task::distribute_slots (std::vector<std::unique_ptr<concurrency_slot>> &slots,
+      std::vector<concurrency_slot_subscriber *> &subs)
+  {
+    std::vector<std::pair<concurrency_slot_pool *, float>> scores;
+    std::size_t i = 0;
+
+    for (auto &sub : subs)
+      {
+	auto pool = static_cast<concurrency_slot_pool *> (sub);
+	scores.emplace_back (pool, pool->get_score ());
+      }
+    subs.clear ();
+
+    std::sort (scores.begin (), scores.end (), [] (const auto &a, const auto &b)
+    {
+      return a.second > b.second;
+    });
+
+    while (!slots.empty ())
+      {
+	auto slot = std::move (slots.back ());
+	slots.pop_back ();
+
+	auto pool = static_cast<concurrency_slot_pool *> (scores[i++ % scores.size ()].first);
+	pool->release_slot (std::move (slot));
+	if (std::find (subs.begin (), subs.end (), static_cast<concurrency_slot_subscriber *> (pool)) == subs.end ())
+	  {
+	    subs.push_back (pool);
+	  }
+      }
+  }
+
+  void
+  concurrency_slot_daemon_task::wakeup_workers (void *identifier, std::vector<concurrency_slot_subscriber *> &subs)
+  {
+    auto *base = static_cast<worker_pool *> (identifier);
+    if (dynamic_cast<worker_pool_elastic<stats_t::on> *> (base))
+      {
+	for (concurrency_slot_subscriber *it : subs)
+	  {
+	    auto parent = static_cast<concurrency_slot_pool *> (it)->get_parent ();
+	    assert (parent);
+
+	    static_cast<worker_pool_elastic<stats_t::on>::core_elastic *> (const_cast<void *> (parent))->adjust_workers ();
+	  }
+      }
+    else if (dynamic_cast<worker_pool_elastic<stats_t::off> *> (base))
+      {
+	for (concurrency_slot_subscriber *it : subs)
+	  {
+	    auto parent = static_cast<concurrency_slot_pool *> (it)->get_parent ();
+	    assert (parent);
+
+	    static_cast<worker_pool_elastic<stats_t::off>::core_elastic *> (const_cast<void *> (parent))->adjust_workers ();
+	  }
+      }
   }
 
   void
@@ -574,8 +839,8 @@ namespace cubthread
 	return;
       }
 
-    looper loop = looper (std::chrono::milliseconds (1000));
-    concurrency_slot_daemon_task *daemon_task = new concurrency_slot_daemon_task ();
+    looper loop = looper (std::chrono::milliseconds (50));
+    concurrency_slot_daemon_task *daemon_task = new concurrency_slot_daemon_task (this);
 
     m_daemon = cubthread::get_manager ()->create_daemon (loop, daemon_task, "concurrency");
   }
