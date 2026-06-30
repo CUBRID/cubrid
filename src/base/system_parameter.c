f27f8ea87cbdff693dea2202604f6503766dddb3
hyahong <yechan0815@naver.com>
2026-06-30 08:59:03 +0000

    change the unit of pgbuf_latch_timeout to msec

--- src/base/system_parameter.c
+++ src/base/system_parameter.c
@@ -679,7 +679,7 @@ static const char sysprm_ha_conf_file_name[] = "cubrid_ha.conf";
 
 #define PRM_NAME_USE_STAT_ESTIMATION "use_stat_estimation"
 
-#define PRM_NAME_PAGE_LATCH_TIMEOUT "page_latch_timeout"
+#define PRM_NAME_PAGE_LATCH_TIMEOUT_IN_MSECS "page_latch_timeout_in_msecs"
 
 #define PRM_NAME_RECOVERY_PROGRESS_LOGGING_INTERVAL "recovery_progress_logging_interval"
 
@@ -2303,11 +2303,11 @@ bool PRM_STATDUMP_FORCE_ADD_INT_MAX = false;
 static bool prm_statdump_force_add_int_max_default = false;
 static unsigned int prm_statdump_force_add_int_max_flag = 0;
 
-int PRM_PAGE_LATCH_TIMEOUT = 300;
-static int prm_page_latch_timeout_default = 300;
-static int prm_page_latch_timeout_upper = 3000;
-static int prm_page_latch_timeout_lower = 0;
-static unsigned int prm_page_latch_timeout_flag = 0;
+int PRM_PAGE_LATCH_TIMEOUT_IN_MSECS = 300 * 1000;
+static int prm_page_latch_timeout_in_msecs_default = 300 * 1000;
+static int prm_page_latch_timeout_in_msecs_upper = 3000 * 1000;
+static int prm_page_latch_timeout_in_msecs_lower = 0;
+static unsigned int prm_page_latch_timeout_in_msecs_flag = 0;
 
 int PRM_RECOVERY_PROGRESS_LOGGING_INTERVAL = 0;
 static int prm_recovery_progress_logging_interval_default = 0;
@@ -5935,15 +5935,15 @@ static SYSPRM_PARAM prm_Def[] = {
    (char *) NULL,
    (DUP_PRM_FUNC) NULL,
    (DUP_PRM_FUNC) NULL},
-  {PRM_ID_PAGE_LATCH_TIMEOUT,
-   PRM_NAME_PAGE_LATCH_TIMEOUT,
+  {PRM_ID_PAGE_LATCH_TIMEOUT_IN_MSECS,
+   PRM_NAME_PAGE_LATCH_TIMEOUT_IN_MSECS,
    (PRM_FOR_SERVER | PRM_HIDDEN),
    PRM_INTEGER,
-   &prm_page_latch_timeout_flag,
-   (void *) &prm_page_latch_timeout_default,
-   (void *) &PRM_PAGE_LATCH_TIMEOUT,
-   (void *) &prm_page_latch_timeout_upper,
-   (void *) &prm_page_latch_timeout_lower,
+   &prm_page_latch_timeout_in_msecs_flag,
+   (void *) &prm_page_latch_timeout_in_msecs_default,
+   (void *) &PRM_PAGE_LATCH_TIMEOUT_IN_MSECS,
+   (void *) &prm_page_latch_timeout_in_msecs_upper,
+   (void *) &prm_page_latch_timeout_in_msecs_lower,
    (char *) NULL,
    (DUP_PRM_FUNC) NULL,
    (DUP_PRM_FUNC) NULL},
