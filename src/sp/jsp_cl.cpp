ae1d3d0d25ec449f6e44ed8537c78503543a23f0
childyouth <39540545+childyouth@users.noreply.github.com>
2026-07-10 13:24:25 +0900

    Revert "[CBRD-27030] Fix null system datetime when evaluating default express…"
    This reverts commit ebf08d0803487c1a8c4b35850b3857050ca67783.

--- src/sp/jsp_cl.cpp
+++ src/sp/jsp_cl.cpp
@@ -753,7 +753,8 @@ jsp_call_stored_procedure (PARSER_CONTEXT *parser, PT_NODE *statement)
       PT_NODE *default_next_node_list = jsp_get_default_expr_node_list (parser, sig, &flag_si_datetime);
       if (default_next_node_list != NULL && flag_si_datetime)
 	{
-	  error = db_ensure_server_info (parser, SI_SYS_DATETIME);
+	  // consider using 'db_calculate_current_server_time' in db_vdb.c for less server communication
+	  error = qp_get_server_info (parser, SI_SYS_DATETIME);
 	}
       statement->info.method_call.arg_list = parser_append_node (default_next_node_list,
 					     statement->info.method_call.arg_list);
