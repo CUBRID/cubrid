339cd1c132d6d967458faaf96742a0398efa814a
Byungwook Kim <bwkim@cubrid.com>
2026-07-10 15:38:27 +0900

    modifed as commented

--- src/transaction/boot_sr.c
+++ src/transaction/boot_sr.c
@@ -2766,10 +2766,10 @@ error:
   vacuum_stop_master (thread_p);
 
 #if defined(SERVER_MODE)
-  pl_server_destroy ();
 #ifdef CCI_XA
   dblink_2pc_daemon_stop ();
 #endif
+  pl_server_destroy ();
   cdc_daemons_destroy ();
 
   BO_DISABLE_FLUSH_DAEMONS ();
