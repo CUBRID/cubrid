32598eff8577675ebbb53675c92319a9af7b3c36
jongmin-won <kceeck@gmail.com>
2026-07-30 13:50:36 +0900

    [CBRD-27016] Add comment explaining LEAVE->NORMAL for collection string elements
    컬렉션 문자열 원소의 collation_flag를 LEAVE에서 NORMAL로 확정하는 두 지점
    (pt_node_to_db_domain, pt_node_data_type_to_db_domain)에, 왜 NORMAL로
    승격해도 안전한지를 설명하는 짧은 주석을 추가한다.
    
    LEAVE는 인자가 모두 MAYBE(bind 변수)인 문자열 결과일 때만 설정되며 이때
    codeset/collation은 이미 LANG_SYS로 초기화되어 있다. 컬렉션 원소는 스칼라와
    달리 실행 시점에 LEAVE를 해소하는 경로가 없으므로 생성 시점에 확정한다.

--- src/parser/parse_dbi.c
+++ src/parser/parse_dbi.c
@@ -2127,6 +2127,8 @@ pt_node_data_type_to_db_domain (PARSER_CONTEXT * parser, PT_NODE * dt, PT_TYPE_E
 	  domain = pt_data_type_to_db_domain (parser, dt, NULL);
 	  if (domain)
 	    {
+	      /* LEAVE is set only for an all-maybes string result, whose codeset/collation are already LANG_SYS.
+	       * Collection elements have no runtime resolution path, so finalize to NORMAL here. */
 	      if (TP_TYPE_HAS_COLLATION (TP_DOMAIN_TYPE (domain)) && domain->collation_flag == TP_DOMAIN_COLL_LEAVE)
 		{
 		  domain->collation_flag = TP_DOMAIN_COLL_NORMAL;
@@ -2261,6 +2263,8 @@ pt_node_to_db_domain (PARSER_CONTEXT * parser, PT_NODE * node, const char *class
 		    }
 		  else
 		    {
+		      /* LEAVE is set only for an all-maybes string result, whose codeset/collation are already LANG_SYS.
+		       * Collection elements have no runtime resolution path, so finalize to NORMAL here. */
 		      if (TP_TYPE_HAS_COLLATION (TP_DOMAIN_TYPE (domain))
 			  && domain->collation_flag == TP_DOMAIN_COLL_LEAVE)
 			{
