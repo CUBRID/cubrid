7374c5188767de8eb9fef5387c8745ee8c912fbf
ctshim <78332782+ctshim@users.noreply.github.com>
2026-08-11 09:03:00 +0900

    [CBRD-26768] Fix stack-use-after-scope in do_prepare_select() (#7144) (#7638)
    http://jira.cubrid.org/browse/CBRD-26768
    
    * Fix stack-use-after-scope in do_prepare_select() function.
    * backport#7144

--- src/query/execute_statement.c
+++ src/query/execute_statement.c
@@ -14528,6 +14528,7 @@ do_prepare_select (PARSER_CONTEXT * parser, PT_NODE * statement)
 
   COMPILE_CONTEXT *contextp;
   XASL_STREAM stream;
+  XASL_NODE_HEADER xasl_header = { 0 };
 
   contextp = &parser->context;
 
@@ -14589,7 +14590,6 @@ do_prepare_select (PARSER_CONTEXT * parser, PT_NODE * statement)
   contextp->recompile_xasl = statement->flag.recompile;
   if (statement->flag.recompile == 0)
     {
-      XASL_NODE_HEADER xasl_header;
       stream.xasl_header = &xasl_header;
 
       err = prepare_query (contextp, &stream);
