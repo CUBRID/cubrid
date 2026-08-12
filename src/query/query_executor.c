40e826654eb664b18d67073b28882e660dd0e5d5
Ilhan <70995854+xmilex-git@users.noreply.github.com>
2026-08-12 15:32:22 +0900

    [CBRD-26927] [Backport] Read hash-aggregate tuple before freeing its list page
    http://jira.cubrid.org/browse/CBRD-26927
    
    Move the tuple read before the page unfix in `qexec_hash_gby_put_next()` so the tuple is always read from a valid, fixed buffer. When the freed page is reused by another worker, the corrupted bytes are interpreted as a tuple, causing the server to SIGABRT on an `or_advance` assert.
    
    Clean cherry-pick of develop 097af16f8 (#7346).

--- src/query/query_executor.c
+++ src/query/query_executor.c
@@ -3991,6 +3991,14 @@ qexec_hash_gby_put_next (THREAD_ENTRY * thread_p, const RECDES * recdes, void *a
 	    {
 	      peek = PEEK;	/* avoid unnecessary COPY */
 	    }
+
+	  /* read tuple into value */
+	  if (qdata_load_agg_hentry_from_tuple (thread_p, data, context->temp_part_key, context->temp_part_value,
+						context->key_domains, context->accumulator_domains) != NO_ERROR)
+	    {
+	      qmgr_free_old_page_and_init (thread_p, page, list_idp->tfile_vfid);
+	      return ER_FAILED;
+	    }
 	  qmgr_free_old_page_and_init (thread_p, page, list_idp->tfile_vfid);
 	}
       else
@@ -4004,13 +4012,13 @@ qexec_hash_gby_put_next (THREAD_ENTRY * thread_p, const RECDES * recdes, void *a
 	      return ER_FAILED;
 	    }
 	  data = context->tuple_recdes.data;
-	}
 
-      /* read tuple into value */
-      if (qdata_load_agg_hentry_from_tuple (thread_p, data, context->temp_part_key, context->temp_part_value,
-					    context->key_domains, context->accumulator_domains) != NO_ERROR)
-	{
-	  return ER_FAILED;
+	  /* read tuple into value */
+	  if (qdata_load_agg_hentry_from_tuple (thread_p, data, context->temp_part_key, context->temp_part_value,
+						context->key_domains, context->accumulator_domains) != NO_ERROR)
+	    {
+	      return ER_FAILED;
+	    }
 	}
 
       /* aggregate */
