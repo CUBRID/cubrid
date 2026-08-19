f55a6c9b169ffe9d01765f5b904d73c5c92a4c33
Byungwook Kim <bwkim@cubrid.com>
2026-08-19 09:33:15 +0900

    slip

--- src/query/query_executor.c
+++ src/query/query_executor.c
@@ -25889,7 +25889,6 @@ qexec_execute_build_columns (THREAD_ENTRY * thread_p, XASL_NODE * xasl, XASL_STA
 			 + (default_expr_format ? strlen (default_expr_format) : 0));
 
 		  default_value_string = (char *) db_private_alloc (thread_p, len + 1);
->>>>>>> 61137e9a3 ([CBRD-27273] core dump at 'show columns' for default to_char column)
 		  if (default_value_string == NULL)
 		    {
 		      GOTO_EXIT_ON_ERROR;
