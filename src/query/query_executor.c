8ad12284ac6742cb00b48e567b7b33685f9fa2bd
Byungwook Kim <bwkim@cubrid.com>
2026-08-19 12:55:34 +0900

    [CBRD-27273] core dump at 'show columns' for default to_char column (#7737)
    http://jira.cubrid.org/browse/CBRD-27273 (backport 11.4)

--- src/query/query_executor.c
+++ src/query/query_executor.c
@@ -25888,7 +25888,7 @@ qexec_execute_build_columns (THREAD_ENTRY * thread_p, XASL_NODE * xasl, XASL_STA
 			 + 6 /* parenthesis, a comma, a blank and quotes */  + strlen (default_expr_type_string)
 			 + (default_expr_format ? strlen (default_expr_format) : 0));
 
-		  default_value_string = (char *) malloc (len + 1);
+		  default_value_string = (char *) db_private_alloc (thread_p, len + 1);
 		  if (default_value_string == NULL)
 		    {
 		      GOTO_EXIT_ON_ERROR;
