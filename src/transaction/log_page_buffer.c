e2df9d088667ac9b6367e6e48b2c7f1529c566e1
Lee Yunjin <gzblues61@daum.net>
2026-09-05 12:39:35 +0900

    [CBRD-26996] Normalize std::atomic template spacing in log_page_buffer.c

--- src/transaction/log_page_buffer.c
+++ src/transaction/log_page_buffer.c
@@ -195,8 +195,8 @@ static int rv;
 typedef struct log_buffer LOG_BUFFER;
 struct log_buffer
 {
-  std::atomic < LOG_PAGEID > pageid;	/* Logical page of the log. (Page identifier of the infinite log) */
-  std::atomic < LOG_PHY_PAGEID > phy_pageid;	/* Physical pageid for the active log portion */
+  std::atomic<LOG_PAGEID> pageid;	/* Logical page of the log. (Page identifier of the infinite log) */
+  std::atomic<LOG_PHY_PAGEID> phy_pageid;	/* Physical pageid for the active log portion */
   bool dirty;			/* Is page dirty */
   LOG_PAGE *logpage;		/* The actual buffered log page */
 };
