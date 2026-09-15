537f6f5872e78f8acaa539e1936e02be6e990a0a
Na, Hyunik <hyunikn@gmail.com>
2026-09-15 17:52:01 +0900

    Revert "fill in omitted default arguments of direct SP/package calls"
    This reverts commit fc765484d241033c3d8a807234d31c23b426cc46.

--- src/method/method_callback.cpp
+++ src/method/method_callback.cpp
@@ -974,25 +974,9 @@ namespace cubmethod
 		      param_info.type = db_get_int (&arg_type);
 		    }
 
-		  // a parameter "has a default" iff it is optional; the default value itself may be NULL
-		  // (for ':= NULL'), so it must not be inferred from the default_value column being null
-		  if (db_get (arg_mop_p, SP_ARG_ATTR_IS_OPTIONAL, &has_default) == NO_ERROR)
+		  if (db_get (arg_mop_p, SP_ARG_ATTR_DEFAULT_VALUE, &has_default) == NO_ERROR)
 		    {
-		      param_info.has_default =
-			      (!DB_IS_NULL (&has_default) && db_get_int (&has_default) != 0) ? 1 : 0;
-		    }
-		  if (param_info.has_default)
-		    {
-		      DB_VALUE dflt;
-		      if (db_get (arg_mop_p, SP_ARG_ATTR_DEFAULT_VALUE, &dflt) == NO_ERROR)
-			{
-			  const char *dv = DB_IS_NULL (&dflt) ? NULL : db_get_string (&dflt);
-			  if (dv != NULL)
-			    {
-			      param_info.default_value.assign (dv);
-			    }
-			  pr_clear_value (&dflt);
-			}
+		      param_info.has_default = DB_IS_NULL (&has_default) ? 0 : 1;
 		    }
 
 		  pr_clear_value (&mode);
--- src/sp/pl_struct_compile.cpp
+++ src/sp/pl_struct_compile.cpp
@@ -505,7 +505,6 @@ namespace cubpl
     serializator.pack_int (scale);
     serializator.pack_int (charset);
     serializator.pack_int (has_default);
-    serializator.pack_string (default_value);
 
     if (!DB_IS_NULL (&value))
       {
@@ -532,7 +531,6 @@ namespace cubpl
     size += serializator.get_packed_int_size (size); // scale
     size += serializator.get_packed_int_size (size); // charset
     size += serializator.get_packed_int_size (size); // has_default
-    size += serializator.get_packed_string_size (default_value, size);
 
     size += serializator.get_packed_int_size (size); // value is null
     if (!DB_IS_NULL (&value))
@@ -557,7 +555,6 @@ namespace cubpl
     deserializator.unpack_int (scale);
     deserializator.unpack_int (charset);
     deserializator.unpack_int (has_default);
-    deserializator.unpack_string (default_value);
 
     int value_is_null;
     deserializator.unpack_int (value_is_null);
