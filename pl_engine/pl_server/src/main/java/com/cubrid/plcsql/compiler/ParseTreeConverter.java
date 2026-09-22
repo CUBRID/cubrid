58040164a3f73a9eebee514c2b23fc6c45358c54
Na, Hyunik <hyunikn@gmail.com>
2026-09-22 16:40:56 +0900

    ban record types in top-level SPs' parameter types and return types

--- pl_engine/pl_server/src/main/java/com/cubrid/plcsql/compiler/ParseTreeConverter.java
+++ pl_engine/pl_server/src/main/java/com/cubrid/plcsql/compiler/ParseTreeConverter.java
@@ -465,13 +465,15 @@ public class ParseTreeConverter extends PlcParserBaseVisitor<AstNode> {
                 }
 
                 if (ofTopLevel) {
-                    if (dp.typeSpec.type == Type.BOOLEAN
-                            || dp.typeSpec.type == Type.SYS_REFCURSOR) {
-                        throw new SemanticError(
-                                Misc.getLineColumnOf(pc), // s064
-                                "type "
-                                        + dp.typeSpec.type.plcName
-                                        + " cannot be used as a paramter type of stored procedures");
+                    switch (dp.typeSpec.type.idx) {
+                        case Type.IDX_RECORD:
+                        case Type.IDX_BOOLEAN:
+                        case Type.IDX_SYS_REFCURSOR:
+                            throw new SemanticError(
+                                    Misc.getLineColumnOf(pc), // s064
+                                    "type "
+                                            + dp.typeSpec.type.plcName
+                                            + " cannot be used as a paramter type of stored procedures");
                     }
                 } else {
                     if (dp.comment != null) {
@@ -3444,12 +3446,15 @@ public class ParseTreeConverter extends PlcParserBaseVisitor<AstNode> {
 
                 Type retType = retTypeSpec.type;
                 if (scopeLevel == DECL_TOP_LEVEL) { // at top level
-                    if (retType == Type.BOOLEAN) {
-                        throw new SemanticError(
-                                Misc.getLineColumnOf(ctx.type_spec()), // s065
-                                "type "
-                                        + retType.plcName
-                                        + " cannot be used as a return type of stored functions");
+                    switch (retType.idx) {
+                        case Type.IDX_RECORD:
+                        case Type.IDX_BOOLEAN:
+                        case Type.IDX_SYS_REFCURSOR:
+                            throw new SemanticError(
+                                    Misc.getLineColumnOf(ctx.type_spec()), // s065
+                                    "type "
+                                            + retType.plcName
+                                            + " cannot be used as a return type of stored functions");
                     }
                 }
                 DeclFunc ret =
